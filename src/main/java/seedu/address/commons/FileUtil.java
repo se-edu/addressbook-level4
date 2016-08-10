c940c8f61c513afe30b8db47b53efe9448735295
m133225 <m133225@hotmail.com>
2016-08-10 10:32:46 +0800

    Removed unused FileUtil methods.

--- src/main/java/seedu/address/commons/FileUtil.java
+++ src/main/java/seedu/address/commons/FileUtil.java
@@ -22,10 +22,6 @@ public class FileUtil {
         return isFileExists(new File(filepath));
     }
 
-    public static boolean isDirExists(File dir) {
-        return dir.exists() && dir.isDirectory();
-    }
-
     public static void createIfMissing(File file) throws IOException {
         if (!isFileExists(file)) {
             createFile(file);
@@ -47,44 +43,8 @@ public class FileUtil {
         return file.createNewFile();
     }
 
-    public static void deleteFile(String filepath) throws IOException {
-        deleteFile(new File(filepath));
-    }
-
     public static void deleteFile(File file) throws IOException {
         Files.delete(file.toPath());
-    }
-
-    public static void deleteFileIfExists(String filepath) throws IOException {
-        deleteFileIfExists(new File(filepath));
-    }
-
-    public static void deleteFileIfExists(File file) throws IOException {
-        if (!isFileExists(file)) {
-            return;
-        }
-
-        deleteFile(file);
-    }
-
-    /**
-     * Lists all files in directory and its subdirectories
-     */
-    public static List<File> listFilesInDir(Path directory) {
-        List<File> filepaths = new ArrayList<>();
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
-            for (Path path : stream) {
-                if (path.toFile().isDirectory()) {
-                    filepaths.addAll(listFilesInDir(path));
-                } else {
-                    filepaths.add(path.toFile());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return filepaths;
     }
 
     /**
@@ -111,71 +71,10 @@ public class FileUtil {
     }
 
     /**
-     * Move file from source to dest
-     * @param isOverwrite set true to overwrite source
-     */
-    public static void moveFile(Path source, Path dest, boolean isOverwrite) throws IOException {
-        if (isOverwrite) {
-            Files.move(source, dest, StandardCopyOption.REPLACE_EXISTING);
-        } else {
-            Files.move(source, dest);
-        }
-    }
-
-    /**
-     *
-     * @param source
-     * @param dest
-     * @param isOverwrite
-     * @throws IOException
-     */
-    public static void copyFile(Path source, Path dest, boolean isOverwrite) throws IOException {
-        if (isOverwrite) {
-            Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
-        } else {
-            Files.copy(source, dest);
-        }
-    }
-
-    /**
-     * @return List of filenames failed to be moved
-     */
-    public static List<String> moveContentOfADirectoryToAnother(String sourceDir, String targetDir) {
-        List<File> sourceFiles = FileUtil.listFilesInDir(Paths.get(sourceDir));
-        List<String> failedToMoveFiles = new ArrayList<>();
-
-        for (File sourceFile : sourceFiles) {
-            Path sourceFilePath = sourceFile.toPath();
-            Path targetFilePath = Paths.get(targetDir + File.separator + sourceFile.getName());
-
-            try {
-                FileUtil.moveFile(sourceFilePath, targetFilePath, true);
-            } catch (IOException e) {
-                e.printStackTrace();
-                failedToMoveFiles.add(sourceFile.getPath());
-            }
-        }
-
-        return failedToMoveFiles;
-    }
-
-    /**
      * Assumes file exists
      */
     public static String readFromFile(File file) throws IOException {
         return new String(Files.readAllBytes(file.toPath()), CHARSET);
-    }
-
-    /**
-     * Writes the content of a stream into a file
-     *
-     * Overwrites any existing file found at filePath
-     * @param in
-     * @param filePath
-     * @throws IOException
-     */
-    public static void writeStreamIntoFile(InputStream in, Path filePath) throws IOException {
-        Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
     }
 
     /**
@@ -191,10 +90,6 @@ public class FileUtil {
         return s.hasNext() ? s.next() : "";
     }
 
-    public static File getJarFileOfClass(Class givenClass) throws URISyntaxException {
-        return new File(givenClass.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-    }
-
     /**
      * Converts a string to a platform-specific file path
      * @param pathWithForwardSlash A String representing a file path but using '/' as the separator
@@ -204,18 +99,6 @@ public class FileUtil {
         assert pathWithForwardSlash != null;
         assert pathWithForwardSlash.contains("/");
         return pathWithForwardSlash.replace("/", File.separator);
-    }
-
-    /**
-     * Gets the file name from the given file path, assuming that
-     * path components are '/'-separated
-     *
-     * @param filePath should not be null
-     * @return
-     */
-    public static String getFileName(String filePath) {
-        String[] pathComponents = filePath.split("/");
-        return pathComponents[pathComponents.length - 1];
     }
 
     public static <T> void serializeObjectToJsonFile(File jsonFile, T objectToSerialize) throws IOException {
