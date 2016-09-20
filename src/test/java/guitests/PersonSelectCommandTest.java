4daf489ac8b81c31104664aa2787adbd435b9e04
Damith C. Rajapakse <damith@gmail.com>
2016-09-20 15:34:42 +0800

    PersonSelectCommandTest: remove duplicated code.

--- src/test/java/guitests/PersonSelectCommandTest.java
+++ src/test/java/guitests/PersonSelectCommandTest.java
@@ -10,28 +10,35 @@ public class PersonSelectCommandTest extends AddressBookGuiTest {
 
     @Test
     public void selectPerson_firstPerson_successful() {
+        assertListNotEmpty();
+        assertNoPersonSelected();
+        personListPanel.enterCommandAndApply("select 1");
+        assertPersonSelectedIs(1);
+    }
+
+    private void assertPersonSelectedIs(int index) {
+        assertEquals("Selected Person: "+index, resultDisplay.getText());
+        assertEquals(personListPanel.getSelectedPersons().size(), 1);
+        ReadOnlyPerson selectedPerson = personListPanel.getSelectedPersons().get(0);
+        assertEquals(personListPanel.getPerson(index-1), selectedPerson);
+    }
+
+    private void assertNoPersonSelected() {
+        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+    }
+
+    private void assertListNotEmpty() {
         int numberOfPeople = personListPanel.getNumberOfPeople();
         assertTrue(numberOfPeople > 0);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
-
-        personListPanel.enterCommandAndApply("select 1");
-        assertEquals("Selected Person: 1", resultDisplay.getText());
-        assertEquals(personListPanel.getSelectedPersons().size(), 1);
-        ReadOnlyPerson firstPerson = personListPanel.getSelectedPersons().get(0);
-        assertEquals(personListPanel.getPerson(0), firstPerson);
     }
 
     @Test
     public void selectPerson_lastPerson_successful() {
         int numberOfPeople = personListPanel.getNumberOfPeople();
         assertTrue(numberOfPeople > 0);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
-
+        assertNoPersonSelected();
         personListPanel.enterCommandAndApply("select " + numberOfPeople);
-        assertEquals("Selected Person: " + numberOfPeople, resultDisplay.getText());
-        assertEquals(personListPanel.getSelectedPersons().size(), 1);
-        ReadOnlyPerson firstPerson = personListPanel.getSelectedPersons().get(0);
-        assertEquals(personListPanel.getPerson(numberOfPeople - 1), firstPerson);
+        assertPersonSelectedIs(numberOfPeople);
     }
 
 
@@ -39,25 +46,21 @@ public class PersonSelectCommandTest extends AddressBookGuiTest {
     public void selectPerson_middlePerson_successful() {
         int numberOfPeople = personListPanel.getNumberOfPeople();
         assertTrue(numberOfPeople > 5);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
-
+        assertNoPersonSelected();
         personListPanel.enterCommandAndApply("select 3");
-        assertEquals("Selected Person: 3", resultDisplay.getText());
-        assertEquals(personListPanel.getSelectedPersons().size(), 1);
-        ReadOnlyPerson thirdPerson = personListPanel.getSelectedPersons().get(0);
-        assertEquals(personListPanel.getPerson(2), thirdPerson);
+        assertPersonSelectedIs(3);
     }
 
     @Test
     public void selectPerson_outOfBoundIndex_fail() {
         int numberOfPeople = personListPanel.getNumberOfPeople();
         assertTrue(numberOfPeople > 0);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertNoPersonSelected();
 
         personListPanel.enterCommandAndApply("select " + (numberOfPeople + 1));
         assertEquals("The person index provided is invalid", resultDisplay.getText());
 
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertNoPersonSelected();
         personListPanel.enterCommandAndApply("select 1");
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
 
@@ -67,18 +70,18 @@ public class PersonSelectCommandTest extends AddressBookGuiTest {
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
         ReadOnlyPerson firstPerson = personListPanel.getSelectedPersons().get(0);
         assertEquals(personListPanel.getPerson(0), firstPerson);
+
     }
 
     @Test
     public void selectPerson_zeroIndex_fail() {
-        int numberOfPeople = personListPanel.getNumberOfPeople();
-        assertTrue(numberOfPeople > 0);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertListNotEmpty();
+        assertNoPersonSelected();
 
         personListPanel.enterCommandAndApply("select 0");
         assertEquals("The person index provided is invalid", resultDisplay.getText());
 
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertNoPersonSelected();
         personListPanel.enterCommandAndApply("select 1");
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
 
@@ -88,18 +91,18 @@ public class PersonSelectCommandTest extends AddressBookGuiTest {
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
         ReadOnlyPerson firstPerson = personListPanel.getSelectedPersons().get(0);
         assertEquals(personListPanel.getPerson(0), firstPerson);
+
     }
 
     @Test
     public void selectPerson_negativeIndex_fail() {
-        int numberOfPeople = personListPanel.getNumberOfPeople();
-        assertTrue(numberOfPeople > 0);
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertListNotEmpty();
+        assertNoPersonSelected();
 
         personListPanel.enterCommandAndApply("select -1");
         assertEquals("The person index provided is invalid", resultDisplay.getText());
 
-        assertEquals(personListPanel.getSelectedPersons().size(), 0);
+        assertNoPersonSelected();
         personListPanel.enterCommandAndApply("select 1");
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
 
@@ -109,5 +112,6 @@ public class PersonSelectCommandTest extends AddressBookGuiTest {
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
         ReadOnlyPerson firstPerson = personListPanel.getSelectedPersons().get(0);
         assertEquals(personListPanel.getPerson(0), firstPerson);
+
     }
 }
