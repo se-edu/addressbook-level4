bf0c4b8cc6a2e5a5a3d56c63c7ccd5514c09e93b
Damith C. Rajapakse <damith@gmail.com>
2016-09-21 18:41:37 +0800

    ShowHelpEvent: add missing header comment

--- src/main/java/seedu/address/events/controller/ShowHelpEvent.java
+++ src/main/java/seedu/address/events/controller/ShowHelpEvent.java
@@ -3,7 +3,7 @@ package seedu.address.events.controller;
 import seedu.address.events.BaseEvent;
 
 /**
- *
+ * An event requesting to view the help page.
  */
 public class ShowHelpEvent extends BaseEvent {
 
