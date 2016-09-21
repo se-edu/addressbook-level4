64e4fba535dacdab162c5c5c6ce3092b4181f629
Damith C. Rajapakse <damith@gmail.com>
2016-09-21 21:37:56 +0800

    [80] Refine browser to be a UiPart
    * Make Browser a UiPart
    
    * Move BrowserPanel to controller package
    
    * Refine BrowserPanel
    
    * fix anchor issue

---
+++ src/main/java/seedu/address/controller/BrowserPanel.java
@@ -0,0 +1,68 @@
+package seedu.address.controller;
+
+import javafx.event.Event;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+import seedu.address.commons.FxViewUtil;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.util.LoggerManager;
+
+import java.util.logging.Logger;
+
+/**
+ * The Browser Panel of the App.
+ */
+public class BrowserPanel extends UiPart{
+
+    private static Logger logger = LoggerManager.getLogger(BrowserPanel.class);
+    private WebView browser;
+
+    /**
+     * Constructor is kept private as {@link #load(AnchorPane)} is the only way to create a BrowserPanel.
+     */
+    private BrowserPanel() {
+
+    }
+
+    @Override
+    public void setNode(Node node) {
+        //not applicable
+    }
+
+    @Override
+    public String getFxmlPath() {
+        return null; //not applicable
+    }
+
+    /**
+     * Factory method for creating a Browser Panel.
+     * This method should be called after the FX runtime is initialized and in FX application thread.
+     * @param placeholder The AnchorPane where the BrowserPanel must be inserted
+     */
+    public static BrowserPanel load(AnchorPane placeholder){
+        logger.info("Initializing browser");
+        BrowserPanel browserPanel = new BrowserPanel();
+        browserPanel.browser = new WebView();
+        placeholder.setOnKeyPressed(Event::consume); // To prevent triggering events for typing inside the loaded Web page.
+        FxViewUtil.applyAnchorBoundaryParameters(browserPanel.browser, 0.0, 0.0, 0.0, 0.0);
+        placeholder.getChildren().add(browserPanel.browser);
+        return browserPanel;
+    }
+
+    public void loadPersonPage(ReadOnlyPerson person) {
+        loadPage("https://www.google.com.sg/#safe=off&q=" + person.getName().fullName.replaceAll(" ", "+"));
+    }
+
+    public void loadPage(String url){
+        browser.getEngine().load(url);
+    }
+
+    /**
+     * Frees resources allocated to the browser.
+     */
+    public void freeResources() {
+        browser = null;
+    }
+
+}
