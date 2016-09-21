fc37e4324fe5386969ba8a8f6ee1f7ba35536805
Damith C. Rajapakse <damith@gmail.com>
2016-09-21 23:13:07 +0800

    Remove unused messages from Message

--- src/main/java/seedu/address/commons/Messages.java
+++ src/main/java/seedu/address/commons/Messages.java
@@ -5,16 +5,9 @@ package seedu.address.commons;
  */
 public class Messages {
 
-    public static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
-    public static final String MESSAGE_GOODBYE = "Good bye!";
-    public static final String MESSAGE_INIT_FAILED = "Failed to initialise address book application. Exiting...";
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSON_NOT_IN_ADDRESSBOOK = "Person could not be found in address book";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: " +
-            "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
-    public static final String MESSAGE_WELCOME = "Welcome to your Address Book!";
-    public static final String MESSAGE_USING_STORAGE_FILE = "Using storage file : %1$s";
 }
