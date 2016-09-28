221d9962dae0e2dfcef629c2526539dc38fc1fae
Joshua Lee <me@jolly.sg>
2016-09-28 17:14:41 +0800

    removed OsDetector and all related methods

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -1,8 +1,6 @@
 package guitests;
 
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.MouseButton;
 import org.testfx.api.FxRobot;
 import seedu.address.testutil.TestUtil;
 
@@ -12,33 +10,8 @@ import seedu.address.testutil.TestUtil;
  */
 public class GuiRobot extends FxRobot {
 
-    public GuiRobot push(KeyCode... keyCodes){
-        return (GuiRobot) super.push(TestUtil.scrub(keyCodes));
-    }
-
     public GuiRobot push(KeyCodeCombination keyCodeCombination){
         return (GuiRobot) super.push(TestUtil.scrub(keyCodeCombination));
     }
 
-    public GuiRobot press(KeyCode... keyCodes) {
-        return (GuiRobot) super.press(TestUtil.scrub(keyCodes));
-    }
-
-    public GuiRobot release(KeyCode... keyCodes) {
-        return (GuiRobot) super.release(TestUtil.scrub(keyCodes));
-    }
-
-    public GuiRobot type(KeyCode... keyCodes) {
-        return (GuiRobot) super.type(TestUtil.scrub(keyCodes));
-    }
-
-    @Override
-    public GuiRobot clickOn(String query, MouseButton... buttons) {
-        return (GuiRobot) super.clickOn(query, buttons);
-    }
-
-    @Override
-    public GuiRobot drag(String query, MouseButton... buttons) {
-        return (GuiRobot) super.drag(query, buttons);
-    }
 }
