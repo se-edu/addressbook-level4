7359b0bc913aeaee57b5863a12d562a418f07e17
Damith C. Rajapakse <damith@gmail.com>
2016-10-12 15:16:32 +0800

    Add test case design info to StringUtilTest::isUnsignedPositiveInteger

--- src/test/java/seedu/address/commons/util/StringUtilTest.java
+++ src/test/java/seedu/address/commons/util/StringUtilTest.java
@@ -18,20 +18,32 @@ public class StringUtilTest {
 
     @Test
     public void isUnsignedPositiveInteger() {
+
+        // Equivalence partition: null
         assertFalse(StringUtil.isUnsignedInteger(null));
-        assertFalse(StringUtil.isUnsignedInteger(""));
+
+        // EP: empty strings
+        assertFalse(StringUtil.isUnsignedInteger("")); //boundary value
+        assertFalse(StringUtil.isUnsignedInteger("  "));
+
+        // EP: not a number
         assertFalse(StringUtil.isUnsignedInteger("a"));
         assertFalse(StringUtil.isUnsignedInteger("aaa"));
-        assertFalse(StringUtil.isUnsignedInteger("  "));
-        assertFalse(StringUtil.isUnsignedInteger("-1"));
-        assertFalse(StringUtil.isUnsignedInteger("0"));
-        assertFalse(StringUtil.isUnsignedInteger("+1")); //should be unsigned
-        assertFalse(StringUtil.isUnsignedInteger("-1")); //should be unsigned
-        assertFalse(StringUtil.isUnsignedInteger(" 10")); //should not contain whitespaces
-        assertFalse(StringUtil.isUnsignedInteger("10 ")); //should not contain whitespaces
-        assertFalse(StringUtil.isUnsignedInteger("1 0")); //should not contain whitespaces
 
-        assertTrue(StringUtil.isUnsignedInteger("1"));
+        // EP: zero
+        assertFalse(StringUtil.isUnsignedInteger("0"));
+
+        // EP: signed numbers
+        assertFalse(StringUtil.isUnsignedInteger("-1"));
+        assertFalse(StringUtil.isUnsignedInteger("+1"));
+
+        // EP: numbers with white space
+        assertFalse(StringUtil.isUnsignedInteger(" 10"));
+        assertFalse(StringUtil.isUnsignedInteger("10 "));
+        assertFalse(StringUtil.isUnsignedInteger("1 0"));
+
+        // EP: valid numbers, should return true
+        assertTrue(StringUtil.isUnsignedInteger("1")); //boundary value
         assertTrue(StringUtil.isUnsignedInteger("10"));
     }
 
