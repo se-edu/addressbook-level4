f26f2415bcddc389e94085a8d27bf8df83623b95
ndt <thiennguyen1993@gmail.com>
2016-10-13 15:39:30 +0800

    Update tests

--- src/test/java/seedu/address/logic/parser/ArgumentTokenizerTest.java
+++ src/test/java/seedu/address/logic/parser/ArgumentTokenizerTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,98 +21,92 @@ public class ArgumentTokenizerTest {
     }
 
     @Test
-    public void parse_validAddCmdArgsNoTags() {
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(addCmdArgs);
-        ParsedArguments result = argsParser.tokenize(
-                "John Doe p/98765432 e/johnd@gmail.com a/John street, block 123, #01-01");
+    public void tokenize_validAddCmdArgsNoTags() {
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(phoneNumberPrefix, emailPrefix,
+                                                                addressPrefix, tagsPrefix);
+        argsTokenizer.tokenize("John Doe p/98765432 e/johnd@gmail.com a/John street, block 123, #01-01");
 
-        assertEquals("John Doe", result.getArgumentValue(nameArg).get());
-        assertEquals("98765432", result.getArgumentValue(phoneNumberArg).get());
-        assertEquals("johnd@gmail.com", result.getArgumentValue(Parser.emailArg).get());
-        assertEquals("John street, block 123, #01-01", result.getArgumentValue(addressArg).get());
+        assertEquals("John Doe", argsTokenizer.getPreamble().get());
+        assertEquals("98765432", argsTokenizer.getValue(phoneNumberPrefix).get());
+        assertEquals("johnd@gmail.com", argsTokenizer.getValue(Parser.emailPrefix).get());
+        assertEquals("John street, block 123, #01-01", argsTokenizer.getValue(addressPrefix).get());
     }
 
     @Test
-    public void parse_validAddCmdArgsWithTags() {
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(addCmdArgs);
-        ArgumentTokenizer.ParsedArguments result = argsParser.tokenize(
-                "Betsy Crowe p/1234567 e/betsycrowe@gmail.com a/Newgate Prison t/criminal t/friend");
+    public void tokenize_validAddCmdArgsWithTags() {
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(phoneNumberPrefix, emailPrefix,
+                                                                addressPrefix, tagsPrefix);
+        argsTokenizer.tokenize("Betsy Crowe p/1234567 e/betsycrowe@gmail.com a/Newgate Prison t/criminal t/friend");
         List<String> tags = new ArrayList<>();
         tags.add("criminal");
         tags.add("friend");
 
-        assertEquals("Betsy Crowe", result.getArgumentValue(nameArg).get());
-        assertEquals("1234567", result.getArgumentValue(phoneNumberArg).get());
-        assertEquals("betsycrowe@gmail.com", result.getArgumentValue(Parser.emailArg).get());
-        assertEquals("Newgate Prison", result.getArgumentValue(addressArg).get());
-        assertEquals(tags, result.getArgumentValue(Parser.tagArgs).get());
+        assertEquals("Betsy Crowe", argsTokenizer.getPreamble().get());
+        assertEquals("1234567", argsTokenizer.getValue(phoneNumberPrefix).get());
+        assertEquals("betsycrowe@gmail.com", argsTokenizer.getValue(Parser.emailPrefix).get());
+        assertEquals("Newgate Prison", argsTokenizer.getValue(addressPrefix).get());
+        assertEquals(tags, argsTokenizer.getValues(tagsPrefix).get());
     }
 
     @Test
-    public void parse_validAddCmdArgsChangeOrder() {
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(addCmdArgs);
-        ParsedArguments result = argsParser.tokenize(
-                "John Doe e/johnd@gmail.com a/John street, block 123, #01-01 p/98765432");
+    public void tokenize_validAddCmdArgsChangeOrder() {
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(phoneNumberPrefix, emailPrefix,
+                                                                addressPrefix, tagsPrefix);
+        argsTokenizer.tokenize("John Doe e/johnd@gmail.com a/John street, block 123, #01-01 p/98765432");
 
-        assertEquals("John Doe", result.getArgumentValue(nameArg).get());
-        assertEquals("98765432", result.getArgumentValue(phoneNumberArg).get());
-        assertEquals("johnd@gmail.com", result.getArgumentValue(Parser.emailArg).get());
-        assertEquals("John street, block 123, #01-01", result.getArgumentValue(addressArg).get());
+        assertEquals("John Doe", argsTokenizer.getPreamble().get());
+        assertEquals("98765432", argsTokenizer.getValue(phoneNumberPrefix).get());
+        assertEquals("johnd@gmail.com", argsTokenizer.getValue(Parser.emailPrefix).get());
+        assertEquals("John street, block 123, #01-01", argsTokenizer.getValue(addressPrefix).get());
     }
 
     @Test
-    public void parse_repeatedNonRepeatableArgument() {
-        NonRepeatableArgument aArg = new NonRepeatableArgument("a", "a:");
-        NonRepeatableArgument bArg = new NonRepeatableArgument("b", "b:");
-        List<Argument> args = Arrays.asList(aArg, bArg);
+    public void tokenize_repeatedPrefixes() {
+        Prefix aArg = new Prefix("a:");
+        Prefix bArg = new Prefix("b:");
 
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(args);
-        ParsedArguments result = argsParser.tokenize("a: aaa b: bbb a: AAA");
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(aArg, bArg);
+        argsTokenizer.tokenize("a: aaa b: bbb a: AAA");
 
-        assertEquals("AAA", result.getArgumentValue(aArg).get());
-        assertEquals("bbb", result.getArgumentValue(bArg).get());
+        assertEquals("AAA", argsTokenizer.getValue(aArg).get());
+        assertEquals("bbb", argsTokenizer.getValue(bArg).get());
     }
 
     @Test
-    public void parse_interleavingRepeatableArguments() {
-        NonRepeatableArgument aArg = new NonRepeatableArgument("a", "a\\");
-        NonRepeatableArgument bArg = new NonRepeatableArgument("b", "b\\");
-        RepeatableArgument cArgs = new RepeatableArgument("c", "c\\");
-        List<Argument> args = Arrays.asList(aArg, bArg, cArgs);
+    public void tokenize_interleavingRepeatedPrefixes() {
+        Prefix aArg = new Prefix("a\\");
+        Prefix bArg = new Prefix("b\\");
+        Prefix cArgs = new Prefix("c\\");
         List<String> cVals = Arrays.asList("c1", "c2");
 
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(args);
-        ParsedArguments result = argsParser.tokenize("a\\ aaa c\\ c1 b\\ bbb c\\ c2");
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(aArg, bArg, cArgs);
+        argsTokenizer.tokenize("a\\ aaa c\\ c1 b\\ bbb c\\ c2");
 
-        assertEquals("aaa", result.getArgumentValue(aArg).get());
-        assertEquals("bbb", result.getArgumentValue(bArg).get());
-        assertEquals(cVals, result.getArgumentValue(cArgs).get());
+        assertEquals("aaa", argsTokenizer.getValue(aArg).get());
+        assertEquals("bbb", argsTokenizer.getValue(bArg).get());
+        assertEquals(cVals, argsTokenizer.getValues(cArgs).get());
     }
 
     @Test
-    public void parse_noPrefixedArguments() {
-        NonPrefixedArgument aArg = new NonPrefixedArgument("a");
-        List<Argument> args = Arrays.asList(aArg);
+    public void tokenize_noPrefix() {
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer();
+        argsTokenizer.tokenize("Micky Mouse p/12345");
 
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(args);
-        ParsedArguments result = argsParser.tokenize("Micky Mouse p/12345");
-
-        assertEquals("Micky Mouse p/12345", result.getArgumentValue(aArg).get());
+        assertEquals("Micky Mouse p/12345", argsTokenizer.getPreamble().get());
     }
 
     @Test
-    public void parse_duplicatedRepeatableArguments() {
-        NonRepeatableArgument aArg = new NonRepeatableArgument("a", "<a>");
-        NonRepeatableArgument bArg = new NonRepeatableArgument("b", "<b>");
-        RepeatableArgument cArgs = new RepeatableArgument("c", "<c>");
-        List<Argument> args = Arrays.asList(aArg, bArg, cArgs);
+    public void tokenize_duplicatedRepeatedPrefix() {
+        Prefix aArg = new Prefix("<a>");
+        Prefix bArg = new Prefix("<b>");
+        Prefix cArgs = new Prefix("<c>");
         List<String> cVals = Arrays.asList("c1", "c2", "c1");
 
-        ArgumentTokenizer argsParser = new ArgumentTokenizer(args);
-        ParsedArguments result = argsParser.tokenize("<a>aaa<c>c1<b>bbb<c>c2<c>c1");
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(aArg, bArg, cArgs);
+        argsTokenizer.tokenize("<a>aaa<c>c1<b>bbb<c>c2<c>c1");
 
-        assertEquals("aaa", result.getArgumentValue(aArg).get());
-        assertEquals("bbb", result.getArgumentValue(bArg).get());
-        assertEquals(cVals, result.getArgumentValue(cArgs).get());
+        assertEquals("aaa", argsTokenizer.getValue(aArg).get());
+        assertEquals("bbb", argsTokenizer.getValue(bArg).get());
+        assertEquals(cVals, argsTokenizer.getValues(cArgs).get());
     }
 }
