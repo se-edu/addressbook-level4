f4fb61de441562a3bc9c1994fd0a816ff0d581a2
Joshua Lee <me@jolly.sg>
2016-10-15 15:19:03 +0800

    moved more properties to ResultDisplay.fxml

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import javafx.fxml.FXML;
 import com.google.common.eventbus.Subscribe;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -19,15 +20,18 @@ import java.util.logging.Logger;
 public class ResultDisplay extends UiPart {
 
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
-    private static final String RESULT_DISPLAY_ID = "resultDisplay";
-    private static final String STATUS_BAR_STYLE_SHEET = "result-display";
-    private final StringProperty displayed = new SimpleStringProperty("");
-
     private static final String FXML = "ResultDisplay.fxml";
 
+    private final StringProperty displayed = new SimpleStringProperty("");
+
+    @FXML
     private AnchorPane placeHolder;
 
+    @FXML
     private AnchorPane mainPane;
+
+    @FXML
+    private TextArea resultDisplay;
 
     public static ResultDisplay load(Stage primaryStage, AnchorPane placeHolder) {
         ResultDisplay resultDisplay = UiPartLoader.loadUiPart(primaryStage, placeHolder, new ResultDisplay());
@@ -36,15 +40,9 @@ public class ResultDisplay extends UiPart {
     }
 
     private void configure() {
-        TextArea resultDisplayArea = new TextArea();
-        resultDisplayArea.setEditable(false);
-        resultDisplayArea.setId(RESULT_DISPLAY_ID);
-        resultDisplayArea.getStyleClass().removeAll();
-        resultDisplayArea.getStyleClass().add(STATUS_BAR_STYLE_SHEET);
-        resultDisplayArea.setText("");
-        resultDisplayArea.textProperty().bind(displayed);
-        FxViewUtil.applyAnchorBoundaryParameters(resultDisplayArea, 0.0, 0.0, 0.0, 0.0);
-        mainPane.getChildren().add(resultDisplayArea);
+        resultDisplay.textProperty().bind(displayed);
+        FxViewUtil.applyAnchorBoundaryParameters(resultDisplay, 0.0, 0.0, 0.0, 0.0);
+        mainPane.getChildren().add(resultDisplay);
         FxViewUtil.applyAnchorBoundaryParameters(mainPane, 0.0, 0.0, 0.0, 0.0);
         placeHolder.getChildren().add(mainPane);
         registerAsAnEventHandler(this);
