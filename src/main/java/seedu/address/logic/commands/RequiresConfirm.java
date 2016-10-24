ff68f003a9fd9bc6ffe206c2fa8ac35a5b146a96
kennethtxytqw <kennethtxytqw@gmail.com>
2016-10-25 03:37:09 +0800

    Created RequiresConfirm interface

---
+++ src/main/java/seedu/address/logic/commands/RequiresConfirm.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+public interface RequiresConfirm{
+    String PROMPT_MESSAGE = "Are you sure you want to execute ' %1$s ' ? Type ' yes ' to confirm.";    
+    default CommandResult prompt(String commandWord){
+        return new CommandResult(String.format(PROMPT_MESSAGE, commandWord));
+    }
+
+    public CommandResult prompt();
+}
