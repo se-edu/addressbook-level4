1fce4cca25ff32d91db843627289411dda533259
Filbert <filbert.cheong@gmail.com>
2016-10-26 03:21:23 +0800

    fix

--- src/main/java/seedu/address/model/task/Time.java
+++ src/main/java/seedu/address/model/task/Time.java
@@ -27,13 +27,13 @@ public class Time implements Comparable<Time> {
      *
      * @author Alok Chaudhary, Filbert(with the help of Java regex converter)
      */
-    public static final String DATE_VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|"
+    public static final String DATE_VALIDATION_REGEX = "(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|"
             + "(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|"
             + "(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^"
             + "(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|"
             + "(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|"
             + "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))"
-            + "\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+            + "\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})";
 
     public static final String TIME_PARSE_FORMAT_CHOICE_12HR = "[h:mma]" + "[h.mma]" + "[hmma]";
     public static final String TIME_PARSE_FORMAT_CHOICE_24HR = "[k:mm]" + "[k.mm]" + "[kkmm]";
