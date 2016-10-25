5846abe9297abbac591893647aefbfb5bbca3a02
Byunghun Moon <mbhh1234@gmail.com>
2016-10-26 00:08:42 +0800

    Added Filbert's unused code

--- src/main/java/seedu/address/ui/TitleCard.java
+++ src/main/java/seedu/address/ui/TitleCard.java
@@ -45,6 +45,31 @@ public class TitleCard extends UiPart{
         completeStatus.setSelected(task.getCompleted());
         setDesign();
     }
+    
+    //@@author A0121261Y-unused
+    /*public void setDateTimeText(){
+        if (task.getTime().isPresent()) {
+            time.setText(task.getTime().get().getStartDateString());
+           if (task.getTime().get().getUntimedStatus()) {
+               startTime.setText(BLANK);
+               endTime.setText(BLANK);
+           } else if (task.getTime().get().getEndDate().isPresent()) {
+               startTime.setText("Starts at: " + task.getTime().get().getStartDate().toLocalTime()
+                       .format(DateTimeFormatter.ofPattern(Time.TIME_PRINT_FORMAT)));
+               endTime.setText("Ends at: " + task.getTime().get().getEndDate().get().toLocalTime()
+                       .format(DateTimeFormatter.ofPattern(Time.TIME_PRINT_FORMAT)));
+           } else {
+               startTime.setText("Starts at: " + task.getTime().get().getStartDate().toLocalTime()
+                       .format(DateTimeFormatter.ofPattern(Time.TIME_PRINT_FORMAT)));
+               endTime.setText(BLANK);
+           }
+       } else {
+           time.setText(BLANK);
+           startTime.setText(BLANK);
+           endTime.setText(BLANK);
+       }
+
+    }*/
 
 
     @FXML
