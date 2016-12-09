e488d2bce83aedd93d0441988e9cec0dce53189f
clarencechee <clarencechee94@gmail.com>
2016-12-09 22:17:03 +0800

    Parser: morph to work with new arguments * Clean up imports
    * add regex to match ranged indices format (non-whitespace, non-range-indicator,
      non-empty characters surrounding a range indicator)
        * Valid matches if range indicator is "-" : "123123zx-zg1" or ".-." or "1-2"
    
    * Shift all helper methods to the bottom for readability
    
    * prepareDelete() - not much has changed, now reliant on parseMixedIndices() instead
      of parseIndex() to parse delete args
    
    * parseMixedIndices(List<String>) - extracts both ranged/non-ranged indices
      (hence 'mixed') from `tokens`
        * these indices can be duplicates and can be unordered
    
    * parseIndices(List<String>) - extracts non-ranged indices from a list
      of tokens
    
    * parseRangedIndex(String) - extracts an `IndexRange` from a token
    
    * parseRangedIndices(List<String>) - extracts exact indices from ranged indices
      in a list of tokens
    
    * Created `IndexRange` container class to hold start and end indices of an index range

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -1,16 +1,35 @@
 package seedu.address.logic.parser;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.StringUtil;
-import seedu.address.logic.commands.*;
-import seedu.address.logic.parser.ArgumentTokenizer.*;
-
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
 
 /**
  * Parses user input.
@@ -24,8 +43,17 @@ public class Parser {
 
     private static final Pattern PERSON_INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
 
+    private static final String PERSON_INDEX_RANGE_INDICATOR = "-";
+
+    private static final Pattern PERSON_INDEX_RANGE_ARGS_FORMAT =
+            Pattern.compile(
+                    "(?<rangeStart>[^" + PERSON_INDEX_RANGE_INDICATOR + "\\s]+)"
+                    + PERSON_INDEX_RANGE_INDICATOR
+                    + "(?<rangeEnd>[^" + PERSON_INDEX_RANGE_INDICATOR + "\\s]+)");
+
     private static final Pattern KEYWORDS_ARGS_FORMAT =
             Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
+
     private static final Prefix phoneNumberPrefix = new Prefix("p/");
     private static final Prefix emailPrefix = new Prefix("e/");
     private static final Prefix addressPrefix = new Prefix("a/");
@@ -101,11 +129,6 @@ public class Parser {
         }
     }
 
-    private Set<String> toSet(Optional<List<String>> tagsOptional) {
-        List<String> tags = tagsOptional.orElse(Collections.emptyList());
-        return new HashSet<>(tags);
-    }
-
     /**
      * Parses arguments in the context of the delete person command.
      *
@@ -113,14 +136,14 @@ public class Parser {
      * @return the prepared command
      */
     private Command prepareDelete(String args) {
-
-        Optional<Integer> index = parseIndex(args);
-        if (!index.isPresent()) {
+        final List<String> tokenized = Arrays.asList(args.trim().split("\\s+"));
+        final Optional<List<Integer>> indices = parseMixedIndices(tokenized);
+        if (!indices.isPresent()) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
 
-        return new DeleteCommand(index.get());
+        return new DeleteCommand(indices.get());
     }
 
     /**
@@ -140,24 +163,6 @@ public class Parser {
     }
 
     /**
-     * Returns the specified index in the {@code command} if it is a positive unsigned integer
-     * Returns an {@code Optional.empty()} otherwise.
-     */
-    private Optional<Integer> parseIndex(String command) {
-        final Matcher matcher = PERSON_INDEX_ARGS_FORMAT.matcher(command.trim());
-        if (!matcher.matches()) {
-            return Optional.empty();
-        }
-
-        String index = matcher.group("targetIndex");
-        if (!StringUtil.isUnsignedInteger(index)) {
-            return Optional.empty();
-        }
-        return Optional.of(Integer.parseInt(index));
-
-    }
-
-    /**
      * Parses arguments in the context of the find person command.
      *
      * @param args full command args string
@@ -174,6 +179,145 @@ public class Parser {
         final String[] keywords = matcher.group("keywords").split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindCommand(keywordSet);
+    }
+
+    /*
+     * =========================================================================
+     *                             Helper Methods
+     * =========================================================================
+     */
+
+    /**
+     * Returns a list of indices extracted from {@code tokens}.<br>
+     *
+     * These tokens must also contain either: <br>
+     * <ul>
+     *   <li> a positive unsigned integer i.e. a non-ranged index </li>
+     *   <li> or positive unsigned integers surrounding a {@code PERSON_INDEX_RANGE_INDICATOR}
+     *          i.e. a ranged index</li>
+     * </ul>
+     *
+     * Hence 'mixed' as {@code tokens} may contain both ranged and non-ranged indices. <br>
+     * The returned indices may be unordered and have duplicates.
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    private Optional<List<Integer>> parseMixedIndices(List<String> tokens) {
+
+        // Partitioning tokens into ranged indices and non-ranged indices.
+        final Map<Boolean, List<String>> areRangedTokens = tokens.stream()
+                .collect(Collectors.partitioningBy(token -> token.contains(PERSON_INDEX_RANGE_INDICATOR)));
+
+        // Extracting indices from tokens.
+        final Optional<List<Integer>> indicesFromRangedTokens = parseRangedIndices(areRangedTokens.get(true));
+        final Optional<List<Integer>> indicesFromNonRangedTokens = parseIndices(areRangedTokens.get(false));
+        if (!indicesFromRangedTokens.isPresent() || !indicesFromNonRangedTokens.isPresent()) {
+            return Optional.empty();
+        }
+
+        final List<Integer> indices = new ArrayList<>();
+        indices.addAll(indicesFromRangedTokens.get());
+        indices.addAll(indicesFromNonRangedTokens.get());
+
+        return Optional.of(indices);
+    }
+
+    /**
+     * Returns an {@code IndexRange} extracted from {@code token} if it contains positive
+     * unsigned integers surrounding a {@code PERSON_INDEX_RANGE_INDICATOR}.
+     *
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    private Optional<IndexRange> parseRangedIndex(String token) {
+        final Matcher matcher = PERSON_INDEX_RANGE_ARGS_FORMAT.matcher(token.trim());
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        final Optional<Integer> startIndex = parseIndex(matcher.group("rangeStart"));
+        final Optional<Integer> endIndex = parseIndex(matcher.group("rangeEnd"));
+        if (!startIndex.isPresent() || !endIndex.isPresent()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new IndexRange(startIndex.get(), endIndex.get()));
+    }
+
+    /**
+     * Returns a list of indices extracted from {@code tokens} if each token contains positive
+     * unsigned integers surrounding a {@code PERSON_INDEX_RANGE_INDICATOR}.
+     *
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    private Optional<List<Integer>> parseRangedIndices(List<String> tokens) {
+        final List<Integer> indices = new ArrayList<>();
+        for (final String token : tokens) {
+            final Optional<IndexRange> indexRange = parseRangedIndex(token);
+            if (!indexRange.isPresent()) {
+                return Optional.empty();
+            }
+
+            // Adding every index from start to end (inclusive) to 'indices'.
+            IntStream.rangeClosed(indexRange.get().start, indexRange.get().end).forEach(indices::add);
+        }
+
+        return Optional.of(indices);
+    }
+
+    /**
+     * Returns a single specified index in {@code token} if it is a positive unsigned integer.
+     *
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    private Optional<Integer> parseIndex(String token) {
+        final Matcher matcher = PERSON_INDEX_ARGS_FORMAT.matcher(token.trim());
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        String index = matcher.group("targetIndex");
+        if (!StringUtil.isUnsignedInteger(index)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Integer.parseInt(index));
+    }
+
+    /**
+     * Returns a list of indices extracted from {@code tokens} if each token contains a positive unsigned integer.
+     *
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    private Optional<List<Integer>> parseIndices(List<String> tokens) {
+        final List<Integer> indices = new ArrayList<>();
+        for (final String token : tokens) {
+            final Optional<Integer> index = parseIndex(token);
+            if (!index.isPresent()) {
+                return Optional.empty();
+            }
+            indices.add(index.get());
+        }
+
+        return Optional.of(indices);
+    }
+
+    private Set<String> toSet(Optional<List<String>> tagsOptional) {
+        List<String> tags = tagsOptional.orElse(Collections.emptyList());
+        return new HashSet<>(tags);
+    }
+
+    /**
+     * Represents an ascending index range.
+     */
+    private class IndexRange {
+        public final int start;
+        public final int end;
+
+        private IndexRange(int start, int end) {
+            assert start > 0 && end > 0;
+            // Forcing ascending order.
+            this.start = Math.min(start, end);
+            this.end = Math.max(start, end);
+        }
     }
 
 }
