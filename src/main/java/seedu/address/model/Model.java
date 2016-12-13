5078420e34a1ca85a1f748ee5306c3b7a056b0e3
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2016-12-13 13:54:33 +0800

    teach classes in Model to execute edit

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -4,6 +4,7 @@ import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.UniquePersonList.DuplicatePersonException;
 
 import java.util.Set;
 
@@ -22,6 +23,18 @@ public interface Model {
 
     /** Adds the given person */
     void addPerson(Person person) throws UniquePersonList.DuplicatePersonException;
+
+    /**
+     * Updates the person located at {@code targetIndexInLastShownList} with {@code editedPerson}.
+     *
+     * @throws DuplicatePersonException if updating the person's details causes the person to be equivalent to
+     *      another existing person in the list, or if the person is being updated with the person's existing values
+     *      i.e person located at {@code targetIndexInLastShownList} is equivalent to {@code editedPerson}.
+     * @throws IndexOutOfBoundsException if {@code targetIndexInLastShownList} < 0
+     *      or >= the size of the last shown list.
+     */
+    void updatePerson(int targetIndexInLastShownList, ReadOnlyPerson editedPerson)
+            throws UniquePersonList.DuplicatePersonException, IndexOutOfBoundsException;
 
     /** Returns the filtered person list as an {@code UnmodifiableObservableList<ReadOnlyPerson>} */
     UnmodifiableObservableList<ReadOnlyPerson> getFilteredPersonList();
