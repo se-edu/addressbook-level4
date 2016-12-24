1da3e359b65879d0fde3b1c9849441087e1883d1
Paul Tan <pyokagan@gmail.com>
2016-12-24 14:39:37 +0800

    BrowserPanel: migrate to use FXML loading constructor
    UiPartLoader.loadUiPart() is deprecated. Migrate the initialization code
    of BrowserPanel to use the new `UiPart(String)` constructor.

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -2,50 +2,31 @@ package seedu.address.ui;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import seedu.address.commons.util.FxViewUtil;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.commons.core.LogsCenter;
-
-import java.util.logging.Logger;
 
 /**
  * The Browser Panel of the App.
  */
-public class BrowserPanel extends UiPart {
+public class BrowserPanel extends UiPart<Region> {
 
-    private static Logger logger = LogsCenter.getLogger(BrowserPanel.class);
     private static final String FXML = "BrowserPanel.fxml";
 
     @FXML
     private WebView browser;
 
-    @Override
-    public void setNode(Node node) {
-        //not applicable
-    }
-
-    @Override
-    public String getFxmlPath() {
-        return FXML;
-    }
-
     /**
-     * Creates a BrowserPanel.
-     * This is the factory method for creating a Browser Panel.
-     * This method should be called after the FX runtime is initialized and in FX application thread.
      * @param placeholder The AnchorPane where the BrowserPanel must be inserted
      */
-    public static BrowserPanel load(AnchorPane placeholder) {
-        logger.info("Initializing browser");
-        BrowserPanel browserPanel = UiPartLoader.loadUiPart(placeholder, new BrowserPanel());
+    public BrowserPanel(AnchorPane placeholder) {
+        super(FXML);
         placeholder.setOnKeyPressed(Event::consume); // To prevent triggering events for typing inside the
                                                      // loaded Web page.
-        FxViewUtil.applyAnchorBoundaryParameters(browserPanel.browser, 0.0, 0.0, 0.0, 0.0);
-        placeholder.getChildren().add(browserPanel.browser);
-        return browserPanel;
+        FxViewUtil.applyAnchorBoundaryParameters(browser, 0.0, 0.0, 0.0, 0.0);
+        placeholder.getChildren().add(browser);
     }
 
     public void loadPersonPage(ReadOnlyPerson person) {
