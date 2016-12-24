2560ee5d921684e37ad81462365d5dd940840f0e
clarencechee <clarencechee94@gmail.com>
2016-12-24 19:52:17 +0800

    IntegerUtil: add applyOffset(...)

---
+++ src/main/java/seedu/address/commons/util/IntegerUtil.java
@@ -0,0 +1,19 @@
+package seedu.address.commons.util;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * Utility methods related to Integers
+ */
+public class IntegerUtil {
+
+    /** Applies an integer {@code offset} to each integer element of {@code source}. */
+    public static void applyOffset(Collection<Integer> source, int offset) {
+        assert !CollectionUtil.isAnyNull(source);
+        Collection<Integer> offsetted = source.stream().map(i -> i + offset).collect(Collectors.toList());
+        source.clear();
+        source.addAll(offsetted);
+    }
+
+}
