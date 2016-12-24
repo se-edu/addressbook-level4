0c7cc550f88f6b21481e4c078dc0d08c40cbc834
Paul Tan <pyokagan@gmail.com>
2016-12-24 15:47:44 +0800

    MainWindow: migrate to use FXML loading constructor
    UiPartLoader.loadUiPart() is deprecated. Migrate the initialization code
    of MainWindow to use the new `UiPart(String)` FXML loading constructor.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -2,14 +2,13 @@ package seedu.address.ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.GuiSettings;
@@ -23,7 +22,7 @@ import seedu.address.model.person.ReadOnlyPerson;
  * The Main Window. Provides the basic application layout containing
  * a menu bar and space where other JavaFX elements can be placed.
  */
-public class MainWindow extends UiPart {
+public class MainWindow extends UiPart<Region> {
 
     private static final String ICON = "/images/address_book_32.png";
     private static final String FXML = "MainWindow.fxml";
@@ -37,10 +36,6 @@ public class MainWindow extends UiPart {
     private BrowserPanel browserPanel;
     private PersonListPanel personListPanel;
     private Config config;
-
-    // Handles to elements of this Ui container
-    private VBox rootLayout;
-
 
     @FXML
     private AnchorPane browserPlaceholder;
@@ -60,28 +55,8 @@ public class MainWindow extends UiPart {
     @FXML
     private AnchorPane statusbarPlaceholder;
 
-    @Override
-    public void setNode(Node node) {
-        rootLayout = (VBox) node;
-    }
-
-    @Override
-    public String getFxmlPath() {
-        return FXML;
-    }
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public static MainWindow load(Stage primaryStage, Config config, UserPrefs prefs, Logic logic) {
-        MainWindow mainWindow = UiPartLoader.loadUiPart(new MainWindow());
-        mainWindow.configure(primaryStage, config.getAppTitle(), config.getAddressBookName(), config, prefs, logic);
-        return mainWindow;
-    }
-
-    private void configure(Stage primaryStage, String appTitle, String addressBookName, Config config,
-                           UserPrefs prefs, Logic logic) {
+    public MainWindow(Stage primaryStage, Config config, UserPrefs prefs, Logic logic) {
+        super(FXML);
 
         // Set dependencies
         this.primaryStage = primaryStage;
@@ -89,14 +64,18 @@ public class MainWindow extends UiPart {
         this.config = config;
 
         // Configure the UI
-        setTitle(appTitle);
+        setTitle(config.getAppTitle());
         setIcon(ICON);
         setWindowMinSize();
         setWindowDefaultSize(prefs);
-        Scene scene = new Scene(rootLayout);
+        Scene scene = new Scene(getRoot());
         primaryStage.setScene(scene);
 
         setAccelerators();
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     private void setAccelerators() {
@@ -125,7 +104,7 @@ public class MainWindow extends UiPart {
          * help window purposely so to support accelerators even when focus is
          * in CommandBox or ResultDisplay.
          */
-        rootLayout.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+        getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
                 menuItem.getOnAction().handle(new ActionEvent());
                 event.consume();
