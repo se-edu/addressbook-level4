66ac50f2d9edd23c07eb536ba539a5ee3515ad12
Paul Tan <pyokagan@gmail.com>
2016-12-24 15:22:29 +0800

    StatusBarFooter: migrate to use FXML loading constructor
    UiPartLoader.loadUiPart() is deprecated. Migrate the initialization code
    of StatusBarFooter to use the new `UiPart(String)` FXML loading
    constructor.

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -2,9 +2,9 @@ package seedu.address.ui;
 
 import com.google.common.eventbus.Subscribe;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
 import org.controlsfx.control.StatusBar;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
 /**
  * A ui for the status bar that is displayed at the footer of the application.
  */
-public class StatusBarFooter extends UiPart {
+public class StatusBarFooter extends UiPart<Region> {
     private static final Logger logger = LogsCenter.getLogger(StatusBarFooter.class);
 
     @FXML
@@ -24,28 +24,19 @@ public class StatusBarFooter extends UiPart {
     @FXML
     private StatusBar saveLocationStatus;
 
-    private GridPane mainPane;
-
-    private AnchorPane placeHolder;
-
     private static final String FXML = "StatusBarFooter.fxml";
 
-    public static StatusBarFooter load(AnchorPane placeHolder, String saveLocation) {
-        StatusBarFooter statusBarFooter = UiPartLoader.loadUiPart(placeHolder, new StatusBarFooter());
-        statusBarFooter.configure(saveLocation);
-        return statusBarFooter;
-    }
-
-    public void configure(String saveLocation) {
-        addMainPane();
+    public StatusBarFooter(AnchorPane placeHolder, String saveLocation) {
+        super(FXML);
+        addMainPane(placeHolder);
         setSyncStatus("Not updated yet in this session");
         setSaveLocation("./" + saveLocation);
         registerAsAnEventHandler(this);
     }
 
-    private void addMainPane() {
-        FxViewUtil.applyAnchorBoundaryParameters(mainPane, 0.0, 0.0, 0.0, 0.0);
-        placeHolder.getChildren().add(mainPane);
+    private void addMainPane(AnchorPane placeHolder) {
+        FxViewUtil.applyAnchorBoundaryParameters(getRoot(), 0.0, 0.0, 0.0, 0.0);
+        placeHolder.getChildren().add(getRoot());
     }
 
     private void setSaveLocation(String location) {
@@ -54,21 +45,6 @@ public class StatusBarFooter extends UiPart {
 
     private void setSyncStatus(String status) {
         this.syncStatus.setText(status);
-    }
-
-    @Override
-    public void setNode(Node node) {
-        mainPane = (GridPane) node;
-    }
-
-    @Override
-    public void setPlaceholder(AnchorPane placeholder) {
-        this.placeHolder = placeholder;
-    }
-
-    @Override
-    public String getFxmlPath() {
-        return FXML;
     }
 
     @Subscribe
