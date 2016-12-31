bcfcf5e618082d176b20aa6bf38c2855378c1d21
clarencechee <clarencechee94@gmail.com>
2016-12-31 16:52:24 +0800

    IntegerUtilTest: minimize expected logic * we instead pass in expected values and check for equality

--- src/test/java/seedu/address/commons/util/IntegerUtilTest.java
+++ src/test/java/seedu/address/commons/util/IntegerUtilTest.java
@@ -34,30 +34,27 @@ public class IntegerUtilTest {
 
     @Test
     public void applyOffset_emptyCollection_emptyResult() throws Exception {
-        Collection<Integer> emptyList = Collections.emptyList();
-        IntegerUtil.applyOffset(emptyList, 1);
-        assertEquals(Collections.emptyList(), emptyList);
+        assertApplyOffsetSuccess(1, new Integer[0], new Integer[0]);
     }
 
     @Test
     public void applyOffset_validOffset_correctResult() throws Exception {
         // positive offset
-        assertApplyOffsetSuccess(2, 1, 5, 0);
-        assertApplyOffsetSuccess(5, -1, 4, 3);
+        assertApplyOffsetSuccess(2, new Integer[] { 1, 5, 0 }, new Integer[] { 3, 7, 2 });
+        assertApplyOffsetSuccess(5, new Integer[] { -1, 4, 3 }, new Integer[] { 4, 9, 8 });
 
         // negative offset
-        assertApplyOffsetSuccess(-3, -10, 0, 2);
-        assertApplyOffsetSuccess(-1, 0, 0, 0);
+        assertApplyOffsetSuccess(-3, new Integer[] { -10, 0, 2 }, new Integer[] { -13, -3, -1 });
+        assertApplyOffsetSuccess(-1, new Integer[] { 0, 0, 0 }, new Integer[] { -1, -1, -1 });
 
         // zero offset
-        assertApplyOffsetSuccess(0, -1, -6, -8);
-        assertApplyOffsetSuccess(0, 1, 0 , -1);
+        assertApplyOffsetSuccess(0, new Integer[] { -1, -6, -8 }, new Integer[] { -1, -6, -8 });
+        assertApplyOffsetSuccess(0, new Integer[] { 1, 0, -1 }, new Integer[] { 1, 0, -1 });
     }
 
-    private void assertApplyOffsetSuccess(int offset, Integer... integers) {
-        Collection<Integer> expectedIntegers = Stream.of(integers).map(i -> i + offset).collect(Collectors.toList());
-        Collection<Integer> testIntegers = new ArrayList<>(Arrays.asList(integers));
+    private void assertApplyOffsetSuccess(int offset, Integer[] test, Integer[] expectedIntegers) {
+        Collection<Integer> testIntegers = new ArrayList<>(Arrays.asList(test));
         IntegerUtil.applyOffset(testIntegers, offset);
-        assertEquals(expectedIntegers, testIntegers);
+        assertEquals(Arrays.asList(expectedIntegers), testIntegers);
     }
 }
