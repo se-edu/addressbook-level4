4d461aea14c52e34e6c6ffda9bee73d3afda9c3e
clarencechee <clarencechee94@gmail.com>
2016-12-31 17:54:58 +0800

    DeleteCommandTest: refactor testcases into assertDeleteSuccess * also added utility method 'assertDeleteCommandResult' * renamed getDeletedRemainder -> getRemainder

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -1,6 +1,7 @@
 package guitests;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
 
 import java.util.Arrays;
@@ -8,89 +9,57 @@ import java.util.List;
 
 import org.junit.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.util.ListUtil;
 import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.testutil.TestPerson;
 import seedu.address.testutil.TestUtil;
 
 public class DeleteCommandTest extends AddressBookGuiTest {
 
+    public final int size = td.getTypicalPersons().length;
+
     @Test
     public void delete_nonRangedIndices() {
-        TestPerson[] currentList = td.getTypicalPersons();
-
-        // deleting from front
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 0);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("1", expectedRemainder, expectedPersonsDeleted);
-        currentList = expectedRemainder;
-
-        // deleting from rear
-        expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), currentList.length - 1);
-        expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("" + currentList.length, expectedRemainder, expectedPersonsDeleted);
-        currentList = expectedRemainder;
-
-        // deleting from middle
-        expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), currentList.length / 2 - 1);
-        expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("" + currentList.length / 2, expectedRemainder, expectedPersonsDeleted);
+        Integer[] expectedTargetIndices = { 0, size / 2 - 1, size - 1 };
+        // deleting first, middle and last item
+        assertDeleteSuccess("1 " + (size / 2) + " " + size, expectedTargetIndices);
     }
 
     @Test
-    public void delete_rangedIndices() {
-        TestPerson[] currentList = td.getTypicalPersons();
+    public void delete_rangedIndicesAscending() {
+        assertDeleteSuccess("1-3", 0, 1, 2);
+    }
 
-        // deleting ascending range
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 0, 1, 2);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("1-3", expectedRemainder, expectedPersonsDeleted);
-        currentList = expectedRemainder;
-
-        // deleting descending range
-        expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 1, 2, 3);
-        expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("4-2", expectedRemainder, expectedPersonsDeleted);
+    @Test
+    public void delete_rangedIndicesDescending() {
+        assertDeleteSuccess("4-2", 1, 2, 3);
     }
 
     @Test
     public void delete_multipleIndicesOrdered() {
-        TestPerson[] currentList = td.getTypicalPersons();
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 0, 1, 2, 4, 6);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("1-3 5 7", expectedRemainder, expectedPersonsDeleted);
+        assertDeleteSuccess("1-3 5 7", 0, 1, 2, 4, 6);
     }
 
     @Test
     public void delete_multipleIndicesUnordered() {
-        TestPerson[] currentList = td.getTypicalPersons();
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 0, 1, 2, 4, 6);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("5 1-3 7", expectedRemainder, expectedPersonsDeleted);
+        assertDeleteSuccess("5 1-3 7", 0, 1, 2, 4, 6);
     }
 
     @Test
     public void delete_duplicateIndices_duplicatesIgnored() {
-        TestPerson[] currentList = td.getTypicalPersons();
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 0);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("1 1 1 1 1", expectedRemainder, expectedPersonsDeleted);
+        assertDeleteSuccess("1 1 1 1 1", 0);
     }
 
     @Test
     public void delete_overlappingRanges() {
-        TestPerson[] currentList = td.getTypicalPersons();
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 3, 4, 5);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("4-5 4-6 5-6 4-4 5-5 6-6", expectedRemainder, expectedPersonsDeleted);
+        assertDeleteSuccess("4-5 4-6 5-6 4-4 5-5 6-6", 3, 4, 5);
     }
 
     @Test
     public void delete_excessiveWhitespaces() {
-        TestPerson[] currentList = td.getTypicalPersons();
-        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), 1, 2);
-        TestPerson[] expectedRemainder = getDeletedRemainder(currentList, expectedPersonsDeleted);
-        assertDeleteSuccess("         2      3      ", expectedRemainder, expectedPersonsDeleted);
+        assertDeleteSuccess("         2      3      ", 1, 2);
     }
 
     @Test
@@ -108,27 +77,31 @@ public class DeleteCommandTest extends AddressBookGuiTest {
 
     /**
      * Runs the delete command to delete persons specified by {@code deleteArgs} and confirms the result
-     * is correct i.e. list deleted {@code expectedPersonsDeleted} and reflects {@code expectedRemainder} to user.
+     * is correct i.e. list deleted persons specified by {@code expectedTargetIndices}.
      */
-    private void assertDeleteSuccess(String deleteArgs, TestPerson[] expectedRemainder,
-            List<TestPerson> expectedPersonsDeleted) {
+    private void assertDeleteSuccess(String deleteArgs, Integer... expectedTargetIndices) {
+        TestPerson[] currentList = td.getTypicalPersons();
+        List<TestPerson> expectedPersonsDeleted = ListUtil.subList(Arrays.asList(currentList), expectedTargetIndices);
+        TestPerson[] expectedRemainder = getRemainder(currentList, expectedPersonsDeleted);
 
-        commandBox.runCommand("delete " + deleteArgs);
+        String expectedMessage = String.format(MESSAGE_DELETE_PERSON_SUCCESS, expectedPersonsDeleted.size(),
+                                               StringUtil.toIndexedListString(expectedPersonsDeleted));
 
-        //confirm the list now contains all previous persons except the deleted person
+        assertDeleteCommandResult(deleteArgs, expectedMessage);
+
+        //confirm the list now contains all previous persons except the deleted
         assertTrue(personListPanel.isListMatching(expectedRemainder));
-
-        String expectedFeedback = StringUtil.toIndexedListString(expectedPersonsDeleted);
-
-        //confirm the result message is correct
-        assertResultMessage(
-                String.format(MESSAGE_DELETE_PERSON_SUCCESS, expectedPersonsDeleted.size(), expectedFeedback));
     }
 
-    /** Returns a copy of {@code currentList} without {@code personsToDelete}. */
-    private TestPerson[] getDeletedRemainder(TestPerson[] currentList, List<TestPerson> personsToDelete) {
-        return TestUtil.removePersonsFromList(currentList,
-                personsToDelete.toArray(new TestPerson[personsToDelete.size()]));
+    /** Runs the delete command specified by {@code deleteArgs} and confirms result matches {@code expectedMessage}. */
+    private void assertDeleteCommandResult(String deleteArgs, String expectedMessage) {
+        commandBox.runCommand("delete " + deleteArgs);
+        assertResultMessage(expectedMessage);
     }
 
+    /** Returns a copy of {@code currentList} but without {@code personsToDelete}. */
+    private TestPerson[] getRemainder(TestPerson[] currentList, List<TestPerson> personsToDelete) {
+        return TestUtil.removePersonsFromList(
+                currentList, personsToDelete.toArray(new TestPerson[personsToDelete.size()]));
+    }
 }
