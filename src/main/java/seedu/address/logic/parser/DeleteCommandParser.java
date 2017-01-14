fb83d13ce6c204d354c6a1ef0c576fb6f723a27a
MightyCupcakes <gohydj2003@gmail.com>
2017-01-14 19:42:23 +0800

    Split the parser for delete command into DeleteCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Optional;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.IncorrectCommand;
+
+/**
+ * Parses input arguments and creates a new DeleteCommand object
+ */
+public class DeleteCommandParser extends CommandParser {
+
+    @Override
+    public Command prepareCommand(String args) {
+        Optional<Integer> index = Parser.parseIndex(args);
+        if (!index.isPresent()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+
+        return new DeleteCommand(index.get());
+    }
+
+}
