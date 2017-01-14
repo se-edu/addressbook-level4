35cb70d06436e89712d85198c9869f5b8aa335ed
clarencechee <clarencechee94@gmail.com>
2017-01-14 23:28:50 +0800

    Model: implement deletePersons
    * ModelManager::deletePerson -> ModelManager::deletePersons
    * AddressBook::removePerson -> AddressBook::removePersons
    * add UniquePersonList::removeAll
    * PersonNotFoundException -> PersonsNotFoundException
    * update DG of API changes

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -1,5 +1,6 @@
 package seedu.address.model;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -13,7 +14,7 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.UniquePersonList;
-import seedu.address.model.person.UniquePersonList.PersonNotFoundException;
+import seedu.address.model.person.UniquePersonList.PersonsNotFoundException;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -59,9 +60,11 @@ public class ModelManager extends ComponentManager implements Model {
     }
 
     @Override
-    public synchronized void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException {
-        addressBook.removePerson(target);
-        indicateAddressBookChanged();
+    public synchronized void deletePersons(Collection<ReadOnlyPerson> persons) throws PersonsNotFoundException {
+        addressBook.removePersons(persons);
+        if (!persons.isEmpty()) {
+            indicateAddressBookChanged();
+        }
     }
 
     @Override
