3ecfdd52cd8cba3cb9e6adf1be2c597f26447f32
MightyCupcakes <gohydj2003@gmail.com>
2017-01-14 22:14:40 +0800

    Implement TestParserRegistry for testing purposes

---
+++ src/test/java/seedu/address/testutil/TestParserRegistry.java
@@ -0,0 +1,23 @@
+package seedu.address.testutil;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import seedu.address.logic.parser.CommandParser;
+import seedu.address.logic.parser.ParserRegistry;
+
+/**
+ * A parser registry class solely used for testing
+ *
+ */
+public class TestParserRegistry extends ParserRegistry {
+
+    public Map<String, Class<? extends CommandParser>> getCommandRegistry() {
+        return commandRegistry;
+    }
+
+    public void setCommandRegistry(Map<String, Class<? extends CommandParser>> newRegistry) {
+        commandRegistry.clear();
+        commandRegistry.putAll(newRegistry);
+    }
+}
