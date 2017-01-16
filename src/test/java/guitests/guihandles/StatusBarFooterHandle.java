5f71eb7adabcc22e630dd8aa61c7614777ab51fa
chao1995 <huangchao0825@gmail.com>
2017-01-16 21:48:11 +0800

    Add GUI test for last update feature

---
+++ src/test/java/guitests/guihandles/StatusBarFooterHandle.java
@@ -0,0 +1,27 @@
+package guitests.guihandles;
+
+import guitests.GuiRobot;
+import javafx.stage.Stage;
+import org.controlsfx.control.StatusBar;
+import seedu.address.TestApp;
+
+/**
+ * A handle for the status bar at the footer of the application.
+ */
+public class StatusBarFooterHandle extends GuiHandle {
+
+    public static final String SYNC_STATUS_ID = "#syncStatus";
+    public static final String SAVE_LOCATION_STATUS_ID = "#saveLocationStatus";
+
+    public StatusBarFooterHandle(GuiRobot guiRobot, Stage primaryStage) {
+        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    }
+
+    public String getSyncStatus() {
+        return ((StatusBar) getNode(SYNC_STATUS_ID)).getText();
+    }
+
+    public String getSaveLocation() {
+        return ((StatusBar) getNode(SAVE_LOCATION_STATUS_ID)).getText();
+    }
+}
