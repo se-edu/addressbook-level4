80bebd353e3499e4fbd99d934b0a3244546c63ab
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-16 20:33:38 +0800

    Add unit tests for person's fields classes
    The person's fields classes (Phone, Email, Address, Name) are
    previously being tested indirectly by integration testing, through
    testing of the "Add" command, which happened to use these classes.
    
    Therefore, this commit will add unit tests for them. By doing so, we
    are able to test the validation code for these classes directly, and
    use inputs that were previously impossible to test in the integration
    testings (such as having a blank address with spaces: " ", which
    cannot be entered through the command box).
    
    The Address and Email will fail their unit tests for this commit (will
    be fixed by the next two commits).

---
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -0,0 +1,27 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PhoneTest {
+
+    @Test
+    public void isValidPhone_emptyString_returnsFalse() {
+        assertFalse(Phone.isValidPhone(""));
+    }
+
+    @Test
+    public void isValidPhone_containsNonDigits_returnsFalse() {
+        assertFalse(Phone.isValidPhone(" "));
+        assertFalse(Phone.isValidPhone("phone"));
+        assertFalse(Phone.isValidPhone("9011p041"));
+        assertFalse(Phone.isValidPhone("9312 1534"));
+    }
+
+    @Test
+    public void isValidPhone_digitsOnly_returnsTrue() {
+        assertTrue(Phone.isValidPhone("93121534"));
+    }
+}
