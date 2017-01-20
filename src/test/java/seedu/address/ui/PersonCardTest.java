d7627dbb4d263a77c0caf8d170716a26fc6da3d6
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-20 11:17:09 +0800

    Add PersonCardTest
    Create PersonCardTest to test if the labels and tags are displayed
    correctly with the right content when assigned to a valid person.
    
    An alternative method of checking the labels would be using reflections.
    However, reflections are not suitable for level 4 learning objectives.

---
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -0,0 +1,72 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import guitests.guihandles.PersonCardHandle;
+import seedu.address.model.tag.UniqueTagList;
+import seedu.address.testutil.TestPerson;
+import seedu.address.testutil.TypicalTestPersons;
+
+public class PersonCardTest {
+
+    GuiUnitTestApp testApp;
+
+    @Before
+    public void setUp() throws Exception {
+        int desiredWidth = 200;
+        int desiredHeight = 120;
+
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.hideStage();
+        testApp = (GuiUnitTestApp) FxToolkit.setupApplication(() ->
+                                                 new GuiUnitTestApp(desiredWidth, desiredHeight));
+    }
+
+    @Test
+    public void personCard_validPersonWithNoTags_labelsShowCorrectly() {
+        assertCardDisplay(0, new TypicalTestPersons().george);
+    }
+
+    @Test
+    public void personCard_validPersonWithTags_labelsShowCorrectly() {
+        assertCardDisplay(1, new TypicalTestPersons().alice);
+    }
+
+    /**
+     * Asserts that the card displays the details correctly, given a valid person.
+     */
+    private void assertCardDisplay(int validId, TestPerson validPerson) {
+        PersonCard personCard = new PersonCard(validPerson, validId);
+        testApp.addUiPart(personCard);
+        PersonCardHandle personCardHandle = new PersonCardHandle(testApp.getGuiRobot(), testApp.getStage(),
+                                                                    personCard.getRoot());
+
+        assertEquals(Integer.toString(validId) + ". ", personCardHandle.getId());
+        assertEquals(validPerson.getName().toString(), personCardHandle.getFullName());
+        assertEquals(validPerson.getPhone().toString(), personCardHandle.getPhone());
+        assertEquals(validPerson.getAddress().toString(), personCardHandle.getAddress());
+        assertEquals(validPerson.getEmail().toString(), personCardHandle.getEmail());
+        assertTagsContent(validPerson.getTags(), personCardHandle.getTags());
+    }
+
+    /**
+     * Asserts that the card creates all the tag labels correctly, given a list of valid tags.
+     */
+    private void assertTagsContent(UniqueTagList expectedTags, List<String> actualTags) {
+        Set<String> expectedTagsSet = new HashSet<String>();
+        Set<String> actualTagsSet = new HashSet<String>();
+
+        expectedTags.forEach((tag) -> expectedTagsSet.add(tag.tagName));
+        actualTags.forEach((tag) -> actualTagsSet.add(tag));
+        assertTrue(expectedTagsSet.equals(actualTagsSet));
+    }
+}
