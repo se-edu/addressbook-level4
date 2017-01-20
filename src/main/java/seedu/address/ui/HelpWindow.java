4545d8f25ff8339d220071322196c70ebd0cbad3
MightyCupcakes <gohydj2003@gmail.com>
2017-01-20 11:22:00 +0800

    Modify HelpWindow.java to point to the HTML user guide
    The current URL points to the GitHub repo view of our user guide,
    which contains several UI elements which makes it look less like a
    formal user guide document and more like a file in a Git repo.
    Furthermore, the new HTML user guide is more aesthetically
    pleasing and might encourage users to read on for more details.

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -20,7 +20,7 @@ public class HelpWindow extends UiPart<Region> {
     private static final String FXML = "HelpWindow.fxml";
     private static final String TITLE = "Help";
     private static final String USERGUIDE_URL =
-            "https://github.com/se-edu/addressbook-level4/blob/master/docs/UserGuide.md";
+            "https://se-edu.github.io/addressbook-level4/docs/UserGuide.html";
 
     @FXML
     private WebView browser;
