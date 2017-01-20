230830264536ea8405b734e5523dcfa4cf90ce27
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-01-21 01:37:54 +0800

    ParserUtil: remove unnecessary toSet()
    ParserUtil#toSet() converts a List<String> into Set<String>, ensuring
    the uniqueness of String elements in a collection.
    
    It is redundant code as it is superseded by the existing
    ParserUtil#parseTags() and UniqueTagList class.
    
    Let's remove toSet() and update the logic to make use of
    ParserUtil#parseTags() and UniqueTagList instead.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -32,7 +32,7 @@ public class AddCommandParser {
                     argsTokenizer.getValue(PREFIX_PHONE).get(),
                     argsTokenizer.getValue(PREFIX_EMAIL).get(),
                     argsTokenizer.getValue(PREFIX_ADDRESS).get(),
-                    ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
+                    ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG))
             );
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
