42bea467f5b3d1a6f3ed8fb643edc82215b173b7
Ka Yi <chua.kayi@yahoo.com.sg>
2017-01-21 21:49:07 +0800

    Make name validation regex less restrictive
    The name validation only accepts alphanumeric and whitespace
    characters. This is too restrictive and rejects valid names as well,
    such as non-english names, hyphenated names and names containing dots,
    commas or apostrophes.
    
    Hence, update the regular expression for names to include unicode
    letters, unicode marks, dots, commas, single quotes and dashes.
    
    Unicode marks are required in addition to unicode letters because
    certain names cannot be written using letters alone (e.g. Nepali name
    containing vowels, see: http://meta.stackexchange.com/a/171866).
    
    However, allowing marks means certain accented letters can be written
    in two different ways:
     1) precomposed character: one unicode letter
     2) decomposed characters: one unicode letter and one unicode mark
    While these look the same to the user, note that Java's
    String.equalsIgnoreCase() method considers letters input in these two
    ways as different letters.
    
    Furthermore, Unicode uses case folding for case insensitive comparisons
    because case mapping (mapping [a-z] to [A-Z]) does not work when other
    languages are considered (see:
    https://www.elastic.co/guide/en/elasticsearch/guide/current/case-folding.html)
    However, Java's String.equalsignoreCase() method only uses simple case
    folding, so case insensitive comparisons do not work properly for
    certain languages.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -9,14 +9,19 @@ import seedu.address.commons.exceptions.IllegalValueException;
 public class Name {
 
     public static final String MESSAGE_NAME_CONSTRAINTS =
-            "Person names should only contain alphanumeric characters and spaces, and it should not be blank";
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
+            "Person names should only contain spaces, letters or characters, numbers "
+            + "or the following punctuation marks: . , ' -";
+    /**
+     * Names can contain spaces and the following:
+     *   unicode letters (e.g. German name with accents, where accented letters are written using one character),
+     *   unicode marks (e.g. accented names, where an accented letter is made up of a letter and an accent character)
+     *   numbers,
+     *   dots (e.g. John Paul Jr.),
+     *   commas (e.g. Smith, John),
+     *   apostrophes (e.g. d'Souza),
+     *   dashes (e.g. Jolie-Pitt)
      */
-    public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
-
+    public static final String NAME_VALIDATION_REGEX = "^[ \\p{L}\\p{M}0-9.,'-]+$";
     public final String fullName;
 
     /**
