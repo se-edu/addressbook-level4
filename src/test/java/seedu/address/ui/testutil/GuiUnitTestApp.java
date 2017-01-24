b27fc16a7c59c8e43c0105adf0306e5799ea8064
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-24 13:15:14 +0800

    Add GuiUnitTestApp
    The GUI tests are system tests, which would make use of the main
    application and create all of the GUI parts.
    
    There is no easy way to unit test the GUI parts without launching the
    entire application.
    
    Let's add a GuiUnitTestApp class that allows creation of only a single
    GUI part that we want to unit test.

---
+++ src/test/java/seedu/address/ui/testutil/GuiUnitTestApp.java
@@ -0,0 +1,62 @@
+package seedu.address.ui.testutil;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import seedu.address.ui.UiPart;
+
+/**
+ * A dummy application for unit testing of a single GUI component.
+ *
+ * Instead of creating the entire GUI (like {@link seedu.address.TestApp}),
+ * {@link GuiUnitTestApp} creates a blank window, and allows insertion of a
+ * single UiPart. This allows the GUI part under test to be created without
+ * creating the rest of the GUI.
+ */
+public class GuiUnitTestApp extends Application {
+
+    private static final int DEFAULT_STAGE_WIDTH = 400;
+    private static final int DEFAULT_STAGE_HEIGHT = 400;
+
+    private Stage stage;
+    private AnchorPane mainPane;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        this.mainPane = new AnchorPane();
+
+        Scene scene = new Scene(mainPane, DEFAULT_STAGE_WIDTH, DEFAULT_STAGE_HEIGHT);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Adds a new UI part that is being tested into the scene.
+     */
+    public void addUiPart(UiPart<Region> part) {
+        Platform.runLater(() -> mainPane.getChildren().add(part.getRoot()));
+    }
+
+    /**
+     * Clears all UI parts added to the scene.
+     */
+    public void clearUiParts() {
+        Platform.runLater(() -> mainPane.getChildren().clear());
+    }
+
+    public void setStageWidth(int desiredWidth) {
+        this.stage.setWidth(desiredWidth);
+    }
+
+    public void setStageHeight(int desiredHeight) {
+        this.stage.setHeight(desiredHeight);
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+}
