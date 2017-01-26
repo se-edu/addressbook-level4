6709ccff7b5463a57e11f3fb3b4e3fe4117b16fa
MightyCupcakes <gohydj2003@gmail.com>
2017-01-26 18:00:58 +0800

    Extract FindCommandParser from Parser class
    Parser class implements all different
    command parsers as private methods within the Parser class.
    Parser::parseCommand is responsible for performing an initial
    parsing on the given input stringand from there determine
    the correct method to parse the String to.
    
    This causes the Parser file to became very long
    containing all command parser methods
    and their associated helper methods that is only utlized in that
    one method itself.
    
    Let's move the functionality of parsing of the
    find command from Parser to the more concrete
    FindCommandParser class which is in line with the
    Single Responsiblity Principle.

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -2,11 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,7 +58,7 @@ public class Parser {
             return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
-            return prepareFind(arguments);
+            return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
@@ -76,25 +72,6 @@ public class Parser {
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
         }
-    }
-
-    /**
-     * Parses arguments in the context of the find person command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareFind(String args) {
-        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
-        if (!matcher.matches()) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    FindCommand.MESSAGE_USAGE));
-        }
-
-        // keywords delimited by whitespace
-        final String[] keywords = matcher.group("keywords").split("\\s+");
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindCommand(keywordSet);
     }
 
 }
