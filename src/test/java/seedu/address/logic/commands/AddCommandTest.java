5c18663c0f14355e8624c33f4465ad3b5f88c3db
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-26 23:17:28 +0800

    Interdiff between v4 and v5

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,11 +15,13 @@ import org.junit.rules.ExpectedException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ModelManager;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.person.UniquePersonList.DuplicatePersonException;
 import seedu.address.model.tag.UniqueTagList;
-import seedu.address.testutil.TestPerson;
-import seedu.address.testutil.TypicalTestPersons;
 
 public class AddCommandTest {
 
@@ -28,19 +30,8 @@ public class AddCommandTest {
     private static final String VALID_EMAIL = "valid.email@mail.com";
     private static final String VALID_ADDRESS = "valid address";
 
-
-    private ModelStub model;
-
-    private TypicalTestPersons typicalTestPersons;
-
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void setUp() throws Exception {
-        typicalTestPersons = new TypicalTestPersons();
-        model = new ModelStub();
-    }
 
     @Test
     public void constructor_invalidName_throwsIllegalValueException() throws Exception {
@@ -68,69 +59,62 @@ public class AddCommandTest {
 
     @Test
     public void execute_newPersonNotInList_addSuccessful() throws Exception {
-        AddCommand command = getAddCommandForPerson(typicalTestPersons.alice);
-        assertCommandBehaviour(String.format(AddCommand.MESSAGE_SUCCESS, typicalTestPersons.alice), command);
+        ModelStub modelStub = new ModelStub();
+        Person validPerson = new Person(new Name(VALID_NAME), new Phone(VALID_PHONE), new Email(VALID_EMAIL),
+                                        new Address(VALID_ADDRESS), new UniqueTagList());
+
+        AddCommand command = getAddCommandForPerson(validPerson, modelStub);
+        CommandResult commandResult = command.execute();
+
+        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPerson), commandResult.feedbackToUser);
+        assertTrue(modelStub.isPersonAdded(validPerson));
     }
 
     @Test
     public void execute_duplicatePerson_throwsDuplicatePersonException() throws Exception {
-        // set up model such that alice is our target person for testing duplicates
-        model.addPerson(new Person(typicalTestPersons.alice));
+        ModelStub modelStub = new ModelStub();
+        Person validPerson = new Person(new Name(VALID_NAME), new Phone(VALID_PHONE), new Email(VALID_EMAIL),
+                                        new Address(VALID_ADDRESS), new UniqueTagList());
+
+        modelStub.addPerson(validPerson);
 
         thrown.expect(CommandException.class);
         thrown.expectMessage(AddCommand.MESSAGE_DUPLICATE_PERSON);
 
-        AddCommand command = getAddCommandForPerson(typicalTestPersons.alice);
+        AddCommand command = getAddCommandForPerson(validPerson, modelStub);
         command.execute();
-    }
-
-    /**
-     * Asserts that the command produced the correct message after execution.
-     */
-    public void assertCommandBehaviour(String expectedString, Command command) throws CommandException {
-        CommandResult commandResult = command.execute();
-
-        assertEquals(expectedString, commandResult.feedbackToUser);
     }
 
     /**
      * Generates a new AddCommand with the details of the given person.
      */
-    private AddCommand getAddCommandForPerson(TestPerson testPerson) throws IllegalValueException {
-        Set<String> tags = convertTagsToStringSet(testPerson.getTags());
+    private AddCommand getAddCommandForPerson(Person person, ModelStub modelStub) throws IllegalValueException {
+        Set<String> tags = new HashSet<>();
+        person.getTags().forEach((t) -> tags.add(t.tagName));
 
-        AddCommand command = new AddCommand(testPerson.getName().toString(),
-                                            testPerson.getPhone().toString(),
-                                            testPerson.getEmail().toString(),
-                                            testPerson.getAddress().toString(),
+        AddCommand command = new AddCommand(person.getName().toString(), person.getPhone().toString(),
+                                            person.getEmail().toString(), person.getAddress().toString(),
                                             tags);
 
-        command.setData(model);
+        command.setData(modelStub);
         return command;
-    }
-
-    /**
-     * Converts the list of tags into a set of strings.
-     */
-    private Set<String> convertTagsToStringSet(UniqueTagList uniqueTagList) {
-        Set<String> stringSet = new HashSet<String>();
-        uniqueTagList.forEach((t) -> stringSet.add(t.tagName));
-        return stringSet;
     }
 
     /**
      * A model stub injected into the add command, for testing add's functionality.
      *
-     * Only {@link #addPerson(Person)} is implemented for this stub.
+     * We do not want the correctness of {@link ModelManager#addPerson(Person)} to affect the unit testing
+     * of AddCommand. Therefore, it is overridden to simplify the logic.
      */
     private class ModelStub extends ModelManager {
 
         private List<Person> personInList;
 
         public ModelStub() {
-            personInList = new ArrayList<Person>();
+            personInList = new ArrayList<>();
         }
 
+        @Override
         public void addPerson(Person person) throws DuplicatePersonException {
             for (Person existingPerson : personInList) {
                 if (existingPerson.equals(person)) {
@@ -139,6 +123,13 @@ public class AddCommandTest {
             }
 
             personInList.add(person);
+        }
+
+        /**
+         * Checks whether the particular person is added.
+         */
+        public boolean isPersonAdded(Person person) {
+            return personInList.contains(person);
         }
     }
 }
