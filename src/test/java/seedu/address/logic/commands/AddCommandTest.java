a137970a07a40978ccdefb13246e3f41c6a79917
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-26 23:17:28 +0800

    Add unit test for AddCommand
    AddCommand does not have unit tests.
    
    Let's write AddCommandTest (unit test version), which injects a model
    stub into the command, so that the behavior of AddCommand can be
    tested in isolation.

---
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -0,0 +1,120 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList.DuplicatePersonException;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TestPerson;
+
+public class AddCommandTest {
+
+    private static final String VALID_NAME = "valid name";
+    private static final String VALID_PHONE = "61239876";
+    private static final String VALID_EMAIL = "valid.email@mail.com";
+    private static final String VALID_ADDRESS = "valid address";
+
+    private TestPerson validPerson;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        validPerson = new PersonBuilder().withName(VALID_NAME).withPhone(VALID_PHONE).withEmail(VALID_EMAIL)
+                .withAddress(VALID_ADDRESS).build();
+    }
+
+    /*
+     * Constructor tests ensure that any invalid values actually throws an exception.
+     *
+     * It does not test the validation logic of the values, that is the job of the unit
+     * test of Name, Phone, Email, and Address.
+     */
+
+    @Test
+    public void constructor_invalidName_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand("", VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidPhone_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand(VALID_NAME, "", VALID_EMAIL, VALID_ADDRESS, new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidEmail_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand(VALID_NAME, VALID_PHONE, "", VALID_ADDRESS, new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidAddress_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand(VALID_NAME, VALID_PHONE, VALID_EMAIL, "", new HashSet<>());
+    }
+
+    @Test
+    public void execute_newPersonNotInList_addSuccessful() throws Exception {
+        ModelManager modelStub = new ModelStubAcceptingPersonAdded();
+
+        CommandResult commandResult = getAddCommandForPerson(validPerson, modelStub).execute();
+
+        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPerson), commandResult.feedbackToUser);
+    }
+
+    @Test
+    public void execute_duplicatePerson_throwsDuplicatePersonException() throws Exception {
+        ModelManager modelStub = new ModelStubThrowingDuplicatePersonException();
+
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddCommand.MESSAGE_DUPLICATE_PERSON);
+
+        getAddCommandForPerson(validPerson, modelStub).execute();
+    }
+
+    /**
+     * Generates a new AddCommand with the details of the given person.
+     */
+    private AddCommand getAddCommandForPerson(TestPerson person, Model model) throws IllegalValueException {
+        Set<String> tags = person.getTagsSet();
+
+        AddCommand command = new AddCommand(person.getName().toString(), person.getPhone().toString(),
+                person.getEmail().toString(), person.getAddress().toString(), tags);
+
+        command.setData(model);
+        return command;
+    }
+
+    /**
+     * A Model stub that always throw a DuplicatePersonException when trying to add a person.
+     */
+    private class ModelStubThrowingDuplicatePersonException extends ModelManager {
+        public void addPerson(Person person) throws DuplicatePersonException {
+            throw new DuplicatePersonException();
+        }
+    }
+
+    /**
+     * A Model stub that always accept the person being added.
+     */
+    private class ModelStubAcceptingPersonAdded extends ModelManager {
+        public void addPerson(Person person) throws DuplicatePersonException {
+            // pretend to accept (but actually does nothing)
+        }
+    }
+}
