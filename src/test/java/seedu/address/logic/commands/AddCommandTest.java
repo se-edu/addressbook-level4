98c977e20873bc8e858f1f46682d23872e77ebd5
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-26 23:17:28 +0800

    Interdiff between v14 and v15

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -33,65 +33,16 @@ public class AddCommandTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    /*
-     * Constructor tests ensure that any invalid values actually throws an exception.
-     *
-     * It does not test the validation logic of the values, that is the job of the unit
-     * test of Name, Phone, Email, and Address.
-     */
-
     @Test
-    public void constructor_invalidName_throwsIllegalValueException() throws Exception {
+    public void constructor_invalidParameters_throwsIllegalValueException() throws Exception {
         thrown.expect(IllegalValueException.class);
         new AddCommand("", VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, new UniqueTagList());
     }
 
     @Test
-    public void constructor_invalidPhone_throwsIllegalValueException() throws Exception {
-        thrown.expect(IllegalValueException.class);
-        new AddCommand(VALID_NAME, "", VALID_EMAIL, VALID_ADDRESS, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_invalidEmail_throwsIllegalValueException() throws Exception {
-        thrown.expect(IllegalValueException.class);
-        new AddCommand(VALID_NAME, VALID_PHONE, "", VALID_ADDRESS, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_invalidAddress_throwsIllegalValueException() throws Exception {
-        thrown.expect(IllegalValueException.class);
-        new AddCommand(VALID_NAME, VALID_PHONE, VALID_EMAIL, "", new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_nullName_throwsAssertionError() throws Exception {
-        thrown.expect(AssertionError.class);
-        new AddCommand(null, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_nullPhone_throwsAssertionError() throws Exception {
+    public void constructor_nullParameters_throwsAssertionError() throws Exception {
         thrown.expect(AssertionError.class);
         new AddCommand(VALID_NAME, null, VALID_EMAIL, VALID_ADDRESS, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_nullEmail_throwsAssertionError() throws Exception {
-        thrown.expect(AssertionError.class);
-        new AddCommand(VALID_NAME, VALID_PHONE, null, VALID_ADDRESS, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_nullAddress_throwsAssertionError() throws Exception {
-        thrown.expect(AssertionError.class);
-        new AddCommand(VALID_NAME, VALID_PHONE, VALID_EMAIL, null, new UniqueTagList());
-    }
-
-    @Test
-    public void constructor_nullTags_throwsNullPointerException() throws Exception {
-        thrown.expect(NullPointerException.class);
-        new AddCommand(VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, null);
     }
 
     @Test
@@ -107,7 +58,7 @@ public class AddCommandTest {
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() throws Exception {
-        Model modelStub = new ModelStubThrowingDuplicatePersonException();
+        ModelStub modelStub = new ModelStubThrowingDuplicatePersonException();
         TestPerson validPerson = createValidPerson();
 
         thrown.expect(CommandException.class);
@@ -136,11 +87,12 @@ public class AddCommandTest {
     }
 
     /**
-     * A Model stub that always throw a DuplicatePersonException when trying to add a person.
+     * A default model stub that have all of the methods failing.
      */
-    private class ModelStubThrowingDuplicatePersonException implements Model {
+    private class ModelStub implements Model {
+        @Override
         public void addPerson(Person person) throws DuplicatePersonException {
-            throw new DuplicatePersonException();
+            fail("This method should not be called.");
         }
 
         @Override
@@ -183,11 +135,22 @@ public class AddCommandTest {
     }
 
     /**
+     * A Model stub that always throw a DuplicatePersonException when trying to add a person.
+     */
+    private class ModelStubThrowingDuplicatePersonException extends ModelStub {
+        @Override
+        public void addPerson(Person person) throws DuplicatePersonException {
+            throw new DuplicatePersonException();
+        }
+    }
+
+    /**
      * A Model stub that always accept the person being added.
      */
-    private class ModelStubAcceptingPersonAdded implements Model {
+    private class ModelStubAcceptingPersonAdded extends ModelStub {
         private Person personAdded = null;
 
+        @Override
         public void addPerson(Person person) throws DuplicatePersonException {
             if (personAdded != null) {
                 fail("This method should not be called twice.");
@@ -198,44 +161,6 @@ public class AddCommandTest {
 
         public boolean isAddPersonMethodCalledWith(TestPerson person) {
             return personAdded.equals(person);
-        }
-
-        @Override
-        public void resetData(ReadOnlyAddressBook newData) {
-            fail("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyAddressBook getAddressBook() {
-            fail("This method should not be called.");
-            return null;
-        }
-
-        @Override
-        public void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException {
-            fail("This method should not be called.");
-        }
-
-        @Override
-        public void updatePerson(int filteredPersonListIndex, ReadOnlyPerson editedPerson)
-                throws DuplicatePersonException {
-            fail("This method should not be called.");
-        }
-
-        @Override
-        public UnmodifiableObservableList<ReadOnlyPerson> getFilteredPersonList() {
-            fail("This method should not be called.");
-            return null;
-        }
-
-        @Override
-        public void updateFilteredListToShowAll() {
-            fail("This method should not be called.");
-        }
-
-        @Override
-        public void updateFilteredPersonList(Set<String> keywords) {
-            fail("This method should not be called.");
         }
     }
 
