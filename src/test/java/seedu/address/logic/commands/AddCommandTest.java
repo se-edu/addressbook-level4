0c9a47d141aaf88c1e8b430fe7c16a5e950ba233
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-26 23:17:28 +0800

    Interdiff between v2 and v3

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -2,7 +2,9 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
@@ -10,35 +12,27 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ModelManager;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.UniquePersonList.DuplicatePersonException;
-import seedu.address.model.person.UniquePersonList.PersonNotFoundException;
 import seedu.address.model.tag.UniqueTagList;
 import seedu.address.testutil.TestPerson;
 import seedu.address.testutil.TypicalTestPersons;
 
 public class AddCommandTest {
 
-    private AddModelStub model;
+    private ModelStub model;
 
-    private TestPerson newPerson;
-    private TestPerson duplicatePerson;
+    private TypicalTestPersons typicalTestPersons;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
-        TypicalTestPersons typicalTestPersons = new TypicalTestPersons();
-
-        newPerson = typicalTestPersons.alice;
-        duplicatePerson = typicalTestPersons.daniel;
-        model = new AddModelStub(duplicatePerson);
+        typicalTestPersons = new TypicalTestPersons();
+        model = new ModelStub();
     }
 
     @Test
@@ -67,13 +61,16 @@ public class AddCommandTest {
 
     @Test
     public void execute_newPersonNotInList_addSuccessful() throws Exception {
-        AddCommand command = getAddCommandForPerson(newPerson);
-        assertCommandBehaviour(String.format(AddCommand.MESSAGE_SUCCESS, newPerson), command);
+        AddCommand command = getAddCommandForPerson(typicalTestPersons.alice);
+        assertCommandBehaviour(String.format(AddCommand.MESSAGE_SUCCESS, typicalTestPersons.alice), command);
     }
 
     @Test
     public void execute_duplicatePerson_throwsDuplicatePersonException() throws Exception {
-        AddCommand command = getAddCommandForPerson(duplicatePerson);
+        // set up model such that alice is our target person for testing duplicates
+        model.addPerson(new Person(typicalTestPersons.alice));
+
+        AddCommand command = getAddCommandForPerson(typicalTestPersons.alice);
         assertCommandBehaviour(AddCommand.MESSAGE_DUPLICATE_PERSON, command);
     }
 
@@ -116,55 +113,22 @@ public class AddCommandTest {
      *
      * Only {@link #addPerson(Person)} is implemented for this stub.
      */
-    private class AddModelStub implements Model {
+    private class ModelStub extends ModelManager {
 
-        private TestPerson personInList;
+        private List<Person> personInList;
 
-        protected AddModelStub(TestPerson personInList) {
-            this.personInList = personInList;
+        public ModelStub() {
+            personInList = new ArrayList<Person>();
         }
 
-        @Override
-        public void resetData(ReadOnlyAddressBook newData) {
-            // do nothing
-        }
-
-        @Override
-        public ReadOnlyAddressBook getAddressBook() {
-            return null;
-        }
-
-        @Override
-        public void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException {
-            // do nothing
-        }
-
-        @Override
         public void addPerson(Person person) throws DuplicatePersonException {
-            if (person.equals(personInList)) {
-                throw new DuplicatePersonException();
+            for (Person existingPerson : personInList) {
+                if (existingPerson.equals(person)) {
+                    throw new DuplicatePersonException();
+                }
             }
-        }
 
-        @Override
-        public void updatePerson(int filteredPersonListIndex, ReadOnlyPerson editedPerson)
-                throws DuplicatePersonException {
-            // do nothing
-        }
-
-        @Override
-        public UnmodifiableObservableList<ReadOnlyPerson> getFilteredPersonList() {
-            return null;
-        }
-
-        @Override
-        public void updateFilteredListToShowAll() {
-            // do nothing
-        }
-
-        @Override
-        public void updateFilteredPersonList(Set<String> keywords) {
-            // do nothing
+            personInList.add(person);
         }
     }
 }
