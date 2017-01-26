36b5d9c6bd1c30ef1d6bed07852ad1a1ec0dea3d
Tan Wang Leng <tanwangleng@outlook.com>
2017-01-26 23:13:28 +0800

    Interdiff between v1 and v2

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -23,10 +24,13 @@ import seedu.address.testutil.TypicalTestPersons;
 
 public class AddCommandTest {
 
-    private TestAddModel model;
+    private AddModelStub model;
 
     private TestPerson newPerson;
     private TestPerson duplicatePerson;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -34,7 +38,31 @@ public class AddCommandTest {
 
         newPerson = typicalTestPersons.alice;
         duplicatePerson = typicalTestPersons.daniel;
-        model = new TestAddModel(duplicatePerson);
+        model = new AddModelStub(duplicatePerson);
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand("", "61239876", "valid.email@mail.com", "valid address", new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidPhone_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand("valid name", "", "valid.email@mail.com", "valid address", new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidEmail_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand("valid name", "61239876", "", "valid address", new HashSet<>());
+    }
+
+    @Test
+    public void constructor_invalidAddress_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        new AddCommand("valid name", "61239876", "valid.email@mail.com", "", new HashSet<>());
     }
 
     @Test
@@ -84,14 +112,15 @@ public class AddCommandTest {
     }
 
     /**
-     * A specially crafted model injected into add command
-     * for testing add's functionality.
+     * A model stub injected into the add command, for testing add's functionality.
+     *
+     * Only {@link #addPerson(Person)} is implemented for this stub.
      */
-    private class TestAddModel implements Model {
+    private class AddModelStub implements Model {
 
         private TestPerson personInList;
 
-        protected TestAddModel(TestPerson personInList) {
+        protected AddModelStub(TestPerson personInList) {
             this.personInList = personInList;
         }
 
