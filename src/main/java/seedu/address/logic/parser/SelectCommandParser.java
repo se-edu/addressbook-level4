e16ddc29e55d6a3fa33953bd95c17f7ffabbf230
MightyCupcakes <gohydj2003@gmail.com>
2017-01-26 17:57:01 +0800

    Extract SelectCommandParser from Parser class
    Parser class implements all different
    command parsers as private methods within the Parser class.
    Parser::parseCommand is responsible for performing an initial
    parsing on the given input stringand from there determine
    the correct method to parse the String to.
    
    This causes the Parser file to became very long
    containing all command parser methods
    and their associated helper methods that is only utlized in that
    one method itself.
    
    Let's move the functionality of parsing of the
    select command from Parser to the more concrete
    SelectCommandParser class which is in line with the
    Single Responsiblity Principle.

---
+++ src/main/java/seedu/address/logic/parser/SelectCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Optional;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.commands.SelectCommand;
+
+/**
+ * Parses input arguments and creates a new SelectCommand object
+ */
+public class SelectCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SelectCommand
+     * and returns an SelectCommand object for execution.
+     */
+    public Command parse(String args) {
+        Optional<Integer> index = ParserUtil.parseIndex(args);
+        if (!index.isPresent()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
+        }
+
+        return new SelectCommand(index.get());
+    }
+
+}
