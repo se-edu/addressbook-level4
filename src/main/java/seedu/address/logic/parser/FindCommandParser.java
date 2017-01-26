444a961ce53022486218f93ca6f7e461e9e4e22d
MightyCupcakes <gohydj2003@gmail.com>
2017-01-26 18:00:58 +0800

    Extract FindCommandParser from Parser class
    Parser class implements all different
    command parsers as private methods within the Parser class.
    Parser::parseCommand is responsible for performing an initial
    parsing on the given input stringand from there determine
    the correct method to parse the String to.
    
    This causes the Parser file to became very long
    containing all command parser methods
    and their associated helper methods that is only utlized in that
    one method itself.
    
    Let's move the functionality of parsing of the
    find command from Parser to the more concrete
    FindCommandParser class which is in line with the
    Single Responsiblity Principle.

---
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.IncorrectCommand;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class FindCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns an FindCommand object for execution.
+     */
+    public Command parse(String args) {
+        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
+        return new FindCommand(keywordSet);
+    }
+
+}
