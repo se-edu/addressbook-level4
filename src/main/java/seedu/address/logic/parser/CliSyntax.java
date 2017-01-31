35020ccffd430fee55f6696f9883e90244458482
MightyCupcakes <gohydj2003@gmail.com>
2017-01-31 11:58:33 +0800

    Extract common parser constants/methods to seperate classes
    The CliSyntax class will now act as a source of reference for all
    Parsing related constants. In a similar fashion, the ParserUtil class
    will now act as a container for all parsing-related utility methods
    used when parsing various commands.
    
    This is in line with the Single Responsiblity Principle which states
    that all classes should only have one responsiblity and one single
    reason to change.
    
    The functionality of parsing various commands from Parser will be
    moved to the more concrete *Parser classes. Thus,
    separating these constants and utility methods from Parser
    will avoid the troublesome bi-directional dependency between Parser
    and the various *Parser classes as the various *Parser classes don't
    need to depend on the main Parser class for utility methods and
    constants.

---
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import java.util.regex.Pattern;
+
+import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
+
+/**
+ * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
+ */
+public class CliSyntax {
+
+    /* Prefix definitions */
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
+
+    /* Patterns definitions */
+
+    public static final Pattern INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
+
+    /** Used for initial separation of command word and args. */
+    public static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+
+    public static final Pattern KEYWORDS_ARGS_FORMAT =
+            Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
+
+}
