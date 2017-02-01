4cc7fba8234e16df402a89b9d5956121252015f6
MightyCupcakes <gohydj2003@gmail.com>
2017-02-01 21:49:19 +0800

    Interdiff between v10 and v11

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -28,7 +28,8 @@ public class FindCommandParser {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        final String[] keywords = matcher.group("keywords").split("\\s+"); // keywords delimited by whitespace
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindCommand(keywordSet);
     }
