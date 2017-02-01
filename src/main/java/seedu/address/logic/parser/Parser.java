1ea0df19b3308a739eb0ac8882929ad70019d731
MightyCupcakes <gohydj2003@gmail.com>
2017-02-01 21:49:19 +0800

    Interdiff between v12 and v13

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -23,7 +23,9 @@ import seedu.address.logic.commands.SelectCommand;
  */
 public class Parser {
 
-    /** Used for initial separation of command word and args. */
+    /**
+     * Used for initial separation of command word and args.
+     */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
 
     /**
