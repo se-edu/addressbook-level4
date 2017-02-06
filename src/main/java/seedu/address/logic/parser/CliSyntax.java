c0218ecec4f3ab99bb113063244d2eb932ba9aab
MightyCupcakes <gohydj2003@gmail.com>
2017-02-06 19:01:27 +0800

    Extract common parser constants/methods to seperate classes
    Parser class contains code for parsing all command types.
    This makes the Parser class bloated and can be considered
    a violation of the Single Responsibility Principle. The solution
    is to extract code for parsing each command into a separate
    *CommandParser class. However, that can result in bidirectional
    links between those classes and the Parser class because some
    of the constants and utility methods in Parser class will be used
    by those classes as well.
    
    Let's extract reusable constants into a new CliSyntax class and
    utility methods into a new ParserUtil class so that such
    bidirectional links can be avoided.

---
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.parser;
+
+import java.util.regex.Pattern;
+
+import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
+
+/**
+ * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
+ */
+public class CliSyntax {
+
+    /* Prefix definitions */
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
+
+    /* Patterns definitions */
+    public static final Pattern KEYWORDS_ARGS_FORMAT =
+            Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
+
+}
