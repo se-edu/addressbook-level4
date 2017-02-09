324cdf9f8b54d469392a14d97b29628156af5524
chao1995 <huangchao0825@gmail.com>
2017-02-09 10:01:05 +0800

    Interdiff between v19 and v20

--- src/test/java/guitests/CommandBoxTest.java
+++ src/test/java/guitests/CommandBoxTest.java
@@ -50,7 +50,7 @@ public class CommandBoxTest extends AddressBookGuiTest {
 
     /**
      * Runs a command that fails, then verifies that
-     * - the return value of runCommand is false,
+     * - the return value of runCommand(...) is false,
      * - the text remains,
      * - the command box has only one ERROR_STYLE_CLASS, with other style classes untouched.
      */
@@ -62,7 +62,7 @@ public class CommandBoxTest extends AddressBookGuiTest {
 
     /**
      * Runs a command that succeeds, then verifies that
-     * - the return value of runCommand is true,
+     * - the return value of runCommand(...) is true,
      * - the text is cleared,
      * - the command box does not have any ERROR_STYLE_CLASS, with style classes the same as default.
      */
