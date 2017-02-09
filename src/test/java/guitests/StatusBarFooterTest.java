d44f5ab5ce675321c9bc5df7465c2b88000d555e
chao1995 <huangchao0825@gmail.com>
2017-02-09 10:01:05 +0800

    StatusBarFooter: Add GUI test for last update feature
    We don't have GUI tests for verifying the last updated feature, i.e.
    the sync status in the left corner of StatusBarFooter.
    
    Let's add an GUI test to verify it.

---
+++ src/test/java/guitests/StatusBarFooterTest.java
@@ -0,0 +1,60 @@
+package guitests;
+
+import static org.junit.Assert.assertEquals;
+
+import static seedu.address.ui.StatusBarFooter.INITIAL_STATUS;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.ui.StatusBarFooter;
+
+public class StatusBarFooterTest extends AddressBookGuiTest {
+
+    private Clock originalClock;
+    private Clock injectedClock;
+
+    @Before
+    public void injectFixedClock() {
+        originalClock = StatusBarFooter.getClock();
+        injectedClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        StatusBarFooter.setClock(injectedClock);
+    }
+
+    @After
+    public void restoreOriginalClock() {
+        StatusBarFooter.setClock(originalClock);
+    }
+
+    @Test
+    public void syncStatus_mutatingCommandSucceeds_statusUpdated() {
+        assertEquals(INITIAL_STATUS, statusBarFooter.getSyncStatus()); // verify initial value
+        commandBox.runCommand(td.hoon.getAddCommand());
+        String timestamp = new Date(injectedClock.millis()).toString();
+        String expected = String.format(StatusBarFooter.UPDATED_STATUS, timestamp);
+        assertEquals(expected, statusBarFooter.getSyncStatus());
+    }
+
+    @Test
+    public void syncStatus_nonMutatingCommandSucceeds_statusRemainsUnchanged() {
+        assertEquals(INITIAL_STATUS, statusBarFooter.getSyncStatus()); // verify initial value
+        commandBox.runCommand(ListCommand.COMMAND_WORD);
+        assertResultMessage(ListCommand.MESSAGE_SUCCESS); // verify the list command succeeds
+        assertEquals(INITIAL_STATUS, statusBarFooter.getSyncStatus());
+    }
+
+    @Test
+    public void syncStatus_commandFails_statusRemainsUnchanged() {
+        assertEquals(INITIAL_STATUS, statusBarFooter.getSyncStatus()); // verify initial value
+        commandBox.runCommand("invalid command");
+        assertEquals(INITIAL_STATUS, statusBarFooter.getSyncStatus());
+    }
+
+}
