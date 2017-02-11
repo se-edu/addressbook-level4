f9cf9ade26595e3df33b1b4e9ad88d6cb6e2621b
MightyCupcakes <gohydj2003@gmail.com>
2017-02-11 09:20:43 +0800

    Extract common parser constants/methods to seperate classes
    Parser class implements all different
    command parsers as private methods within the Parser class.
    Parser::parseCommand is responsible for performing an initial
    parsing on the given input string and from there determine
    the correct method to parse the String to.
    
    This causes the Parser file to become very long
    containing all command parser methods and their
    associated helper methods that is only utilized in that
    one method itself.
    
    The design can be improved by applying the Single Responsibility
    Principle as follows: limit the Parser's responsibility to determining
    which type of command is being parsed and move the responsibility
    of parsing each type of command to a separate *CommandParser class.
    
    However, this will result in bidirectional links between these various
    *CommandParser classes and the main Parser class because some
    of the constants and utility methods in Parser class will be used
    by the various *CommandParser classes as well.
    
    As a step toward the design proposed above, let's extract reusable
    constants into a new CliSyntax class and utility methods into a new
    ParserUtil class to prevent bidirectional dependencies between
    the Parser class and the proposed *CommandPraser classes.

---
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -0,0 +1,112 @@
+package seedu.address.logic.parser;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Contains utility methods used for parsing strings in the various *Parser classes
+ */
+public class ParserUtil {
+
+    private static final Pattern INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
+
+    /**
+     * Returns the specified index in the {@code command} if it is a positive unsigned integer
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    public static Optional<Integer> parseIndex(String command) {
+        final Matcher matcher = INDEX_ARGS_FORMAT.matcher(command.trim());
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        String index = matcher.group("targetIndex");
+        if (!StringUtil.isUnsignedInteger(index)) {
+            return Optional.empty();
+        }
+        return Optional.of(Integer.parseInt(index));
+
+    }
+
+    /**
+     * Returns a new Set populated by all elements in the given list of strings
+     * Returns an empty set if the given {@code Optional} is empty,
+     * or if the list contained in the {@code Optional} is empty
+     */
+    public static Set<String> toSet(Optional<List<String>> list) {
+        List<String> elements = list.orElse(Collections.emptyList());
+        return new HashSet<>(elements);
+    }
+
+    /**
+    * Splits a preamble string into ordered fields.
+    * @return A list of size {@code numFields} where the ith element is the ith field value if specified in
+    *         the input, {@code Optional.empty()} otherwise.
+    */
+    public static List<Optional<String>> splitPreamble(String preamble, int numFields) {
+        return Arrays.stream(Arrays.copyOf(preamble.split("\\s+", numFields), numFields))
+                .map(Optional::ofNullable)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Parses a {@code Optional<String> name} into an {@code Optional<Name>} if {@code name} is present.
+     */
+    public static Optional<Name> parseName(Optional<String> name) throws IllegalValueException {
+        assert name != null;
+        return name.isPresent() ? Optional.of(new Name(name.get())) : Optional.empty();
+    }
+
+    /**
+     * Parses a {@code Optional<String> phone} into an {@code Optional<Phone>} if {@code phone} is present.
+     */
+    public static Optional<Phone> parsePhone(Optional<String> phone) throws IllegalValueException {
+        assert phone != null;
+        return phone.isPresent() ? Optional.of(new Phone(phone.get())) : Optional.empty();
+    }
+
+    /**
+     * Parses a {@code Optional<String> address} into an {@code Optional<Address>} if {@code address} is present.
+     */
+    public static Optional<Address> parseAddress(Optional<String> address) throws IllegalValueException {
+        assert address != null;
+        return address.isPresent() ? Optional.of(new Address(address.get())) : Optional.empty();
+    }
+
+    /**
+     * Parses a {@code Optional<String> email} into an {@code Optional<Email>} if {@code email} is present.
+     */
+    public static Optional<Email> parseEmail(Optional<String> email) throws IllegalValueException {
+        assert email != null;
+        return email.isPresent() ? Optional.of(new Email(email.get())) : Optional.empty();
+    }
+
+    /**
+     * Parses {@code Collection<String> tags} into an {@code UniqueTagList}.
+     */
+    public static UniqueTagList parseTags(Collection<String> tags) throws IllegalValueException {
+        assert tags != null;
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        return new UniqueTagList(tagSet);
+    }
+}
