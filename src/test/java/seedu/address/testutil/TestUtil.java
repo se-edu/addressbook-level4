dda26c81b1a29826ac5ed3430d23d74074f9c28d
MightyCupcakes <gohydj2003@gmail.com>
2017-02-12 19:19:39 +0800

    TestUtil: Remove unused public methods
    The TestUtil contains many utility public API methods used by various
    test classes.
    
    However, many of these methods are not used in any of the test
    classes. Adopting the YAGNI (You aint gonna need it) principle, we
    should not populate this class with methods that are potentially useful
    in future cases but are unused as it will decrease the code
    maintainability and make the file needlessly large.
    
    Let's remove all these unused API utility methods.

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -2,50 +2,33 @@ package seedu.address.testutil;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
-
-import org.loadui.testfx.GuiTest;
-import org.testfx.api.FxToolkit;
-
-import com.google.common.io.Files;
 
 import guitests.guihandles.PersonCardHandle;
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import junit.framework.AssertionFailedError;
-import seedu.address.TestApp;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.XmlUtil;
-import seedu.address.model.AddressBook;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
-import seedu.address.storage.XmlSerializableAddressBook;
 
 /**
  * A utility class for test cases.
  */
 public class TestUtil {
-
-    public static final String LS = System.lineSeparator();
 
     /**
      * Folder used for temp files created during testing. Ignored by Git.
@@ -53,8 +36,6 @@ public class TestUtil {
     public static final String SANDBOX_FOLDER = FileUtil.getPath("./src/test/data/sandbox/");
 
     public static final Person[] SAMPLE_PERSON_DATA = getSamplePersonData();
-
-    public static final Tag[] SAMPLE_TAG_DATA = getSampleTagData();
 
     public static void assertThrows(Class<? extends Throwable> expected, Runnable executable) {
         try {
@@ -93,20 +74,6 @@ public class TestUtil {
         }
     }
 
-
-    private static Tag[] getSampleTagData() {
-        try {
-            return new Tag[]{
-                new Tag("relatives"),
-                new Tag("friends")
-            };
-        } catch (IllegalValueException e) {
-            assert false;
-            return null;
-            //not possible
-        }
-    }
-
     public static List<Person> generateSamplePersonData() {
         return Arrays.asList(SAMPLE_PERSON_DATA);
     }
@@ -126,10 +93,6 @@ public class TestUtil {
         return SANDBOX_FOLDER + fileName;
     }
 
-    public static void createDataFileWithSampleData(String filePath) {
-        createDataFileWithData(generateSampleStorageAddressBook(), filePath);
-    }
-
     public static <T> void createDataFileWithData(T data, String filePath) {
         try {
             File saveFileForTesting = new File(filePath);
@@ -138,14 +101,6 @@ public class TestUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static void main(String... s) {
-        createDataFileWithSampleData(TestApp.SAVE_LOCATION_FOR_TESTING);
-    }
-
-    public static XmlSerializableAddressBook generateSampleStorageAddressBook() {
-        return new XmlSerializableAddressBook(new AddressBook());
     }
 
     /**
@@ -170,68 +125,6 @@ public class TestUtil {
         return keys.toArray(new KeyCode[]{});
     }
 
-    public static boolean isHeadlessEnvironment() {
-        String headlessProperty = System.getProperty("testfx.headless");
-        return headlessProperty != null && headlessProperty.equals("true");
-    }
-
-    public static void captureScreenShot(String fileName) {
-        File file = GuiTest.captureScreenshot();
-        try {
-            Files.copy(file, new File(fileName + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String descOnFail(Object... comparedObjects) {
-        return "Comparison failed \n"
-                + Arrays.asList(comparedObjects).stream()
-                .map(Object::toString)
-                .collect(Collectors.joining("\n"));
-    }
-
-    public static void setFinalStatic(Field field, Object newValue) throws NoSuchFieldException,
-                                                                           IllegalAccessException {
-        field.setAccessible(true);
-        // remove final modifier from field
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        // ~Modifier.FINAL is used to remove the final modifier from field so that its value is no longer
-        // final and can be changed
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        field.set(null, newValue);
-    }
-
-    public static void initRuntime() throws TimeoutException {
-        FxToolkit.registerPrimaryStage();
-        FxToolkit.hideStage();
-    }
-
-    public static void tearDownRuntime() throws Exception {
-        FxToolkit.cleanupStages();
-    }
-
-    /**
-     * Gets private method of a class
-     * Invoke the method using method.invoke(objectInstance, params...)
-     *
-     * Caveat: only find method declared in the current Class, not inherited from supertypes
-     */
-    public static Method getPrivateMethod(Class<?> objectClass, String methodName) throws NoSuchMethodException {
-        Method method = objectClass.getDeclaredMethod(methodName);
-        method.setAccessible(true);
-        return method;
-    }
-
-    public static void renameFile(File file, String newFileName) {
-        try {
-            Files.copy(file, new File(newFileName));
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-
     /**
      * Gets mid point of a node relative to the screen.
      * @param node
@@ -243,40 +136,8 @@ public class TestUtil {
         return new Point2D(x, y);
     }
 
-    /**
-     * Gets mid point of a node relative to its scene.
-     * @param node
-     * @return
-     */
-    public static Point2D getSceneMidPoint(Node node) {
-        double x = getScenePos(node).getMinX() + node.getLayoutBounds().getWidth() / 2;
-        double y = getScenePos(node).getMinY() + node.getLayoutBounds().getHeight() / 2;
-        return new Point2D(x, y);
-    }
-
-    /**
-     * Gets the bound of the node relative to the parent scene.
-     * @param node
-     * @return
-     */
-    public static Bounds getScenePos(Node node) {
-        return node.localToScene(node.getBoundsInLocal());
-    }
-
     public static Bounds getScreenPos(Node node) {
         return node.localToScreen(node.getBoundsInLocal());
-    }
-
-    public static double getSceneMaxX(Scene scene) {
-        return scene.getX() + scene.getWidth();
-    }
-
-    public static double getSceneMaxY(Scene scene) {
-        return scene.getX() + scene.getHeight();
-    }
-
-    public static Object getLastElement(List<?> list) {
-        return list.get(list.size() - 1);
     }
 
     /**
@@ -302,18 +163,6 @@ public class TestUtil {
     }
 
     /**
-     * Replaces persons[i] with a person.
-     * @param persons The array of persons.
-     * @param person The replacement person
-     * @param index The index of the person to be replaced.
-     * @return
-     */
-    public static TestPerson[] replacePersonFromList(TestPerson[] persons, TestPerson person, int index) {
-        persons[index] = person;
-        return persons;
-    }
-
-    /**
      * Appends persons to the array of persons.
      * @param persons A array of persons.
      * @param personsToAdd The persons that are to be appended behind the original array.
@@ -335,26 +184,6 @@ public class TestUtil {
 
     public static boolean compareCardAndPerson(PersonCardHandle card, ReadOnlyPerson person) {
         return card.isSamePerson(person);
-    }
-
-    public static Tag[] getTagList(String tags) {
-        if ("".equals(tags)) {
-            return new Tag[]{};
-        }
-
-        final String[] split = tags.split(", ");
-
-        final List<Tag> collect = Arrays.asList(split).stream().map(e -> {
-            try {
-                return new Tag(e.replaceFirst("Tag: ", ""));
-            } catch (IllegalValueException e1) {
-                //not possible
-                assert false;
-                return null;
-            }
-        }).collect(Collectors.toList());
-
-        return collect.toArray(new Tag[split.length]);
     }
 
 }
