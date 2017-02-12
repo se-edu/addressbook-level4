8f9373723267ee88fc5a1e07fbc421c0cd91f122
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-02-12 16:00:07 +0800

    FindCommandParser: remove unnecessary regex usage
    CliSyntax#KEYWORDS_ARGS_FORMAT is a regex that greedily grabs one or
    more characters, excluding any leading or trailing whitespaces. It is
    used with String#trim() to check that a string contains at least one
    non-whitespace character.
    
    It is unnecessarily complex as the same task could be accomplished by
    just using simple String utility methods. String#trim() is sufficient in
    removing leading and trailing whitespaces, and in combination with
    String#isEmpty(), can perform the same check.
    
    Let's use String utility methods instead.

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,10 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Matcher;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.FindCommand;
@@ -22,14 +20,14 @@ public class FindCommandParser {
      * and returns an FindCommand object for execution.
      */
     public Command parse(String args) {
-        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
-        if (!matcher.matches()) {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         // keywords delimited by whitespace
-        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final String[] keywords = trimmedArgs.split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindCommand(keywordSet);
     }
