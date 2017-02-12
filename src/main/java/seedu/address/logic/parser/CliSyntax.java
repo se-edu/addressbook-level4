9639cc785d5f21d6c3627019504482cbfded5533
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-02-12 16:00:07 +0800

    CliSyntax: simplify regex KEYWORDS_ARGS_FORMAT
    CliSyntax#KEYWORDS_ARGS_FORMAT is a regex that greedily grabs one or
    more words, and uses capture groups to split these words. Words here are
    defined as non-empty strings, and they are separated by whitespace.
    
    It is unnecessarily complex as the same task could be accomplished by
    just using simple String utility methods.
    
    Let's use String#trim() and String#isEmpty() to check for one or more
    words instead, and let's use String#split() to get words instead.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import java.util.regex.Pattern;
-
 import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
 
 /**
@@ -14,9 +12,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
-    /* Patterns definitions */
-    public static final Pattern KEYWORDS_ARGS_FORMAT =
-            Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
 
 }
