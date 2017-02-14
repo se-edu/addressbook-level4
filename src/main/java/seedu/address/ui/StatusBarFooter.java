06dc563d22eaf771f7a5d0fc0952fb0a3a59b24d
chao1995 <huangchao0825@gmail.com>
2017-02-14 15:34:46 +0800

    StatusBarFooter: enable dependency injection of a Clock
    When an AddressBookChangeEvent occurs, the sync status's timestamp is
    updated using 'new Date()'.
    
    However, because we do not know the arrival time of the event during
    testing, we are unable to verify the accuracy of the time stamp in the
    updated sync status.
    
    To gain testability, StatusBarFooter needs to support dependency
    injection. And to support dependency injection, we can't directly
    call new Date().
    
    Let's add a Clock, and get the time stamp from it, so we can inject
    a fixed Clock during testing.

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import java.time.Clock;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -18,6 +19,17 @@ import seedu.address.commons.util.FxViewUtil;
  * A ui for the status bar that is displayed at the footer of the application.
  */
 public class StatusBarFooter extends UiPart<Region> {
+
+    /**
+     * Used to generate time stamps.
+     *
+     * TODO: change clock to an instance variable.
+     * We leave it as a static variable because manual dependency injection
+     * will require passing down the clock reference all the way from MainApp,
+     * but it should be easier once we have factories/DI frameworks.
+     */
+    private static Clock clock = Clock.systemDefaultZone();
+
     private static final Logger logger = LogsCenter.getLogger(StatusBarFooter.class);
 
     @FXML
@@ -26,6 +38,20 @@ public class StatusBarFooter extends UiPart<Region> {
     private StatusBar saveLocationStatus;
 
     private static final String FXML = "StatusBarFooter.fxml";
+
+    /**
+     * Sets the clock used to determine the current time.
+     */
+    public static void setClock(Clock clock) {
+        StatusBarFooter.clock = clock;
+    }
+
+    /**
+     * Returns the clock currently in use.
+     */
+    public static Clock getClock() {
+        return StatusBarFooter.clock;
+    }
 
     public StatusBarFooter(AnchorPane placeHolder, String saveLocation) {
         super(FXML);
@@ -50,7 +76,8 @@ public class StatusBarFooter extends UiPart<Region> {
 
     @Subscribe
     public void handleAddressBookChangedEvent(AddressBookChangedEvent abce) {
-        String lastUpdated = (new Date()).toString();
+        long now = StatusBarFooter.clock.millis();
+        String lastUpdated = new Date(now).toString();
         logger.info(LogsCenter.getEventHandlingLogMessage(abce, "Setting last updated status to " + lastUpdated));
         setSyncStatus("Last Updated: " + lastUpdated);
     }
