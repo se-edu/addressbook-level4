e6b8e2f9fbd1abe2874e2425e0acd6797e8c7e70
Ka Yi <chua.kayi@yahoo.com.sg>
2017-02-22 10:24:47 +0800

    CommandBox: limit no. of ERROR_STYLE_CLASS added to 1
    After multiple failed commands are entered, the CommandBox continues
    showing the error style even when a successful command is entered.
    
    This is because CommandTextField receives a new copy of
    ERROR_STYLE_CLASS each time a failed command is entered, but only one
    is removed when a command entered is successful, so one or more
    ERROR_STYLE_CLASS remains. Thus, the CommandBox shows the error style
    even though the latest command is successful.
    
    Let's teach CommandBox to add an ERROR_STYLE_CLASS for failed commands
    only if the style class does not contain any. This ensures there is at
    most one ERROR_STYLE_CLASS at any point in time, so removing an
    ERROR_STYLE_CLASS in successful commands always works.

--- src/test/java/guitests/CommandBoxTest.java
+++ src/test/java/guitests/CommandBoxTest.java
@@ -30,30 +30,35 @@ public class CommandBoxTest extends AddressBookGuiTest {
     }
 
     @Test
-    public void commandBox_commandSucceeds_textClearedAndStyleClassRemainsTheSame() {
-        commandBox.runCommand(COMMAND_THAT_SUCCEEDS);
-
-        assertEquals("", commandBox.getCommandInput());
-        assertEquals(defaultStyleOfCommandBox, commandBox.getStyleClass());
+    public void commandBox_commandSucceeds() {
+        testSuccessfulCommand();
     }
 
     @Test
-    public void commandBox_commandFails_textStaysAndErrorStyleClassAdded() {
+    public void commandBox_commandFails() {
+        testFailedCommand();
+
+        testSuccessfulCommand();
+
+        // run multiple failed commands
+        testFailedCommand();
+        testFailedCommand();
+
+        testSuccessfulCommand();
+    }
+
+    private void testFailedCommand() {
         commandBox.runCommand(COMMAND_THAT_FAILS);
 
-        assertEquals(COMMAND_THAT_FAILS, commandBox.getCommandInput());
-        assertEquals(errorStyleOfCommandBox, commandBox.getStyleClass());
+        assertEquals(COMMAND_THAT_FAILS, commandBox.getCommandInput()); // text remains
+        assertEquals(errorStyleOfCommandBox, commandBox.getStyleClass()); // error style class added
     }
 
-    @Test
-    public void commandBox_commandSucceedsAfterFailedCommand_textClearedAndErrorStyleClassRemoved() {
-        // add error style to simulate a failed command
-        commandBox.getStyleClass().add(CommandBox.ERROR_STYLE_CLASS);
-
+    private void testSuccessfulCommand() {
         commandBox.runCommand(COMMAND_THAT_SUCCEEDS);
 
-        assertEquals("", commandBox.getCommandInput());
-        assertEquals(defaultStyleOfCommandBox, commandBox.getStyleClass());
+        assertEquals("", commandBox.getCommandInput()); // text cleared
+        assertEquals(defaultStyleOfCommandBox, commandBox.getStyleClass()); // style class unchanged
     }
 
 }
