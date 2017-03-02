fbe4f3008cc1757d48790108a9582f97c79ba282
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-02 13:26:21 +0800

    Add UniquePersonList#stream()
    UniquePersonList does not support use of functional programming
    paradigm, as it does not implement the stream() method.
    
    Let's add UniquePersonList#stream() to expose such functionality.

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -2,6 +2,7 @@ package seedu.address.model.person;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -110,6 +111,10 @@ public class UniquePersonList implements Iterable<Person> {
     @Override
     public int hashCode() {
         return internalList.hashCode();
+    }
+
+    public Stream<Person> stream() {
+        return internalList.stream();
     }
 
     /**
