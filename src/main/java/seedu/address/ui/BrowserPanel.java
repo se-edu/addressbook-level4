9dcf989315f5b226c06c7861419e74e25199a46f
MightyCupcakes <gohydj2003@gmail.com>
2017-03-02 13:36:48 +0800

    BrowserPanel: Load a default blank page
    BrowserPanel does not load any page when no person is selected. This
    results in a very contrasting white background in a large box among
    the dark background.
    
    As the background of the BrowserPanel cannot be modified by CSS, we
    can instead load a blank HTML page with a matching background color
    to the rest of the application on initialization.
    
    Let's load a blank HTML page in the BrowserPanel on initialization.

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,10 +1,13 @@
 package seedu.address.ui;
 
+import java.net.URL;
+
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
+import seedu.address.MainApp;
 import seedu.address.commons.util.FxViewUtil;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -14,6 +17,7 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class BrowserPanel extends UiPart<Region> {
 
     private static final String FXML = "BrowserPanel.fxml";
+    private static final String DEFAULT_BLANK_PAGE = "blank.html";
 
     @FXML
     private WebView browser;
@@ -27,6 +31,8 @@ public class BrowserPanel extends UiPart<Region> {
                                                      // loaded Web page.
         FxViewUtil.applyAnchorBoundaryParameters(browser, 0.0, 0.0, 0.0, 0.0);
         placeholder.getChildren().add(browser);
+
+        loadBlankPage();
     }
 
     public void loadPersonPage(ReadOnlyPerson person) {
@@ -35,6 +41,15 @@ public class BrowserPanel extends UiPart<Region> {
 
     public void loadPage(String url) {
         browser.getEngine().load(url);
+    }
+
+    /**
+     * Loads an empty HTML file with a matching background on start-up
+     * to better fit the general theme.
+     */
+    private void loadBlankPage() {
+        URL blankPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_BLANK_PAGE);
+        loadPage(blankPage.toExternalForm());
     }
 
     /**
