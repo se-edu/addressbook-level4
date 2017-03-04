45a52363fcb26b7915dc799fe252db7394e1432a
MightyCupcakes <gohydj2003@gmail.com>
2017-03-04 10:58:02 +0800

    Move AddCommand's parsing responsibilities to AddCommandParser
    AddCommandParser parses user input into individual String attributes.
    AddCommand then parses the String attributes into Object attributes
    and creates a new Person.
    
    This behaviour is not consistent as the AddCommand requires a fully
    parsed UniqueTagList while accepting the raw Strings of the rest of the
    attributes. Furthermore, AddCommand belongs outside the Parser package
    therefore it is only natural that it does not do any parsing of
    user input.
    
    Let's modify the construction of the add command as follows:
        * Make AddCommandParser do the parsing and create a
          fully parsed Person object.
        * Make AddCommand's constructor accept a fully parsed
          ReadOnlyPerson.
        * Make AddCommandParser#isCompulsoryPrefixesPresent()
          verify that all prefixes exists in the given arguments
          string first to in order to keep the behaviour of AddCommandParser
          consistent.
    
    Benefits:
        * The parser will always warn the user of an invalid command
          format first even though the arguments string may contain invalid
          values for a Person attribute.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -12,6 +12,14 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.tag.UniqueTagList;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -27,18 +35,38 @@ public class AddCommandParser {
                 new ArgumentTokenizer(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
         argsTokenizer.tokenize(args);
         try {
-            return new AddCommand(
-                    argsTokenizer.getPreamble(),
-                    argsTokenizer.getValue(PREFIX_PHONE).get(),
-                    argsTokenizer.getValue(PREFIX_EMAIL).get(),
-                    argsTokenizer.getValue(PREFIX_ADDRESS).get(),
-                    ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG))
-            );
+            if (!isCompulsoryPrefixesPresent(argsTokenizer, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
+                throw new NoSuchElementException();
+            }
+
+            Name name = new Name(argsTokenizer.getPreamble());
+            Phone phone = ParserUtil.parsePhone(argsTokenizer.getValue(PREFIX_PHONE)).get();
+            Email email = ParserUtil.parseEmail(argsTokenizer.getValue(PREFIX_EMAIL)).get();
+            Address address = ParserUtil.parseAddress(argsTokenizer.getValue(PREFIX_ADDRESS)).get();
+            UniqueTagList tagList = ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG));
+
+            ReadOnlyPerson person = new Person(name, phone, email, address, tagList);
+
+            return new AddCommand(person);
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         } catch (IllegalValueException ive) {
             return new IncorrectCommand(ive.getMessage());
         }
+    }
+
+    /**
+     * Returns true if all the given prefixes do not contain empty {@code Optional} values in the given
+     * {@code ArgumentTokenizer}.
+     */
+    private boolean isCompulsoryPrefixesPresent(ArgumentTokenizer argsTokenizer, Prefix... compulsoryPrefixes) {
+        for (Prefix prefix : compulsoryPrefixes) {
+            if (!argsTokenizer.getValue(prefix).isPresent()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
