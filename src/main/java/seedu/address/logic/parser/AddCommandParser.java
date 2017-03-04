64ab2fc3be3aab3dfbdc6244a5c9527f4e39d349
MightyCupcakes <gohydj2003@gmail.com>
2017-03-04 10:58:02 +0800

    Move AddCommand's parsing responsibilities to AddCommandParser
    AddCommandParser parses user input into individual String attributes.
    AddCommand then parses the String attributes into Object attributes
    and creates a new Person.
    
    This behaviour is not consistent as the AddCommand requires a fully
    parsed UniqueTagList while accepting the raw Strings of the rest of the
    attributes. Furthermore, AddCommand belongs outside the Parser package
    therefore it is only natural that it does not do any parsing of
    user input.
    
    Let's modify AddCommand's constructor to accept the respective Object
    for the various Person attributes and let AddCommandParser do the
    parsing instead.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -12,6 +12,12 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.UniqueTagList;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -27,13 +33,15 @@ public class AddCommandParser {
                 new ArgumentTokenizer(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
         argsTokenizer.tokenize(args);
         try {
-            return new AddCommand(
-                    argsTokenizer.getPreamble(),
-                    argsTokenizer.getValue(PREFIX_PHONE).get(),
-                    argsTokenizer.getValue(PREFIX_EMAIL).get(),
-                    argsTokenizer.getValue(PREFIX_ADDRESS).get(),
-                    ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG))
-            );
+            Address address = ParserUtil.parseAddress(argsTokenizer.getValue(PREFIX_ADDRESS)).get();
+            Email email = ParserUtil.parseEmail(argsTokenizer.getValue(PREFIX_EMAIL)).get();
+            Name name = new Name(argsTokenizer.getPreamble());
+            Phone phone = ParserUtil.parsePhone(argsTokenizer.getValue(PREFIX_PHONE)).get();
+            UniqueTagList tagList = ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG));
+
+            Person person = new Person(name, phone, email, address, tagList);
+
+            return new AddCommand(person);
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         } catch (IllegalValueException ive) {
