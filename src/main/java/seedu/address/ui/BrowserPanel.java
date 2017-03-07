3c428c55ec3171a1aa738f29c1c249e60add1f0e
MightyCupcakes <gohydj2003@gmail.com>
2017-03-07 19:22:01 +0800

    Interdiff between v5 and v6

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -44,8 +44,7 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     /**
-     * Loads an default HTML file with a matching background on start-up
-     * to better fit the general theme.
+     * Loads a default HTML file with a background that matches the general theme.
      */
     private void loadDefaultPage() {
         URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
