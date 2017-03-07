8e39586b39357fe871b2ec6e43cb8b6bba3d8f00
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-03-07 22:13:29 +0800

    ArgumentTokenizer: extract getters into Arguments class
    ArgumentTokenizer's tokenized argument getters can be accessed even
    before ArgumentTokenizer is given arguments to tokenize through
    ArgumentTokenizer#tokenize(String).
    
    This behaviour is problematic as the getters can be called before
    ArgumentTokenizer#tokenize(...) without any warnings, and would return
    empty optionals, lists, and strings that are indistinguishable from
    normal empty results.
    
    Lets extract the getters into a new Arguments class, and have
    ArgumentTokenizer#tokenize(...) return an instance of that class so that
    the getters cannot be accessed before tokenization.

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -31,8 +31,8 @@ public class EditCommandParser {
         assert args != null;
         ArgumentTokenizer argsTokenizer =
                 new ArgumentTokenizer(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
-        argsTokenizer.tokenize(args);
-        List<Optional<String>> preambleFields = ParserUtil.splitPreamble(argsTokenizer.getPreamble(), 2);
+        Arguments arguments = argsTokenizer.tokenize(args);
+        List<Optional<String>> preambleFields = ParserUtil.splitPreamble(arguments.getPreamble(), 2);
 
         Optional<Integer> index = preambleFields.get(0).flatMap(ParserUtil::parseIndex);
         if (!index.isPresent()) {
@@ -42,10 +42,10 @@ public class EditCommandParser {
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
         try {
             editPersonDescriptor.setName(ParserUtil.parseName(preambleFields.get(1)));
-            editPersonDescriptor.setPhone(ParserUtil.parsePhone(argsTokenizer.getValue(PREFIX_PHONE)));
-            editPersonDescriptor.setEmail(ParserUtil.parseEmail(argsTokenizer.getValue(PREFIX_EMAIL)));
-            editPersonDescriptor.setAddress(ParserUtil.parseAddress(argsTokenizer.getValue(PREFIX_ADDRESS)));
-            editPersonDescriptor.setTags(parseTagsForEdit(argsTokenizer.getAllValues(PREFIX_TAG)));
+            editPersonDescriptor.setPhone(ParserUtil.parsePhone(arguments.getValue(PREFIX_PHONE)));
+            editPersonDescriptor.setEmail(ParserUtil.parseEmail(arguments.getValue(PREFIX_EMAIL)));
+            editPersonDescriptor.setAddress(ParserUtil.parseAddress(arguments.getValue(PREFIX_ADDRESS)));
+            editPersonDescriptor.setTags(parseTagsForEdit(arguments.getAllValues(PREFIX_TAG)));
         } catch (IllegalValueException ive) {
             return new IncorrectCommand(ive.getMessage());
         }
