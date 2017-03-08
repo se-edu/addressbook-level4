e3a68726dd677059cba920f12187119cd27136a6
Crsytral <shouyu2@hotmail.com>
2017-03-08 22:46:22 +0800

    Interdiff between v2 and v3

--- src/main/java/seedu/address/commons/util/IndexUtil.java
+++ src/main/java/seedu/address/commons/util/IndexUtil.java
@@ -3,7 +3,7 @@ package seedu.address.commons.util;
 public class IndexUtil {
 
     /**
-     * Utility method related to one-based to zero-based index transformation
+     * Returns the zero-based index given the one-based index as input
      */
     public static int oneToZeroIndex(int index) {
         return index - 1;
