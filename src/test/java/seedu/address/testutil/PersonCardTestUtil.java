79d06795110616ab140071ded239b036360116a2
linhan <yonglin12@hotmail.com>
2017-03-08 19:21:19 +0800

    Move person card-related methods into PersonCardTestUtil class

---
+++ src/test/java/seedu/address/testutil/PersonCardTestUtil.java
@@ -0,0 +1,21 @@
+package seedu.address.testutil;
+
+import guitests.guihandles.PersonCardHandle;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Utility class for PersonCard tests.
+ */
+public class PersonCardTestUtil {
+
+    /**
+     * Returns true if {@code person} is the same as the person on {@code card}.
+     * The persons are considered to be the same if they have the same name, phone, email,
+     * address, and tags.
+     * Returns false otherwise.
+     */
+    public static boolean isPersonSameAsPersonOnCard(PersonCardHandle card, ReadOnlyPerson person) {
+        return card.isSamePerson(person);
+    }
+
+}
