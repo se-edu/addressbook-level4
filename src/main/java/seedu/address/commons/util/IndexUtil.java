2d0e261ce5cf517bde504f35eb16bccb3c87c615
Crsytral <shouyu2@hotmail.com>
2017-03-08 22:46:22 +0800

    Implement IndexUtil class
    "i - 1" is sometimes used to convert 'i' from a one-based index to a
    zero-based index.
    
    Comments have to be added to describe the purpose of "i - 1", as it is
    not obvious whether we are taking the item before 'i', or whether we are
    converting 'i' from a one-based index into a zero-based index, which
    decreases code readability.
    
    Let's implement a IndexUtil#oneToZeroIndex() method which returns the
    zero-based index given the one-based index as input.
    
    The use of IndexUtil.oneToZeroIndex(i) removes the need for a comment
    when we convert 'i' from a one-based index to a zero-based index.

---
+++ src/main/java/seedu/address/commons/util/IndexUtil.java
@@ -0,0 +1,11 @@
+package seedu.address.commons.util;
+
+public class IndexUtil {
+
+    /**
+     * Returns the zero-based index given the one-based index as input
+     */
+    public static int oneToZeroIndex(int index) {
+        return index - 1;
+    }
+}
