fc5d5fedee4ff618a870c433fed753a781d41996
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-03-08 22:27:52 +0800

    ArgumentTokenizer: extract getters into ArgumentMultimap class
    ArgumentTokenizer's tokenized argument getters can be accessed even
    before ArgumentTokenizer is given arguments to tokenize through
    ArgumentTokenizer#tokenize(String).
    
    This behaviour is problematic as the getters can be called before
    ArgumentTokenizer#tokenize(...), and would return empty optionals,
    lists, and strings that are indistinguishable from normal empty results.
    
    Let's extract the getters into a new ArgumentMultimap class, and have
    ArgumentTokenizer#tokenize(...) return an instance of that class so that
    these getters cannot be accessed before tokenization.

---
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -0,0 +1,66 @@
+package seedu.address.logic.parser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Stores mapping of prefixes to their respective arguments.
+ * Each key may be associated with multiple argument values.
+ * Values for a given key are stored in a list, and the insertion ordering is maintained.
+ */
+public class ArgumentMultimap {
+
+    /** Prefixes mapped to their respective arguments**/
+    private final Map<Prefix, List<String>> argMultimap;
+
+    /**
+     * Creates an ArgumentMultimap object that stores the mapping of prefixes to their respective arguments.
+     */
+    public ArgumentMultimap() {
+        argMultimap = new HashMap<>();
+    }
+
+    /**
+     * Associates the specified argument value with the specified prefix key in this map.
+     * If the map previously contained a mapping for the key, the new value is appended to the list of existing values.
+     *
+     * @param prefix   Prefix key with which the specified argument value is to be associated
+     * @param argValue Argument value to be associated with the specified prefix key
+     */
+    public void put(Prefix prefix, String argValue) {
+        List<String> argValues = getAllValues(prefix);
+        argValues.add(argValue);
+        argMultimap.put(prefix, argValues);
+    }
+
+    /**
+     * Returns the last value of the given prefix.
+     */
+    public Optional<String> getValue(Prefix prefix) {
+        List<String> values = getAllValues(prefix);
+        return values.isEmpty() ? Optional.empty() : Optional.of(values.get(values.size() - 1));
+    }
+
+    /**
+     * Returns all values of the given prefix.
+     * If the prefix does not exist or has no values, this will return an empty list.
+     * Modifying the returned list will not affect the underlying data structure of the ArgumentMultimap.
+     */
+    public List<String> getAllValues(Prefix prefix) {
+        if (!argMultimap.containsKey(prefix)) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(argMultimap.get(prefix));
+    }
+
+    /**
+     * Returns the preamble (text before the first valid prefix). Trims any leading/trailing spaces.
+     */
+    public String getPreamble() {
+        Optional<String> storedPreamble = getValue(new Prefix(""));
+        return storedPreamble.orElse("");
+    }
+}
