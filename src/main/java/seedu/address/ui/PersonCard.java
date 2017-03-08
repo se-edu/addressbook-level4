cf5279298a48f082c22153efb322014bd32bf96c
MightyCupcakes <gohydj2003@gmail.com>
2017-03-08 18:45:25 +0800

    PersonCard: Bind text labels to their respective Person properties
    The PersonCard does not observe changes in the underlying Person attribute.
    Therefore, any changes to the Person is not reflected in the UI.
    
    This brought about the need to implement a workaround of changing
    the observablelist of Persons in order to trigger an UI redraw.
    
    The solution proposed is more complicated than it needs to be
    because of the way guiTests are run. Initially, a WeakChangeListener
    is used to listen for any changes in the Person object. This allows for
    garbage collection of the listener whenever the PersonCard object
    is non-existent.
    
    However, the guiTests for EditCommand started to act in a non-
    deterministic manner as in it will pass or fail seemingly at random.
    The true cause was not known but it is assumed that guiTests
    maintains a weak reference to all PersonCard cells, causing the
    weakly referenced listeners these cells to be garbaged collected
    almost immediately, causing the UI to fail to update upon a
    successful edit operation.
    
    Hence, only strongly referenced listeners can be used and the
    tests started to act in a deterministic way. Therefore, it is only reasonable
    to conclude that weak references causes the tests to fail at random.
    
    However, while strongly referenced listeners will allow the tests
    to run in a predictable manner, it causes a huge memory leak in the
    main application itself. This is because of how JavaFx manages its
    ListView cells.
    
    Due to the way JavaFx implements ListView, PersonCard are deleted when
    the user scrolls down resulting in the PersonCard being out-of-view.
    When the user scrolls back up, the same PersonCard is recreated again.
    This causes the listener registration to happen repeatedly for the same
    person and such listeners belongs to non-existant PersonCard. This
    will result in a very bad memory leak very quickly.
    
    Therefore, to resolve the problem, the following solutions are
    implemented:
    
        1. Maintain a map of Person objects as the key, and the value being
        the listeners registered to that Person object.
    
    As Person objects are unique as guaranteed by UniquePersonList, Person
    objects will act as key. Whenever a new PersonCard is created and wishes
    to register its change listeners, it will first check and remove any existing
    listeners in the map for the Person object. This will ensure that there will
    only be a set of listeners for each Person object.
    
        2. The values in the aforementioned map will be weakly referenced.
    
    Another problem of this implementation surfaces when the underlying
    Person itself is deleted. This results in a set of listeners that are listening
    to the changes of an non-existent object. The solution then is to have
    the map weakly reference these listeners so that it can be garbaged
    collected eventually.
    
    Let's make PersonCard register listeners to their respective ObservablePerson
    so that such workarounds are no longer needed.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,15 +1,34 @@
 package seedu.address.ui;
 
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Maps;
+
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import seedu.address.model.person.ObservablePerson.PersonProperty;
 import seedu.address.model.person.ReadOnlyPerson;
 
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+    private static final PersonProperty[] personProperties = PersonProperty.values();
+
+    /**
+     * A map to contain all the change listeners associated to a Person object.
+     * All values are weakly referenced to allow for garbage collection if the Person is deleted.
+     */
+    private static final ListMultimap<ReadOnlyPerson, WeakReference<ChangeListener<Object>>> LISTENER_MAP =
+            ArrayListMultimap.create();
 
     @FXML
     private HBox cardPane;
@@ -26,6 +45,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    private final Map<PersonProperty, Consumer<ReadOnlyPerson>> personPropertyConsumerMap;
+
     public PersonCard(ReadOnlyPerson person, int displayedIndex) {
         super(FXML);
         name.setText(person.getName().fullName);
@@ -34,9 +55,57 @@ public class PersonCard extends UiPart<Region> {
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         initTags(person);
+
+        personPropertyConsumerMap = Maps.newEnumMap(PersonProperty.class);
+
+        populatePersonPropertyConsumerMap();
+        bindListeners(person);
     }
 
     private void initTags(ReadOnlyPerson person) {
+        tags.getChildren().clear();
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    private void populatePersonPropertyConsumerMap() {
+        personPropertyConsumerMap.put(PersonProperty.ADDRESS, (person) -> address.setText(person.getAddress().value));
+        personPropertyConsumerMap.put(PersonProperty.EMAIL, (person) -> email.setText(person.getEmail().value));
+        personPropertyConsumerMap.put(PersonProperty.NAME, (person) -> name.setText(person.getName().fullName));
+        personPropertyConsumerMap.put(PersonProperty.PHONE, (person) -> phone.setText(person.getPhone().value));
+        personPropertyConsumerMap.put(PersonProperty.TAG, this::initTags);
+    }
+
+    private void bindListeners(ReadOnlyPerson person) {
+        for (PersonProperty property : personProperties) {
+            removeListenersForPerson(person);
+
+            ChangeListener<Object> changeListener =
+                (o, oldVal, newVal) -> personPropertyConsumerMap.get(property).accept(person);
+
+            person.registerListener(property, changeListener);
+
+            LISTENER_MAP.put(person, new WeakReference<>(changeListener));
+        }
+    }
+
+    private void removeListenersForPerson (ReadOnlyPerson person) {
+        if (!LISTENER_MAP.containsKey(person)) {
+            // If there is no listeners currently registered for this person, exit.
+            return;
+        }
+
+        List<WeakReference<ChangeListener<Object>>> listenerReferenceList = LISTENER_MAP.get(person);
+
+        if (listenerReferenceList.size() != personProperties.length) {
+            // Don't remove listeners for this Person before all are registered.
+            return;
+        }
+
+        // Remove listeners in the same order they are registered (guaranteed by the enum list)
+        for (int i = 0; i < personProperties.length; i++) {
+            person.removeListener(personProperties[i], listenerReferenceList.get(i).get());
+        }
+
+        LISTENER_MAP.removeAll(person);
     }
 }
