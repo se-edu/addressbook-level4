c8d06eada48f12e2682008301d2c02cd57307158
PierceAndy <pierceanderson.fu+github@gmail.com>
2017-03-09 17:10:34 +0800

    Interdiff between v13 and v14

--- src/main/java/seedu/address/logic/parser/Prefix.java
+++ src/main/java/seedu/address/logic/parser/Prefix.java
@@ -2,7 +2,7 @@ package seedu.address.logic.parser;
 
 /**
  * A prefix that marks the beginning of an argument in an arguments string.
- * E.g. '/t' in 'add James /t friend'.
+ * E.g. 't/' in 'add James t/ friend'.
  */
 public class Prefix {
     private final String prefix;
