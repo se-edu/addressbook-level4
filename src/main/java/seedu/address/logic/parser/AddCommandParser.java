0fd5372f5b9285f4b89fffe20faf690bc3194c3a
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 10:22:29 +0800

    Change command parsers' parse(String) return type to be more specific
    All command parsers' parse(String) methods return a generic Command
    type, instead of the specific command that they are tasked to parse
    (for example, AddCommand for AddCommandParser).
    
    Let's change all command parsers' parse(String)'s return type from the
    generic Command type to their specific command type that they are
    tasked to parse.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -11,7 +11,6 @@ import java.util.stream.Stream;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.Command;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -23,7 +22,7 @@ public class AddCommandParser {
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
      */
-    public Command parse(String args) throws ParseException {
+    public AddCommand parse(String args) throws ParseException {
         ArgumentTokenizer argsTokenizer =
                 new ArgumentTokenizer(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
         argsTokenizer.tokenize(args);
