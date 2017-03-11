4cfb285f78b7f7235dde31efe768eaff3afafad3
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 10:15:25 +0800

    Modify all command parsers' parse(String) to throw ParseException
    All command parsers return an IncorrectCommand object when they
    encounter a parse error.
    
    The parse error can be made more visible if an exception was thrown
    instead. Otherwise, the caller is unable to handle any parsing errors
    such as IllegalValueException. Context information, such as the
    stacktrace, is also lost when wrapping the exception instead of
    returning it directly.
    
    Let's change all command parsers' parse(String) methods such that they
    throw ParseException instead of returning an IncorrectCommand object.

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -3,6 +3,7 @@ package seedu.address.logic;
 import javafx.collections.ObservableList;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.ReadOnlyPerson;
 
 /**
@@ -14,8 +15,9 @@ public interface Logic {
      * @param commandText The command as entered by the user.
      * @return the result of the command execution.
      * @throws CommandException If an error occurs during command execution.
+     * @throws ParseException If an error occurs during parsing.
      */
-    CommandResult execute(String commandText) throws CommandException;
+    CommandResult execute(String commandText) throws CommandException, ParseException;
 
     /** Returns the filtered list of persons */
     ObservableList<ReadOnlyPerson> getFilteredPersonList();
