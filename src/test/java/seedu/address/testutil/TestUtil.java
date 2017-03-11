70c1bd01bd6ecf4903064556e8d0141547496096
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 11:58:51 +0800

    Person: Change constructor to take Set<Tag> instead of UniqueTagList
    Person#Person(Name, Phone, Email, Address, UniqueTagList) itself creates
    a new UniqueTagList so that callers would not be able to modify the list
    later.
    
    Due to changes in ReadOnlyPerson#getTags(), some callers of
    Person#setTags(UniqueTagList) might be dealing with Set<Tag>. This
    results in the caller having to convert them back into UniqueTagList,
    only for Person#Person(...) to create another new UniqueTagList.
    
    Such conversions are unnecessary and tedious.
    
    Let's convert Person#Person(...) to use Set<Tag> as a
    parameter so that callers do not need to deal with the conversions
    themselves.

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -26,7 +26,6 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.tag.UniqueTagList;
 
 /**
  * A utility class for test cases.
@@ -59,15 +58,15 @@ public class TestUtil {
         try {
             //CHECKSTYLE.OFF: LineLength
             return new Person[]{
-                new Person(new Name("Ali Muster"), new Phone("9482424"), new Email("hans@google.com"), new Address("4th street"), new UniqueTagList()),
-                new Person(new Name("Boris Mueller"), new Phone("87249245"), new Email("ruth@google.com"), new Address("81th street"), new UniqueTagList()),
-                new Person(new Name("Carl Kurz"), new Phone("95352563"), new Email("heinz@yahoo.com"), new Address("wall street"), new UniqueTagList()),
-                new Person(new Name("Daniel Meier"), new Phone("87652533"), new Email("cornelia@google.com"), new Address("10th street"), new UniqueTagList()),
-                new Person(new Name("Elle Meyer"), new Phone("9482224"), new Email("werner@gmail.com"), new Address("michegan ave"), new UniqueTagList()),
-                new Person(new Name("Fiona Kunz"), new Phone("9482427"), new Email("lydia@gmail.com"), new Address("little tokyo"), new UniqueTagList()),
-                new Person(new Name("George Best"), new Phone("9482442"), new Email("anna@google.com"), new Address("4th street"), new UniqueTagList()),
-                new Person(new Name("Hoon Meier"), new Phone("8482424"), new Email("stefan@mail.com"), new Address("little india"), new UniqueTagList()),
-                new Person(new Name("Ida Mueller"), new Phone("8482131"), new Email("hans@google.com"), new Address("chicago ave"), new UniqueTagList())
+                new Person(new Name("Ali Muster"), new Phone("9482424"), new Email("hans@google.com"), new Address("4th street"), new HashSet<>()),
+                new Person(new Name("Boris Mueller"), new Phone("87249245"), new Email("ruth@google.com"), new Address("81th street"), new HashSet<>()),
+                new Person(new Name("Carl Kurz"), new Phone("95352563"), new Email("heinz@yahoo.com"), new Address("wall street"), new HashSet<>()),
+                new Person(new Name("Daniel Meier"), new Phone("87652533"), new Email("cornelia@google.com"), new Address("10th street"), new HashSet<>()),
+                new Person(new Name("Elle Meyer"), new Phone("9482224"), new Email("werner@gmail.com"), new Address("michegan ave"), new HashSet<>()),
+                new Person(new Name("Fiona Kunz"), new Phone("9482427"), new Email("lydia@gmail.com"), new Address("little tokyo"), new HashSet<>()),
+                new Person(new Name("George Best"), new Phone("9482442"), new Email("anna@google.com"), new Address("4th street"), new HashSet<>()),
+                new Person(new Name("Hoon Meier"), new Phone("8482424"), new Email("stefan@mail.com"), new Address("little india"), new HashSet<>()),
+                new Person(new Name("Ida Mueller"), new Phone("8482131"), new Email("hans@google.com"), new Address("chicago ave"), new HashSet<>())
             };
             //CHECKSTYLE.ON: LineLength
         } catch (IllegalValueException e) {
