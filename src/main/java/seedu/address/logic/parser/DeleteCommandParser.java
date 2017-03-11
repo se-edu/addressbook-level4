55bc7291cb91fab3c7161980448b607b4088f301
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 10:16:03 +0800

    Modify all CommandParser#parse(String) to throw ParseErrorException
    All CommandParsers return an IncorrectCommand when the parser encounters
    a parse error.
    
    This behavior is not explicit, especially when the CommandParsers are
    expected to return only the specific type of command that they are
    tasked to parse (for example, AddCommandParser is expected to return
    AddCommand, not IncorrectCommand).
    
    Let's change all CommandParser#parse(String) methods such that they
    throw ParseErrorException instead of returning an IncorrectCommand.
    That way, the caller can handle a parse error as they deem fit, rather
    than letting the parsers handle that themselves.

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.IncorrectCommand;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -17,11 +16,11 @@ public class DeleteCommandParser {
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns an DeleteCommand object for execution.
      */
-    public Command parse(String args) {
+    public Command parse(String args) throws ParseErrorException  {
 
         Optional<Integer> index = ParserUtil.parseIndex(args);
         if (!index.isPresent()) {
-            return new IncorrectCommand(
+            throw new ParseErrorException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
 
