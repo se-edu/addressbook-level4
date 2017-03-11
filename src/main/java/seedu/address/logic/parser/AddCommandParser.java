be28436c8bdc3476ede4c84d442d018d2fb06ae7
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 12:01:57 +0800

    AddCommand: Change constructor to use Set<Tag> instead of UniqueTagList
    AddCommand#AddCommand(...) converts the UniqueTagList to a Set<Tag>,
    only to have the Person's constructor converting it back to a
    UniqueTagList.
    
    Such conversions are unnecessary and tedious.
    
    Let's change AddCommand#AddCommand(...) to only accept a Set<Tag>
    instead of UniqueTagList to avoid the conversion back and forth.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -12,7 +12,6 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.IncorrectCommand;
-import seedu.address.model.tag.UniqueTagList;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -33,7 +32,7 @@ public class AddCommandParser {
                     argsTokenizer.getValue(PREFIX_PHONE).get(),
                     argsTokenizer.getValue(PREFIX_EMAIL).get(),
                     argsTokenizer.getValue(PREFIX_ADDRESS).get(),
-                    new UniqueTagList(ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG)))
+                    ParserUtil.parseTags(argsTokenizer.getAllValues(PREFIX_TAG))
             );
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
