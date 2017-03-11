64975b90f42e6e96be32d4ea3e2dcaf1fee10614
Tan Wang Leng <tanwangleng@outlook.com>
2017-03-11 10:15:25 +0800

    Modify all command parsers' parse(String) to throw ParseException
    All command parsers return an IncorrectCommand object when they
    encounter a parse error.
    
    The parse error can be made more visible if an exception was thrown
    instead. Otherwise, the caller is unable to handle any parsing errors
    such as IllegalValueException. Context information, such as the
    stacktrace, is also lost when wrapping the exception instead of
    returning it directly.
    
    Let's change all command parsers' parse(String) methods such that they
    throw ParseException instead of returning an IncorrectCommand object.

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -14,9 +14,9 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.IncorrectCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses user input.
@@ -33,11 +33,12 @@ public class Parser {
      *
      * @param userInput full user input string
      * @return the command based on the user input
+     * @throws ParseException if any illegal values are found
      */
-    public Command parseCommand(String userInput) {
+    public Command parseCommand(String userInput) throws ParseException {
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
         if (!matcher.matches()) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
         }
 
         final String commandWord = matcher.group("commandWord");
@@ -72,7 +73,7 @@ public class Parser {
             return new HelpCommand();
 
         default:
-            return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 
