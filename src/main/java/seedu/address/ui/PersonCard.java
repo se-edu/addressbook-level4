4d455c0c14763971c3b1ddfffab3217450675784
MightyCupcakes <gohydj2003@gmail.com>
2017-03-16 17:39:53 +0800

    PersonCard: Add documentation about reserved keywords in JavaFX
    JavaFX stores all the fields in a namespace map which maps the name of
    the field to the corresponding JavaFX object. JavaFX also defines some
    keys as a constant - in particular LOCATION_KEY as the String
    "location" and RESOURCES_KEY as the String "resources".
    
    Using any of these strings as a field will result in JavaFX trying to
    cast the field to the type of the corresponding value in the namespace.
    For example, any fields with the name "location" will be cast into an
    URL object which is not allowed.
    
    Let's document these findings down in the controller so as to inform
    other developers of the existence of such keywords.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -11,6 +11,14 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
 
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
     @FXML
     private HBox cardPane;
     @FXML
