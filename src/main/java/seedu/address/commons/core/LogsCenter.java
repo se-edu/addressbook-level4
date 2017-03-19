1b71477306b5a40e57690b1db5c03440b555f8a7
VeryLazyBoy <frank16874@gmail.com>
2017-03-19 18:51:42 +0800

    Fix minor typos in LogsCenter

--- src/main/java/seedu/address/commons/core/LogsCenter.java
+++ src/main/java/seedu/address/commons/core/LogsCenter.java
@@ -38,7 +38,7 @@ public class LogsCenter {
     }
 
     /**
-     * Creates a logger with the given name the given name.
+     * Creates a logger with the given name.
      */
     public static Logger getLogger(String name) {
         Logger logger = Logger.getLogger(name);
