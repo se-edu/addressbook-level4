5db85f8098d03cd687c406cdb0e15144ab8b9e33
chao1995 <huangchao0825@gmail.com>
2017-03-21 01:38:48 +0800

    Interdiff between v5 and v6

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -1,16 +1,22 @@
 package guitests;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Test;
 
 import guitests.guihandles.PersonCardHandle;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.EditCommand;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.TestPerson;
 
+// TODO: reduce GUI tests by transferring some tests to be covered by lower level tests.
 public class EditCommandTest extends AddressBookGuiTest {
 
     // The list of persons in the person list panel is expected to match this list.
@@ -18,7 +24,7 @@ public class EditCommandTest extends AddressBookGuiTest {
     TestPerson[] expectedPersonsList = td.getTypicalPersons();
 
     @Test
-    public void edit_validCommand_success() throws Exception {
+    public void edit_allFieldsSpecified_success() throws Exception {
         String detailsToEdit = "Bobby p/91234567 e/bobby@gmail.com a/Block 123, Bobby Street 3 t/husband";
         int addressBookIndex = 1;
 
@@ -29,9 +35,82 @@ public class EditCommandTest extends AddressBookGuiTest {
     }
 
     @Test
-    public void edit_invalidCommand_failure() {
-        assertFalse(commandBox.runCommand("edit 8 Bobby"));
+    public void edit_notAllFieldsSpecified_success() throws Exception {
+        String detailsToEdit = "t/sweetie t/bestie";
+        int addressBookIndex = 2;
+
+        TestPerson personToEdit = expectedPersonsList[addressBookIndex - 1];
+        TestPerson editedPerson = new PersonBuilder(personToEdit).withTags("sweetie", "bestie").build();
+
+        assertEditSuccess(addressBookIndex, addressBookIndex, detailsToEdit, editedPerson);
+    }
+
+    @Test
+    public void edit_clearTags_success() throws Exception {
+        String detailsToEdit = "t/";
+        int addressBookIndex = 2;
+
+        TestPerson personToEdit = expectedPersonsList[addressBookIndex - 1];
+        TestPerson editedPerson = new PersonBuilder(personToEdit).withTags().build();
+
+        assertEditSuccess(addressBookIndex, addressBookIndex, detailsToEdit, editedPerson);
+    }
+
+    @Test
+    public void edit_findThenEdit_success() throws Exception {
+        commandBox.runCommand("find Elle");
+
+        String detailsToEdit = "Belle";
+        int filteredPersonListIndex = 1;
+        int addressBookIndex = 5;
+
+        TestPerson personToEdit = expectedPersonsList[addressBookIndex - 1];
+        TestPerson editedPerson = new PersonBuilder(personToEdit).withName("Belle").build();
+
+        assertEditSuccess(filteredPersonListIndex, addressBookIndex, detailsToEdit, editedPerson);
+    }
+
+    @Test
+    public void edit_missingPersonIndex_failure() {
+        commandBox.runCommand("edit Bobby");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void edit_invalidPersonIndex_failure() {
+        commandBox.runCommand("edit 8 Bobby");
         assertResultMessage(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void edit_noFieldsSpecified_failure() {
+        commandBox.runCommand("edit 1");
+        assertResultMessage(EditCommand.MESSAGE_NOT_EDITED);
+    }
+
+    @Test
+    public void edit_invalidValues_failure() {
+        commandBox.runCommand("edit 1 *&");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("edit 1 p/abcd");
+        assertResultMessage(Phone.MESSAGE_PHONE_CONSTRAINTS);
+
+        commandBox.runCommand("edit 1 e/yahoo!!!");
+        assertResultMessage(Email.MESSAGE_EMAIL_CONSTRAINTS);
+
+        commandBox.runCommand("edit 1 a/");
+        assertResultMessage(Address.MESSAGE_ADDRESS_CONSTRAINTS);
+
+        commandBox.runCommand("edit 1 t/*&");
+        assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+    }
+
+    @Test
+    public void edit_duplicatePerson_failure() {
+        commandBox.runCommand("edit 3 Alice Pauline p/85355255 e/alice@gmail.com "
+                                + "a/123, Jurong West Ave 6, #08-111 t/friends");
+        assertResultMessage(EditCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     /**
