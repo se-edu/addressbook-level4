27cb4621dca1bf474b2838352a90873f3fbcb4ef
Pierce Anderson Fu <pierceanderson.fu+github@gmail.com>
2017-03-25 16:08:12 +0800

    [#351] Outdated prefixes in ArgumentTokenizer (#352)
    The prefixes in ArgumentTokenizer's comments and tests use the old
    prefix format of "/t" instead of the current "t/".
    
    This is inconsistent with our production code.
    
    Let's update the comments and tests to be consistent with the current
    prefix format used in the production code.

--- src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
+++ src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
@@ -10,11 +10,11 @@ import java.util.Optional;
 
 /**
  * Tokenizes arguments string of the form: {@code preamble <prefix>value <prefix>value ...}<br>
- *     e.g. {@code some preamble text /t 11.00/dToday /t 12.00 /k /m July}  where prefixes are {@code /t /d /k /m}.<br>
- * 1. An argument's value can be an empty string e.g. the value of {@code /k} in the above example.<br>
+ *     e.g. {@code some preamble text t/ 11.00d/Today t/ 12.00 k/ m/ July}  where prefixes are {@code t/ d/ k/ m/}.<br>
+ * 1. An argument's value can be an empty string e.g. the value of {@code k/} in the above example.<br>
  * 2. Leading and trailing whitespaces of an argument value will be discarded.<br>
- * 3. A prefix need not have leading and trailing spaces e.g. the {@code /d in 11.00/dToday} in the above example<br>
- * 4. An argument may be repeated and all its values will be accumulated e.g. the value of {@code /t}
+ * 3. A prefix need not have leading and trailing spaces e.g. the {@code d/ in 11.00d/Today} in the above example<br>
+ * 4. An argument may be repeated and all its values will be accumulated e.g. the value of {@code t/}
  *    in the above example.<br>
  */
 public class ArgumentTokenizer {
@@ -157,7 +157,7 @@ public class ArgumentTokenizer {
 
     /**
      * A prefix that marks the beginning of an argument.
-     * e.g. '/t' in 'add James /t friend'
+     * e.g. 't/' in 'add James t/ friend'
      */
     public static class Prefix {
         final String prefix;
