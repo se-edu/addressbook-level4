7178cac31f55607f370b9ca58c56f5d45e944d70
eugenepeh <e0012835@u.nus.edu>
2017-03-26 16:14:45 +0800

    Checkstyle: add DeclarationOrder rule
    Class and Interface declaration orders that are not standardized to
    our coding standards are left unchecked.
    
    This is because there are no rules in our checkstyle configuration
    file that enforce this check. They should follow a specific order, in
    order to standardize the coding style, such as having the variables
    follow the following declaration order - public, protected, package
    (no access modifier), private.
    
    Let's fix this by adding DeclarationOrder rule. It enforces the check
    on the Class and Interface declaration order and ensures that they
    follow our coding standards.

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -25,9 +25,11 @@ import seedu.address.model.UserPrefs;
  * The manager of the UI component.
  */
 public class UiManager extends ComponentManager implements Ui {
+
+    public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
+
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
     private static final String ICON_APPLICATION = "/images/address_book_32.png";
-    public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
 
     private Logic logic;
     private Config config;
