7d0efb1088f0e59334f5b01612dd0ef690767ee5
chao1995 <huangchao0825@gmail.com>
2017-03-28 22:12:32 +0800

    TestPerson: remove magic strings in getAddCommand()
    We use magic strings like "add" and "p/" when constructing an add
    command for adding the person.
    
    However, a better way is to get these strings from where they are
    defined.

--- src/test/java/seedu/address/testutil/TestPerson.java
+++ src/test/java/seedu/address/testutil/TestPerson.java
@@ -1,5 +1,7 @@
 package seedu.address.testutil;
 
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.parser.CliSyntax;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -31,11 +33,13 @@ public class TestPerson extends Person {
      */
     public String getAddCommand() {
         StringBuilder sb = new StringBuilder();
-        sb.append("add " + this.getName().fullName + " ");
-        sb.append("a/" + this.getAddress().value + " ");
-        sb.append("p/" + this.getPhone().value + " ");
-        sb.append("e/" + this.getEmail().value + " ");
-        this.getTags().asObservableList().stream().forEach(s -> sb.append("t/" + s.tagName + " "));
+        sb.append(AddCommand.COMMAND_WORD + " " + this.getName().fullName + " ");
+        sb.append(CliSyntax.PREFIX_PHONE.getPrefix() + this.getPhone().value + " ");
+        sb.append(CliSyntax.PREFIX_EMAIL.getPrefix() + this.getEmail().value + " ");
+        sb.append(CliSyntax.PREFIX_ADDRESS.getPrefix() + this.getAddress().value + " ");
+        this.getTags().asObservableList().stream().forEach(
+            s -> sb.append(CliSyntax.PREFIX_TAG.getPrefix() + s.tagName + " ")
+        );
         return sb.toString();
     }
 
