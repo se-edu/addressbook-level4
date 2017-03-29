e3ca3c315e9eefafc3d7734907536687ac3770d5
chao1995 <huangchao0825@gmail.com>
2017-03-29 14:36:10 +0800

    Interdiff between v5 and v6

--- src/test/java/guitests/AddressBookGuiTest.java
+++ src/test/java/guitests/AddressBookGuiTest.java
@@ -28,7 +28,7 @@ import seedu.address.commons.events.BaseEvent;
 import seedu.address.model.AddressBook;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.testutil.TestUtil;
-import seedu.address.testutil.TypicalTestPersons;
+import seedu.address.testutil.TypicalPersons;
 
 /**
  * A GUI Test class for AddressBook.
@@ -41,7 +41,7 @@ public abstract class AddressBookGuiTest {
 
     TestApp testApp;
 
-    protected TypicalTestPersons td = new TypicalTestPersons();
+    protected TypicalPersons td = new TypicalPersons();
 
     /*
      *   Handles to GUI elements present at the start up are created in advance
@@ -91,7 +91,7 @@ public abstract class AddressBookGuiTest {
      */
     protected AddressBook getInitialData() {
         AddressBook ab = new AddressBook();
-        TypicalTestPersons.loadAddressBookWithSampleData(ab);
+        TypicalPersons.loadAddressBookWithSampleData(ab);
         return ab;
     }
 
--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -9,9 +9,9 @@ import seedu.address.model.person.Phone;
 import seedu.address.model.tag.UniqueTagList;
 
 /**
- * A utility class to help with building TestPerson objects.
+ * A utility class to help with building Person objects.
  */
-public class TestPersonBuilder {
+public class PersonBuilder {
 
     public static final String DEFUALT_NAME = "Alice Pauline";
     public static final String DEFUALT_PHONE = "85355255";
@@ -19,50 +19,50 @@ public class TestPersonBuilder {
     public static final String DEFUALT_ADDRESS = "123, Jurong West Ave 6, #08-111";
     public static final String DEFUALT_TAGS = "friends";
 
-    private TestPerson person;
+    private Person person;
 
-    public TestPersonBuilder() throws IllegalValueException {
+    public PersonBuilder() throws IllegalValueException {
         Name defaultName = new Name(DEFUALT_NAME);
         Phone defaultPhone = new Phone(DEFUALT_PHONE);
         Email defaultEmail = new Email(DEFUALT_EMAIL);
         Address defaultAddress = new Address(DEFUALT_ADDRESS);
         UniqueTagList defaultTags = new UniqueTagList(DEFUALT_TAGS);
-        this.person = new TestPerson(defaultName, defaultPhone, defaultEmail, defaultAddress, defaultTags);
+        this.person = new Person(defaultName, defaultPhone, defaultEmail, defaultAddress, defaultTags);
     }
 
     /**
-     * Initializes the TestPersonBuilder with the data of {@code personToCopy}.
+     * Initializes the PersonBuilder with the data of {@code personToCopy}.
      */
-    public TestPersonBuilder(Person personToCopy) {
-        this.person = new TestPerson(personToCopy);
+    public PersonBuilder(Person personToCopy) {
+        this.person = new Person(personToCopy);
     }
 
-    public TestPersonBuilder withName(String name) throws IllegalValueException {
+    public PersonBuilder withName(String name) throws IllegalValueException {
         this.person.setName(new Name(name));
         return this;
     }
 
-    public TestPersonBuilder withTags(String ... tags) throws IllegalValueException {
+    public PersonBuilder withTags(String ... tags) throws IllegalValueException {
         this.person.setTags(new UniqueTagList(tags));
         return this;
     }
 
-    public TestPersonBuilder withAddress(String address) throws IllegalValueException {
+    public PersonBuilder withAddress(String address) throws IllegalValueException {
         this.person.setAddress(new Address(address));
         return this;
     }
 
-    public TestPersonBuilder withPhone(String phone) throws IllegalValueException {
+    public PersonBuilder withPhone(String phone) throws IllegalValueException {
         this.person.setPhone(new Phone(phone));
         return this;
     }
 
-    public TestPersonBuilder withEmail(String email) throws IllegalValueException {
+    public PersonBuilder withEmail(String email) throws IllegalValueException {
         this.person.setEmail(new Email(email));
         return this;
     }
 
-    public TestPerson build() {
+    public Person build() {
         return this.person;
     }
 
