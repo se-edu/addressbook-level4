d54eb13e3eaa42c4c0f27aa855e53a01b2e317de
chao1995 <huangchao0825@gmail.com>
2017-03-29 14:33:35 +0800

    Interdiff between v2 and v3

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,7 +1,5 @@
 package seedu.address.testutil;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.parser.CliSyntax;
 import seedu.address.model.person.Person;
 
 public class PersonUtil {
@@ -11,13 +9,11 @@ public class PersonUtil {
      */
     public static String getAddCommand(Person person) {
         StringBuilder sb = new StringBuilder();
-        sb.append(AddCommand.COMMAND_WORD + " " + person.getName().fullName + " ");
-        sb.append(CliSyntax.PREFIX_PHONE.getPrefix() + person.getPhone().value + " ");
-        sb.append(CliSyntax.PREFIX_EMAIL.getPrefix() + person.getEmail().value + " ");
-        sb.append(CliSyntax.PREFIX_ADDRESS.getPrefix() + person.getAddress().value + " ");
-        person.getTags().stream().forEach(
-            s -> sb.append(CliSyntax.PREFIX_TAG.getPrefix() + s.tagName + " ")
-        );
+        sb.append("add " + person.getName().fullName + " ");
+        sb.append("a/" + person.getAddress().value + " ");
+        sb.append("p/" + person.getPhone().value + " ");
+        sb.append("e/" + person.getEmail().value + " ");
+        person.getTags().stream().forEach(s -> sb.append("t/" + s.tagName + " "));
         return sb.toString();
     }
 }
