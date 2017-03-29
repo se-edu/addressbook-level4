58b7fb3df92aeaa25b695d345059a2793b1dfb6a
chao1995 <huangchao0825@gmail.com>
2017-03-29 14:33:35 +0800

    Remove TestPerson
    TestPerson is used in testing to create a Person without filling in
    all the fields.
    
    However, although it simplifies construction of a person, it also
    violates LSP because TestPerson breaks ReadOnlyPerson's contract.
    For example, its getName() method can possibly return null, and
    its getTags() method doesn't return a deep copy.
    
    Let's remove TestPerson and use Person instead. With default fields,
    it's still easy to construct a person using PersonBuilder. Let's also
    add PersonUtil to hold utility methods for Person, like getAddCommand.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -1,28 +1,43 @@
 package seedu.address.testutil;
 
+import java.util.Set;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
 
 /**
- *
+ * A utility class to help with building Person objects.
  */
 public class PersonBuilder {
 
-    private TestPerson person;
+    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final String DEFAULT_PHONE = "85355255";
+    public static final String DEFAULT_EMAIL = "alice@gmail.com";
+    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_TAGS = "friends";
 
-    public PersonBuilder() {
-        this.person = new TestPerson();
+    private Person person;
+
+    public PersonBuilder() throws IllegalValueException {
+        Name defaultName = new Name(DEFAULT_NAME);
+        Phone defaultPhone = new Phone(DEFAULT_PHONE);
+        Email defaultEmail = new Email(DEFAULT_EMAIL);
+        Address defaultAddress = new Address(DEFAULT_ADDRESS);
+        Set<Tag> defaultTags = SampleDataUtil.getTagSet(DEFAULT_TAGS);
+        this.person = new Person(defaultName, defaultPhone, defaultEmail, defaultAddress, defaultTags);
     }
 
     /**
      * Initializes the PersonBuilder with the data of {@code personToCopy}.
      */
-    public PersonBuilder(TestPerson personToCopy) {
-        this.person = new TestPerson(personToCopy);
+    public PersonBuilder(Person personToCopy) {
+        this.person = new Person(personToCopy);
     }
 
     public PersonBuilder withName(String name) throws IllegalValueException {
@@ -31,7 +46,7 @@ public class PersonBuilder {
     }
 
     public PersonBuilder withTags(String ... tags) throws IllegalValueException {
-        this.person.setTags(new UniqueTagList(tags));
+        this.person.setTags(SampleDataUtil.getTagSet(tags));
         return this;
     }
 
@@ -50,7 +65,7 @@ public class PersonBuilder {
         return this;
     }
 
-    public TestPerson build() {
+    public Person build() {
         return this.person;
     }
 
