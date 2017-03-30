3a7b6e0cda53bc91d6333b23e95748f5e68180f9
chao1995 <huangchao0825@gmail.com>
2017-03-30 14:19:09 +0800

    Replace potentially genuine email addresses with example.com
    Potentially genuine email addresses are used in our documentation and
    testing code, e.g. bobby@gmail.com, hans@google.com, alice@yahoo.com.
    
    We don't want to help spam bots send spam to them.
    
    Let's change these potentially genuine email address to @example.com.

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -15,28 +15,28 @@ public class TypicalPersons {
     public TypicalPersons() {
         try {
             alice = new PersonBuilder().withName("Alice Pauline")
-                    .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@gmail.com")
+                    .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
                     .withPhone("85355255")
                     .withTags("friends").build();
             benson = new PersonBuilder().withName("Benson Meier").withAddress("311, Clementi Ave 2, #02-25")
-                    .withEmail("johnd@gmail.com").withPhone("98765432")
+                    .withEmail("johnd@example.com").withPhone("98765432")
                     .withTags("owesMoney", "friends").build();
             carl = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-                    .withEmail("heinz@yahoo.com").withAddress("wall street").build();
+                    .withEmail("heinz@example.com").withAddress("wall street").build();
             daniel = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-                    .withEmail("cornelia@google.com").withAddress("10th street").build();
+                    .withEmail("cornelia@example.com").withAddress("10th street").build();
             elle = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-                    .withEmail("werner@gmail.com").withAddress("michegan ave").build();
+                    .withEmail("werner@example.com").withAddress("michegan ave").build();
             fiona = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-                    .withEmail("lydia@gmail.com").withAddress("little tokyo").build();
+                    .withEmail("lydia@example.com").withAddress("little tokyo").build();
             george = new PersonBuilder().withName("George Best").withPhone("9482442")
-                    .withEmail("anna@google.com").withAddress("4th street").build();
+                    .withEmail("anna@example.com").withAddress("4th street").build();
 
             // Manually added
             hoon = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-                    .withEmail("stefan@mail.com").withAddress("little india").build();
+                    .withEmail("stefan@example.com").withAddress("little india").build();
             ida = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-                    .withEmail("hans@google.com").withAddress("chicago ave").build();
+                    .withEmail("hans@example.com").withAddress("chicago ave").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
