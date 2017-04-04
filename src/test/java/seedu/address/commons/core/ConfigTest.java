38c7bb76b50028049df581f9c2328d50e68a4419
eugenepeh <e0012835@u.nus.edu>
2017-04-04 10:58:36 +0800

    Checkstyle: enforce operators' placement in line wraps
    Our coding standard for Java requires operators' placement in line
    wraps to follow specific rules, such as having assign operator (=)
    stay attached to the token that precedes it and having the arithmetric
    operator (+) in a new line. These rules are not enforced by
    checkstyle.
    
    Let's teach Checkstyle to be stricter about line wrapping around more
    symbols by adding the relevant OperatorWrap rules.

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -14,9 +14,9 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : Address App\n" +
-                "Current log level : INFO\n" +
-                "Preference file Location : preferences.json";
+        String defaultConfigAsString = "App title : Address App\n"
+                + "Current log level : INFO\n"
+                + "Preference file Location : preferences.json";
 
         assertEquals(defaultConfigAsString, new Config().toString());
     }
