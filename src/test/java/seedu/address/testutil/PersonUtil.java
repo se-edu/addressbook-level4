81ee71e821cadb832cb9a1403ac2050692d0bbe5
chao1995 <huangchao0825@gmail.com>
2017-04-06 04:07:06 +0800

    PersonUtil: add getEditCommand(int, Person)
    Let's add a utility method for creating the command string for
    editing the details of a person in the address book.

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -11,8 +11,22 @@ public class PersonUtil {
      * Returns an add command string for adding the {@code person}.
      */
     public static String getAddCommand(Person person) {
+        return "add " + getPersonDetails(person);
+    }
+
+    /**
+     * Returns an edit command string for editing the person at {@code zeroBasedIndex} to match {@code editedPerson}.
+     */
+    public static String getEditCommand(int zeroBasedIndex, Person editedPerson) {
+        return "edit " + (zeroBasedIndex + 1) + " " + getPersonDetails(editedPerson);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code person}'s details.
+     */
+    private static String getPersonDetails(Person person) {
         StringBuilder sb = new StringBuilder();
-        sb.append("add " + person.getName().fullName + " ");
+        sb.append(person.getName().fullName + " ");
         sb.append("a/" + person.getAddress().value + " ");
         sb.append("p/" + person.getPhone().value + " ");
         sb.append("e/" + person.getEmail().value + " ");
