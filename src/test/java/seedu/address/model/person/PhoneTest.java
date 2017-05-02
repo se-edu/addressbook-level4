906542b9526923aa331a78066d6fd15cea433dac
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-02 14:25:21 +0800

    The shortest known phone numbers are 3 digits (e.g. emergency numbers such as 911 and 999) but the regex used to validate phone numbers accepts numbers shorter than 3 digits.
    Let’s change the regex to reject numbers shorter than 3 digits.

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -12,13 +12,14 @@ public class PhoneTest {
         // invalid phone numbers
         assertFalse(Phone.isValidPhone("")); // empty string
         assertFalse(Phone.isValidPhone(" ")); // spaces only
+        assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
 
         // valid phone numbers
+        assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
         assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("4")); // short phone numbers
         assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
     }
 }
