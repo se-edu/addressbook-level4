a28c2f474da431925acda98b7da588d1d8d1f723
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-02 15:06:53 +0800

    Refactor Parser Tests for AddCommand
    LogicManagerTest is too ambitious. It contains 500+ lines of code at the
    moment and tests all commands in integration test level.
    
    Currently, the tests pertaining to parsing of commands appear to
    be doing integration tests instead, since these commands will be
    executed upon being parsed followed by checks to ensure that the
    model is what is being expected.
    
    However, the tests for parsing of commands should only concern
    itself with whether the commands have been parsed successfully.
    
    Let’s use a different metric to test the parsing of commands, and
    let’s refactor out tests pertaining to parsing of AddCommand.

---
+++ src/test/java/seedu/address/testutil/ParserTestUtil.java
@@ -0,0 +1,28 @@
+package seedu.address.testutil;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.parser.Parser;
+
+/**
+ * Static utility class for parser tests.
+ */
+public class ParserTestUtil {
+    private static Parser parser = new Parser();
+
+    public static void assertParseFailure(String userInput, String expectedMessage) {
+        Command command = parser.parseCommand(userInput);
+
+        // The current implementation of the parsing of commands will always return
+        // an IncorrectCommand if the parsing failed.
+        if (!(command instanceof IncorrectCommand)) {
+            fail();
+        }
+
+        IncorrectCommand incorrectCommand = (IncorrectCommand) command;
+        assertEquals(expectedMessage, incorrectCommand.feedbackToUser);
+    }
+}
