287a2033a1491f437e802d3992041a5511165818
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 07:22:11 +0800

    Interdiff between v5 and v6

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -18,56 +18,56 @@ import seedu.address.model.tag.Tag;
 
 public class EditCommandParserTest {
 
-    private Parser parser = new Parser();
+    private EditCommandParser parser = new EditCommandParser();
 
     @Test
     public void parse_failure() {
         // no index specified
-        assertParseFailure("edit Bobby p/911", String.format(
+        assertParseFailure("Bobby p/911", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
 
         // negative index
-        assertParseFailure("edit -5 Bobby e/ bob@yahoo.com",
+        assertParseFailure("-5 Bobby e/ bob@yahoo.com",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
 
         // no fields specified
-        assertParseFailure("edit 1", EditCommand.MESSAGE_NOT_EDITED);
+        assertParseFailure("1", EditCommand.MESSAGE_NOT_EDITED);
 
         // unknown field specified - unknown field will be parsed as name
-        assertParseFailure("edit 1 q/unknown", Name.MESSAGE_NAME_CONSTRAINTS);
+        assertParseFailure("1 q/unknown", Name.MESSAGE_NAME_CONSTRAINTS);
 
         // only invalid values
-        assertParseFailure("edit 1 *&", Name.MESSAGE_NAME_CONSTRAINTS);
-        assertParseFailure("edit 1 p/abcd", Phone.MESSAGE_PHONE_CONSTRAINTS);
-        assertParseFailure("edit 1 e/yahoo!!!", Email.MESSAGE_EMAIL_CONSTRAINTS);
-        assertParseFailure("edit 1 a/", Address.MESSAGE_ADDRESS_CONSTRAINTS);
-        assertParseFailure("edit 1 t/*&", Tag.MESSAGE_TAG_CONSTRAINTS);
+        assertParseFailure("1 *&", Name.MESSAGE_NAME_CONSTRAINTS);
+        assertParseFailure("1 p/abcd", Phone.MESSAGE_PHONE_CONSTRAINTS);
+        assertParseFailure("1 e/yahoo!!!", Email.MESSAGE_EMAIL_CONSTRAINTS);
+        assertParseFailure("1 a/", Address.MESSAGE_ADDRESS_CONSTRAINTS);
+        assertParseFailure("1 t/*&", Tag.MESSAGE_TAG_CONSTRAINTS);
 
         // invalid values with valid values of different type
-        assertParseFailure("edit 1 p/abcd e/bob@yahoo.com.sg", Phone.MESSAGE_PHONE_CONSTRAINTS);
+        assertParseFailure("1 p/abcd e/bob@yahoo.com.sg", Phone.MESSAGE_PHONE_CONSTRAINTS);
 
         // invalid values with valid values of same type
-        assertParseFailure("edit 1 t/hi t/", Tag.MESSAGE_TAG_CONSTRAINTS);
-        assertParseFailure("edit 1 p/911 p/abcd", Phone.MESSAGE_PHONE_CONSTRAINTS);
+        assertParseFailure("1 t/hi t/", Tag.MESSAGE_TAG_CONSTRAINTS);
+        assertParseFailure("1 p/911 p/abcd", Phone.MESSAGE_PHONE_CONSTRAINTS);
 
         // invalid ordering - entire string parsed as email
-        assertParseFailure("edit 1 e/bob@gmail.com Bobby", Email.MESSAGE_EMAIL_CONSTRAINTS);
+        assertParseFailure("1 e/bob@gmail.com Bobby", Email.MESSAGE_EMAIL_CONSTRAINTS);
     }
 
     @Test
     public void parse_success() {
         // all fields specified
-        assertParseSuccess("edit 2 Bobby p/91234567 t/hubby e/bobby@example.com a/Block 123, Bobby Street 3 t/husband");
+        assertParseSuccess("2 Bobby p/91234567 t/hubby e/bobby@example.com a/Block 123, Bobby Street 3 t/husband");
 
         // some fields specified
-        assertParseSuccess("edit 1 t/ p/91234567");
+        assertParseSuccess("1 t/ p/91234567");
 
         // same fields specified - unintended behavior, will be changed in future versions
-        assertParseSuccess("edit 1 p/91234567 p/911");
+        assertParseSuccess("1 p/91234567 p/911");
 
         // one field specified
-        assertParseSuccess("edit 3 Bub");
-        assertParseSuccess("edit 3 t/");
+        assertParseSuccess("3 Bub");
+        assertParseSuccess("3 t/");
     }
 
     /**
@@ -75,7 +75,7 @@ public class EditCommandParserTest {
      * equals to {@code expectedMessage}
      */
     protected void assertParseFailure(String userInput, String expectedMessage) {
-        Command command = parser.parseCommand(userInput);
+        Command command = parser.parse(userInput);
 
         // The current implementation of the parsing of commands will always return
         // an IncorrectCommand if the parsing failed.
@@ -89,7 +89,7 @@ public class EditCommandParserTest {
      * Asserts {@code userInput} is successfully parsed
      */
     protected void assertParseSuccess(String userInput) {
-        Command command = parser.parseCommand(userInput);
+        Command command = parser.parse(userInput);
 
         assertFalse(command instanceof IncorrectCommand);
     }
