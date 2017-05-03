d5a201f459dbd3a0b1b22cc96a281f8e9f2375de
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 07:22:11 +0800

    Interdiff between v27 and v28

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -36,28 +36,23 @@ public class EditPersonDescriptorTest {
         assertFalse(DESC_AMY.equals(DESC_BOB));
 
         // different name -> returns false
-        EditPersonDescriptorBuilder editedAmy = new EditPersonDescriptorBuilder(DESC_AMY);
-        editedAmy.withName(VALID_NAME_BOB);
-        assertFalse(DESC_AMY.equals(editedAmy.build()));
+        EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
 
         // different phone -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY);
-        editedAmy.withPhone(VALID_PHONE_BOB);
-        assertFalse(DESC_AMY.equals(editedAmy.build()));
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
 
         // different email -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY);
-        editedAmy.withEmail(VALID_EMAIL_BOB);
-        assertFalse(DESC_AMY.equals(editedAmy.build()));
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withEmail(VALID_EMAIL_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
 
         // different address -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY);
-        editedAmy.withAddress(VALID_ADDRESS_BOB);
-        assertFalse(DESC_AMY.equals(editedAmy.build()));
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withAddress(VALID_ADDRESS_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
 
         // different tags -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY);
-        editedAmy.withTags(VALID_TAG_HUSBAND);
-        assertFalse(DESC_AMY.equals(editedAmy.build()));
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
     }
 }
