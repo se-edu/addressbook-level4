3fecdac592a6b6e8b439516a9b6780fccfeb35b4
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 11:53:53 +0800

    Add ReversibleCommand and update some commands to extend it
    All commands extend Command.
    
    This disallows us from identifying which commands can be reversed.
    
    Let’s create ReversibleCommand and update the relevant commands to
    extend it.
    
    Also, we implemented the Template Pattern for ReversibleCommand#execute()
    which ensures that every command that extends ReversibleCommand will
    have to call saveAddressBookSnapshot(), thus allowing the command
    to be undone and redone in the future.

---
+++ src/main/java/seedu/address/logic/commands/ReversibleCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.OutOfElementsException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
+
+/**
+ * Represents a command which can be undone and redone.
+ */
+public abstract class ReversibleCommand extends Command {
+    private ReadOnlyAddressBook previousAddressBook;
+
+    abstract CommandResult executeReversibleCommand() throws CommandException;
+
+    /**
+     * Stores the current state of {@code model#addressBook}.
+     */
+    private void saveAddressBookSnapshot() {
+        requireNonNull(model);
+        this.previousAddressBook = new AddressBook(model.getAddressBook());
+    }
+
+    /**
+     * Reverts the AddressBook to the state before this command
+     * was executed and updates the filtered person list to
+     * show all persons.
+     */
+    protected final void undo() {
+        requireAllNonNull(model, previousAddressBook);
+        model.resetData(previousAddressBook);
+        model.updateFilteredListToShowAll();
+    }
+
+    /**
+     * Executes the command and updates the filtered person
+     * list to show all persons.
+     */
+    protected final void redo() throws OutOfElementsException {
+        requireNonNull(model);
+        try {
+            executeReversibleCommand();
+        } catch (CommandException ce) {
+            throw new AssertionError("The command has been successfully executed previously; "
+                    + "it should not fail now");
+        }
+        model.updateFilteredListToShowAll();
+    }
+
+    @Override
+    public final CommandResult execute() throws CommandException {
+        saveAddressBookSnapshot();
+        return executeReversibleCommand();
+    }
+}
