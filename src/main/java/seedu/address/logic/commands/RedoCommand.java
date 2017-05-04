4987ce0e33a76402864f784f37212192299e0a59
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 08:32:17 +0800

    UndoCommand & RedoCommand: Finish implementation
    UndoCommand & RedoCommand are not fully implemented.
    
    Let’s fully implement them.

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,10 +1,38 @@
 package seedu.address.logic.commands;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.OutOfElementsException;
+import seedu.address.model.Model;
+
+/**
+ * Redo the previously undone command.
+ */
 public class RedoCommand extends Command {
+
+    public static final String COMMAND_WORD = "redo";
+    public static final String MESSAGE_SUCCESS = "Redo success!";
     public static final String MESSAGE_FAILURE = "There's no more commands left to be redone!";
 
     @Override
-    public CommandResult execute() {
-        return new CommandResult("stub");
+    public CommandResult execute() throws CommandException {
+        checkNotNull(model);
+        checkNotNull(undoRedoStack);
+
+        try {
+            undoRedoStack.popRedo().redo();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (OutOfElementsException ooee) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+    }
+
+    @Override
+    public void setData(Model model, CommandHistory commandHistory, UndoRedoStack undoRedoStack) {
+        this.model = model;
+        this.undoRedoStack = undoRedoStack;
     }
 }
---
+++ src/test/java/seedu/address/logic/commands/UndoAndRedoCommandTest.java
@@ -0,0 +1,74 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.testutil.TypicalPersons;
+
+public class UndoAndRedoCommandTest {
+    private static final CommandHistory HISTORY = new CommandHistory();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private Model model = new ModelManager(new TypicalPersons().getTypicalAddressBook(), new UserPrefs());
+    private UndoRedoStack undoRedoStack = new UndoRedoStack();
+    private UndoCommand undoCommand = new UndoCommand();
+    private RedoCommand redoCommand = new RedoCommand();
+
+    @Before
+    public void setUp() throws Exception {
+        undoCommand.setData(model, HISTORY, undoRedoStack);
+        redoCommand.setData(model, HISTORY, undoRedoStack);
+    }
+
+    @Test
+    public void undoCommandExecute_noCommandInHistory_throwsCommandException() throws Exception {
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(UndoCommand.MESSAGE_FAILURE);
+
+        undoCommand.execute();
+    }
+
+    @Test
+    public void redoCommandExecute_noCommandInHistory_throwsCommandException() throws Exception {
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(RedoCommand.MESSAGE_FAILURE);
+
+        redoCommand.execute();
+    }
+
+    @Test
+    public void undoAndRedoCommandExecute_oneCommandInHistory_success() throws Exception {
+        Model expectedModel = new ModelManager(new TypicalPersons().getTypicalAddressBook(), new UserPrefs());
+
+        deleteFirstPerson();
+        assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        ReadOnlyPerson toDelete = expectedModel.getFilteredPersonList().get(0);
+        expectedModel.deletePerson(toDelete);
+        assertCommandSuccess(redoCommand, model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    /**
+     * Executes {@code DeleteCommand} to delete the first person in {@code model}, and updates the {@code undoRedoStack}
+     */
+    private void deleteFirstPerson() throws CommandException {
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_PERSON);
+        deleteCommand.setData(model, new CommandHistory(), undoRedoStack);
+        deleteCommand.execute();
+        undoRedoStack.add(deleteCommand);
+    }
+}
