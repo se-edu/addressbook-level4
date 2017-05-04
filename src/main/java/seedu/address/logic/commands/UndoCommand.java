9b086101c9147df0baaac3036498a96b09d212c2
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 08:32:17 +0800

    UndoCommand & RedoCommand: Finish implementation
    UndoCommand & RedoCommand are not fully implemented.
    
    Let’s fully implement them.

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,10 +1,38 @@
 package seedu.address.logic.commands;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.OutOfElementsException;
+import seedu.address.model.Model;
+
+/**
+ * Undo the popUndo command.
+ */
 public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_SUCCESS = "Undo success!";
     public static final String MESSAGE_FAILURE = "There's no more commands left to be undone!";
 
     @Override
-    public CommandResult execute() {
-        return new CommandResult("stub");
+    public CommandResult execute() throws CommandException {
+        checkNotNull(model);
+        checkNotNull(undoRedoStack);
+
+        try {
+            undoRedoStack.popUndo().undo();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (OutOfElementsException ooee) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+    }
+
+    @Override
+    public void setData(Model model, CommandHistory commandHistory, UndoRedoStack undoRedoStack) {
+        this.model = model;
+        this.undoRedoStack = undoRedoStack;
     }
 }
