4c55c589a33067085085db6691c94e8f50655993
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 09:02:56 +0800

    Logic: Create HistoryManager
    Addressbook does not keep track of entered commands.
    
    This makes it difficult for the user to trace previously entered commands
    and also unable to correct any typos using past entered commands.
    
    As a step towards implementing history functionality, let’s create
    the class HistoryManager.

---
+++ src/test/java/seedu/address/logic/HistoryManagerTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HistoryManagerTest {
+    private History history;
+
+    @Before
+    public void setUp() {
+        history = new HistoryManager();
+    }
+
+    @Test
+    public void add() {
+        final String validCommand = "clear";
+        final String invalidCommand = "adds Bob";
+
+        history.add(validCommand);
+        history.add(invalidCommand);
+        assertEquals(Arrays.asList(validCommand, invalidCommand), history.getHistory());
+    }
+}
