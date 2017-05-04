8a7baf212bd3b358cb3ae8001a1dea351e0957d4
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 11:53:53 +0800

    Add UndoableCommand and update some commands to extend it
    All commands extend Command.
    
    This disallows us from identifying which commands can be reversed.
    
    Let’s create UndoableCommand and update the relevant commands to
    extend it.
    
    Also, we implemented the Template Pattern for UndoableCommand#execute()
    which ensures that every command that extends UndoableCommand will
    automatically call saveAddressBookSnapshot() when execute() is called
    to facilitate the undo redo mechanism. Classes that inherits from
    UndoableCommand will have to override executeUndoableCommand() instead
    of execute() as a result.

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -10,7 +10,7 @@ import seedu.address.model.person.exceptions.PersonNotFoundException;
 /**
  * Deletes a person identified using it's last displayed index from the address book.
  */
-public class DeleteCommand extends Command {
+public class DeleteCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "delete";
 
@@ -29,7 +29,7 @@ public class DeleteCommand extends Command {
 
 
     @Override
-    public CommandResult execute() throws CommandException {
+    public CommandResult executeUndoableCommand() throws CommandException {
 
         UnmodifiableObservableList<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
 
