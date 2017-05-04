c72c667dcef5ae632c90df35a54ccd2d0ab3641a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-04 11:53:53 +0800

    Add UndoableCommand and update some commands to extend it
    All commands extend Command.
    
    This disallows us from identifying which commands can be reversed.
    
    Let’s create UndoableCommand and update the relevant commands to
    extend it.
    
    Also, we implemented the Template Pattern for UndoableCommand#execute()
    which ensures that every command that extends UndoableCommand will
    automatically call saveAddressBookSnapshot() when execute() is called
    to facilitate the undo redo mechanism. Classes that inherits from
    UndoableCommand will have to override executeUndoableCommand() instead
    of execute() as a result.

---
+++ src/test/java/seedu/address/logic/commands/UndoableCommandTest.java
@@ -0,0 +1,67 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.CommandTestUtil.deleteFirstPerson;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+public class UndoableCommandTest {
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final DummyCommand dummyCommand = new DummyCommand(model);
+
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void executeUndo() throws Exception {
+        dummyCommand.execute();
+        deleteFirstPerson(expectedModel);
+        assertEquals(expectedModel, model);
+
+        showFirstPersonOnly(model);
+
+        // undo() should cause the model's filtered list to show all persons
+        dummyCommand.undo();
+        expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        assertEquals(expectedModel, model);
+    }
+
+    @Test
+    public void redo() throws Exception {
+        showFirstPersonOnly(model);
+
+        // redo() should cause the model's filtered list to show all persons
+        dummyCommand.redo();
+        deleteFirstPerson(expectedModel);
+        assertEquals(expectedModel, model);
+    }
+
+    /**
+     * Deletes the first person in the model's filtered list.
+     */
+    class DummyCommand extends UndoableCommand {
+        DummyCommand(Model model) {
+            this.model = model;
+        }
+
+        @Override
+        public CommandResult executeUndoableCommand() throws CommandException {
+            ReadOnlyPerson personToDelete = model.getFilteredPersonList().get(0);
+            try {
+                model.deletePerson(personToDelete);
+            } catch (PersonNotFoundException pnfe) {
+                fail("Impossible: personToDelete was retrieved from model.");
+            }
+            return new CommandResult("");
+        }
+    }
+}
