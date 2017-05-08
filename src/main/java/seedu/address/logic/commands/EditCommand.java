75361cd44ba051f742c0843dc9e047809a901736
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-08 13:35:43 +0800

    Add EditCommand#equals(Object)

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -97,6 +97,24 @@ public class EditCommand extends Command {
         return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags);
     }
 
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditCommand)) {
+            return false;
+        }
+
+        // state check
+        EditCommand e = (EditCommand) other;
+        return filteredPersonListIndex == e.filteredPersonListIndex
+                && editPersonDescriptor.equals(e.editPersonDescriptor);
+    }
+
     /**
      * Stores the details to edit the person with. Each non-empty field value will replace the
      * corresponding field value of the person.
