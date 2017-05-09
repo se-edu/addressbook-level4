250cb09aeb0a261a93c68b8669e4da438c1e55d8
Tan Wang Leng <tanwangleng@outlook.com>
2017-05-09 11:10:07 +0800

    Remove FxViewUtil#applyAnchorBoundaryParameters()
    FxViewUtil#applyAnchorBoundaryParameters() is not used anywhere in the
    codebase.
    
    Let's remove it.

--- src/main/java/seedu/address/commons/util/FxViewUtil.java
+++ src/main/java/seedu/address/commons/util/FxViewUtil.java
@@ -1,20 +1,11 @@
 package seedu.address.commons.util;
 
-import javafx.scene.Node;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
  * Contains utility methods for JavaFX views
  */
 public class FxViewUtil {
-
-    public static void applyAnchorBoundaryParameters(Node node, double left, double right, double top, double bottom) {
-        AnchorPane.setBottomAnchor(node, bottom);
-        AnchorPane.setLeftAnchor(node, left);
-        AnchorPane.setRightAnchor(node, right);
-        AnchorPane.setTopAnchor(node, top);
-    }
 
     /**
      * Sets the given image as the icon for the given stage.
