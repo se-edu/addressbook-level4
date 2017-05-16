bff4ddc7f3b19cf5e78afd4203c0eeacb0211a72
Tan Wang Leng <tanwangleng@outlook.com>
2017-05-16 16:55:16 +0800

    HelpWindow: Use offline version of user guide
    The help window accesses the user guide at:
    https://se-edu.github.io/addressbook-level4/docs/UserGuide.
    
    The user will not be able to access it if he has no internet connection.
    Furthermore, the user guide may not match the version that he is using.
    
    Let's change the user guide used by HelpWindow to an offline version.
    
    The offline documentation files are stored at 'src/main/resources/docs',
    so that gradle's shadowJar deployment task will automatically copy them
    to the output .jar file. It also allows HelpWindow to access them using
    a simple filepath.

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -19,8 +19,7 @@ public class HelpWindow extends UiPart<Region> {
     private static final String ICON = "/images/help_icon.png";
     private static final String FXML = "HelpWindow.fxml";
     private static final String TITLE = "Help";
-    private static final String USERGUIDE_URL =
-            "https://se-edu.github.io/addressbook-level4/docs/UserGuide.html";
+    private static final String USERGUIDE_FILE_PATH = "/docs/UserGuide.html";
 
     @FXML
     private WebView browser;
@@ -35,7 +34,8 @@ public class HelpWindow extends UiPart<Region> {
         dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
         FxViewUtil.setStageIcon(dialogStage, ICON);
 
-        browser.getEngine().load(USERGUIDE_URL);
+        String userGuideUrl = getClass().getResource(USERGUIDE_FILE_PATH).toString();
+        browser.getEngine().load(userGuideUrl);
     }
 
     public void show() {
