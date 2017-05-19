fc13a6090ff4d8acd2461d6fb12e290ad90da671
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-19 12:30:45 +0800

    Update CollectionUtil#isAnyNull to throw NullPointerException
    -copy from Wang Leng’s issue-

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -1,7 +1,7 @@
 package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.CollectionUtil.requireNonNull;
 
 import java.util.Set;
 import java.util.logging.Logger;
@@ -11,7 +11,6 @@ import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -33,7 +32,7 @@ public class ModelManager extends ComponentManager implements Model {
      */
     public ModelManager(ReadOnlyAddressBook addressBook, UserPrefs userPrefs) {
         super();
-        checkArgument(!CollectionUtil.isAnyNull(addressBook, userPrefs));
+        requireNonNull(addressBook, userPrefs);
 
         logger.fine("Initializing with address book: " + addressBook + " and user prefs " + userPrefs);
 
