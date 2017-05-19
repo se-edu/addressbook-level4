8b8fb6e641f0c1de40de18dc143f74bc0be7e84a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-19 12:30:45 +0800

    Interdiff between v8 and v9

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -25,5 +25,23 @@ public class AppUtilTest {
         AppUtil.getImage(null);
     }
 
+    @Test
+    public void checkArgument_true_nothingHappens() {
+        AppUtil.checkArgument(true);
+        AppUtil.checkArgument(true, "");
+    }
 
+    @Test
+    public void checkArgument_falseWithoutErrorMessage_throwsIllegalArgumentException() {
+        thrown.expect(IllegalArgumentException.class);
+        AppUtil.checkArgument(false);
+    }
+
+    @Test
+    public void checkArgument_falseWithErrorMessage_throwsIllegalArgumentException() {
+        String errorMessage = "error message";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage(errorMessage);
+        AppUtil.checkArgument(false,  errorMessage);
+    }
 }
