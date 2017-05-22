38d110c491f56166a048a3c75ad2173bbecb1c14
limmlingg <limmlingg@gmail.com>
2017-05-22 11:42:05 +0800

    Use predicates in ModelManager
    ModelManager#updateFilteredPersonList(Set<String>) searches for name
    using a set of keywords. To add more filtering methods, the model source
    code must be edited directly.
    
    Using Java Predicates allow us to be more flexible by allowing the
    caller to search for fields other than name and different combinations
    of fields.
    
    Let's replace ModelManager#updateFilteredPersonList(Set<String>) with
    ModelManager#updateFilteredPersonList(Predicate<ReadOnlyPerson>) to
    allow more flexible searches.

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -2,18 +2,20 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.function.Predicate;
 
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.model.person.ReadOnlyPerson;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser {
+
+    private Predicate<ReadOnlyPerson> predicate;
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -26,10 +28,32 @@ public class FindCommandParser {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        // keywords delimited by whitespace
-        final String[] keywords = trimmedArgs.split("\\s+");
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindCommand(keywordSet);
+        String[] nameKeywords = args.split("\\s+");
+
+        predicate = createEmptyPredicate();
+
+        for (String name : nameKeywords) {
+            if (!name.isEmpty()) {
+                predicate = predicate.or(nameContains(name));
+            }
+        }
+
+        return new FindCommand(predicate);
+    }
+
+    private Predicate<ReadOnlyPerson> nameContains(String name) {
+        return p -> StringUtil.containsWordIgnoreCase(p.getName().fullName, name);
+    }
+
+    /**
+     * @return a predicate that is always false
+     */
+    private Predicate<ReadOnlyPerson> createEmptyPredicate() {
+        return p -> false;
+    }
+
+    public Predicate<ReadOnlyPerson> getPredicate() {
+        return this.predicate;
     }
 
 }
