54432ff135e0452dca9a427ecef9aa9ca002e765
limmlingg <limmlingg@gmail.com>
2017-05-23 11:40:44 +0800

    Add unit tests for ParserUtil#split()

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -39,24 +41,56 @@ public class ParserUtilTest {
     public final ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void parseIndex_invalidInput_throwsIllegalValueException() throws Exception {
-        thrown.expect(IllegalValueException.class);
-        ParserUtil.parseIndex("10 a");
+    public void splitPreamble_nullPreamble_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        ParserUtil.splitPreamble(null, 0);
     }
 
     @Test
-    public void parseIndex_outOfRangeInput_throwsNumberFormatException() throws Exception {
-        thrown.expect(NumberFormatException.class);
-        ParserUtil.parseIndex("3423423423423432");
+    public void splitPreamble_negativeNumFields_throwsNegativeArraySizeException() {
+        thrown.expect(NegativeArraySizeException.class);
+        ParserUtil.splitPreamble("abc", -1);
     }
 
     @Test
-    public void parseIndex_validInput_success() throws Exception {
+    public void splitPreamble_validInput_success() {
+        // Zero numFields
+        assertPreambleListCorrect("abc", 0, optionalList());
+
+        // Empty string
+        assertPreambleListCorrect("", 1, optionalList(""));
+
         // No whitespaces
-        assertEquals(1, ParserUtil.parseIndex("1"));
+        assertPreambleListCorrect("abc", 1, optionalList("abc"));
 
-        // Leading and trailing whitespaces
-        assertEquals(1, ParserUtil.parseIndex("  1  "));
+        // Single whitespace between fields
+        assertPreambleListCorrect("abc 123", 2, optionalList("abc", "123"));
+
+        // Multiple whitespaces between fields
+        assertPreambleListCorrect("abc  \n qwe \t  123", 3, optionalList("abc", "qwe", "123"));
+
+        // More whitespaces than numFields
+        assertPreambleListCorrect("abc 123 qwe 456", 2,  optionalList("abc", "123 qwe 456"));
+
+        // More numFields than whitespaces
+        assertPreambleListCorrect("abc", 2,  optionalList("abc", null));
+    }
+
+    /**
+     * Splits {@code string} into ordered fields of size {@code numOfParts}
+     * and checks if the result is the same as {@code expectedValues}
+     */
+    private void assertPreambleListCorrect(String string, int numOfParts, List<Optional<String>> expectedValues) {
+        List<Optional<String>> list = ParserUtil.splitPreamble(string, numOfParts);
+
+        assertTrue(list.equals(expectedValues));
+    }
+
+    /**
+     * Returns {@code strings} as {@code List<Optional<String>>}
+     */
+    private List<Optional<String>> optionalList(String... strings) {
+        return Arrays.stream(strings).map(Optional::ofNullable).collect(Collectors.toList());
     }
 
     @Test
