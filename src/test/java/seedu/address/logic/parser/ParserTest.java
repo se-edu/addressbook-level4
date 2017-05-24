d0c576cdc747dce6f8ac26591fdd1492def94373
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-24 10:41:03 +0800

    Teach Parser to parse HistoryCommand
    Addressbook does not keep track of entered commands.
    
    This makes it difficult for the user to trace previously entered commands
    and also unable to correct any typos using past entered commands.
    
    Let’s teach Parser to parse HistoryCommand to allow the user to see
    what he has entered previously.

---
+++ src/test/java/seedu/address/logic/parser/ParserTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ParserTest {
+    private final Parser parser = new Parser();
+
+    @Test
+    public void parseCommand_history() throws Exception {
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
+
+        try {
+            parser.parseCommand("histories");
+            fail("The expected ParseException was not thrown.");
+        } catch (ParseException pe) {
+            assertEquals(MESSAGE_UNKNOWN_COMMAND, pe.getMessage());
+        }
+    }
+}
