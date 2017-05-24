555a6fb6733194057ad0654d7669a9ca8ddf210a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-24 10:41:27 +0800

    Update LogicManagerTest: add verifyHistory test

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
@@ -37,6 +38,7 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -459,6 +461,26 @@ public class LogicManagerTest {
                 expectedList);
     }
 
+    @Test
+    public void execute_verifyHistory_success() {
+        String validCommand = "clear";
+        try {
+            logic.execute(validCommand);
+        } catch (CommandException ce) {
+            fail("expected command was not executed successfully.");
+        }
+
+        String invalidCommand = "   adds   Bob   ";
+        try {
+            logic.execute(invalidCommand);
+            fail("expected CommandException was not thrown.");
+        } catch (CommandException ce) {
+            assertEquals(MESSAGE_UNKNOWN_COMMAND, ce.getMessage());
+        }
+
+        String expectedMessage = String.format(HistoryCommand.MESSAGE_SUCCESS, validCommand + "\n" + invalidCommand);
+        assertCommandSuccess("history", expectedMessage, model.getAddressBook(), model.getFilteredPersonList());
+    }
 
     /**
      * A utility class to generate test data.
