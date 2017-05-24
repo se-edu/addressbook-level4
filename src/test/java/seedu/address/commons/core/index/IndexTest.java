413275f9f022ee1c350c9fff276df87d84d985a8
Tan Wang Leng <tanwangleng@outlook.com>
2017-05-24 11:32:38 +0800

    Create Index class
    The usages of zero-based indices and one-based indices are mixed
    throughout the codebase. Both use int, and are only differentiated by
    different prefixes for the variable names (e.g zeroBasedIndex,
    oneBasedIndex).
    
    The index base can cause confusion and is prone to error. The
    conversion also takes place at different areas of the codebase and is
    inconsistent.
    
    Let's create an Index class, allowing methods to specify their expected
    base for the input index without explicitly converting it.

---
+++ src/test/java/seedu/address/commons/core/index/IndexTest.java
@@ -0,0 +1,93 @@
+package seedu.address.commons.core.index;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class IndexTest {
+
+    @Test
+    public void createOneBasedIndex() {
+        // invalid index
+        assertCreateOneBasedFailure(0);
+
+        // check equality using the same base
+        assertEquals(1, Index.fromOneBased(1).getOneBased());
+        assertEquals(5, Index.fromOneBased(5).getOneBased());
+
+        // convert from one-based index to zero-based index
+        assertEquals(0, Index.fromOneBased(1).getZeroBased());
+        assertEquals(4, Index.fromOneBased(5).getZeroBased());
+    }
+
+    @Test
+    public void createZeroBasedIndex() {
+        // invalid index
+        assertCreateZeroBasedFailure(-1);
+
+        // check equality using the same base
+        assertEquals(0, Index.fromZeroBased(0).getZeroBased());
+        assertEquals(5, Index.fromZeroBased(5).getZeroBased());
+
+        // convert from zero-based index to one-based index
+        assertEquals(1, Index.fromZeroBased(0).getOneBased());
+        assertEquals(6, Index.fromZeroBased(5).getOneBased());
+    }
+
+    /**
+     * Executes {@code Index#fromZeroBased(int)} with {@code invalidZeroBasedIndex}, confirms that an
+     * {@code IndexOutOfBoundsException} is thrown.
+     */
+    private void assertCreateZeroBasedFailure(int invalidZeroBasedIndex) {
+        assertCreateFailure(invalidZeroBasedIndex, true);
+    }
+
+    /**
+     * Executes {@code Index#fromOneBased(int)} with {@code invalidOneBasedIndex}, confirms that an
+     * {@code IndexOutOfBoundsException} is thrown.
+     */
+    private void assertCreateOneBasedFailure(int invalidOneBasedIndex) {
+        assertCreateFailure(invalidOneBasedIndex, false);
+    }
+
+    /**
+     * Executes either {@code Index#fromZeroBased(int)} (if it is zero based), or {@code Index#fromOneBased(int)}
+     * (if it is one based), and confirms that an {@code IndexOutOfBoundsException} is thrown.
+     */
+    private void assertCreateFailure(int invalidIndex, boolean isZeroBased) {
+        try {
+            if (isZeroBased) {
+                Index.fromZeroBased(invalidIndex);
+            } else {
+                Index.fromOneBased(invalidIndex);
+            }
+            fail("The expected IndexOutOfBoundsException was not thrown.");
+        } catch (IndexOutOfBoundsException ie) {
+            // expected behaviour
+        }
+    }
+
+    @Test
+    public void equals() {
+        final Index fifthPersonIndex = Index.fromOneBased(5);
+
+        // same values -> returns true
+        assertTrue(fifthPersonIndex.equals(Index.fromOneBased(5)));
+        assertTrue(fifthPersonIndex.equals(Index.fromZeroBased(4)));
+
+        // same object -> returns true
+        assertTrue(fifthPersonIndex.equals(fifthPersonIndex));
+
+        // null -> returns false
+        assertFalse(fifthPersonIndex.equals(null));
+
+        // different types -> returns false
+        assertFalse(fifthPersonIndex.equals(5.0f));
+
+        // different index -> returns false
+        assertFalse(fifthPersonIndex.equals(Index.fromOneBased(1)));
+    }
+}
