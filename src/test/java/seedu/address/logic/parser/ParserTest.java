75b35da7199fd01ee820be1b8b36077dba95c7ac
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-24 10:41:03 +0800

    Teach Parser to parse HistoryCommand
    User is unable to see what he has entered previously.
    
    User is unable to trace out how the address book got to the current
    state.
    
    Let’s teach Parser to parse HistoryCommand to allow the user to see
    what he has entered previously.

---
+++ src/test/java/seedu/address/logic/parser/ParserTest.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.parser;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.HistoryCommand;
+
+public class ParserTest {
+    private final Parser parser = new Parser();
+
+    @Test
+    public void parseCommand_history() {
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
+        assertFalse(parser.parseCommand("histories") instanceof HistoryCommand);
+    }
+}
