6c1caf9783b9340a822a677e8f7864cb6be5d9a1
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-05-24 10:40:54 +0800

    Create HistoryCommand
    User is unable to see what he has entered previously.
    
    User is unable to trace out how the address book got to the current
    state.
    
    As a step towards allowing the user to see what he has entered previously,
    let’s create HistoryCommand.

---
+++ src/main/java/seedu/address/logic/commands/HistoryCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Lists all the commands entered by user from the start of app launch.
+ */
+public class HistoryCommand extends Command {
+
+    public static final String COMMAND_WORD = "history";
+    public static final String MESSAGE_SUCCESS = "Entered commands (from earliest to most recent):\n%1$s";
+    public static final String MESSAGE_NO_HISTORY = "You have not yet entered any commands.";
+
+    @Override
+    public CommandResult execute() {
+        List<String> previousCommands = history.getHistory();
+
+        return previousCommands.isEmpty() ? new CommandResult(MESSAGE_NO_HISTORY)
+                : new CommandResult(String.format(MESSAGE_SUCCESS,
+                previousCommands.stream().collect(Collectors.joining("\n"))));
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
@@ -0,0 +1,53 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.History;
+import seedu.address.logic.HistoryManager;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class HistoryCommandTest {
+    private HistoryCommand historyCommand;
+    private History history;
+
+    @Before
+    public void setUp() {
+        Model model = new ModelManager();
+        history = new HistoryManager();
+        historyCommand = new HistoryCommand();
+        historyCommand.setData(model, history);
+    }
+
+    @Test
+    public void execute_nonEmptyHistory() {
+        assertCommandResult(historyCommand, HistoryCommand.MESSAGE_NO_HISTORY);
+
+        history.add(ClearCommand.COMMAND_WORD);
+        assertCommandResult(historyCommand, String.format(HistoryCommand.MESSAGE_SUCCESS, ClearCommand.COMMAND_WORD));
+
+        String randomCommandString = "randomCommand";
+        String selectString = "select 1";
+        history.add(randomCommandString);
+        history.add(selectString);
+
+        String expectedMessage = String.format(HistoryCommand.MESSAGE_SUCCESS, Arrays.stream(
+                new String[]{ClearCommand.COMMAND_WORD, randomCommandString, selectString})
+                .collect(Collectors.joining("\n")));
+
+        assertCommandResult(historyCommand, expectedMessage);
+    }
+
+    /**
+     * Asserts that the result message from the execution of {@code historyCommand} equals to {@code expectedMessage}
+     */
+    private void assertCommandResult(HistoryCommand historyCommand, String expectedMessage) {
+        assertEquals(expectedMessage, historyCommand.execute().feedbackToUser);
+    }
+}
