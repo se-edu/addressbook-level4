015b57d6ae29042e4281de6a644a0a2f950f418a
Miao Ling <limmlingg@gmail.com>
2017-05-26 15:10:08 +0800

    [#401] Create unit tests for ParserUtil#parseIndex() (#423)

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -22,15 +22,15 @@ import seedu.address.model.tag.Tag;
 public class ParserUtil {
 
     /**
-     * Returns the specified index in the {@code command}.
-     * @throws IllegalValueException if the specified index is invalid (not positive unsigned integer).
+     * Parses {@code index} into an integer and returns it. Leading and trailing whitespaces will be trimmed.
+     * @throws IllegalValueException if the specified index is invalid (not non-zero unsigned integer).
      */
-    public static int parseIndex(String command) throws IllegalValueException {
-        String index = command.trim();
-        if (!StringUtil.isUnsignedInteger(index)) {
-            throw new IllegalValueException("Index is not a positive unsigned integer.");
+    public static int parseIndex(String index) throws IllegalValueException {
+        String trimmedIndex = index.trim();
+        if (!StringUtil.isUnsignedInteger(trimmedIndex)) {
+            throw new IllegalValueException("Index is not a non-zero unsigned integer.");
         }
-        return Integer.parseInt(index);
+        return Integer.parseInt(trimmedIndex);
 
     }
 
