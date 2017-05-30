fecd75ee253b994e6143bc39da2025cf59a543a6
Paul Tan <pyokagan@gmail.com>
2017-05-30 18:40:47 +0800

    Move Tag validation step from model to parser and storage layers
    The Tag(String) constructor validates the passed in tag String,
    throwing a checked IllegalValueException error if the input fails
    validation.
    
    However, according to our architecture, the model layer should only be
    concerned with the storage of data in memory. Any data which reaches the
    model layer should have already been validated. As such, if for any
    reason the input tag String fails validation in the model layer, it is
    most likely a programmer error, and a runtime exception should thus be
    thrown instead.
    
    Fix this by moving the input validation step, and the throwing of the
    checked IllegalValueException, from the Tag constructor to the parser
    and storage layers. The Tag constructor will instead use the
    checkArgument(...) helper function to verify the "valid tag format"
    precondition.
    
    While we are here, rename the argument `name` to the more descriptive
    `tagName`.

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -12,6 +12,12 @@ public class TagTest {
     }
 
     @Test
+    public void constructor_invalidTagName_throwsIllegalArgumentException() {
+        String invalidTagName = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Tag(invalidTagName));
+    }
+
+    @Test
     public void isValidTagName() {
         // null tag name
         Assert.assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
