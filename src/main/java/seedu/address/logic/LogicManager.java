bc148505fa5f1dc6d255951fffca5a8645fdcc20
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-01 10:41:42 +0800

    Allow up and down buttons to get previous and next input respectively
    The user is only able to see his previously typed commands through
    the history command.
    
    The user may need to access their previously typed commands quickly in
    order to fix previous typo, or to reuse the input for another variation.
    
    Let's add a functionality to the up and down button key press to
    retrieve the previous and next input in the history.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -45,4 +45,9 @@ public class LogicManager extends ComponentManager implements Logic {
     public ObservableList<ReadOnlyPerson> getFilteredPersonList() {
         return model.getFilteredPersonList();
     }
+
+    @Override
+    public NonAlternatingListIterator<String> getHistoryIterator() {
+        return history.reversedListIterator();
+    }
 }
