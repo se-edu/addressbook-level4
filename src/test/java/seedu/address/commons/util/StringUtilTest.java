eed5d986bc9f88afa7c34c223863a87e9e9ce1ae
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-01 08:53:31 +0800

    StringUtil#isUnsignedInteger(String): Fix unhandled NFE
    StringUtil#isUnsignedInteger(String) attempts to convert any given
    number to an integer (as a step in checking if it is an unsigned integer).
    
    When the input is a number higher than Integer.MAX_VALUE,
    the method fails with an exception instead of returning false.
    
    Let’s teach StringUtil#isUnsignedInteger(String) to return false if
    the input number is higher than Integer.MAX_VALUE.

--- src/test/java/seedu/address/commons/util/StringUtilTest.java
+++ src/test/java/seedu/address/commons/util/StringUtilTest.java
@@ -21,9 +21,6 @@ public class StringUtilTest {
     @Test
     public void isUnsignedPositiveInteger() {
 
-        // Equivalence partition: null
-        assertFalse(StringUtil.isUnsignedInteger(null));
-
         // EP: empty strings
         assertFalse(StringUtil.isUnsignedInteger("")); // Boundary value
         assertFalse(StringUtil.isUnsignedInteger("  "));
@@ -42,6 +39,10 @@ public class StringUtilTest {
         // EP: numbers with white space
         assertFalse(StringUtil.isUnsignedInteger(" 10 ")); // Leading/trailing spaces
         assertFalse(StringUtil.isUnsignedInteger("1 0"));  // Spaces in the middle
+
+        // EP: number larger than Integer.MAX_VALUE
+        Long value = new Long(Integer.MAX_VALUE + 1);
+        assertFalse(StringUtil.isUnsignedInteger(value.toString()));
 
         // EP: valid numbers, should return true
         assertTrue(StringUtil.isUnsignedInteger("1")); // Boundary value
