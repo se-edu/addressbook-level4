fea5ab8f95a1346998b856301f115237735d0762
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-01 08:53:31 +0800

    Interdiff between v6 and v7

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.commons.util;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
