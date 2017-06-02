65ebc4d3dc059a375d35566e86b60a6eb6b03e7a
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-02 12:10:15 +0800

    ErrorDialogGuiTest: Implement smart wait for showErrorDialogs()
    ErrorDialogGuiTest#showErrorDialogs() posts a 'DataSavingExceptionEvent',
    which is caught by the GUI, which launches an Alert dialog box. This is
    not handled immediately, and requires a delay.
    
    The current delay is fixed for 1000ms. Furthermore, it does not check
    that the Alert dialog box is actually opened after 1000ms has passed.
    
    Let's add guiRobot.waitForEvent() to add increment wait, and also ensure
    that the Alert dialog box is actually opened before proceeding. It will
    also fail if our wait becomes too long.

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -11,11 +11,18 @@ import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 
 public class ErrorDialogGuiTest extends AddressBookGuiTest {
 
+    private final String errorDialogStageTitle = "File Op Error";
+
     @Test
     public void showErrorDialogs() throws InterruptedException {
-        //Test DataSavingExceptionEvent dialog
+        GuiRobot guiRobot = new GuiRobot();
+
         raise(new DataSavingExceptionEvent(new IOException("Stub")));
-        AlertDialogHandle alertDialog = mainGui.getAlertDialog("File Op Error");
+
+        int eventWaitTimeout = 5000;
+        guiRobot.waitForEvent(() -> guiRobot.isWindowActive(errorDialogStageTitle), eventWaitTimeout);
+
+        AlertDialogHandle alertDialog = mainGui.getAlertDialog(errorDialogStageTitle);
         assertTrue(alertDialog.isMatching("Could not save data", "Could not save data to file" + ":\n"
                                                                          + "java.io.IOException: Stub"));
 
