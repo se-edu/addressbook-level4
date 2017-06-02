745c7b5b4dac3b8afe51958856cd012d7a0b7455
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-02 09:43:00 +0800

    GuiRobot: Add pauseForHuman(int)
    Most GUI tests uses GuiRobot#sleep(int) to prevent the tests from
    executing too fast, preventing us from examining the effects of the
    test.
    
    GuiRobot#sleep(int) should only be used when there's an actual delay
    in the operation of the application.
    
    Let's add GuiRobot#pauseForHuman(int) to make the usage of pausing the
    test execution clearer, and also to allow headless mode to disable these
    unnecessary pauses.

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -7,4 +7,24 @@ import org.testfx.api.FxRobot;
  * Extends {@link FxRobot} by adding some customized functionality and workarounds.
  */
 public class GuiRobot extends FxRobot {
+
+    public static final String PROPERTY_GUITESTS_HEADLESS = "guitests.headless";
+
+    private boolean isHeadlessMode = false;
+
+    public GuiRobot() {
+        String headlessPropertyValue = System.getProperty(PROPERTY_GUITESTS_HEADLESS);
+        isHeadlessMode = (headlessPropertyValue != null && headlessPropertyValue.equals("true"));
+    }
+
+    /**
+     * Allows GUI tests to pause its execution for a human to examine the effects of the test.
+     * This method should be disabled when a non-human is executing the GUI test to avoid
+     * unnecessary delay.
+     */
+    public void pauseForHuman(int duration) {
+        if (!isHeadlessMode) {
+            sleep(duration);
+        }
+    }
 }
