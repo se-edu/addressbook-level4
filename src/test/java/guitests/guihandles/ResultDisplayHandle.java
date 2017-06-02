ba4e09dc918c0f3094ea123515a1e04ba3d11dab
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-02 15:17:17 +0800

    GuiHandle: Remove primaryStage constructor parameter
    The primaryStage variable is not used anywhere.
    
    Let's remove it.

--- src/test/java/guitests/guihandles/ResultDisplayHandle.java
+++ src/test/java/guitests/guihandles/ResultDisplayHandle.java
@@ -1,7 +1,6 @@
 package guitests.guihandles;
 
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
 import seedu.address.TestApp;
 
 /**
@@ -11,8 +10,8 @@ public class ResultDisplayHandle extends GuiHandle {
 
     public static final String RESULT_DISPLAY_ID = "#resultDisplay";
 
-    public ResultDisplayHandle(Stage primaryStage) {
-        super(primaryStage, TestApp.APP_TITLE);
+    public ResultDisplayHandle() {
+        super(TestApp.APP_TITLE);
     }
 
     /**
