196bdf888da3524e3322e3849b93f880b7fd0dd5
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-02 09:18:59 +0800

    GuiRobot: Remove push(KeyCodeCombination)
    GuiRobot#push(KeyCodeCombination) is not used in the codebase.
    
    Let's remove it.
    
    TestUtil.scrub(KeyCodeCombination) is also no longer used as a result.
    
    Let's remove it too.

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -2,17 +2,9 @@ package guitests;
 
 import org.testfx.api.FxRobot;
 
-import javafx.scene.input.KeyCodeCombination;
-import seedu.address.testutil.TestUtil;
-
 /**
  * Robot used to simulate user actions on the GUI.
  * Extends {@link FxRobot} by adding some customized functionality and workarounds.
  */
 public class GuiRobot extends FxRobot {
-
-    public GuiRobot push(KeyCodeCombination keyCodeCombination) {
-        return (GuiRobot) super.push(TestUtil.scrub(keyCodeCombination));
-    }
-
 }
