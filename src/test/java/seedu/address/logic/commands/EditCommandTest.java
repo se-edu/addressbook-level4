d3d3279d061677d0822ae51fd05fca8fb98829d4
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-05 09:50:28 +0800

    EditCommandTest: Move assertCommandFailure(...) to CommandTestUtil
    EditCommandTest#assertCommandFailure(EditCommand, String) can be
    used to test commands other than EditCommand.
    
    Let's extract EditCommandTest#assertCommandFailure(EditCommand, String)
    out to CommandTestUtil, and make the command parameter more generic so
    that it can be used by any type of commands.

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -3,14 +3,12 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static seedu.address.testutil.EditCommandTestUtil.DESC_AMY;
 import static seedu.address.testutil.EditCommandTestUtil.DESC_BOB;
 import static seedu.address.testutil.EditCommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.INDEX_SECOND_PERSON;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -62,7 +60,9 @@ public class EditCommandTest {
         Person firstPerson = new Person(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()));
         EditPersonDescriptor descriptor = createEditPersonDescriptor(firstPerson);
         EditCommand editCommand = prepareCommand(INDEX_SECOND_PERSON, descriptor);
-        assertCommandFailure(editCommand, EditCommand.MESSAGE_DUPLICATE_PERSON);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        CommandTestUtil.assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON, expectedModel);
     }
 
     @Test
@@ -70,7 +70,10 @@ public class EditCommandTest {
         Index outOfBoundIndex = Index.fromZeroBased(model.getFilteredPersonList().size());
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
         EditCommand editCommand = prepareCommand(outOfBoundIndex, descriptor);
-        assertCommandFailure(editCommand, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        CommandTestUtil.assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX,
+                expectedModel);
     }
 
     @Test
@@ -134,25 +137,5 @@ public class EditCommandTest {
         assertEquals(expectedMessage, result.feedbackToUser);
         assertEquals(expectedAddressBook, model.getAddressBook());
         assertEquals(expectedFilteredList, model.getFilteredPersonList());
-    }
-
-    /**
-     * Executes the given {@code editCommand}, confirms that <br>
-     * - a {@code CommandException} is thrown <br>
-     * - the CommandException message matches {@code expectedMessage} <br>
-     * - the address book in the model remains unchanged <br>
-     * - the filtered person list in the model remains unchanged
-     */
-    private void assertCommandFailure(EditCommand editCommand, String expectedMessage) {
-        AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
-        List<ReadOnlyPerson> expectedFilteredList = new ArrayList<>(model.getFilteredPersonList());
-        try {
-            editCommand.execute();
-            fail("The expected CommandException was not thrown.");
-        } catch (CommandException e) {
-            assertEquals(expectedMessage, e.getMessage());
-            assertEquals(expectedAddressBook, model.getAddressBook());
-            assertEquals(expectedFilteredList, model.getFilteredPersonList());
-        }
     }
 }
