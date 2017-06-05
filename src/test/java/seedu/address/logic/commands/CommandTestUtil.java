33817ef99332c25f7e6d4109a2d30847fc644081
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-05 09:50:28 +0800

    EditCommandTest: Move assertCommandFailure(...) to CommandTestUtil
    EditCommandTest#assertCommandFailure(EditCommand, String) can be
    used to test commands other than EditCommand.
    
    Let's extract EditCommandTest#assertCommandFailure(EditCommand, String)
    out to CommandTestUtil, and make the command parameter more generic so
    that it can be used by any type of commands.

---
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Contains helper methods for testing commands.
+ */
+public class CommandTestUtil {
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - a {@code CommandException} is thrown <br>
+     * - the CommandException message matches {@code expectedMessage} <br>
+     * - the address book and the filtered person list in the {@code actualModel} matches that of {@code expectedModel}
+     */
+    public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage,
+            Model expectedModel) {
+        try {
+            command.execute();
+            fail("The expected CommandException was not thrown.");
+        } catch (CommandException e) {
+            assertEquals(expectedMessage, e.getMessage());
+            assertEquals(expectedModel, actualModel);
+        }
+    }
+}
