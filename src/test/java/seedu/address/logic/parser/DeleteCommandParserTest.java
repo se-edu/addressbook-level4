8731b0ffc706725224188f443783da8505d31080
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-05 14:01:33 +0800

    Interdiff between v2 and v3

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,16 +1,25 @@
 package seedu.address.logic.parser;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * As we are only doing white-box testing, we are not running the tests using complicated arguments
+ * such as {@code parser.parse("1 abc 2")} as they should have been covered by the tests for
+ * {@code ParserUtil#parseIndex(String)}.
+ */
 public class DeleteCommandParserTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private DeleteCommandParser parser = new DeleteCommandParser();
 
@@ -18,22 +27,13 @@ public class DeleteCommandParserTest {
     public void parse_validArgs_returnsDeleteCommand() throws Exception {
         DeleteCommand command = parser.parse("1");
         assertEquals(INDEX_FIRST_PERSON, command.targetIndex);
-        command = parser.parse("  1 ");
-        assertEquals(INDEX_FIRST_PERSON, command.targetIndex);
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() throws Exception {
-        assertParseFailure("a");
-        assertParseFailure("1 abc");
-    }
+        thrown.expect(ParseException.class);
+        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
 
-    private void assertParseFailure(String args) {
-        try {
-            parser.parse(args);
-            fail("The expected ParseException was not thrown.");
-        } catch (ParseException pe) {
-            assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe.getMessage());
-        }
+        parser.parse("a");
     }
 }
