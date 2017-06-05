a57b693970dea480954af7fa59844e6a515553ef
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-05 14:01:33 +0800

    Interdiff between v3 and v4

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -12,9 +12,11 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * As we are only doing white-box testing, we are not running the tests using complicated arguments
- * such as {@code parser.parse("1 abc 2")} as they should have been covered by the tests for
- * {@code ParserUtil#parseIndex(String)}.
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
  */
 public class DeleteCommandParserTest {
 
