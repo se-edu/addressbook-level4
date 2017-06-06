289b244a47c15d470ce3afca9138341116bcfb62
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-06 13:39:40 +0800

    Add tests for HelpCommand

---
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.eventbus.Subscribe;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+
+public class HelpCommandTest {
+    private boolean isEventCaught = false;
+
+    @Subscribe
+    private void handleShowHelpRequestEvent(ShowHelpRequestEvent shre) {
+        isEventCaught = true;
+    }
+
+    @Before
+    public void setUp() {
+        EventsCenter.getInstance().registerHandler(this);
+    }
+
+    @Test
+    public void execute_help_success() {
+        CommandResult result = new HelpCommand().execute();
+        assertEquals(SHOWING_HELP_MESSAGE, result.feedbackToUser);
+        assertTrue(isEventCaught);
+    }
+}
