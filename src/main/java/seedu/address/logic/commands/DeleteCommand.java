652b4fd39df5dc47f1f746974182a7eb7e1c3fb9
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-06 16:04:54 +0800

    Extract exceptions in UniquePersonList
    Model API refers to UniquePersonList.DuplicatePersonException and
    UniquePersonList.PersonNotFoundException.
    
    UniquePersonList is a class inside the model component which need not
    be exposed to classes outside the Model component.
    
    Let's extract these two exception as standalone classes so that the
    Model API no longer exposes the UniquePersonList class.

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -5,7 +5,7 @@ import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.UniquePersonList.PersonNotFoundException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  * Deletes a person identified using it's last displayed index from the address book.
