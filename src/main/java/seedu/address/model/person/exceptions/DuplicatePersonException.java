bb969b4ad5fe3a9f0076fee48af794aa5cf7fadc
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-06 16:04:54 +0800

    Interdiff between v2 and v3

--- src/main/java/seedu/address/model/person/exceptions/DuplicatePersonException.java
+++ src/main/java/seedu/address/model/person/exceptions/DuplicatePersonException.java
@@ -3,7 +3,7 @@ package seedu.address.model.person.exceptions;
 import seedu.address.commons.exceptions.DuplicateDataException;
 
 /**
- * Signals that the operation will result in 2 or more people having the same particulars.
+ * Signals that the operation will result in duplicate Person objects.
  */
 public class DuplicatePersonException extends DuplicateDataException {
     public DuplicatePersonException() {
