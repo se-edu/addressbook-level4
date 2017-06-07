857090e443120e18c45deb59c15089c64cce11d5
Zhi Yuan <yongzhiyuan@u.nus.edu>
2017-06-07 12:48:50 +0800

    Remove IndexUtil#oneToZeroIndex(int) (#463)
    The creation of Index class in #447 makes IndexUtil#oneToZeroIndex(int)
    obsolete.
    
    Let’s standardise the codebase to use Index instead.

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -2,10 +2,11 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
+import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
 import org.junit.Test;
 
-import seedu.address.commons.util.IndexUtil;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TestUtil;
@@ -17,17 +18,17 @@ public class DeleteCommandTest extends AddressBookGuiTest {
 
         //delete the first in the list
         Person[] currentList = td.getTypicalPersons();
-        int targetIndex = 1;
+        Index targetIndex = INDEX_FIRST_PERSON;
         assertDeleteSuccess(targetIndex, currentList);
 
         //delete the last in the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-        targetIndex = currentList.length;
+        targetIndex = Index.fromOneBased(currentList.length);
         assertDeleteSuccess(targetIndex, currentList);
 
         //delete from the middle of the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-        targetIndex = currentList.length / 2;
+        targetIndex = Index.fromOneBased(currentList.length / 2);
         assertDeleteSuccess(targetIndex, currentList);
 
         //invalid index
@@ -37,15 +38,14 @@ public class DeleteCommandTest extends AddressBookGuiTest {
     }
 
     /**
-     * Runs the delete command to delete the person at specified index and confirms the result is correct.
-     * @param targetIndexOneIndexed e.g. index 1 to delete the first person in the list,
+     * Runs the delete command to delete the person at {@code index} and confirms the result is correct.
      * @param currentList A copy of the current list of persons (before deletion).
      */
-    private void assertDeleteSuccess(int targetIndexOneIndexed, final Person[] currentList) {
-        Person personToDelete = currentList[IndexUtil.oneToZeroIndex(targetIndexOneIndexed)];
-        Person[] expectedRemainder = TestUtil.removePersonFromList(currentList, targetIndexOneIndexed);
+    private void assertDeleteSuccess(Index index, final Person[] currentList) {
+        Person personToDelete = currentList[index.getZeroBased()];
+        Person[] expectedRemainder = TestUtil.removePersonFromList(currentList, index);
 
-        commandBox.runCommand(DeleteCommand.COMMAND_WORD + " " + targetIndexOneIndexed);
+        commandBox.runCommand(DeleteCommand.COMMAND_WORD + " " + index.getOneBased());
 
         //confirm the list now contains all previous persons except the deleted person
         assertTrue(personListPanel.isListMatching(expectedRemainder));
--- src/test/java/guitests/SelectCommandTest.java
+++ src/test/java/guitests/SelectCommandTest.java
@@ -1,10 +1,11 @@
 package guitests;
 
 import static org.junit.Assert.assertEquals;
+import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
 import org.junit.Test;
 
-import seedu.address.commons.util.IndexUtil;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -15,16 +16,16 @@ public class SelectCommandTest extends AddressBookGuiTest {
     @Test
     public void selectPerson_nonEmptyList() {
 
-        assertSelectionInvalid(10); // invalid index
+        assertSelectionInvalid(Index.fromOneBased(10)); // invalid index
         assertNoPersonSelected();
 
-        assertSelectionSuccess(1); // first person in the list
-        int personCount = td.getTypicalPersons().length;
+        assertSelectionSuccess(INDEX_FIRST_PERSON); // first person in the list
+        Index personCount = Index.fromOneBased(td.getTypicalPersons().length);
         assertSelectionSuccess(personCount); // last person in the list
-        int middleIndex = personCount / 2;
+        Index middleIndex = Index.fromOneBased(personCount.getOneBased() / 2);
         assertSelectionSuccess(middleIndex); // a person in the middle of the list
 
-        assertSelectionInvalid(personCount + 1); // invalid index
+        assertSelectionInvalid(Index.fromOneBased(personCount.getOneBased() + 1)); // invalid index
         assertPersonSelected(middleIndex); // assert previous selection remains
 
         /* Testing other invalid indexes such as -1 should be done when testing the SelectCommand */
@@ -34,24 +35,24 @@ public class SelectCommandTest extends AddressBookGuiTest {
     public void selectPerson_emptyList() {
         commandBox.runCommand(ClearCommand.COMMAND_WORD);
         assertListSize(0);
-        assertSelectionInvalid(1); //invalid index
+        assertSelectionInvalid(INDEX_FIRST_PERSON); //invalid index
     }
 
-    private void assertSelectionInvalid(int index) {
-        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index);
+    private void assertSelectionInvalid(Index index) {
+        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
         assertResultMessage("The person index provided is invalid");
     }
 
-    private void assertSelectionSuccess(int index) {
-        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index);
-        assertResultMessage("Selected Person: " + index);
+    private void assertSelectionSuccess(Index index) {
+        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
+        assertResultMessage("Selected Person: " + index.getOneBased());
         assertPersonSelected(index);
     }
 
-    private void assertPersonSelected(int index) {
+    private void assertPersonSelected(Index index) {
         assertEquals(personListPanel.getSelectedPersons().size(), 1);
         ReadOnlyPerson selectedPerson = personListPanel.getSelectedPersons().get(0);
-        assertEquals(personListPanel.getPerson(IndexUtil.oneToZeroIndex(index)), selectedPerson);
+        assertEquals(personListPanel.getPerson(index.getZeroBased()), selectedPerson);
         //TODO: confirm the correct page is loaded in the Browser Panel
     }
 
