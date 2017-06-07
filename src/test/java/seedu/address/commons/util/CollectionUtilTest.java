d0c14e3d029c79c55ba4ff1a9aa3184dd9a265a9
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-07 14:55:44 +0800

    Change EditPersonDescriptor’s parameters to be non-Optional
    EditPersonDescriptor’s parameters are of type Optional.
    
    Optional should not be used as parameters. The intended use case is
    for return types to indicate that there is no result.
    
    Let’s update EditPersonDescriptor’s parameters to be non-Optional.

--- src/test/java/seedu/address/commons/util/CollectionUtilTest.java
+++ src/test/java/seedu/address/commons/util/CollectionUtilTest.java
@@ -74,6 +74,14 @@ public class CollectionUtilTest {
     }
 
     @Test
+    public void isAnyNonNull() {
+        assertFalse(CollectionUtil.isAnyNonNull());
+        assertFalse(CollectionUtil.isAnyNonNull((Object) null));
+        assertTrue(CollectionUtil.isAnyNonNull(new Object()));
+        assertTrue(CollectionUtil.isAnyNonNull(new Object(), null));
+    }
+
+    @Test
     public void elementsAreUnique() throws Exception {
         // empty list
         assertAreUnique();
