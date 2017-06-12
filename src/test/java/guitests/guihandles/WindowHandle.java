4e3f77d8665fe36ba1db13957d5949b3163337a8
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-12 14:17:28 +0800

    [squash] Improve WindowHandle

--- src/test/java/guitests/guihandles/WindowHandle.java
+++ src/test/java/guitests/guihandles/WindowHandle.java
@@ -1,5 +1,6 @@
 package guitests.guihandles;
 
+import static guitests.GuiRobotUtil.MEDIUM_WAIT;
 import static java.util.Objects.requireNonNull;
 
 import java.util.logging.Logger;
@@ -14,7 +15,7 @@ import seedu.address.commons.core.LogsCenter;
  * Provides access to a window in a JavaFx application for GUI testing purposes.
  */
 public abstract class WindowHandle {
-    protected final GuiRobot guiRobot = new GuiRobot();
+    protected static final GuiRobot GUI_ROBOT = GuiRobot.getInstance();
     protected final Logger logger = LogsCenter.getLogger(this.getClass());
 
     private Window window;
@@ -24,20 +25,20 @@ public abstract class WindowHandle {
     }
 
     public void closeWindow() {
-        guiRobot.targetWindow(window);
-        guiRobot.interact(() -> ((Stage) window).close());
-        guiRobot.pauseForHuman(500);
+        GUI_ROBOT.targetWindow(window);
+        GUI_ROBOT.interact(() -> ((Stage) window).close());
+        GUI_ROBOT.pauseForHuman(MEDIUM_WAIT);
     }
 
     public void focusOnWindow() {
         String windowTitle = ((Stage) window).getTitle();
         logger.info("Focusing on" + windowTitle);
-        guiRobot.targetWindow(window);
-        guiRobot.interact(() -> window.requestFocus());
+        GUI_ROBOT.targetWindow(window);
+        GUI_ROBOT.interact(() -> window.requestFocus());
         logger.info("Finishing focus on" + windowTitle);
     }
 
-    protected <T extends Node> T getNode(String query) {
-        return guiRobot.from(window.getScene().getRoot()).lookup(query).query();
+    protected <T extends Node> T getChildNode(String query) {
+        return GUI_ROBOT.from(window.getScene().getRoot()).lookup(query).query();
     }
 }
