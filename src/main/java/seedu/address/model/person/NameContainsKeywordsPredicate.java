dbf6c85d2c7800ba566d2e512df825d7a39c3831
limmlingg <limmlingg@gmail.com>
2017-06-12 16:07:58 +0800

    Use predicates in ModelManager
    ModelManager#updateFilteredPersonList(Set<String>) searches for name
    using a set of keywords. To add more filtering methods, the model source
    code must be edited directly.
    
    Using Java Predicates allow us to be more flexible by allowing the
    caller to search for fields other than name and different combinations
    of fields.
    
    Let's replace ModelManager#updateFilteredPersonList(Set<String>) with
    ModelManager#updateFilteredPersonList(Predicate<ReadOnlyPerson>) to
    allow more flexible searches.

---
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -0,0 +1,31 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ */
+public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+    private final List<String> keywords;
+
+    public NameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
