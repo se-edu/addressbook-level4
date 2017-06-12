86daf28ec6e147bd1a4cedbba95c185ace60a321
limmlingg <limmlingg@gmail.com>
2017-06-12 16:07:58 +0800

    Use predicates in ModelManager
    ModelManager#updateFilteredPersonList(Set<String>) searches for name
    using a set of keywords. To add more filtering methods, the model source
    code must be edited directly.
    
    Using Java Predicates allow us to be more flexible by allowing the
    caller to search for fields other than name and different combinations
    of fields.
    
    Let's replace ModelManager#updateFilteredPersonList(Set<String>) with
    ModelManager#updateFilteredPersonList(Predicate<ReadOnlyPerson>) to
    allow more flexible searches.

---
+++ src/main/java/seedu/address/commons/util/PredicateUtil.java
@@ -0,0 +1,22 @@
+package seedu.address.commons.util;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Utility methods related to creating predicates
+ */
+public class PredicateUtil {
+
+    /**
+     * Creates a {@code Predicate} using {@code constructor} with parameters {@code keywords}
+     */
+    public static Predicate<ReadOnlyPerson> createPredicate(String[] keywords,
+                                            Function<String, ? extends Predicate<ReadOnlyPerson>> constructor) {
+        return Arrays.stream(keywords).map(constructor).map(predicate -> (Predicate<ReadOnlyPerson>) predicate)
+                .reduce(Predicate::or).get();
+    }
+}
