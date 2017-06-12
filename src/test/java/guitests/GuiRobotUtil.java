fdb8b338e1b288098732b1913f5ef5f6b8a302e2
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-12 14:53:18 +0800

    GuiRobot#pauseForHuman(int): Use constants instead of magic numbers
    The tests use magic numbers when calling GuiRobot#pauseForHuman(int).
    
    Let's use constants to represent the waiting time instead.

---
+++ src/test/java/guitests/GuiRobotUtil.java
@@ -0,0 +1,10 @@
+package guitests;
+
+/**
+ * Constants used by {@code GuiRobot} and classes that use GuiRobot.
+ */
+public class GuiRobotUtil {
+    public static final int SHORT_WAIT = 200;
+    public static final int MEDIUM_WAIT = 500;
+    public static final int LONG_WAIT = 1000;
+}
