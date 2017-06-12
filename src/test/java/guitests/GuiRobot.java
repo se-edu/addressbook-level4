c00143c400a56e9b62445397e2e571e40cf461d6
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-12 16:08:02 +0800

    Change GuiRobot to be a singleton
    Each test creates its own GuiRobot when using it for testng.
    
    Only a single instance of GuiRobot is required across the entire testing
    codebase.
    
    Let's change GuiRobot to be a singleton.

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -5,6 +5,23 @@ import org.testfx.api.FxRobot;
 /**
  * Robot used to simulate user actions on the GUI.
  * Extends {@link FxRobot} by adding some customized functionality and workarounds.
+ *
+ * {@code GuiRobot} is designed to be a singleton as some functionalities in
+ * {@code FxRobot} also acts like a singleton, such as the {@code windowFinder}
+ * and {@code nodeFinder} of {@code FxRobot}.
  */
 public class GuiRobot extends FxRobot {
+
+    private static GuiRobot instance;
+
+    private GuiRobot() {
+        // to prevent instances from being created
+    }
+
+    public static GuiRobot getInstance() {
+        if (instance == null) {
+            instance = new GuiRobot();
+        }
+        return instance;
+    }
 }
