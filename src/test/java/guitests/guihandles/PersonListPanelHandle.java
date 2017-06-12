2e7158a09773c58c70c1abe97df034e50bd1f12a
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-12 21:44:42 +0800

    PersonListPanelHandle#navigateToPerson(...): Use Person instead of index
    PersonListPanelHandle#navigateToPerson(...) obtains the index of the
    targeted person to scroll to.
    
    We can use the Person object directly instead of obtaining an index.
    
    Let's change PersonListPanelHandle#navigateToPerson(...) to use a Person
    instead of an index. Since the Person may not be in the list, let's
    throw a PersonNotFoundException if the person cannot be found in the list.

--- src/test/java/guitests/guihandles/PersonListPanelHandle.java
+++ src/test/java/guitests/guihandles/PersonListPanelHandle.java
@@ -11,6 +11,7 @@ import javafx.scene.control.ListView;
 import seedu.address.TestApp;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.testutil.TestUtil;
 
 /**
@@ -90,7 +91,7 @@ public class PersonListPanelHandle extends GuiHandle {
         guiRobot.clickOn(point.getX(), point.getY());
     }
 
-    public PersonCardHandle navigateToPerson(String name) {
+    public PersonCardHandle navigateToPerson(String name) throws PersonNotFoundException {
         guiRobot.pauseForHuman();
         final Optional<ReadOnlyPerson> person = getListView().getItems().stream()
                                                     .filter(p -> p.getName().fullName.equals(name))
@@ -105,13 +106,15 @@ public class PersonListPanelHandle extends GuiHandle {
     /**
      * Navigates the listview to display and select the person.
      */
-    private PersonCardHandle navigateToPerson(ReadOnlyPerson person) {
-        int index = getPersonIndex(person);
+    private PersonCardHandle navigateToPerson(ReadOnlyPerson person) throws PersonNotFoundException {
+        if (!getListView().getItems().contains(person)) {
+            throw new PersonNotFoundException();
+        }
 
         guiRobot.interact(() -> {
-            getListView().scrollTo(index);
+            getListView().scrollTo(person);
             guiRobot.pauseForHuman();
-            getListView().getSelectionModel().select(index);
+            getListView().getSelectionModel().select(person);
         });
         guiRobot.pauseForHuman();
         return getPersonCardHandle(person);
