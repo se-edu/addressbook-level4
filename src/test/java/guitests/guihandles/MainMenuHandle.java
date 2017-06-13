3e6fbabbb07a3379a61020e2f041126800834d4e
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:58:06 +0800

    Port MainMenuHandle
    MainMenuHandle uses GuiHandle.
    
    As MainMenuHandle is mainly concerned with manipulating the MainMenu bar
    in MainWindow, it should inherit from NodeHandle instead.
    
    Let's port MainMenuHandle to use NodeHandle.

--- src/test/java/guitests/guihandles/MainMenuHandle.java
+++ src/test/java/guitests/guihandles/MainMenuHandle.java
@@ -2,35 +2,30 @@ package guitests.guihandles;
 
 import java.util.Arrays;
 
-import guitests.GuiRobot;
 import javafx.scene.input.KeyCode;
-import javafx.stage.Stage;
-import seedu.address.TestApp;
 
 /**
  * Provides a handle to the main menu of the app.
  */
-public class MainMenuHandle extends GuiHandle {
-    public MainMenuHandle(GuiRobot guiRobot, Stage primaryStage) {
-        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+public class MainMenuHandle extends NodeHandle {
+
+    private static final String MENU_BAR_ID = "#menuBar";
+
+    public MainMenuHandle(MainWindowHandle mainWindowHandle) {
+        super(mainWindowHandle.getChildNode(MENU_BAR_ID));
     }
 
-    public GuiHandle clickOn(String... menuText) {
-        Arrays.stream(menuText).forEach((menuItem) -> guiRobot.clickOn(menuItem));
-        return this;
-    }
-
-    public HelpWindowHandle openHelpWindowUsingMenu() {
-        clickOn("Help", "F1");
-        return new HelpWindowHandle();
+    public void openHelpWindowUsingMenu() {
+        clickOnMenuItemsSequentially("Help", "F1");
     }
 
     public void openHelpWindowUsingAccelerator() {
-        useF1Accelerator();
+        GUI_ROBOT.push(KeyCode.F1);
+        GUI_ROBOT.pauseForHuman();
     }
 
-    private void useF1Accelerator() {
-        guiRobot.push(KeyCode.F1);
-        guiRobot.pauseForHuman();
+    public void clickOnMenuItemsSequentially(String... menuItems) {
+        Arrays.stream(menuItems).forEach(GUI_ROBOT::clickOn);
     }
+
 }
