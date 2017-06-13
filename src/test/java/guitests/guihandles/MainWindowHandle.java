3714d0f733f2a3a1a4e4881e8ff3cad5984a66e0
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:54:25 +0800

    Port CommandBoxHandle to NodeHandle
    CommandBoxHandle inherits from GuiHandle.
    
    As CommandBoxHandle is mainly concerned with manipulating the
    CommandBox, it should inherit from NodeHandle.
    
    Let's port CommandBoxHandle to use NodeHandle.

--- src/test/java/guitests/guihandles/MainWindowHandle.java
+++ src/test/java/guitests/guihandles/MainWindowHandle.java
@@ -1,7 +1,6 @@
 package guitests.guihandles;
 
 import javafx.stage.Stage;
-import seedu.address.TestApp;
 
 /**
  * Provides a handle for {@code MainWindow}.
@@ -20,7 +19,7 @@ public class MainWindowHandle extends StageHandle {
 
         personListPanel = new PersonListPanelHandle();
         resultDisplay = new ResultDisplayHandle();
-        commandBox = new CommandBoxHandle(TestApp.APP_TITLE);
+        commandBox = new CommandBoxHandle(getChildNode(CommandBoxHandle.COMMAND_INPUT_FIELD_ID));
         statusBarFooter = new StatusBarFooterHandle();
         mainMenu = new MainMenuHandle();
         browserPanel = new BrowserPanelHandle(getChildNode(BrowserPanelHandle.BROWSER_ID));
