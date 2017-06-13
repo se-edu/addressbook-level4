45adf9c504ee6ef6f9e11b7054f24ea4f72462dc
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:50:28 +0800

    Port AlertDialogHandle to StageHandle
    AlertDialogHandle inherits from GuiHandle.
    
    As AlertDialogHandle is mainly concerned with manipulation of the
    AlertDialog, it should inherit from StageHandle instead.
    
    Let's port AlertDialogHandle to use StageHandle.
    
    AlertDialogHandle#isMatching(String, String) no longers check that the
    AlertDialog window is present as the handle can only be created after
    the window is present.

--- src/test/java/guitests/guihandles/AlertDialogHandle.java
+++ src/test/java/guitests/guihandles/AlertDialogHandle.java
@@ -1,32 +1,26 @@
 package guitests.guihandles;
 
 import javafx.scene.control.DialogPane;
+import javafx.stage.Stage;
 import seedu.address.ui.UiManager;
 
 /**
- * A handle for the AlertDialog of the UI
+ * A handle for the {@code AlertDialog} of the UI.
  */
-public class AlertDialogHandle extends GuiHandle {
+public class AlertDialogHandle extends StageHandle {
 
-    public AlertDialogHandle(String dialogTitle) {
-        super(dialogTitle);
-    }
-
-    private DialogPane getDialogPane() {
-        return getNode("#" + UiManager.ALERT_DIALOG_PANE_FIELD_ID);
+    public AlertDialogHandle(Stage stage) {
+        super(stage);
     }
 
     /**
-     * Returns the text of the header in the {@code AlertDialog}.
+     * Checks that the content of the {@code AlertDialog} matches that of {@code headerMessage} and
+     * {@code contentMessage}.
      */
-    public String getHeaderText() {
-        return getDialogPane().getHeaderText();
-    }
-
-    /**
-     * Returns the text of the content in the {@code AlertDialog}.
-     */
-    public String getContentText() {
-        return getDialogPane().getContentText();
+    public boolean isMatching(String headerMessage, String contentMessage) {
+        DialogPane dialogPane = getChildNode("#" + UiManager.ALERT_DIALOG_PANE_FIELD_ID);
+        boolean isMatching = dialogPane.getHeaderText().equals(headerMessage)
+                && dialogPane.getContentText().equals(contentMessage);
+        return isMatching;
     }
 }
