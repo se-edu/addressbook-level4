098c1a10633bed712f73acebcaa91aa6a5e56730
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 10:31:32 +0800

    AddressBookGuiTest: Remove component handles
    AddressBookGuiTest manually obtains the handles of each component from
    MainGuiHandle and stores them in its own variables.
    
    This breaks the encapsulation as MainGuiHandle should be the owner of
    the handles.
    
    Let's add getter methods to AddressBookGuiTest which will obtain the
    handles from MainGuiHandle, rather than assigning the handles into
    our own variables.

--- src/test/java/guitests/SelectCommandTest.java
+++ src/test/java/guitests/SelectCommandTest.java
@@ -34,30 +34,31 @@ public class SelectCommandTest extends AddressBookGuiTest {
 
     @Test
     public void selectPerson_emptyList() {
-        commandBox.runCommand(ClearCommand.COMMAND_WORD);
+        getCommandBox().runCommand(ClearCommand.COMMAND_WORD);
         assertListSize(0);
         assertSelectionInvalid(INDEX_FIRST_PERSON); //invalid index
     }
 
     private void assertSelectionInvalid(Index index) {
-        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
+        getCommandBox().runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
         assertResultMessage("The person index provided is invalid");
     }
 
     private void assertSelectionSuccess(Index index) {
-        commandBox.runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
+        getCommandBox().runCommand(SelectCommand.COMMAND_WORD + " " + index.getOneBased());
         assertResultMessage("Selected Person: " + index.getOneBased());
         assertPersonSelected(index);
     }
 
     private void assertPersonSelected(Index index) {
-        ReadOnlyPerson selectedPerson = personListPanel.getSelectedPerson().get();
-        assertEquals(personListPanel.getPerson(index.getZeroBased()), selectedPerson);
+        assertEquals(getPersonListPanel().getSelectedPersons().size(), 1);
+        ReadOnlyPerson selectedPerson = getPersonListPanel().getSelectedPersons().get(0);
+        assertEquals(getPersonListPanel().getPerson(index.getZeroBased()), selectedPerson);
         //TODO: confirm the correct page is loaded in the Browser Panel
     }
 
     private void assertNoPersonSelected() {
-        assertFalse(personListPanel.getSelectedPerson().isPresent());
+        assertEquals(getPersonListPanel().getSelectedPersons().size(), 0);
     }
 
 }
