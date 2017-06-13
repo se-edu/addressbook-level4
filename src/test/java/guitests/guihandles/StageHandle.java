2d8edc5668ef0b6a965479add06d379989c5efe9
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:27:31 +0800

    Add NodeHandle and StageHandle
    GuiHandle is a messy class. It has no proper encapsulation as it can
    access all nodes in the scene graph. It is also not very cohesive, as
    it handles interactions with text fields, labels and stages.
    
    There should be a handle class that deals with nodes only, and another
    handle class that deals with stages only.
    
    Let's create NodeHandle, which will concern itself with remembering the
    root node of the component, and is able to obtain sub-nodes within the
    component. It will also contain interaction methods that are applicable
    to all components.
    
    Let's also create StageHandle, which points to a Stage in the scene
    graph. It will also be capable of stage interactions, and obtaining
    the nodes within the Stage to allow us to create NodeHandles.
    
    As a result of creating NodeHandle and StageHandle, GuiHandle should no
    longer be used by any new classes. Let's deprecate GuiHandle, and update
    the header comments to mention about the new classes.
    
    NodeHandle#getChildNode(String) and StageHandle#getChildNode(String) may
    fail to find the nodes if the name given is invalid. Let's also create a
    NodeNotFoundException to allow us to warn the callers of such cases.

---
+++ src/test/java/guitests/guihandles/StageHandle.java
@@ -0,0 +1,54 @@
+package guitests.guihandles;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import guitests.GuiRobot;
+import guitests.guihandles.exceptions.NodeNotFoundException;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+
+/**
+ * Provides access to a stage in a JavaFx application for GUI testing purposes.
+ */
+public abstract class StageHandle {
+    protected final GuiRobot guiRobot = new GuiRobot();
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
+
+    private final Stage stage;
+
+    public StageHandle(Stage stage) {
+        this.stage = requireNonNull(stage);
+    }
+
+    /**
+     * Closes {@code stage}.
+     */
+    public void close() {
+        guiRobot.interact(stage::close);
+    }
+
+    /**
+     * Focuses on this {@code stage}.
+     */
+    public void focus() {
+        String windowTitle = stage.getTitle();
+        logger.info("Focusing on" + windowTitle);
+        guiRobot.interact(stage::requestFocus);
+        logger.info("Finishing focus on" + windowTitle);
+    }
+
+    /**
+     * Retrieves the {@code query} node owned by the {@code stage}.
+     *
+     * @param query name of the CSS selector for the node to retrieve.
+     * @throws NodeNotFoundException if no such node exists.
+     */
+    protected <T extends Node> T getChildNode(String query) {
+        Optional<T> node = guiRobot.from(stage.getScene().getRoot()).lookup(query).tryQuery();
+        return node.orElseThrow(NodeNotFoundException::new);
+    }
+}
