a25a380f92acaf96c6ee782ec945ac7955b78890
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 16:01:50 +0800

    Port ResultDisplayHandle
    ResultDisplayHandle uses GuiHandle.
    
    As ResultDisplayHandle is mainly concerned with manipulating
    ResultDisplay, it should inherit from NodeHandle instead.
    
    Let's port ResultDisplayHandle to use NodeHandle.

--- src/test/java/guitests/guihandles/ResultDisplayHandle.java
+++ src/test/java/guitests/guihandles/ResultDisplayHandle.java
@@ -1,33 +1,19 @@
 package guitests.guihandles;
 
-import guitests.GuiRobot;
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
-import seedu.address.TestApp;
 
 /**
  * A handler for the ResultDisplay of the UI
  */
-public class ResultDisplayHandle extends GuiHandle {
+public class ResultDisplayHandle extends NodeHandle {
 
     public static final String RESULT_DISPLAY_ID = "#resultDisplay";
 
-    public ResultDisplayHandle(GuiRobot guiRobot, Stage primaryStage) {
-        super(guiRobot, primaryStage, TestApp.APP_TITLE);
-    }
-
-    /**
-     * Clicks on the TextArea.
-     */
-    public void clickOnTextArea() {
-        guiRobot.clickOn(RESULT_DISPLAY_ID);
+    public ResultDisplayHandle(MainWindowHandle mainWindowHandle) {
+        super(mainWindowHandle.getChildNode(RESULT_DISPLAY_ID));
     }
 
     public String getText() {
-        return getResultDisplay().getText();
-    }
-
-    private TextArea getResultDisplay() {
-        return getNode(RESULT_DISPLAY_ID);
+        return ((TextArea) getNode()).getText();
     }
 }
