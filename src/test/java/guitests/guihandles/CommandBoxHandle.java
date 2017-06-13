8e1bdb4e794255335fff34285864ed7b7c250b58
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:54:25 +0800

    Port CommandBoxHandle
    CommandBoxHandle uses GuiHandle.
    
    As CommandBoxHandle is mainly concerned with manipulating the
    CommandBox, it should inherit from NodeHandle.
    
    Let's port CommandBoxHandle to use NodeHandle.

--- src/test/java/guitests/guihandles/CommandBoxHandle.java
+++ src/test/java/guitests/guihandles/CommandBoxHandle.java
@@ -7,38 +7,21 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.ui.CommandBox;
 
 /**
- * A handle to the Command Box in the GUI.
+ * A handle to the {@code CommandBox} in the GUI.
  */
-public class CommandBoxHandle extends GuiHandle {
+public class CommandBoxHandle extends NodeHandle {
 
     private static final String COMMAND_INPUT_FIELD_ID = "#commandTextField";
 
-    public CommandBoxHandle(String stageTitle) {
-        super(stageTitle);
+    public CommandBoxHandle(MainWindowHandle mainWindowHandle) {
+        super(mainWindowHandle.getChildNode(COMMAND_INPUT_FIELD_ID));
     }
 
     /**
-     * Clicks on the TextField.
+     * Gets the text of the command box.
      */
-    public void clickOnTextField() {
-        guiRobot.clickOn(COMMAND_INPUT_FIELD_ID);
-    }
-
-    public void enterCommand(String command) {
-        TextField textField = getNode(COMMAND_INPUT_FIELD_ID);
-        guiRobot.clickOn(textField);
-        guiRobot.interact(() -> textField.setText(command));
-        guiRobot.pauseForHuman();
-    }
-
     public String getCommandInput() {
-        TextField textField = getNode(COMMAND_INPUT_FIELD_ID);
-        return textField.getText();
-    }
-
-    public void pressEnter() {
-        guiRobot.type(KeyCode.ENTER);
-        guiRobot.pauseForHuman();
+        return ((TextField) getNode()).getText();
     }
 
     /**
@@ -46,19 +29,28 @@ public class CommandBoxHandle extends GuiHandle {
      * @return true if the command succeeded, false otherwise.
      */
     public boolean runCommand(String command) {
-        enterCommand(command);
-        pressEnter();
-        guiRobot.pauseForHuman();
+        GUI_ROBOT.clickOn(getNode());
+        GUI_ROBOT.interact(() -> ((TextField) getNode()).setText(command));
+        GUI_ROBOT.pauseForHuman();
+
+        GUI_ROBOT.type(KeyCode.ENTER);
+        GUI_ROBOT.pauseForHuman();
         return !getStyleClass().contains(CommandBox.ERROR_STYLE_CLASS);
     }
 
+    /**
+     * Types {@code HelpCommand#COMMAND_WORD} and executes it.
+     * @return the handle of the help window.
+     */
     public HelpWindowHandle runHelpCommand() {
-        enterCommand(HelpCommand.COMMAND_WORD);
-        pressEnter();
+        runCommand(HelpCommand.COMMAND_WORD);
         return new HelpWindowHandle();
     }
 
+    /**
+     * Gets the list of style classes present in the command box.
+     */
     public ObservableList<String> getStyleClass() {
-        return getNode(COMMAND_INPUT_FIELD_ID).getStyleClass();
+        return getNode().getStyleClass();
     }
 }
