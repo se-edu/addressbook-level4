67243f874d8c00c30544e41a0a15fb49a3c9e13d
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:58:54 +0800

    Port PersonCardHandle
    PersonCardHandle uses GuiHandle.
    
    As PersonCardHandle is mainly concerned with manipulating the
    PersonCards, it should inherit from NodeHandle instead.
    
    Let's port PersonCardHandle to use NodeHandle.

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -4,18 +4,17 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import guitests.GuiRobot;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.layout.Region;
-import javafx.stage.Stage;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.tag.Tag;
 
 /**
  * Provides a handle to a person card in the person list panel.
  */
-public class PersonCardHandle extends GuiHandle {
+public class PersonCardHandle extends NodeHandle {
     private static final String NAME_FIELD_ID = "#name";
     private static final String ADDRESS_FIELD_ID = "#address";
     private static final String PHONE_FIELD_ID = "#phone";
@@ -24,29 +23,36 @@ public class PersonCardHandle extends GuiHandle {
 
     private Node node;
 
-    public PersonCardHandle(GuiRobot guiRobot, Stage primaryStage, Node node) {
-        super(guiRobot, primaryStage, null);
-        this.node = node;
-    }
+    private Label nameLabel;
+    private Label addressLabel;
+    private Label phoneLabel;
+    private Label emailLabel;
+    private Region tagsContainer;
 
-    protected String getTextFromLabel(String fieldId) {
-        return getTextFromLabel(fieldId, node);
+    public PersonCardHandle(Node cardNode) {
+        super(cardNode);
+
+        this.nameLabel = (Label) getChildNode(NAME_FIELD_ID);
+        this.addressLabel = (Label) getChildNode(ADDRESS_FIELD_ID);
+        this.phoneLabel = (Label) getChildNode(PHONE_FIELD_ID);
+        this.emailLabel = (Label) getChildNode(EMAIL_FIELD_ID);
+        this.tagsContainer = (Region) getChildNode(TAGS_FIELD_ID);
     }
 
     public String getFullName() {
-        return getTextFromLabel(NAME_FIELD_ID);
+        return nameLabel.getText();
     }
 
     public String getAddress() {
-        return getTextFromLabel(ADDRESS_FIELD_ID);
+        return addressLabel.getText();
     }
 
     public String getPhone() {
-        return getTextFromLabel(PHONE_FIELD_ID);
+        return phoneLabel.getText();
     }
 
     public String getEmail() {
-        return getTextFromLabel(EMAIL_FIELD_ID);
+        return emailLabel.getText();
     }
 
     public List<String> getTags() {
@@ -69,7 +75,7 @@ public class PersonCardHandle extends GuiHandle {
     }
 
     private Region getTagsContainer() {
-        return guiRobot.from(node).lookup(TAGS_FIELD_ID).query();
+        return tagsContainer;
     }
 
     public boolean isSamePerson(ReadOnlyPerson person) {
