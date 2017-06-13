682146fce68c0c84fa261fd683c976a33fc70121
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-13 15:50:28 +0800

    Port AlertDialogHandle
    AlertDialogHandle uses GuiHandle.
    
    As AlertDialogHandle is mainly concerned with manipulation of the
    AlertDialog, it should inherit from StageHandle instead.
    
    Let's port AlertDialogHandle to use StageHandle.
    
    AlertDialogHandle#isMatching(String, String) no longers check that the
    AlertDialog window is present as the handle must only be created after
    the window is present.

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -14,13 +14,13 @@ public class ErrorDialogGuiTest extends AddressBookGuiTest {
     private static final String ERROR_DIALOG_STAGE_TITLE = "File Op Error";
 
     @Test
-    public void showErrorDialogs() throws InterruptedException {
+    public void showErrorDialogs() throws Exception {
         GuiRobot guiRobot = new GuiRobot();
 
         raise(new DataSavingExceptionEvent(new IOException("Stub")));
 
         guiRobot.waitForEvent(() -> guiRobot.isWindowShown(ERROR_DIALOG_STAGE_TITLE));
-        AlertDialogHandle alertDialog = new AlertDialogHandle(ERROR_DIALOG_STAGE_TITLE);
+        AlertDialogHandle alertDialog = new AlertDialogHandle(guiRobot.getStage(ERROR_DIALOG_STAGE_TITLE).get());
         assertTrue(alertDialog.isMatching("Could not save data", "Could not save data to file" + ":\n"
                                                                          + "java.io.IOException: Stub"));
 
