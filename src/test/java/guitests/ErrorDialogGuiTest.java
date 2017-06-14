e65b73741c376b8b87cc89a8ccf7a31e342012ec
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-14 13:32:27 +0800

    GuiRobot#waitForEvent(): Add timed pause
    We need a delay after an event is triggered for reaction time.
    
    Let's add a timed pause for GuiRobot#waitForEvent().

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -1,7 +1,6 @@
 package guitests;
 
 import static guitests.GuiRobotUtil.EVENT_TIMEOUT;
-import static guitests.GuiRobotUtil.LONG_WAIT;
 import static junit.framework.TestCase.assertTrue;
 
 import java.io.IOException;
@@ -23,7 +22,6 @@ public class ErrorDialogGuiTest extends AddressBookGuiTest {
         raise(new DataSavingExceptionEvent(new IOException()));
 
         guiRobot.waitForEvent(() -> guiRobot.isWindowActive(ERROR_DIALOG_STAGE_TITLE), EVENT_TIMEOUT);
-        guiRobot.pauseForHuman(LONG_WAIT);
 
         AlertDialogHandle alertDialog = new AlertDialogHandle(ERROR_DIALOG_STAGE_TITLE);
         assertTrue(alertDialog.isMatchingContent(ERROR_HEADER_MESSAGE, ERROR_CONTENT_MESSAGE + ":\n"
