ad0b141375e4f26d64dcdd8dcccaadda7789a70a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-14 09:34:01 +0800

    LogicManagerTest: Remove unused methods
    After removing repeated tests from LogicManagerTest, some methods are
    now unused.
    
    Let's remove them.

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -5,19 +5,6 @@ import static org.junit.Assert.fail;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.util.SampleDataUtil.getTagSet;
-import static seedu.address.testutil.TypicalPersons.INDEX_THIRD_PERSON;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
@@ -29,7 +16,6 @@ import com.google.common.eventbus.Subscribe;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
@@ -40,12 +26,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 
 
 public class LogicManagerTest {
@@ -98,14 +78,6 @@ public class LogicManagerTest {
     }
 
     /**
-     * Executes the command, confirms that a CommandException is thrown and that the result message is correct.
-     * @see #assertCommandBehavior(Class, String, String, Model)
-     */
-    private void assertCommandException(String inputCommand, String expectedMessage) {
-        assertCommandFailure(inputCommand, CommandException.class, expectedMessage);
-    }
-
-    /**
      * Executes the command, confirms that a ParseException is thrown and that the result message is correct.
      * @see #assertCommandBehavior(Class, String, String, Model)
      */
@@ -150,41 +122,6 @@ public class LogicManagerTest {
         assertParseException(unknownCommand, MESSAGE_UNKNOWN_COMMAND);
     }
 
-    /**
-     * Confirms the 'invalid argument index number behaviour' for the given command
-     * targeting a single person in the shown list, using visible index.
-     * @param commandWord to test assuming it targets a single person in the last shown list
-     *                    based on visible index.
-     */
-    private void assertIncorrectIndexFormatBehaviorForCommand(String commandWord, String expectedMessage)
-            throws Exception {
-        assertParseException(commandWord , expectedMessage); //index missing
-        assertParseException(commandWord + " +1", expectedMessage); //index should be unsigned
-        assertParseException(commandWord + " -1", expectedMessage); //index should be unsigned
-        assertParseException(commandWord + " 0", expectedMessage); //index cannot be 0
-        assertParseException(commandWord + " not_a_number", expectedMessage);
-    }
-
-    /**
-     * Confirms the 'invalid argument index number behaviour' for the given command
-     * targeting a single person in the shown list, using visible index.
-     * @param commandWord to test assuming it targets a single person in the last shown list
-     *                    based on visible index.
-     */
-    private void assertIndexNotFoundBehaviorForCommand(String commandWord) throws Exception {
-        String expectedMessage = MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
-        TestDataHelper helper = new TestDataHelper();
-        List<Person> personList = helper.generatePersonList(2);
-
-        // set AB state to 2 persons
-        model.resetData(new AddressBook());
-        for (Person p : personList) {
-            model.addPerson(p);
-        }
-
-        assertCommandException(commandWord + " " + INDEX_THIRD_PERSON.getOneBased(), expectedMessage);
-    }
-
     @Test
     public void execute_verifyHistory_success() throws Exception {
         String validCommand = "clear";
@@ -209,126 +146,5 @@ public class LogicManagerTest {
         String expectedMessage = String.format(HistoryCommand.MESSAGE_SUCCESS,
                 String.join("\n", validCommand, invalidCommandParse, invalidCommandExecute));
         assertCommandSuccess("history", expectedMessage, model);
-    }
-
-    /**
-     * A utility class to generate test data.
-     */
-    class TestDataHelper {
-
-        Person adam() throws Exception {
-            Name name = new Name("Adam Brown");
-            Phone privatePhone = new Phone("111111");
-            Email email = new Email("adam@example.com");
-            Address privateAddress = new Address("111, alpha street");
-
-            return new Person(name, privatePhone, email, privateAddress,
-                    getTagSet("tag1", "longertag2"));
-        }
-
-        /**
-         * Generates a valid person using the given seed.
-         * Running this function with the same parameter values guarantees the returned person will have the same state.
-         * Each unique seed will generate a unique Person object.
-         *
-         * @param seed used to generate the person data field values
-         */
-        Person generatePerson(int seed) throws Exception {
-            // to ensure that phone numbers are at least 3 digits long, when seed is less than 3 digits
-            String phoneNumber = String.join("", Collections.nCopies(3, String.valueOf(Math.abs(seed))));
-
-            return new Person(
-                    new Name("Person " + seed),
-                    new Phone(phoneNumber),
-                    new Email(seed + "@email"),
-                    new Address("House of " + seed),
-                    getTagSet("tag" + Math.abs(seed), "tag" + Math.abs(seed + 1)));
-        }
-
-        /** Generates the correct add command based on the person given */
-        String generateAddCommand(Person p) {
-            StringBuffer cmd = new StringBuffer();
-
-            cmd.append(AddCommand.COMMAND_WORD);
-
-            cmd.append(" " + PREFIX_NAME.getPrefix()).append(p.getName());
-            cmd.append(" " + PREFIX_EMAIL.getPrefix()).append(p.getEmail());
-            cmd.append(" " + PREFIX_PHONE.getPrefix()).append(p.getPhone());
-            cmd.append(" " + PREFIX_ADDRESS.getPrefix()).append(p.getAddress());
-
-            Set<Tag> tags = p.getTags();
-            for (Tag t: tags) {
-                cmd.append(" " + PREFIX_TAG.getPrefix()).append(t.tagName);
-            }
-
-            return cmd.toString();
-        }
-
-        /**
-         * Generates an AddressBook with auto-generated persons.
-         */
-        AddressBook generateAddressBook(int numGenerated) throws Exception {
-            AddressBook addressBook = new AddressBook();
-            addToAddressBook(addressBook, numGenerated);
-            return addressBook;
-        }
-
-        /**
-         * Generates an AddressBook based on the list of Persons given.
-         */
-        AddressBook generateAddressBook(List<Person> persons) throws Exception {
-            AddressBook addressBook = new AddressBook();
-            addToAddressBook(addressBook, persons);
-            return addressBook;
-        }
-
-        /**
-         * Adds auto-generated Person objects to the given AddressBook
-         * @param addressBook The AddressBook to which the Persons will be added
-         */
-        void addToAddressBook(AddressBook addressBook, int numGenerated) throws Exception {
-            addToAddressBook(addressBook, generatePersonList(numGenerated));
-        }
-
-        /**
-         * Adds the given list of Persons to the given AddressBook
-         */
-        void addToAddressBook(AddressBook addressBook, List<Person> personsToAdd) throws Exception {
-            for (Person p: personsToAdd) {
-                addressBook.addPerson(p);
-            }
-        }
-
-        /**
-         * Adds auto-generated Person objects to the given model
-         * @param model The model to which the Persons will be added
-         */
-        void addToModel(Model model, int numGenerated) throws Exception {
-            addToModel(model, generatePersonList(numGenerated));
-        }
-
-        /**
-         * Adds the given list of Persons to the given model
-         */
-        void addToModel(Model model, List<Person> personsToAdd) throws Exception {
-            for (Person p: personsToAdd) {
-                model.addPerson(p);
-            }
-        }
-
-        /**
-         * Generates a list of Persons based on the flags.
-         */
-        List<Person> generatePersonList(int numGenerated) throws Exception {
-            List<Person> persons = new ArrayList<>();
-            for (int i = 1; i <= numGenerated; i++) {
-                persons.add(generatePerson(i));
-            }
-            return persons;
-        }
-
-        List<Person> generatePersonList(Person... persons) {
-            return Arrays.asList(persons);
-        }
     }
 }
