bcbb109aff4438af633f793b4c408a3649573dde
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-14 10:05:44 +0800

    Interdiff between v2 and v3

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -90,7 +90,7 @@ public class LogicManagerTest {
     }
 
     @Test
-    public void execute_deleteOutOfBounds_throwsCommandException() {
+    public void execute_commandExecutionError_throwsCommandException() {
         String deleteCommand = "delete 9";
         assertCommandException(deleteCommand, MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
