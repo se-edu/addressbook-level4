ae8dd82d850d293cd81e3a3ad0bed7240724ee6e
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-15 10:33:40 +0800

    Add tests for AddCommandParser

--- src/test/java/seedu/address/testutil/CommandTestUtil.java
+++ src/test/java/seedu/address/testutil/CommandTestUtil.java
@@ -25,6 +25,7 @@ public class CommandTestUtil {
     public static final String VALID_TAG_FRIEND = "friend";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
+    public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
     public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
     public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
