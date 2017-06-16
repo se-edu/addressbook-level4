5fdb7339aad7e7305fb9c1b87283aab8d7a879dd
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-16 16:24:46 +0800

    GuiRobot#waitForEvent(BooleanSupplier, int): Add default timeout value
    GuiRobot#waitForEvent(BooleanSupplier, int) takes in a parameter for the
    timeout value.
    
    For most events, a reasonable waiting time would be 5000ms.
    
    Let's add a default timeout value for GuiRobot#waitForEvent().

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -14,6 +14,7 @@ public class GuiRobot extends FxRobot {
 
     private static final String PROPERTY_TESTFX_HEADLESS = "testfx.headless";
     private static final int PAUSE_FOR_HUMAN_DELAY_MILLISECONDS = 250;
+    private static final int DEFAULT_WAIT_FOR_EVENT_TIMEOUT_MILLISECONDS = 5000;
 
     private final boolean isHeadlessMode;
 
@@ -33,6 +34,15 @@ public class GuiRobot extends FxRobot {
         }
 
         sleep(PAUSE_FOR_HUMAN_DELAY_MILLISECONDS);
+    }
+
+    /**
+     * Wait for {@code event} to be true by 500ms.
+     *
+     * @throws EventTimeoutException if the time taken exceeds 500ms.
+     */
+    public void waitForEvent(BooleanSupplier event) {
+        waitForEvent(event, DEFAULT_WAIT_FOR_EVENT_TIMEOUT_MILLISECONDS);
     }
 
     /**
