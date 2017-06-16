f6cbc1dcae7fea5d34fb42ed660cbbc1a314d6b5
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-16 14:11:29 +0800

    Move UiManager#handleJumpToListRequestEvent(…) to PersonListPanel
    UiManager is catching JumpToListRequestEvent.
    
    The PersonListPanel should be responsible for catching it because it is
    responsible for handling it as well. There is no need for a higher
    level component like UiManager to catch the event.
    
    Let’s move this method out.

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,6 +2,8 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,7 +12,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -28,6 +32,7 @@ public class PersonListPanel extends UiPart<Region> {
         super(FXML);
         setConnections(personList);
         addToPlaceholder(personListPlaceholder);
+        EventsCenter.getInstance().registerHandler(this);
     }
 
     private void setConnections(ObservableList<ReadOnlyPerson> personList) {
@@ -51,11 +56,17 @@ public class PersonListPanel extends UiPart<Region> {
                 });
     }
 
-    public void scrollTo(int index) {
+    private void scrollTo(int index) {
         Platform.runLater(() -> {
             personListView.scrollTo(index);
             personListView.getSelectionModel().clearAndSelect(index);
         });
+    }
+
+    @Subscribe
+    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        scrollTo(event.targetIndex);
     }
 
     class PersonListViewCell extends ListCell<ReadOnlyPerson> {
