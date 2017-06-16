bf00b54fd2fd6fb91e83e4fb9657e3db786f66f3
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-16 16:24:46 +0800

    GuiRobot#waitForEvent(BooleanSupplier, int): Add default timeout value
    GuiRobot#waitForEvent(BooleanSupplier, int) takes in a parameter for the
    timeout value.
    
    For most events, a reasonable waiting time would be 5000ms.
    
    Let's add a default timeout value for GuiRobot#waitForEvent().

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -19,8 +19,7 @@ public class ErrorDialogGuiTest extends AddressBookGuiTest {
 
         raise(new DataSavingExceptionEvent(new IOException("Stub")));
 
-        int eventWaitTimeout = 5000;
-        guiRobot.waitForEvent(() -> guiRobot.isWindowShown(ERROR_DIALOG_STAGE_TITLE), eventWaitTimeout);
+        guiRobot.waitForEvent(() -> guiRobot.isWindowShown(ERROR_DIALOG_STAGE_TITLE));
         AlertDialogHandle alertDialog = new AlertDialogHandle(new GuiRobot(), stage, "File Op Error");
         assertTrue(alertDialog.isMatching("Could not save data", "Could not save data to file" + ":\n"
                                                                          + "java.io.IOException: Stub"));
