0c92ab91a735608db874487cb7cec251fdbc2329
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-19 08:41:56 +0800

    Add CommandBoxDelegate and teach MainWindow to implement it
    CommandBox operates on the Model directly.
    
    The Model belongs to the MainWindow, as such it is the responsibility of
    MainWindow to operate on it. The CommandBox ought to simply pass the
    user input back to the MainWindow.
    
    Let’s add a new interface CommandBoxDelegate and teach MainWindow to
    implement it.

---
+++ src/main/java/seedu/address/ui/ExecuteDelegate.java
@@ -0,0 +1,15 @@
+package seedu.address.ui;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Executes the user input entered into the {@code CommandBox} on its behalf.
+ */
+public interface ExecuteDelegate {
+    /**
+     * Executes {@code userInput} and returns the result.
+     */
+    CommandResult execute(String userInput) throws CommandException, ParseException;
+}
