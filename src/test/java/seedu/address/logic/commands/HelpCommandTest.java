ef9d10aae426177efbc08445c2d6505d7fe308d7
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-19 09:13:15 +0800

    HelpCommandTest: Use EventsCollector to receive events
    HelpCommandTest manually subscribes to ShowHelpRequestEvent to test that
    the event is actually being raised.
    
    EventsCollector allows us to check that the event is raised without
    having to manually subscribe to the event.
    
    Let's use EventsCollector in our HelpCommandTest instead.

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -4,31 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.eventbus.Subscribe;
-
-import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+import seedu.address.testutil.EventsCollector;
 
 public class HelpCommandTest {
-    private boolean isEventCaught = false;
-
-    @Subscribe
-    private void handleShowHelpRequestEvent(ShowHelpRequestEvent shre) {
-        isEventCaught = true;
-    }
-
-    @Before
-    public void setUp() {
-        EventsCenter.getInstance().registerHandler(this);
-    }
 
     @Test
     public void execute_help_success() {
+        EventsCollector eventCollector = new EventsCollector();
+
         CommandResult result = new HelpCommand().execute();
         assertEquals(SHOWING_HELP_MESSAGE, result.feedbackToUser);
-        assertTrue(isEventCaught);
+        assertTrue(eventCollector.getMostRecent() instanceof ShowHelpRequestEvent);
     }
 }
