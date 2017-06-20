bd69405609dc083d10d37b68ad405d40b1bf4bb6
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-20 14:51:38 +0800

    GuiHandle: Remove guiRobot from constructor parameters
    It is not necessary to pass in a guiRobot when each handle can create
    one on their own.
    
    Let's remove guiRobot from the constructor parameters.

--- src/test/java/guitests/guihandles/MainWindowHandle.java
+++ src/test/java/guitests/guihandles/MainWindowHandle.java
@@ -1,6 +1,5 @@
 package guitests.guihandles;
 
-import guitests.GuiRobot;
 import javafx.stage.Stage;
 import seedu.address.TestApp;
 
@@ -16,15 +15,15 @@ public class MainWindowHandle extends GuiHandle {
     private final MainMenuHandle mainMenu;
     private final BrowserPanelHandle browserPanel;
 
-    public MainWindowHandle(GuiRobot guiRobot, Stage primaryStage) {
-        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    public MainWindowHandle(Stage primaryStage) {
+        super(primaryStage, TestApp.APP_TITLE);
 
-        personListPanel = new PersonListPanelHandle(guiRobot, primaryStage);
-        resultDisplay = new ResultDisplayHandle(guiRobot, primaryStage);
-        commandBox = new CommandBoxHandle(guiRobot, primaryStage, TestApp.APP_TITLE);
-        statusBarFooter = new StatusBarFooterHandle(guiRobot, primaryStage);
-        mainMenu = new MainMenuHandle(guiRobot, primaryStage);
-        browserPanel = new BrowserPanelHandle(guiRobot, primaryStage);
+        personListPanel = new PersonListPanelHandle(primaryStage);
+        resultDisplay = new ResultDisplayHandle(primaryStage);
+        commandBox = new CommandBoxHandle(primaryStage, TestApp.APP_TITLE);
+        statusBarFooter = new StatusBarFooterHandle(primaryStage);
+        mainMenu = new MainMenuHandle(primaryStage);
+        browserPanel = new BrowserPanelHandle(primaryStage);
     }
 
     public PersonListPanelHandle getPersonListPanel() {
