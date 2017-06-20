357325788727f5f5e81d10ab07ba50f18532067c
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-20 14:51:38 +0800

    GuiHandle: Remove guiRobot from constructor parameters
    It is not necessary to pass in a guiRobot when each handle can create
    one on their own.
    
    Let's remove guiRobot from the constructor parameters.

--- src/test/java/guitests/guihandles/PersonListPanelHandle.java
+++ src/test/java/guitests/guihandles/PersonListPanelHandle.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import guitests.GuiRobot;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
@@ -27,8 +26,8 @@ public class PersonListPanelHandle extends GuiHandle {
 
     private static final String PERSON_LIST_VIEW_ID = "#personListView";
 
-    public PersonListPanelHandle(GuiRobot guiRobot, Stage primaryStage) {
-        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    public PersonListPanelHandle(Stage primaryStage) {
+        super(primaryStage, TestApp.APP_TITLE);
     }
 
     public List<ReadOnlyPerson> getSelectedPersons() {
@@ -154,10 +153,10 @@ public class PersonListPanelHandle extends GuiHandle {
     public PersonCardHandle getPersonCardHandle(ReadOnlyPerson person) {
         Set<Node> nodes = getAllCardNodes();
         Optional<Node> personCardNode = nodes.stream()
-                .filter(n -> new PersonCardHandle(guiRobot, primaryStage, n).isSamePerson(person))
+                .filter(n -> new PersonCardHandle(primaryStage, n).isSamePerson(person))
                 .findFirst();
         if (personCardNode.isPresent()) {
-            return new PersonCardHandle(guiRobot, primaryStage, personCardNode.get());
+            return new PersonCardHandle(primaryStage, personCardNode.get());
         } else {
             return null;
         }
