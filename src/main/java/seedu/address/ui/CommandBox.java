575cc043f344275ca28295c56706f14d25de7377
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-20 14:47:29 +0800

    Allow up and down buttons to get previous and next input respectively
    The user is only able to see their previously typed commands through
    the history command.
    
    The user may need to access their previously typed commands quickly in
    order to fix a previous typo, or to reuse the input for another
    variation.
    
    Let's add a function that allows users to retrieve the previous and
    next input in the history by pressing the up and down arrow keys
    respectively.

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,13 +1,16 @@
 package seedu.address.ui;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
+import seedu.address.logic.ListElementPointer;
 import seedu.address.logic.Logic;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -20,6 +23,7 @@ public class CommandBox extends UiPart<Region> {
 
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
+    private ListElementPointer<String> pointer;
 
     @FXML
     private TextField commandTextField;
@@ -27,6 +31,57 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(Logic logic) {
         super(FXML);
         this.logic = logic;
+        pointer = new ListElementPointer<>(logic.getUserInputHistory());
+    }
+
+    @FXML
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+        case UP:
+            // As up and down buttons will alter the position of the caret,
+            // consuming it causes the caret's position to remain unchanged
+            keyEvent.consume();
+
+            navigateToPreviousInput();
+            break;
+        case DOWN:
+            keyEvent.consume();
+            navigateToNextInput();
+            break;
+        default:
+            // let JavaFx handle the keypress
+        }
+    }
+
+    /**
+     * Retrieves the previous input from the current cursor in {@code pointer}
+     * and updates the text field with it. Moves the cursor in {@code pointer} backward by 1.
+     */
+    private void navigateToPreviousInput() {
+        assert pointer != null;
+        if (!pointer.hasPrevious()) {
+            return;
+        }
+
+        setText(pointer.previous());
+    }
+
+    /**
+     * Retrieves the next input from the current cursor in {@code pointer}
+     * and updates the text field with it. Moves the cursor in {@code pointer} forward by 1.
+     */
+    private void navigateToNextInput() {
+        assert pointer != null;
+        setText(!pointer.hasNext() ? "" : pointer.next());
+    }
+
+    /**
+     * Sets {@code CommandBox}'s text field with {@code text} and
+     * positions the caret to the end of the {@code text}.
+     */
+    private void setText(String text) {
+        commandTextField.setText(text);
+        commandTextField.positionCaret(commandTextField.getText().length());
     }
 
     @FXML
@@ -45,6 +100,10 @@ public class CommandBox extends UiPart<Region> {
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             raise(new NewResultAvailableEvent(e.getMessage()));
+        } finally {
+            List<String> userInputHistory = logic.getUserInputHistory();
+            userInputHistory.add("");
+            pointer = new ListElementPointer<>(userInputHistory, userInputHistory.size() - 1);
         }
     }
 
