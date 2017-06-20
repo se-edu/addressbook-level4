ea949f2bd5e50a2fc308408439e5660ce401de60
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-20 14:47:29 +0800

    Allow up and down buttons to get previous and next input respectively
    The user is only able to see his previously typed commands through
    the history command.
    
    The user may need to access their previously typed commands quickly in
    order to fix previous typo, or to reuse the input for another variation.
    
    Let's add a functionality to the up and down button key press to
    retrieve the previous and next input in the history.

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,15 +1,18 @@
 package seedu.address.ui;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
+import seedu.address.logic.ListElementPointer;
 import seedu.address.logic.Logic;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -22,6 +25,7 @@ public class CommandBox extends UiPart<Region> {
 
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
+    private ListElementPointer pointer;
 
     @FXML
     private TextField commandTextField;
@@ -29,7 +33,63 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(Pane commandBoxPlaceholder, Logic logic) {
         super(FXML);
         this.logic = logic;
+        pointer = new ListElementPointer(logic.getUserInputHistory());
         addToPlaceholder(commandBoxPlaceholder);
+    }
+
+    @FXML
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+        case UP:
+            // As up and down buttons will alter the position of the caret,
+            // consuming it causes the caret's position to remain unchanged
+            keyEvent.consume();
+
+            navigateToPreviousInput();
+            break;
+        case DOWN:
+            keyEvent.consume();
+            navigateToNextInput();
+            break;
+        default:
+            // let JavaFx handle the keypress
+        }
+    }
+
+    /**
+     * Retrieves the previous input from the current cursor in {@code pointer}
+     * and updates the text field with it. Moves the cursor in {@code pointer} backward by 1.
+     */
+    private void navigateToPreviousInput() {
+        assert pointer != null;
+        if (!pointer.hasPrecedingElement()) {
+            return;
+        }
+
+        setText(pointer.getPrecedingElement());
+    }
+
+    /**
+     * Retrieves the next input from the current cursor in {@code pointer}
+     * and updates the text field with it. Moves the cursor in {@code pointer} forward by 1.
+     */
+    private void navigateToNextInput() {
+        assert pointer != null;
+        if (!pointer.hasSubsequentElement()) {
+            setText("");
+            return;
+        }
+
+        setText(pointer.getSubsequentElement());
+    }
+
+    /**
+     * Sets {@code CommandBox}'s text field with {@code text} and
+     * positions the caret to the end of the {@code text}.
+     */
+    private void setText(String text) {
+        commandTextField.setText(text);
+        commandTextField.positionCaret(commandTextField.getText().length());
     }
 
     private void addToPlaceholder(Pane placeHolderPane) {
@@ -53,6 +113,9 @@ public class CommandBox extends UiPart<Region> {
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             raise(new NewResultAvailableEvent(e.getMessage()));
+        } finally {
+            List<String> userInputHistory = logic.getUserInputHistory();
+            pointer = new ListElementPointer(userInputHistory, userInputHistory.size());
         }
     }
 
