c1dfbd1aeda8ef83f200592f979277c54ad67942
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-21 14:12:11 +0800

    Add ListElementPointer
    The native ListIterator’s cursor does not point to an element.
    Rather, it points in between elements. As a result, alternating
    calls of ListIterator#next() and ListIterator#previous() return
    the same element.
    
    This behaviour is not suitable for implementing the functionality
    of allowing up and down buttons to return the previous and next
    input respectively.
    
    As a step towards allowing up and down buttons to get previous and
    next input respectively, let’s create ListElementPointer. ListElementPointer has an index that works as a pointer to an element
    in the list. This allows us to iterate through the list while
    providing better code clarity.
    
    We have tried to create a class that wraps around ListIterator, but
    it does not work well because the cursor still does not point to an
    element.
    
    Also, we have tried to create a PeekingListIterator(an extension
    of PeekingIterator [1]), in which we use peekNext() and peekPrevious()
    to get the next and previous elements accordingly. One of the ways
    is to override the next() and previous() methods such that they do
    not return anything. However, this would violate the Iterator’s
    semantics. Another way is to not override both methods, and simply
    ignore the return values i.e we only access the next and previous
    elements by using peekNext() and peekPrevious() respectively.
    However, this isn’t intuitive to future developers who may wonder
    why we are ignoring the return values.
    
    [1]: http://google.github.io/guava/releases/21.0/api/docs/com/google/common/collect/PeekingIterator.html

---
+++ src/test/java/seedu/address/logic/ListElementPointerTest.java
@@ -0,0 +1,152 @@
+package seedu.address.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class ListElementPointerTest {
+    private static final String FIRST_ELEMENT = "first";
+    private static final String SECOND_ELEMENT = "second";
+    private static final List<String> POINTER_ELEMENTS = Arrays.asList(FIRST_ELEMENT, SECOND_ELEMENT);
+    private ListElementPointer iterator;
+
+    @Test
+    public void constructor_emptyList() {
+        iterator = new ListElementPointer(Collections.emptyList());
+        assertCurrentFailure();
+        assertPreviousFailure();
+        assertNextFailure();
+    }
+
+    @Test
+    public void constructor_nonEmptyList() {
+        iterator = new ListElementPointer(POINTER_ELEMENTS);
+
+        assertCurrentSuccess(SECOND_ELEMENT);
+
+        assertPreviousSuccess(FIRST_ELEMENT);
+        assertPreviousFailure();
+
+        assertCurrentSuccess(FIRST_ELEMENT);
+
+        assertNextSuccess(SECOND_ELEMENT);
+        assertNextFailure();
+
+        assertCurrentSuccess(SECOND_ELEMENT);
+    }
+
+    @Test
+    public void add() {
+        iterator = new ListElementPointer(POINTER_ELEMENTS);
+        String thirdElement = "third";
+        iterator.add(thirdElement);
+
+        assertCurrentSuccess(SECOND_ELEMENT);
+
+        assertNextSuccess(thirdElement);
+        assertNextFailure();
+
+        assertPreviousSuccess(SECOND_ELEMENT);
+        assertPreviousSuccess(FIRST_ELEMENT);
+        assertPreviousFailure();
+    }
+
+    @Test
+    public void equals() {
+        ListElementPointer firstIterator = new ListElementPointer(Collections.singletonList(FIRST_ELEMENT));
+        ListElementPointer secondIterator = new ListElementPointer(Collections.singletonList(SECOND_ELEMENT));
+
+        // same object -> returns true
+        assertTrue(firstIterator.equals(firstIterator));
+
+        // same values -> returns true
+        ListElementPointer firstIteratorCopy = new ListElementPointer(Collections.singletonList(FIRST_ELEMENT));
+        assertTrue(firstIterator.equals(firstIteratorCopy));
+
+        // different types -> returns false
+        assertFalse(firstIterator.equals(1));
+
+        // null -> returns false
+        assertFalse(firstIterator.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstIterator.equals(secondIterator));
+    }
+
+    /**
+     * Asserts that {@code iterator#hasNext()} returns true and the return value
+     * of {@code iterator#next()} equals to {@code element}.
+     */
+    private void assertNextSuccess(String element) {
+        assertTrue(iterator.hasNext());
+        assertEquals(element, iterator.next());
+    }
+
+    /**
+     * Asserts that {@code iterator#hasPrevious()} returns true and the return value
+     * of {@code iterator#previous()} equals to {@code element}.
+     */
+    private void assertPreviousSuccess(String element) {
+        assertTrue(iterator.hasPrevious());
+        assertEquals(element, iterator.previous());
+    }
+
+    /**
+     * Asserts that {@code iterator#hasCurrent()} returns true and the return value
+     * of {@code iterator#current()} equals to {@code element}.
+     */
+    private void assertCurrentSuccess(String element) {
+        assertTrue(iterator.hasCurrent());
+        assertEquals(element, iterator.current());
+    }
+
+    /**
+     * Asserts that {@code iterator#hasNext()} returns false and the following
+     * {@code iterator#next()} call throws {@code NoSuchElementException}.
+     */
+    private void assertNextFailure() {
+        assertFalse(iterator.hasNext());
+        try {
+            iterator.next();
+            fail("The expected NoSuchElementException was not thrown");
+        } catch (NoSuchElementException e) {
+            // expected exception thrown
+        }
+    }
+
+    /**
+     * Asserts that {@code iterator#hasPrevious()} returns false and the following
+     * {@code iterator#previous()} call throws {@code NoSuchElementException}.
+     */
+    private void assertPreviousFailure() {
+        assertFalse(iterator.hasPrevious());
+        try {
+            iterator.previous();
+            fail("The expected NoSuchElementException was not thrown");
+        } catch (NoSuchElementException e) {
+            // expected exception thrown
+        }
+    }
+
+    /**
+     * Asserts that {@code iterator#hasCurrent()} returns false and the following
+     * {@code iterator#current()} call throws {@code NoSuchElementException}.
+     */
+    private void assertCurrentFailure() {
+        assertFalse(iterator.hasCurrent());
+        try {
+            iterator.current();
+            fail("The expected NoSuchElementException was not thrown");
+        } catch (NoSuchElementException e) {
+            // expected exception thrown
+        }
+    }
+}
