1abfdd9ba72c8a67387db728fe319d9ca0077120
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-27 13:01:37 +0800

    Update visibility modifier of DeleteCommand & SelectCommand
    Both target indices of DeleteCommand & SelectCommand are public.
    
    This breaks the encapsulation of these classes.
    
    Let's change them to private. As DeleteCommandParserTest and
    SelectCommandParserTest no longer have access to the target indices
    directly, let's use DeleteCommand#equals(...) and
    SelectCommand#equals(...) to check that the target indices are correct.

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -21,7 +21,7 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    public final Index targetIndex;
+    private final Index targetIndex;
 
     public DeleteCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
