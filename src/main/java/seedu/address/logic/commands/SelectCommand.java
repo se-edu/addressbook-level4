7f3f744f2c85ac06418023a9367b05f0bd7514a8
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-27 15:11:42 +0800

    Implement equals(Object) for DeleteCommand and SelectCommand

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -42,4 +42,10 @@ public class SelectCommand extends Command {
 
     }
 
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SelectCommand // instanceof handles nulls
+                && this.targetIndex.equals(((SelectCommand) other).targetIndex)); // state check
+    }
 }
