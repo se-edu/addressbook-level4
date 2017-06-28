fc51712fcae0b1ba8b286f0bca716687919e3ae8
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-28 15:15:55 +0800

    ErrorDialogGuiTest: Extract exception constant
    ErrorDialogGuiTest#showErrorDialogs() tests the handling of the dialog
    box launch event by using a custom made exception.
    
    The message of the exception can be taken from the Exception#toString()
    method. However, it is not used in the verification stage, which uses a
    hard-coded magic string instead.
    
    Let's extract the exception so that it can be reused to test that the
    proper error message is shown during the verification stage.

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -11,20 +11,22 @@ import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 
 public class ErrorDialogGuiTest extends AddressBookGuiTest {
 
+    private static final IOException IO_EXCEPTION_STUB = new IOException("Stub");
+
     private static final String ERROR_DIALOG_STAGE_TITLE = "File Op Error";
     private static final String ERROR_HEADER_MESSAGE = "Could not save data";
-    private static final String ERROR_CONTENT_MESSAGE = "Could not save data to file";
+    private static final String ERROR_CONTENT_MESSAGE = "Could not save data to file:\n" + IO_EXCEPTION_STUB.toString();
+
 
     @Test
     public void showErrorDialogs() {
         GuiRobot guiRobot = new GuiRobot();
 
-        raise(new DataSavingExceptionEvent(new IOException("Stub")));
+        raise(new DataSavingExceptionEvent(IO_EXCEPTION_STUB));
 
         guiRobot.waitForEvent(() -> guiRobot.isWindowShown(ERROR_DIALOG_STAGE_TITLE));
         AlertDialogHandle alertDialog = new AlertDialogHandle(ERROR_DIALOG_STAGE_TITLE);
-        assertTrue(alertDialog.isMatching(ERROR_HEADER_MESSAGE, ERROR_CONTENT_MESSAGE + ":\n"
-                                                                         + "java.io.IOException: Stub"));
+        assertTrue(alertDialog.isMatching(ERROR_HEADER_MESSAGE, ERROR_CONTENT_MESSAGE));
 
     }
 
