c122090a2c0857229e5bfd249ca74d40ab14d394
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-28 13:17:11 +0800

    Rename setStyleToIndicateCommandSuccess() to setStyleToDefault()
    This method is no longer called when a command is successfully
    executed. Instead, it is called whenever a key is pressed after an
    incorrect command is entered.
    
    Let’s update the name of this method to reflect this behaviour.

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -27,8 +27,8 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(Logic logic) {
         super(FXML);
         this.logic = logic;
-        // calls #setStyleToIndicateCommandSuccess() whenever there is a change to the text of the command box.
-        commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToIndicateCommandSuccess());
+        // calls #setStyleToDefault() whenever there is a change to the text of the command box.
+        commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
 
     @FXML
@@ -51,9 +51,9 @@ public class CommandBox extends UiPart<Region> {
 
 
     /**
-     * Sets the command box style to indicate a successful command.
+     * Sets the command box style to use the default style.
      */
-    private void setStyleToIndicateCommandSuccess() {
+    private void setStyleToDefault() {
         commandTextField.getStyleClass().remove(ERROR_STYLE_CLASS);
     }
 
