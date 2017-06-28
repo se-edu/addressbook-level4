1aa249965598ba3d4bea970099e513c04e2ef0b0
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-28 15:15:55 +0800

    ErrorDialogGuiTest: Extract exception constant
    ErrorDialogGuiTest#showErrorDialogs() tests the handling of the dialog
    box launch event by using a custom made exception.
    
    The message of the exception can be taken from the Exception#toString()
    method. However, it is not used in the verification stage, which uses a
    hard-coded magic string instead.
    
    Let's extract the exception so that it can be reused to test that the
    proper error message is shown during the verification stage.

--- src/test/java/guitests/ErrorDialogGuiTest.java
+++ src/test/java/guitests/ErrorDialogGuiTest.java
@@ -14,16 +14,18 @@ import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 
 public class ErrorDialogGuiTest extends AddressBookGuiTest {
 
+    private static final IOException IO_EXCEPTION_STUB = new IOException("Stub");
+
     @Test
     public void showErrorDialogs() {
         GuiRobot guiRobot = new GuiRobot();
 
-        raise(new DataSavingExceptionEvent(new IOException("Stub")));
+        raise(new DataSavingExceptionEvent(IO_EXCEPTION_STUB));
 
         guiRobot.waitForEvent(() -> guiRobot.isWindowShown(FILE_OPS_ERROR_DIALOG_STAGE_TITLE));
         AlertDialogHandle alertDialog = new AlertDialogHandle(FILE_OPS_ERROR_DIALOG_STAGE_TITLE);
         assertTrue(alertDialog.isMatching(FILE_OPS_ERROR_DIALOG_HEADER_MESSAGE,
-                FILE_OPS_ERROR_DIALOG_CONTENT_MESSAGE + ":\n" + "java.io.IOException: Stub"));
+                FILE_OPS_ERROR_DIALOG_CONTENT_MESSAGE + ":\n" + IO_EXCEPTION_STUB.toString()));
 
     }
 
