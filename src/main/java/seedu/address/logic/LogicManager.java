220aa67cc7f497c8a3128da31ac87d4d35ccd43c
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-29 14:17:10 +0800

    Teach Logic component to return the user input history
    CommandBox has no way to access the user input history in Logic as
    CommandHistory’s visibility modifier is private.
    
    This makes it impossible for CommandBox to retrieve what the user
    has input previously.
    
    As a step towards allowing up and down buttons to get the previous and
    next input respectively, let’s teach the Logic component to return
    the user input history in CommandHistory.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -45,4 +45,9 @@ public class LogicManager extends ComponentManager implements Logic {
     public ObservableList<ReadOnlyPerson> getFilteredPersonList() {
         return model.getFilteredPersonList();
     }
+
+    @Override
+    public HistorySnapshot getHistorySnapshot() {
+        return history.getHistory();
+    }
 }
