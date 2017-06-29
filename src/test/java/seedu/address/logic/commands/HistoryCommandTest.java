39cd3340a16957cf9f44c760cd3171a42b02f7c0
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-06-29 14:13:29 +0800

    CommandHistory: Update API to be more History-like

--- src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
@@ -26,13 +26,13 @@ public class HistoryCommandTest {
         assertCommandResult(historyCommand, HistoryCommand.MESSAGE_NO_HISTORY);
 
         String command1 = "clear";
-        history.add(command1);
+        history.addFirst(command1);
         assertCommandResult(historyCommand, String.format(HistoryCommand.MESSAGE_SUCCESS, command1));
 
         String command2 = "randomCommand";
         String command3 = "select 1";
-        history.add(command2);
-        history.add(command3);
+        history.addFirst(command2);
+        history.addFirst(command3);
 
         String expectedMessage = String.format(HistoryCommand.MESSAGE_SUCCESS,
                 String.join("\n", command1, command2, command3));
