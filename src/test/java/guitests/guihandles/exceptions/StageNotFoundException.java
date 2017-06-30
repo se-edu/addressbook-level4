1d2087a7e7e33666af023c299390c54b1dde9973
Tan Wang Leng <tanwangleng@outlook.com>
2017-06-30 09:58:23 +0800

    Add GuiRobot#getStage(String)
    FxRobot#window(String) allows us to get the Window with the
    corresponding window title.
    
    Some tests require access to the Stage rather than the Window, in order
    to get its focus or close the window.
    
    Let's add GuiRobot#getStage(String).
    
    As it is possible that none of the stages matches the name given, let's
    also add StageNotFoundException, so that the tests stop with an
    appropriate error message.

---
+++ src/test/java/guitests/guihandles/exceptions/StageNotFoundException.java
@@ -0,0 +1,9 @@
+package guitests.guihandles.exceptions;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Signals that the stage cannot be found.
+ */
+public class StageNotFoundException extends NoSuchElementException {
+}
