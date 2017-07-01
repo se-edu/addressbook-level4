cd3f39c0e14a90ee3e2743db9cf9f4cdb689a8af
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-01 10:33:52 +0800

    Add CommandBoxTest
    CommandBox is tested in system tests.
    
    A separate unit test for CommandBox does not exist.
    
    Let's add CommandBoxTest.

---
+++ src/test/java/seedu/address/ui/CommandBoxTest.java
@@ -0,0 +1,100 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.CommandBoxHandle;
+import javafx.scene.input.KeyCode;
+import seedu.address.logic.Logic;
+import seedu.address.logic.LogicManager;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class CommandBoxTest extends GuiUnitTest {
+
+    private static final String COMMAND_THAT_SUCCEEDS = ListCommand.COMMAND_WORD;
+    private static final String COMMAND_THAT_FAILS = "invalid command";
+
+    private ArrayList<String> defaultStyleOfCommandBox;
+    private ArrayList<String> errorStyleOfCommandBox;
+
+    private CommandBox commandBox;
+    private CommandBoxHandle commandBoxHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        Model model = new ModelManager();
+        Logic logic = new LogicManager(model);
+
+        guiRobot.interact(() -> commandBox = new CommandBox(logic));
+        commandBoxHandle = new CommandBoxHandle("");
+        uiPartRule.setUiPart(commandBox);
+
+        defaultStyleOfCommandBox = new ArrayList<>(commandBoxHandle.getStyleClass());
+
+        // build style class for error
+        errorStyleOfCommandBox = new ArrayList<>(defaultStyleOfCommandBox);
+        errorStyleOfCommandBox.add(CommandBox.ERROR_STYLE_CLASS);
+    }
+
+    @Test
+    public void commandBox_startingWithSuccessfulCommand() {
+        assertBehaviorForSuccessfulCommand();
+        assertBehaviorForFailedCommand();
+    }
+
+    @Test
+    public void commandBox_startingWithFailedCommand() {
+        assertBehaviorForFailedCommand();
+        assertBehaviorForSuccessfulCommand();
+
+        // verify that style is changed correctly even after multiple consecutive successful/failed commands
+        assertBehaviorForSuccessfulCommand();
+        assertBehaviorForFailedCommand();
+        assertBehaviorForFailedCommand();
+        assertBehaviorForSuccessfulCommand();
+    }
+
+    @Test
+    public void commandBox_handleKeyPress() {
+        commandBoxHandle.runCommand(COMMAND_THAT_FAILS);
+        assertEquals(errorStyleOfCommandBox, commandBoxHandle.getStyleClass());
+        guiRobot.push(KeyCode.ESCAPE);
+        assertEquals(errorStyleOfCommandBox, commandBoxHandle.getStyleClass());
+
+        guiRobot.push(KeyCode.A);
+        guiRobot.pauseForHuman();
+        assertEquals(defaultStyleOfCommandBox, commandBoxHandle.getStyleClass());
+    }
+
+    /**
+     * Runs a command that fails, then verifies that
+     * - the return value of runCommand(...) is false,
+     * - the text remains,
+     * - the command box has only one ERROR_STYLE_CLASS, with other style classes untouched.
+     */
+    private void assertBehaviorForFailedCommand() {
+        assertFalse(commandBoxHandle.runCommand(COMMAND_THAT_FAILS));
+        assertEquals(COMMAND_THAT_FAILS, commandBoxHandle.getCommandInput());
+        assertEquals(errorStyleOfCommandBox, commandBoxHandle.getStyleClass());
+    }
+
+    /**
+     * Runs a command that succeeds, then verifies that
+     * - the return value of runCommand(...) is true,
+     * - the text is cleared,
+     * - the command box does not have any ERROR_STYLE_CLASS, with style classes the same as default.
+     */
+    private void assertBehaviorForSuccessfulCommand() {
+        assertTrue(commandBoxHandle.runCommand(COMMAND_THAT_SUCCEEDS));
+        assertEquals("", commandBoxHandle.getCommandInput());
+        assertEquals(defaultStyleOfCommandBox, commandBoxHandle.getStyleClass());
+    }
+}
