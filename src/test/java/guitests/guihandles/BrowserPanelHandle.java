70eb722618f49ffb7f34a0763f8d2f04f993928c
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-01 10:33:40 +0800

    BrowserPanelHandle: Add getLoadedUrl()
    BrowserPanel GUI tests need to verify that the WebView has been given
    the correct URL to load the webpage from.
    
    There is no way to do such verification through BrowserPanelHandle.
    
    Let's add BrowserPanelHandle#getLoadedUrl() to allow such verification.

--- src/test/java/guitests/guihandles/BrowserPanelHandle.java
+++ src/test/java/guitests/guihandles/BrowserPanelHandle.java
@@ -1,5 +1,9 @@
 package guitests.guihandles;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javafx.scene.web.WebView;
 import seedu.address.TestApp;
 
 /**
@@ -20,4 +24,11 @@ public class BrowserPanelHandle extends GuiHandle {
         guiRobot.clickOn(BROWSER_ID);
     }
 
+    /**
+     * Returns the URL of the currently loaded page.
+     */
+    public URL getLoadedUrl() throws MalformedURLException {
+        WebView webView = getNode(BROWSER_ID);
+        return new URL(webView.getEngine().getLocation());
+    }
 }
