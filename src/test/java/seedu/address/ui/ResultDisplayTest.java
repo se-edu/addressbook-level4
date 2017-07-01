c07b9eaa6f78fdb21ce756c8b3a0b9900bfa2dd0
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-01 10:33:58 +0800

    Add ResultDisplayTest
    ResultDisplay is tested in system tests.
    
    A separate unit test for ResultDisplay does not exist.
    
    Let's add ResultDisplayTest.

---
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -0,0 +1,32 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.ResultDisplayHandle;
+import seedu.address.commons.events.ui.NewResultAvailableEvent;
+
+public class ResultDisplayTest extends GuiUnitTest {
+
+    private static final NewResultAvailableEvent NEW_RESULT_EVENT_STUB = new NewResultAvailableEvent("Stub");
+
+    private ResultDisplay resultDisplay;
+    private ResultDisplayHandle resultDisplayHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        guiRobot.interact(() -> resultDisplay = new ResultDisplay());
+        uiPartRule.setUiPart(resultDisplay);
+
+        resultDisplayHandle = new ResultDisplayHandle();
+    }
+
+    @Test
+    public void display() throws Exception {
+        raise(NEW_RESULT_EVENT_STUB);
+        assertEquals(NEW_RESULT_EVENT_STUB.message, resultDisplayHandle.getText());
+        guiRobot.pauseForHuman();
+    }
+}
