97993d38489939216d8ee4050e88cdd456afdfec
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-03 09:24:37 +0800

    Add PersonListPanelTest
    PersonListPanel is tested in system tests.
    
    A separate unit test for PersonListPanel does not exist.
    
    Let's add PersonListPanelTest.

---
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -0,0 +1,39 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.PersonListPanelHandle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.testutil.TypicalPersons;
+
+public class PersonListPanelTest extends GuiUnitTest {
+    private static final ObservableList<ReadOnlyPerson> TYPICAL_PERSONS =
+            FXCollections.observableList(Arrays.asList(new TypicalPersons().getTypicalPersons()));
+
+    private PersonListPanel personListPanel;
+    private PersonListPanelHandle personListPanelHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        guiRobot.interact(() -> personListPanel = new PersonListPanel(TYPICAL_PERSONS));
+        uiPartRule.setUiPart(personListPanel);
+
+        personListPanelHandle = new PersonListPanelHandle();
+    }
+
+    @Test
+    public void display() throws Exception {
+        for (int i = 0; i < TYPICAL_PERSONS.size(); i++) {
+            personListPanelHandle.navigateToPerson(TYPICAL_PERSONS.get(i));
+
+            assertEquals(TYPICAL_PERSONS.get(i), personListPanelHandle.getPerson(i));
+        }
+    }
+}
