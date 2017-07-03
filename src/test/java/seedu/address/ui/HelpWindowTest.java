be078d3f22d072e1c8a2292738fa5290e5120b20
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-03 10:08:58 +0800

    Add HelpWindowTest
    We need to ensure that the HelpWindow is loading the correct file.
    
    Such test does not exist.
    
    Let's add HelpWindowTest.

---
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -0,0 +1,32 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.ui.HelpWindow.USERGUIDE_FILE_PATH;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import guitests.guihandles.HelpWindowHandle;
+
+public class HelpWindowTest extends GuiUnitTest {
+
+    private HelpWindow helpWindow;
+    private HelpWindowHandle helpWindowHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        guiRobot.interact(() -> helpWindow = new HelpWindow());
+        FxToolkit.setupStage((stage) -> stage.setScene(helpWindow.getRoot().getScene()));
+        FxToolkit.showStage();
+        helpWindowHandle = new HelpWindowHandle();
+    }
+
+    @Test
+    public void display() throws Exception {
+        URL expectedHelpPage = HelpWindow.class.getResource(USERGUIDE_FILE_PATH);
+        assertEquals(expectedHelpPage, new URL(helpWindowHandle.getLoadedUrl()));
+    }
+}
