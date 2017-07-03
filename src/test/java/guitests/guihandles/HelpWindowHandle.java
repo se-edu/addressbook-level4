45f24ce07c9d9743f16a0d360076e2bf61d0a2c3
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-03 10:08:33 +0800

    HelpWindowHandle: Add getLoadedUrl()
    HelpWindow GUI tests need to verify that the WebView has been given
    the correct URL to load the help webpage from.
    
    There is no way to do such verification through HelpWindowHandle.
    
    Let's add HelpWindowHandle#getLoadedUrl() to allow such verification.

--- src/test/java/guitests/guihandles/HelpWindowHandle.java
+++ src/test/java/guitests/guihandles/HelpWindowHandle.java
@@ -1,14 +1,21 @@
 package guitests.guihandles;
 
-import guitests.GuiRobot;
-import javafx.stage.Stage;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javafx.scene.web.WebView;
 
 /**
  * A handle to the {@code HelpWindow} of the application.
  */
 public class HelpWindowHandle extends StageHandle {
 
-    public static final String HELP_WINDOW_TITLE = "Help";
+    private static final String HELP_WINDOW_BROWSER_ID = "#browser";
+
+    public HelpWindowHandle() {
+        super(HELP_WINDOW_TITLE);
+        guiRobot.pauseForHuman();
+    }
 
     public HelpWindowHandle(Stage helpWindowStage) {
         super(helpWindowStage);
@@ -19,5 +26,13 @@ public class HelpWindowHandle extends StageHandle {
      */
     public static boolean isWindowPresent() {
         return new GuiRobot().isWindowShown(HELP_WINDOW_TITLE);
+    }
+
+    /**
+     * Returns the URL of the currently loaded page.
+     */
+    public URL getLoadedUrl() throws MalformedURLException {
+        WebView webView = getNode(HELP_WINDOW_BROWSER_ID);
+        return new URL(webView.getEngine().getLocation());
     }
 }
