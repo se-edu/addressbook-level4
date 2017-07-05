3f001cc89a5cde000d1b18368781fff41e000d0b
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-05 14:11:57 +0800

    AddCommandIntegrationTest: Use CommandTestUtil methods
    We are using the hybrid approach when unit testing our commands. Hence,
    CommandTestUtil#assertCommandSuccess(...) and
    CommandTestUtil#assertCommandFailure(...) verifies the content of the
    model as if it was an integration test. Therefore, such methods are also
    suitable for doing verification in integration tests.
    
    AddCommandIntegrationTest does not use these methods for verification in
    its tests. The logic is inlined instead. This violates the DRY principle.
    
    Let's change AddCommandIntegrationTest to use
    CommandTestUtil#assertCommandSuccess(...) and
    CommandTestUtil#assertCommandFailure(...).

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -1,13 +1,12 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -34,25 +33,14 @@ public class AddCommandIntegrationTest {
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.addPerson(validPerson);
 
-        CommandResult commandResult = prepareCommand(validPerson, model).execute();
-
-        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPerson), commandResult.feedbackToUser);
-        assertEquals(expectedModel, model);
+        assertCommandSuccess(prepareCommand(validPerson, model), model,
+                String.format(AddCommand.MESSAGE_SUCCESS, validPerson), expectedModel);
     }
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() throws Exception {
         Person personInList = new Person(model.getAddressBook().getPersonList().get(0));
-
-        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-
-        try {
-            prepareCommand(personInList, model).execute();
-            fail("The expected CommandException was not thrown.");
-        } catch (CommandException ce) {
-            assertEquals(AddCommand.MESSAGE_DUPLICATE_PERSON, ce.getMessage());
-            assertEquals(expectedModel, model);
-        }
+        assertCommandFailure(prepareCommand(personInList, model), model, AddCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     /**
