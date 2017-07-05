2864fc67abb7f405a2adbb676a596f7ab6fce84d
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-05 14:05:42 +0800

    Combine both CommandTestUtil classes
    There are two classes named CommandTestUtil, located in different
    packages.
    
    There is no need for these classes to be separate as they both have the
    same responsibilities.
    
    Let’s merge these classes.

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -2,13 +2,13 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.testutil.CommandTestUtil.DESC_AMY;
-import static seedu.address.testutil.CommandTestUtil.DESC_BOB;
-import static seedu.address.testutil.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.testutil.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.testutil.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.testutil.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.testutil.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
 import org.junit.Test;
 
