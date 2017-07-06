69d803122118fb988d795eb190ef0793f9fb0ab2
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-06 08:43:06 +0800

    Add Parser interface
    XCommandParsers have common behaviour. However, they are not linked
    by inheritance or interface.
    
    This makes the code look unstructured.
    
    Let’s add Parser as an interface, with the XCommandParsers
    implementing it.

--- src/main/java/seedu/address/logic/parser/SelectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SelectCommandParser.java
@@ -10,7 +10,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new SelectCommand object
  */
-public class SelectCommandParser {
+public class SelectCommandParser implements Parser<SelectCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the SelectCommand
