8d1558c91a3faebdc4d6e70e6a32de10dda0ee15
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-06 09:24:23 +0800

    Add BrowserPanelTest
    BrowserPanel is tested in system tests.
    
    A separate unit test for BrowserPanel does not exist.
    
    Let's add BrowserPanelTest.

---
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -0,0 +1,48 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
+import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
+import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.BrowserPanelHandle;
+import seedu.address.MainApp;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.TypicalPersons;
+
+public class BrowserPanelTest extends GuiUnitTest {
+
+    private static final Person ALICE = new TypicalPersons().alice;
+    private static final PersonPanelSelectionChangedEvent SELECTION_CHANGED_EVENT_STUB =
+            new PersonPanelSelectionChangedEvent(ALICE);
+
+    private BrowserPanel browserPanel;
+    private BrowserPanelHandle browserPanelHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        guiRobot.interact(() -> browserPanel = new BrowserPanel());
+        uiPartRule.setUiPart(browserPanel);
+
+        browserPanelHandle = new BrowserPanelHandle();
+    }
+
+    @Test
+    public void display() throws Exception {
+        // default web page
+        URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        assertEquals(expectedDefaultPageUrl, browserPanelHandle.getLoadedUrl());
+
+        // display associated webpage of a person
+        raise(SELECTION_CHANGED_EVENT_STUB);
+        URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX
+                + ALICE.getName().fullName.replaceAll(" ", "+"));
+        assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
+    }
+}
