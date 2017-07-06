34dbeb9f82743e9b41f8ab1811403883bf812a26
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-06 09:16:09 +0800

    DeleteCommandParserTest: Use helper methods in CommandParserTestUtil
    Following the addition of CommandParserTestUtil, there is no need for
    DeleteCommandParserTest to have its own code for verifying the
    correctness of the test cases.
    
    Let's teach DeleteCommandParserTest to use the helper methods from
    CommandParserTestUtil instead.

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,15 +1,13 @@
 package seedu.address.logic.parser;
 
-import static org.junit.Assert.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
@@ -20,22 +18,15 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class DeleteCommandParserTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     private DeleteCommandParser parser = new DeleteCommandParser();
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() throws Exception {
-        DeleteCommand command = parser.parse("1");
-        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+        assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() throws Exception {
-        thrown.expect(ParseException.class);
-        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-
-        parser.parse("a");
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
