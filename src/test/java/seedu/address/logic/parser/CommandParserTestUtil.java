6f08d250c16857b2aa75d5d0ec9eff5543ea4fe5
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-06 09:12:19 +0800

    Add CommandParserTestUtil
    XCommandParserTests have similar helper methods to verify the success of
    each test case.
    
    Following the DRY principle, we should extract these helper methods into
    a common class.
    
    Let’s create a CommandParserTestUtil, which is a utility class that
    has these helper methods for verifying the success of
    XCommandParserTests.

---
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Contains helper methods for testing command parsers.
+ */
+public class CommandParserTestUtil {
+
+    /**
+     * Asserts that the parsing of {@code userInput} by {@code parser} is successful and the command created
+     * equals to {@code expectedCommand}
+     */
+    public static void assertParseSuccess(Parser parser, String userInput, Command expectedCommand)
+            throws Exception {
+        Command command = parser.parse(userInput);
+        assertEquals(expectedCommand, command);
+    }
+
+    /**
+     * Asserts that the parsing of {@code userInput} by {@code parser} is unsuccessful and the error message
+     * equals to {@code expectedMessage}
+     */
+    public static void assertParseFailure(Parser parser, String userInput, String expectedMessage) {
+        try {
+            parser.parse(userInput);
+            fail("The expected ParseException was not thrown.");
+        } catch (ParseException pe) {
+            assertEquals(expectedMessage, pe.getMessage());
+        }
+    }
+}
