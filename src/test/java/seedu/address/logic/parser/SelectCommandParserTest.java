d4388b6fef69b8ae1d6e24dc7cabf342364ed078
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-06 09:16:48 +0800

    SelectCommandParserTest: Use helper methods in CommandParserTestUtil
    Following the addition of CommandParserTestUtil, there is no need for
    SelectCommandParserTest to have its own code for verifying the
    correctness of the test cases.
    
    Let's teach SelectCommandParserTest to use the helper methods from
    CommandParserTestUtil instead.

--- src/test/java/seedu/address/logic/parser/SelectCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SelectCommandParserTest.java
@@ -1,15 +1,13 @@
 package seedu.address.logic.parser;
 
-import static org.junit.Assert.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Test scope: similar to {@code DeleteCommandParserTest}.
@@ -17,22 +15,15 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class SelectCommandParserTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     private SelectCommandParser parser = new SelectCommandParser();
 
     @Test
     public void parse_validArgs_returnsSelectCommand() throws Exception {
-        SelectCommand command = parser.parse("1");
-        assertEquals(new SelectCommand(INDEX_FIRST_PERSON), command);
+        assertParseSuccess(parser, "1", new SelectCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() throws Exception {
-        thrown.expect(ParseException.class);
-        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
-
-        parser.parse("a");
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
     }
 }
