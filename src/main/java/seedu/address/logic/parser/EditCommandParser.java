402bcb01ec40c9c18435012b45997f89dd826842
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-06 08:43:06 +0800

    Add Parser interface
    XCommandParsers have common behaviour. However, they are not linked
    by inheritance or interface.
    
    This makes the code look unstructured.
    
    Let’s add Parser as an interface, with the XCommandParsers
    implementing it.

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -23,7 +23,7 @@ import seedu.address.model.tag.Tag;
 /**
  * Parses input arguments and creates a new EditCommand object
  */
-public class EditCommandParser {
+public class EditCommandParser implements Parser<EditCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
