e8166e47716f255ed532362389358b552fb7208f
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-07 09:00:57 +0800

    Interdiff between v2 and v3

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -4,7 +4,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Represents a Parser that is able to parse user input into a command.
+ * Represents a Parser that is able to parse user input into a {@code Command} of type {@code T}.
  */
 public interface Parser<T extends Command> {
 
@@ -12,5 +12,5 @@ public interface Parser<T extends Command> {
      * Parses {@code userInput} into a command and returns it.
      * @throws ParseException if {@code userInput} does not conform the expected format
      */
-    abstract T parse(String userInput) throws ParseException;
+    T parse(String userInput) throws ParseException;
 }
