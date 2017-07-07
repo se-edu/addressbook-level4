e03adf9be31524df2088a1441559cd592369a2cb
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-07 08:48:13 +0800

    Rename Parser to AddressBookParser
    Parser parses the commands specific to AddressBook application.
    
    It should have a more specific name: AddressBookParser.
    
    As part of allowing us to create an interface Parser so that
    XCommandParser can implement them, let’s rename Parser to
    AddressBookParser so that the name Parser is available to be used.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -8,7 +8,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -21,19 +21,19 @@ public class LogicManager extends ComponentManager implements Logic {
 
     private final Model model;
     private final CommandHistory history;
-    private final Parser parser;
+    private final AddressBookParser addressBookParser;
 
     public LogicManager(Model model) {
         this.model = model;
         this.history = new CommandHistory();
-        this.parser = new Parser();
+        this.addressBookParser = new AddressBookParser();
     }
 
     @Override
     public CommandResult execute(String commandText) throws CommandException, ParseException {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
         try {
-            Command command = parser.parseCommand(commandText);
+            Command command = addressBookParser.parseCommand(commandText);
             command.setData(model, history);
             return command.execute();
         } finally {
