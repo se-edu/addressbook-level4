fd4389ff6b6b51aec70785d0d4ec0d988827827e
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-07 09:00:57 +0800

    Interdiff between v5 and v6

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import org.junit.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -22,7 +23,8 @@ public class FindCommandParserTest {
     @Test
     public void parse_validArgs_returnsFindCommand() throws Exception {
         // no leading and trailing whitespaces
-        FindCommand expectedFindCommand = new FindCommand(new HashSet<>(Arrays.asList("Alice", "Bob")));
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
         assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
 
         // multiple whitespaces between keywords
