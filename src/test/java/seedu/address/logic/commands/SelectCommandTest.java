799424c9312366a9a5489a2b6f400188f2a91096
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-07 16:38:41 +0800

    Command#setData(...): Add UndoRedoStack parameter
    setData takes in Model and CommandHistory, whereby CommandHistory
    stores the list of user input.
    
    Allowing undo and redo commands to take in UndoRedoStack
    allows easier implementation for undoing and redoing commands, since
    there’s no need to reparse the user input.
    
    Let’s change setData(Model, CommandHistory) to
    setData(Model, CommandHistory, UndoRedoStack).

--- src/test/java/seedu/address/logic/commands/SelectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SelectCommandTest.java
@@ -18,6 +18,7 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -144,7 +145,7 @@ public class SelectCommandTest {
      */
     private SelectCommand prepareCommand(Index index) {
         SelectCommand selectCommand = new SelectCommand(index);
-        selectCommand.setData(model, new CommandHistory());
+        selectCommand.setData(model, new CommandHistory(), new UndoRedoStack());
         return selectCommand;
     }
 }
