e19ba374d0d40d824f813c618388b875b3b32fde
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-10 15:35:04 +0800

    WebViewUtil: Extract out getLoadedUrl()
    Both HelpWindowHandle and BrowserPanelHandle queries the URL of the web
    view.
    
    The logic can be refactored out into a helper method.
    
    Let's extract WebViewUtil#getLoadedUrl().

---
+++ src/test/java/guitests/guihandles/WebViewUtil.java
@@ -0,0 +1,19 @@
+package guitests.guihandles;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javafx.scene.web.WebView;
+
+/**
+ * Helper methods for dealing with {@code WebView}.
+ */
+public class WebViewUtil {
+
+    /**
+     * Returns the {@code URL} of the currently loaded page in the {@code webView}.
+     */
+    public static URL getLoadedUrl(WebView webView) throws MalformedURLException {
+        return new URL(webView.getEngine().getLocation());
+    }
+}
