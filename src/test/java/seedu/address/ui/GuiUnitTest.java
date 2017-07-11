037ee3626f106058d6b10e5cf8116f4ea8575c8e
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-11 10:31:46 +0800

    GuiUnitTest: Add raise(BaseEvent)
    Some GUI components react to events.
    
    We can create a helper method in GuiUnitTest to allow GUI unit tests to
    raise appropriate events.
    
    Let's add GuiUnitTest#raise(BaseEvent).

--- src/test/java/seedu/address/ui/GuiUnitTest.java
+++ src/test/java/seedu/address/ui/GuiUnitTest.java
@@ -3,6 +3,8 @@ package seedu.address.ui;
 import org.junit.Rule;
 
 import guitests.GuiRobot;
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.BaseEvent;
 import seedu.address.ui.testutil.UiPartRule;
 
 /**
@@ -13,4 +15,11 @@ public abstract class GuiUnitTest {
     public final UiPartRule uiPartRule = new UiPartRule();
 
     protected final GuiRobot guiRobot = new GuiRobot();
+
+    /**
+     * Raises an {@code event}.
+     */
+    protected void raise(BaseEvent event) {
+        guiRobot.interact(() -> EventsCenter.getInstance().post(event));
+    }
 }
