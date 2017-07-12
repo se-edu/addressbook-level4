b14e531afb3e6568d6c6423ec6ee0b275b61f43d
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-12 12:35:32 +0800

    Teach Person and ReadOnlyPerson to be observable
    Person is not using *Property for its fields.
    
    This makes Person unobservable.
    
    Let’s replace Person’s fields to use ObjectProperty, and let’s teach
    ReadOnlyPerson to return the ObjectProperty fields.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -7,6 +7,8 @@ import java.util.Collections;
 import java.util.Objects;
 import java.util.Set;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 
@@ -16,23 +18,24 @@ import seedu.address.model.tag.UniqueTagList;
  */
 public class Person implements ReadOnlyPerson {
 
-    private Name name;
-    private Phone phone;
-    private Email email;
-    private Address address;
+    private ObjectProperty<Name> name;
+    private ObjectProperty<Phone> phone;
+    private ObjectProperty<Email> email;
+    private ObjectProperty<Address> address;
 
-    private UniqueTagList tags;
+    private ObjectProperty<UniqueTagList> tags;
 
     /**
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.name = new SimpleObjectProperty<>(name);
+        this.phone = new SimpleObjectProperty<>(phone);
+        this.email = new SimpleObjectProperty<>(email);
+        this.address = new SimpleObjectProperty<>(address);
+        // protect internal tags from changes in the arg list
+        this.tags = new SimpleObjectProperty<>(new UniqueTagList(tags));
     }
 
     /**
@@ -44,39 +47,59 @@ public class Person implements ReadOnlyPerson {
     }
 
     public void setName(Name name) {
-        this.name = requireNonNull(name);
+        this.name.set(requireNonNull(name));
+    }
+
+    @Override
+    public ObjectProperty<Name> nameProperty() {
+        return name;
     }
 
     @Override
     public Name getName() {
-        return name;
+        return name.get();
     }
 
     public void setPhone(Phone phone) {
-        this.phone = requireNonNull(phone);
+        this.phone.set(requireNonNull(phone));
+    }
+
+    @Override
+    public ObjectProperty<Phone> phoneProperty() {
+        return phone;
     }
 
     @Override
     public Phone getPhone() {
-        return phone;
+        return phone.get();
     }
 
     public void setEmail(Email email) {
-        this.email = requireNonNull(email);
+        this.email.set(requireNonNull(email));
+    }
+
+    @Override
+    public ObjectProperty<Email> emailProperty() {
+        return email;
     }
 
     @Override
     public Email getEmail() {
-        return email;
+        return email.get();
     }
 
     public void setAddress(Address address) {
-        this.address = requireNonNull(address);
+        this.address.set(requireNonNull(address));
+    }
+
+    @Override
+    public ObjectProperty<Address> addressProperty() {
+        return address;
     }
 
     @Override
     public Address getAddress() {
-        return address;
+        return address.get();
     }
 
     /**
@@ -85,14 +108,18 @@ public class Person implements ReadOnlyPerson {
      */
     @Override
     public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags.toSet());
+        return Collections.unmodifiableSet(tags.get().toSet());
+    }
+
+    public ObjectProperty<UniqueTagList> tagProperty() {
+        return tags;
     }
 
     /**
      * Replaces this person's tags with the tags in the argument tag set.
      */
     public void setTags(Set<Tag> replacement) {
-        tags.setTags(replacement);
+        tags.set(new UniqueTagList(replacement));
     }
 
     /**
