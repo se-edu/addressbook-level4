7dc238e137af66a2f0124a8bdfc04918915dc045
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-12 12:35:16 +0800

    PersonListPanel: Update ListView<ReadOnlyPerson> to ListView<PersonCard>
    PersonListPanel displays a list of ReadOnlyPersons stored in its ListView.
    
    This can result in cases whereby the PersonListPanel contains a
    particular person, however there is no corresponding PersonCard
    containing the person.
    
    Since Person is now observable, let’s update ListView<ReadOnlyPerson>
    to ListView<PersonCard>, with each PersonCard observing the Person
    that the card is displaying.

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -21,20 +23,39 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
+    private ObservableList<PersonCard> cardList;
 
     @FXML
-    private ListView<ReadOnlyPerson> personListView;
+    private ListView<PersonCard> personListView;
 
     public PersonListPanel(ObservableList<ReadOnlyPerson> personList) {
         super(FXML);
+        initCardList(personList);
         setConnections(personList);
         registerAsAnEventHandler(this);
     }
 
+    private void initCardList(ObservableList<ReadOnlyPerson> personList) {
+        cardList = FXCollections.observableArrayList();
+        for (int i = 1; i <= personList.size(); i++) {
+            cardList.add(new PersonCard(personList.get(i - 1), i));
+        }
+    }
+
     private void setConnections(ObservableList<ReadOnlyPerson> personList) {
-        personListView.setItems(personList);
+        personListView.setItems(cardList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
         setEventHandlerForSelectionChangeEvent();
+        setListenerForListChange(personList);
+    }
+
+    private void setListenerForListChange(ObservableList<ReadOnlyPerson> personList) {
+        personList.addListener((ListChangeListener.Change<? extends ReadOnlyPerson> c) -> {
+            cardList.clear();
+            for (int i = 0; i < personList.size(); i++) {
+                cardList.add(new PersonCard(personList.get(i), i + 1));
+            }
+        });
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
@@ -60,17 +81,17 @@ public class PersonListPanel extends UiPart<Region> {
         scrollTo(event.targetIndex);
     }
 
-    class PersonListViewCell extends ListCell<ReadOnlyPerson> {
+    class PersonListViewCell extends ListCell<PersonCard> {
 
         @Override
-        protected void updateItem(ReadOnlyPerson person, boolean empty) {
+        protected void updateItem(PersonCard person, boolean empty) {
             super.updateItem(person, empty);
 
             if (empty || person == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+                setGraphic(person.getRoot());
             }
         }
     }
