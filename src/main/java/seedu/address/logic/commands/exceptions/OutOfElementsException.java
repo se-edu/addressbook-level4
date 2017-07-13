11c23fb08b803d114b51c4f345df4ff8b5fe809a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-13 15:46:48 +0800

    Add UndoRedoStack
    There are no data structure to store ReversibleCommands to be undone
    and redone.
    
    This implies that we are unable to undo and redo previously executed
    commands.
    
    As part of implementing the undo and redo features, let’s add
    UndoRedoStack to store previously executed commands.

---
+++ src/main/java/seedu/address/logic/commands/exceptions/OutOfElementsException.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.commands.exceptions;
+
+/**
+ * Signals that the operation is unable to find an element to operate on.
+ */
+public class OutOfElementsException extends Exception {
+}
