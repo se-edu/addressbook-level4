18562c8ebdd0d6192cceb5410ad404ea33bd0652
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-13 15:53:15 +0800

    Add UndoCommand & RedoCommand

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Undo the previous {@code UndoableCommand}.
+ */
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_SUCCESS = "Undo success!";
+    public static final String MESSAGE_FAILURE = "No more commands to undo!";
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        requireAllNonNull(model, undoRedoStack);
+
+        if (!undoRedoStack.canUndo()) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+
+        undoRedoStack.popUndo().undo();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public void setData(Model model, CommandHistory commandHistory, UndoRedoStack undoRedoStack) {
+        this.model = model;
+        this.undoRedoStack = undoRedoStack;
+    }
+}
