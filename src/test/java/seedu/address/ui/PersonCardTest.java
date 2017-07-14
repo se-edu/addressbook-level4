baa5988d5bcf9c0d95d3aca56e90ef19e42206e8
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-14 09:42:40 +0800

    PersonListPanelHandle: Update methods
    PersonListPanel now has a ListView that stores PersonCard instead of
    ReadOnlyPerson.
    
    PersonListPanelHandle however, still has methods that return
    ReadOnlyPerson instead of PersonCards.
    
    Let’s update these methods accordingly.
    
    Also, as a result of changing these methods, SelectCommandTest will
    be verifying the equality of the PersonCards instead of the
    ReadOnlyPersons. Let’s implement equals(Object) for PersonCard.

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Rule;
@@ -31,6 +32,32 @@ public class PersonCardTest {
         Person personWithTags = new PersonBuilder().build();
         assertCardDisplay(2, personWithTags);
         guiRobot.pauseForHuman();
+    }
+
+    @Test
+    public void equals() throws Exception {
+        Person person = new PersonBuilder().build();
+        PersonCard personCard = new PersonCard(person, 0);
+
+        // same person, same index -> returns true
+        PersonCard copy = new PersonCard(person, 0);
+        assertTrue(personCard.equals(copy));
+
+        // same object -> returns true
+        assertTrue(personCard.equals(personCard));
+
+        // null -> returns false
+        assertFalse(personCard.equals(null));
+
+        // different types -> returns false
+        assertFalse(personCard.equals(0));
+
+        // different person, same index -> returns false
+        Person differentPerson = new PersonBuilder().withName("differentName").build();
+        assertFalse(personCard.equals(new PersonCard(differentPerson, 0)));
+
+        // same person, different index -> returns false
+        assertFalse(personCard.equals(new PersonCard(person, 1)));
     }
 
     /**
