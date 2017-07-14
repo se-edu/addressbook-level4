682033fd68e8f6e89c5d7bf5eb1d3dd11e7b9919
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-14 11:08:17 +0800

    GuiUnitTest: Add getChildNode()
    Some test require finding the nodes owned by a component. For example,
    the TextField of the CommandBox is stored inside a StackPane, so we have
    to look for it.
    
    Let's add GuiUnitTest#getChildNode() to help us facilitate querying.

--- src/test/java/seedu/address/ui/GuiUnitTest.java
+++ src/test/java/seedu/address/ui/GuiUnitTest.java
@@ -1,8 +1,12 @@
 package seedu.address.ui;
 
+import java.util.Optional;
+
 import org.junit.Rule;
 
 import guitests.GuiRobot;
+import guitests.guihandles.exceptions.NodeNotFoundException;
+import javafx.scene.Node;
 import seedu.address.ui.testutil.UiPartRule;
 
 /**
@@ -13,4 +17,15 @@ public abstract class GuiUnitTest {
     public final UiPartRule uiPartRule = new UiPartRule();
 
     protected final GuiRobot guiRobot = new GuiRobot();
+
+    /**
+     * Retrieves the {@code query} node owned by the {@code rootNode}.
+     *
+     * @param query name of the CSS selector of the node to retrieve.
+     * @throws NodeNotFoundException if no such node exists.
+     */
+    protected <T extends Node> T getChildNode(Node rootNode, String query) {
+        Optional<T> node = guiRobot.from(rootNode).lookup(query).tryQuery();
+        return node.orElseThrow(NodeNotFoundException::new);
+    }
 }
