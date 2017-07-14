9b68bbf9b2ea47dbc834e1093e15148e04b7dbf3
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-14 14:08:04 +0800

    BrowserPanelHandle: Add rememberUrl() and isUrlChanged()
    When executing certain commands (such as adding a person), the browser
    panel is not suppose to change.
    
    Let's add rememberUrl() and isUrlChanged() to allow us to verify
    whether the content has been changed after doing a certain test.

--- src/test/java/guitests/guihandles/BrowserPanelHandle.java
+++ src/test/java/guitests/guihandles/BrowserPanelHandle.java
@@ -12,6 +12,8 @@ public class BrowserPanelHandle extends NodeHandle<Node> {
 
     public static final String BROWSER_ID = "#browser";
 
+    private URL lastUrl;
+
     public BrowserPanelHandle(Node browserPanelNode) {
         super(browserPanelNode);
     }
@@ -21,5 +23,13 @@ public class BrowserPanelHandle extends NodeHandle<Node> {
      */
     public URL getLoadedUrl() throws MalformedURLException {
         return WebViewUtil.getLoadedUrl(getChildNode(BROWSER_ID));
+    }
+
+    public void rememberUrl() throws MalformedURLException {
+        lastUrl = getLoadedUrl();
+    }
+
+    public boolean isUrlChanged() throws MalformedURLException {
+        return !lastUrl.equals(getLoadedUrl());
     }
 }
