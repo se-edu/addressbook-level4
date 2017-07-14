742b8dc3066e5c2e0b5ed0d3b54247dbc75363b9
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-14 16:56:43 +0800

    DeleteCommandTest: Use assertRunCommand() inside assertDeleteSuccess()
    Let's use our new method DeleteCommandTest#assertRunCommand() to
    verify that the application handled the delete command correctly.

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -1,19 +1,14 @@
 package guitests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
-import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_UPDATED;
-
-import java.util.Date;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TestUtil;
 
@@ -24,28 +19,30 @@ public class DeleteCommandTest extends AddressBookGuiTest {
         // assert that the start state is correct
         assertEquals("./" + testApp.getStorageSaveLocation(), getStatusBarFooter().getSaveLocation());
 
-        //delete the first in the list
         Person[] currentList = td.getTypicalPersons();
-        Index targetIndex = INDEX_FIRST_PERSON;
+        AddressBook currentAddressBook = td.getTypicalAddressBook();
 
-        AddressBook expectedAddressBook = td.getTypicalAddressBook();
-        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
-        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
+        //delete the first in the list
+        Index targetIndex = INDEX_FIRST_PERSON;
+        Person personTargeted = currentList[targetIndex.getZeroBased()];
+        assertDeleteSuccess(targetIndex, currentList, currentAddressBook);
 
         //delete the last in the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
+        currentAddressBook.removePerson(personTargeted);
         targetIndex = Index.fromOneBased(currentList.length);
-        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
-        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
+        personTargeted = currentList[targetIndex.getZeroBased()];
+        assertDeleteSuccess(targetIndex, currentList, currentAddressBook);
 
         //delete from the middle of the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
+        currentAddressBook.removePerson(personTargeted);
         targetIndex = Index.fromOneBased(currentList.length / 2);
-        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
-        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
+        assertDeleteSuccess(targetIndex, currentList, currentAddressBook);
 
         //invalid index
-        runCommand(DeleteCommand.COMMAND_WORD + " " + currentList.length + 1);
+        String invalidCommand = DeleteCommand.COMMAND_WORD + " " + currentList.length + 1;
+        runCommand(invalidCommand);
         assertResultMessage("The person index provided is invalid");
 
     }
@@ -53,39 +50,18 @@ public class DeleteCommandTest extends AddressBookGuiTest {
     /**
      * Runs the delete command to delete the person at {@code index} and confirms the result is correct.
      * @param currentList A copy of the current list of persons (before deletion).
+     * @param currentAddressBook A copy of the address book model (before deletion).
      */
-    private void assertDeleteSuccess(Index index, final Person[] currentList, AddressBook expectedAddressBook)
+    private void assertDeleteSuccess(Index index, final Person[] currentList, final AddressBook currentAddressBook)
             throws Exception {
-        Person personToDelete = currentList[index.getZeroBased()];
-        Person[] expectedRemainder = TestUtil.removePersonFromList(currentList, index);
+        String commandToRun = DeleteCommand.COMMAND_WORD + " " + index.getOneBased();
 
-        // ensure that these things do not change
-        getBrowserPanel().rememberUrl();
-        getStatusBarFooter().rememberSaveLocation();
+        String expectedResultMessage = String.format(MESSAGE_DELETE_PERSON_SUCCESS, currentList[index.getZeroBased()]);
+        Person[] expectedList = TestUtil.removePersonFromList(currentList, index);
+        AddressBook expectedAddressBook = new AddressBook(currentAddressBook);
+        expectedAddressBook.removePerson(currentList[index.getZeroBased()]);
 
-        runCommand(DeleteCommand.COMMAND_WORD + " " + index.getOneBased());
-
-        //confirm the list now contains all previous persons except the deleted person
-        assertTrue(getPersonListPanel().isListMatching(expectedRemainder));
-
-        //confirm the result message is correct
-        assertResultMessage(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
-
-        //confirm that the status bar time changed, but not the save location
-        String timestamp = new Date(injectedClock.millis()).toString();
-        String expectedSyncStatus = String.format(SYNC_STATUS_UPDATED, timestamp);
-        assertEquals(expectedSyncStatus, getStatusBarFooter().getSyncStatus());
-        getStatusBarFooter().assertSaveLocationNotChanged();
-
-        //confirm that the browser panel does not change
-        getBrowserPanel().assertUrlNotChanged();
-
-        //confirm that the file is updated correctly
-        ReadOnlyAddressBook actualStorage = testApp.readStorageAddressBook();
-        assertEquals(expectedAddressBook, actualStorage);
-
-        //confirm that the model is correct
-        assertEquals(expectedAddressBook, testApp.getModel().getAddressBook());
+        assertRunCommand(commandToRun, expectedList, expectedAddressBook, expectedResultMessage);
     }
 
     /**
@@ -108,4 +84,5 @@ public class DeleteCommandTest extends AddressBookGuiTest {
         assertStorageFileContentMatch(expectedAddressBook);
         assertModelMatch(expectedAddressBook);
     }
+
 }
