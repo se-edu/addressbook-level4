803bf6f62ffaf8aaef8b056ad83859dd7b5c26db
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-14 14:11:37 +0800

    Add DeleteCommandTest
    DeleteCommandTest is a GUI test.
    
    We already have unit tests for the DeleteCommand and GUI components.
    
    Let's convert DeleteCommandTest to a system test.

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -1,35 +1,83 @@
 package guitests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
+import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_UPDATED;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TestUtil;
+import seedu.address.ui.StatusBarFooter;
 
 public class DeleteCommandTest extends AddressBookGuiTest {
 
+    private static Clock originalClock;
+    private static Clock injectedClock;
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+
+    private static void injectFixedClock() {
+        originalClock = StatusBarFooter.getClock();
+        injectedClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        StatusBarFooter.setClock(injectedClock);
+    }
+
+    private static void restoreOriginalClock() {
+        StatusBarFooter.setClock(originalClock);
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        injectFixedClock();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        restoreOriginalClock();
+    }
+
     @Test
     public void delete() throws Exception {
+        // assert that the start state is correct
+        assertEquals("./" + testApp.getStorageSaveLocation(), getStatusBarFooter().getSaveLocation());
 
         //delete the first in the list
         Person[] currentList = td.getTypicalPersons();
         Index targetIndex = INDEX_FIRST_PERSON;
-        assertDeleteSuccess(targetIndex, currentList);
+
+        AddressBook expectedAddressBook = td.getTypicalAddressBook();
+        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
+        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
 
         //delete the last in the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
         targetIndex = Index.fromOneBased(currentList.length);
-        assertDeleteSuccess(targetIndex, currentList);
+        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
+        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
 
         //delete from the middle of the list
         currentList = TestUtil.removePersonFromList(currentList, targetIndex);
         targetIndex = Index.fromOneBased(currentList.length / 2);
-        assertDeleteSuccess(targetIndex, currentList);
+        expectedAddressBook.removePerson(currentList[targetIndex.getZeroBased()]);
+        assertDeleteSuccess(targetIndex, currentList, expectedAddressBook);
 
         //invalid index
         runCommand(DeleteCommand.COMMAND_WORD + " " + currentList.length + 1);
@@ -41,9 +89,14 @@ public class DeleteCommandTest extends AddressBookGuiTest {
      * Runs the delete command to delete the person at {@code index} and confirms the result is correct.
      * @param currentList A copy of the current list of persons (before deletion).
      */
-    private void assertDeleteSuccess(Index index, final Person[] currentList) throws Exception {
+    private void assertDeleteSuccess(Index index, final Person[] currentList, AddressBook expectedAddressBook)
+            throws Exception {
         Person personToDelete = currentList[index.getZeroBased()];
         Person[] expectedRemainder = TestUtil.removePersonFromList(currentList, index);
+
+        // ensure that these things do not change
+        getBrowserPanel().rememberUrl();
+        getStatusBarFooter().rememberSaveLocation();
 
         runCommand(DeleteCommand.COMMAND_WORD + " " + index.getOneBased());
 
@@ -52,6 +105,22 @@ public class DeleteCommandTest extends AddressBookGuiTest {
 
         //confirm the result message is correct
         assertResultMessage(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+
+        //confirm that the status bar time changed, but not the save location
+        String timestamp = new Date(injectedClock.millis()).toString();
+        String expectedSyncStatus = String.format(SYNC_STATUS_UPDATED, timestamp);
+        assertEquals(expectedSyncStatus, getStatusBarFooter().getSyncStatus());
+        getStatusBarFooter().assertSaveLocationNotChanged();
+
+        //confirm that the browser panel does not change
+        getBrowserPanel().assertUrlNotChanged();
+
+        //confirm that the file is updated correctly
+        ReadOnlyAddressBook actualStorage = testApp.readStorageAddressBook();
+        assertEquals(expectedAddressBook, actualStorage);
+
+        //confirm that the model is correct
+        assertEquals(expectedAddressBook, testApp.getModel().getAddressBook());
     }
 
 }
