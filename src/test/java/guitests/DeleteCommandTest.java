803613d5fbc0e574553d290326814977d82d57f3
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-17 08:16:46 +0800

    TestUtil: Remove removePersonFromList(...)
    removePersonFromList(Person[], Index) removes a Person in the array
    by converting it into an ArrayList for the removal to happen, followed
    by converting it back into an array. This method is only called by
    DeleteCommandTest.
    
    These conversions make the code unnecessarily verbose.
    
    Let’s teach DeleteCommandTest to use an ArrayList<Person> instead of
    Person[]. This results in removePersonsFromList(Person[], Index) to be
    unused, let’s remove it.

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.INDEX_FIRST_PERSON;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.model.person.Person;
-import seedu.address.testutil.TestUtil;
 
 public class DeleteCommandTest extends AddressBookGuiTest {
 
@@ -17,22 +20,20 @@ public class DeleteCommandTest extends AddressBookGuiTest {
     public void delete() throws Exception {
 
         //delete the first in the list
-        Person[] currentList = td.getTypicalPersons();
+        ArrayList<Person> currentList = new ArrayList<>(Arrays.asList(td.getTypicalPersons()));
         Index targetIndex = INDEX_FIRST_PERSON;
         assertDeleteSuccess(targetIndex, currentList);
 
         //delete the last in the list
-        currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-        targetIndex = Index.fromOneBased(currentList.length);
+        targetIndex = Index.fromOneBased(currentList.size());
         assertDeleteSuccess(targetIndex, currentList);
 
         //delete from the middle of the list
-        currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-        targetIndex = Index.fromOneBased(currentList.length / 2);
+        targetIndex = Index.fromOneBased(currentList.size() / 2);
         assertDeleteSuccess(targetIndex, currentList);
 
         //invalid index
-        runCommand(DeleteCommand.COMMAND_WORD + " " + currentList.length + 1);
+        runCommand(DeleteCommand.COMMAND_WORD + " " + currentList.size() + 1);
         assertResultMessage("The person index provided is invalid");
 
     }
@@ -41,14 +42,13 @@ public class DeleteCommandTest extends AddressBookGuiTest {
      * Runs the delete command to delete the person at {@code index} and confirms the result is correct.
      * @param currentList A copy of the current list of persons (before deletion).
      */
-    private void assertDeleteSuccess(Index index, final Person[] currentList) throws Exception {
-        Person personToDelete = currentList[index.getZeroBased()];
-        Person[] expectedRemainder = TestUtil.removePersonFromList(currentList, index);
+    private void assertDeleteSuccess(Index index, final List<Person> currentList) throws Exception {
+        Person personToDelete = currentList.remove(index.getZeroBased());
 
         runCommand(DeleteCommand.COMMAND_WORD + " " + index.getOneBased());
 
         //confirm the list now contains all previous persons except the deleted person
-        assertTrue(getPersonListPanel().isListMatching(expectedRemainder));
+        assertTrue(getPersonListPanel().isListMatching(currentList.toArray(new Person[currentList.size()])));
 
         //confirm the result message is correct
         assertResultMessage(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
