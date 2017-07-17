90d056f0239e6a5e8c583add5094c112cbce6eb0
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-17 13:44:43 +0800

    Add EventsUtil
    Some GUI components react to events.
    
    We can create a helper method in GuiUnitTest to allow GUI unit tests to
    raise appropriate events.
    
    Let's add GuiUnitTest#post(BaseEvent).

---
+++ src/test/java/seedu/address/testutil/EventsUtil.java
@@ -0,0 +1,18 @@
+package seedu.address.testutil;
+
+import guitests.GuiRobot;
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.BaseEvent;
+
+/**
+ * Helper methods related to events.
+ */
+public class EventsUtil {
+    /**
+     * Posts {@code event} to all registered subscribers. This method will return successfully after the {@code event}
+     * has been posted to all subscribers.
+     */
+    public static void post(BaseEvent event) {
+        new GuiRobot().interact(() -> EventsCenter.getInstance().post(event));
+    }
+}
