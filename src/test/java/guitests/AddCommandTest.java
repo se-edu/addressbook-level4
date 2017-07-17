98e35d663fb8c04055786010cfe987b2afc46c15
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-07-17 08:23:28 +0800

    TestUtil: Remove addPersonsToList(...)
    addPersonsToList(Person[], Person...) adds Person objects in the array
    by converting it into an ArrayList for the addition to happen, followed
    by converting it back into an array. This method is only called by
    AddCommandTest.
    
    These conversions make the code unnecessarily verbose.
    
    Let’s teach AddCommandTest to use an ArrayList<Person> instead of
    Person[]. This results in addPersonsToList(Person[], Person...) to be
    unused, let’s remove it.

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -2,6 +2,9 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import guitests.guihandles.PersonCardHandle;
@@ -10,38 +13,36 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonUtil;
-import seedu.address.testutil.TestUtil;
 
 public class AddCommandTest extends AddressBookGuiTest {
 
     @Test
     public void add() throws Exception {
         //add one person
-        Person[] currentList = td.getTypicalPersons();
+        ArrayList<Person> currentList = new ArrayList<>(Arrays.asList(td.getTypicalPersons()));
         Person personToAdd = td.hoon;
         assertAddSuccess(personToAdd, currentList);
-        currentList = TestUtil.addPersonsToList(currentList, personToAdd);
 
         //add another person
         personToAdd = td.ida;
         assertAddSuccess(personToAdd, currentList);
-        currentList = TestUtil.addPersonsToList(currentList, personToAdd);
 
         //add duplicate person
         runCommand(PersonUtil.getAddCommand(td.hoon));
         assertResultMessage(AddCommand.MESSAGE_DUPLICATE_PERSON);
-        assertTrue(getPersonListPanel().isListMatching(currentList));
+        assertTrue(getPersonListPanel().isListMatching(currentList.toArray(new Person[currentList.size()])));
 
         //add to empty list
         runCommand(ClearCommand.COMMAND_WORD);
-        assertAddSuccess(td.alice);
+        currentList.clear();
+        assertAddSuccess(td.alice, currentList);
 
         //invalid command
         runCommand("adds Johnny");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
-    private void assertAddSuccess(Person personToAdd, Person... currentList) throws Exception {
+    private void assertAddSuccess(Person personToAdd, ArrayList<Person> currentList) throws Exception {
         runCommand(PersonUtil.getAddCommand(personToAdd));
 
         //confirm the new card contains the right data
@@ -50,8 +51,8 @@ public class AddCommandTest extends AddressBookGuiTest {
         assertCardMatchesPerson(addedCard, personToAdd);
 
         //confirm the list now contains all previous persons plus the new person
-        Person[] expectedList = TestUtil.addPersonsToList(currentList, personToAdd);
-        assertTrue(getPersonListPanel().isListMatching(expectedList));
+        currentList.add(personToAdd);
+        assertTrue(getPersonListPanel().isListMatching(currentList.toArray(new Person[currentList.size()])));
     }
 
 }
