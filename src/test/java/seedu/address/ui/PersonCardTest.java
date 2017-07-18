41bc885435dc6ba04c4458b886f7e7e2cbc9775d
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-18 12:57:46 +0800

    PersonCardTest: Add test case for subsequent changes to Person
    It is possible that the card might not have been binded properly, but
    only initialized with the starting values of Person.
    
    The test case for any subsequent changes to Person is not tested.
    
    Let's add the test case for subsequent changes to Person.

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -10,6 +10,7 @@ import guitests.guihandles.PersonCardHandle;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TypicalPersons;
 
 public class PersonCardTest extends GuiUnitTest {
 
@@ -17,11 +18,25 @@ public class PersonCardTest extends GuiUnitTest {
     public void display() throws Exception {
         // no tags
         Person personWithNoTags = new PersonBuilder().withTags(new String[0]).build();
-        assertCardDisplay(personWithNoTags, 1);
+        PersonCard personCard = new PersonCard(personWithNoTags, 1);
+        uiPartRule.setUiPart(personCard);
+        assertCardDisplay(personCard, personWithNoTags, 1);
 
         // with tags
         Person personWithTags = new PersonBuilder().build();
-        assertCardDisplay(personWithTags, 2);
+        personCard = new PersonCard(personWithTags, 2);
+        uiPartRule.setUiPart(personCard);
+        assertCardDisplay(personCard, personWithTags, 2);
+
+        // changes made to Person reflects on card
+        Person alice = new TypicalPersons().alice;
+        guiRobot.interact(() -> {
+            personWithTags.setName(alice.getName());
+            personWithTags.setAddress(alice.getAddress());
+            personWithTags.setEmail(alice.getEmail());
+            personWithTags.setPhone(alice.getPhone());
+        });
+        assertCardDisplay(personCard, personWithTags, 2);
     }
 
     @Test
@@ -51,22 +66,19 @@ public class PersonCardTest extends GuiUnitTest {
     }
 
     /**
-     * Asserts that the card in {@code validId} in the card list displays the contact
-     * details of {@code validPerson}.
+     * Asserts that {@code personCard} displays the details of {@code expectedPerson} correctly and matches
+     * {@code expectedId}.
      */
-    private void assertCardDisplay(ReadOnlyPerson validPerson, int validId) throws Exception {
-
-        PersonCard personCard = new PersonCard(validPerson, validId);
-
-        uiPartRule.setUiPart(personCard);
+    private void assertCardDisplay(PersonCard personCard, ReadOnlyPerson expectedPerson, int expectedId)
+            throws Exception {
         guiRobot.pauseForHuman();
 
         PersonCardHandle personCardHandle = new PersonCardHandle(personCard.getRoot());
 
         // verify id is displayed correctly
-        assertEquals(Integer.toString(validId) + ". ", personCardHandle.getId());
+        assertEquals(Integer.toString(expectedId) + ". ", personCardHandle.getId());
 
         // verify person details are displayed correctly
-        assertTrue(personCardHandle.isSamePerson(validPerson));
+        assertTrue(personCardHandle.isSamePerson(expectedPerson));
     }
 }
