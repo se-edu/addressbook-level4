2f2ea326c078e9ddb6e0c5b16fccc35cfb3e25bf
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-21 15:50:33 +0800

    PersonListPanelHandle: Change isListMatching() to assertListMatching()
    PersonListPanelHandle#isListMatching() returns false when it founds an
    unmatched card, and returns true otherwise.
    
    We are deprecating PersonCardHandle#isSamePerson() in favor of
    GuiTestAssert#assertCardDisplaysPerson(...), which does not return a
    boolean.
    
    Let's change isListMatching() to assertListMatching(), that does not
    return a boolean, but rather asserts that each card indeed matches.
    
    As it is now an assertion method, we could no longer put this method
    into PersonListPanelHandle, which should only be concerned with querying
    and commanding the GUI. Let's move assertListMatching() to
    GuiTestAssert.
    
    We would need a way to expose the scrolling functionality for our
    GuiTestAssert#assertListMatching(). Let's also add
    PersonListPanelHandle#scrollTo(int).

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -1,11 +1,16 @@
 package seedu.address.ui.testutil;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.util.stream.Collectors;
 
+import guitests.GuiRobot;
 import guitests.guihandles.PersonCardHandle;
+import guitests.guihandles.PersonListPanelHandle;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  * A set of assertion methods useful for writing GUI tests.
@@ -33,5 +38,25 @@ public class GuiTestAssert {
         assertEquals(expectedPerson.getAddress().value, actualCard.getAddress());
         assertEquals(expectedPerson.getTags().stream().map(tag -> tag.tagName).collect(Collectors.toList()),
                 actualCard.getTags());
+    }
+
+    /**
+     * Returns true if the list in {@code personListPanelHandle} is showing the person details correctly and in the
+     * correct order.
+     * @param persons A list of person in the correct order.
+     */
+    public static void assertListMatching(PersonListPanelHandle personListPanelHandle, ReadOnlyPerson... persons)
+            throws PersonNotFoundException {
+        GuiRobot guiRobot = new GuiRobot();
+        checkArgument(personListPanelHandle.getListSize() == persons.length,
+                "List size mismatched\nExpected " + personListPanelHandle.getListSize() + " persons");
+
+        for (int i = 0; i < persons.length; i++) {
+            final int scrollTo = i; // lambda expression needs i to be final
+            guiRobot.interact(() -> personListPanelHandle.scrollTo(scrollTo));
+            guiRobot.pauseForHuman();
+
+            assertTrue(personListPanelHandle.getPersonCardHandle(i).isSamePerson(persons[i]));
+        }
     }
 }
