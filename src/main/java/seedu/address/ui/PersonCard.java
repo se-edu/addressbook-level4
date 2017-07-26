de0c28c3c214845de29b38c984ba0d5fc279e1e9
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 11:17:01 +0800

    Ui: Connect Remark field to PersonCard
    . Just modify PersonCard's constructor to add the new method.
    . For test, modify GuiTestAssert#assertCardDisplaysPerson(...) to
    compare the now-functioning remark label.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -49,6 +49,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        remark.setText(person.getRemark().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
