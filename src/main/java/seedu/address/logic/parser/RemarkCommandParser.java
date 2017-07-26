caae7a8b8829fecee5f039c1b0d53d63ef2405fb
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 13:04:26 +0800

    Logic: Teach the app to accept 'remark' arguments but still do nothing
    . Modify RemarkCommand to take in an Index and String for remark, and
    print those two parameters as the error message
    . Modify RemarkCommandTest to test the equals method
    . Add RemarkCommandParser that will know how to parse two arguments, one
    index and one with prefix 'r/'
    . Add RemarkCommandParserTest that will test different boundary values
    for RemarkCommandParser.
    . Modify AddressBookParser to use the newly implemented
    RemarkCommandParser
    . Modify AddressBookParserTest to ensure that what the user input
    generated the correct command.

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new {@code RemarkCommand} object
+ */
+public class RemarkCommandParser implements Parser<RemarkCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
+     * and returns a {@code RemarkCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+
+        return new RemarkCommand(index, remark);
+    }
+}
