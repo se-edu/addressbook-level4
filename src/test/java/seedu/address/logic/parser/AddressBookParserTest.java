7e5a7471a61c6161863b9b25fd749349921ba183
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 13:04:26 +0800

    Logic: Teach the app to accept 'remark' arguments but still do nothing
    . Modify RemarkCommand to take in an Index and String for remark, and
    print those two parameters as the error message
    . Modify RemarkCommandTest to test the equals method
    . Add RemarkCommandParser that will know how to parse two arguments, one
    index and one with prefix 'r/'
    . Add RemarkCommandParserTest that will test different boundary values
    for RemarkCommandParser.
    . Modify AddressBookParser to use the newly implemented
    RemarkCommandParser
    . Modify AddressBookParserTest to ensure that what the user input
    generated the correct command.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
@@ -72,7 +73,10 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_remark() throws Exception {
-        assertTrue(parser.parseCommand(RemarkCommand.COMMAND_WORD) instanceof RemarkCommand);
+        final String remark = "Some remark.";
+        RemarkCommand command = (RemarkCommand) parser.parseCommand(RemarkCommand.COMMAND_WORD + " "
+                + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_REMARK + remark);
+        assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, remark), command);
     }
 
     @Test
