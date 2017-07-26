3044dad31de1099c61f77f2fbd7ae69954bc4923
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 13:04:26 +0800

    Logic: Teach the app to accept 'remark' arguments but still do nothing
    . Modify RemarkCommand to take in an Index and String for remark, and
    print those two parameters as the error message
    . Modify RemarkCommandTest to test the equals method
    . Add RemarkCommandParser that will know how to parse two arguments, one
    index and one with prefix 'r/'
    . Add RemarkCommandParserTest that will test different boundary values
    for RemarkCommandParser.
    . Modify AddressBookParser to use the newly implemented
    RemarkCommandParser
    . Modify AddressBookParserTest to ensure that what the user input
    generated the correct command.

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,11 +1,18 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -20,14 +27,41 @@ public class RemarkCommandTest {
 
     @Test
     public void execute() throws Exception {
-        assertCommandFailure(prepareCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+        final String remark = "Some remark";
+
+        assertCommandFailure(prepareCommand(INDEX_FIRST_PERSON, remark), model,
+                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), remark));
+    }
+
+    @Test
+    public void equals() {
+        final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_AMY);
+
+        // same values -> returns true
+        RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_AMY);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON, VALID_REMARK_AMY)));
+
+        // different remark -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_BOB)));
     }
 
     /**
-     * Returns an {@code RemarkCommand}.
+     * Returns an {@code RemarkCommand} with parameters {@code index} and {@code remark}.
      */
-    private RemarkCommand prepareCommand() {
-        RemarkCommand remarkCommand = new RemarkCommand();
+    private RemarkCommand prepareCommand(Index index, String remark) {
+        RemarkCommand remarkCommand = new RemarkCommand(index, remark);
         remarkCommand.setData(model, new CommandHistory());
         return remarkCommand;
     }
