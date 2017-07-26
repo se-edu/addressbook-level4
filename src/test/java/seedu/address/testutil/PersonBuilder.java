983dd104fbd9c66bd20af1cb648535cb092ddd5c
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 11:16:28 +0800

    Test: Add withRemark() for PersonBuilder
    . Add a new method `withRemark()` for `PersonBuilder`. This method will
    create a new `Remark` for the person that it is currently building.
    . Try and use the method on any sample `Person` in `TypicalPersons`.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -91,6 +91,14 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code Remark} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withRemark(String remark) {
+        this.remark = new Remark(remark);
+        return this;
+    }
+
     public Person build() {
         return new Person(name, phone, email, address, remark, tags);
     }
