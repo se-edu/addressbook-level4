63452e86ad9a75846cc1ffbab00850661bac96df
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-26 10:46:24 +0800

    Model: Add Remark class
    . Add Remark to model component (copy from Address, remove the regex and
    rename accordingly).
    . Add test for Remark (to test the equals method).
    . Modify RemarkCommand to now take in a Remark instead of a String.

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,37 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+
+    public static final String MESSAGE_REMARK_CONSTRAINTS =
+            "Person remarks can take any values, can even be blank";
+
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && this.value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
