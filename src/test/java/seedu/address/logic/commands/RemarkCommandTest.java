f2df23333e3c258169e1bc2630cf29ffd6318639
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-27 12:56:45 +0800

    Logic: Teach the app to accept 'remark' but do nothing
    . Add a RemarkCommand that just throws an Exception.
    . Add RemarkCommandTest that will test that executeUndoableCommand()
    throws an Exception.
    . Modify AddressBookParser to accept a RemarkCommand.
    . Add new test to AddressBookParserTest, which will test that typing
    "remark" returns an instance of RemarkCommand.

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ */
+public class RemarkCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute() {
+        assertCommandFailure(new RemarkCommand(), model, new CommandHistory(), MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+}
