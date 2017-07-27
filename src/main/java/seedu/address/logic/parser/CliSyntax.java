16997be262800f1dba375cae90b8006658179e97
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-27 12:56:45 +0800

    Logic: Teach the app to accept 'remark' but do nothing
    . Add a RemarkCommand that just throws an Exception.
    . Add RemarkCommandTest that will test that executeUndoableCommand()
    throws an Exception.
    . Modify AddressBookParser to accept a RemarkCommand.
    . Add new test to AddressBookParserTest, which will test that typing
    "remark" returns an instance of RemarkCommand.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,6 +10,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
