e9d0b5c19421af90afb23b0f33dc18bc0fcbcf95
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-27 13:04:23 +0800

    Ui: Add a placeholder for remark in PersonCard GUI
    . Add label with any random text inside PersonListCard.fxml
    . Add FXML annotation in PersonCard to tie the variable to the actual
    label.
    . Modify PersonCardHandle so that future tests can read the remarks in
    the cards.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -37,6 +37,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label remark;
+    @FXML
     private FlowPane tags;
 
     public PersonCard(Person person, int displayedIndex) {
