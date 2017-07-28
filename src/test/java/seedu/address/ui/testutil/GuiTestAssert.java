9ef31c67f2d4ed2ee1889709e8399cf5b59998b5
Tan Wang Leng <tanwangleng@outlook.com>
2017-07-28 14:44:36 +0800

    PersonCard: Add colors to tag labels
    The tags are all displayed with the same color.
    
    It is hard to differentiate between tags visually across contacts.
    For example, it is hard to visually group all contacts tagged with
    "colleague". It is also hard to differentiate between those contacts
    that are tagged with "colleague", and those that are not tagged with
    "colleague".
    
    Let's add different colors to different tag labels. Tags of the same
    name will always receive the same color, while different tag names
    should receive different colors if possible.
    
    The hashcode of the tag string is used to determine the tag color.
    This will assign a reasonably random color to each tag, and will
    also ensure that the tag color remains the same when the user
    launches the application again in the future (assuming the
    application version remains the same). The idea of using hashcode is
    influenced by a developer's suggestion[1].
    
    [1] https://github.com/nus-cs2103-AY1718S1/forum/issues/199

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -2,6 +2,7 @@ package seedu.address.ui.testutil;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,10 @@ import seedu.address.model.person.Person;
  * A set of assertion methods useful for writing GUI tests.
  */
 public class GuiTestAssert {
+    private static final String LABEL_DEFAULT_STYLE = "label";
+    private static final String[] TAG_COLOR_STYLES =
+        { "teal", "red", "yellow", "blue", "orange", "brown", "green", "pink", "black", "grey" };
+
     /**
      * Asserts that {@code actualCard} displays the same values as {@code expectedCard}.
      */
@@ -24,6 +29,9 @@ public class GuiTestAssert {
         assertEquals(expectedCard.getName(), actualCard.getName());
         assertEquals(expectedCard.getPhone(), actualCard.getPhone());
         assertEquals(expectedCard.getTags(), actualCard.getTags());
+
+        expectedCard.getTags().forEach(tag ->
+            assertEquals(expectedCard.getTagStyleClasses(tag), actualCard.getTagStyleClasses(tag)));
     }
 
     /**
@@ -34,8 +42,30 @@ public class GuiTestAssert {
         assertEquals(expectedPerson.getPhone().value, actualCard.getPhone());
         assertEquals(expectedPerson.getEmail().value, actualCard.getEmail());
         assertEquals(expectedPerson.getAddress().value, actualCard.getAddress());
-        assertEquals(expectedPerson.getTags().stream().map(tag -> tag.tagName).collect(Collectors.toList()),
-                actualCard.getTags());
+
+        assertTagsEqual(expectedPerson, actualCard);
+    }
+
+    /**
+     * Returns the color style for {@code tagName}'s label.
+     */
+    private static String getTagColorStyleFor(String tagName) {
+        // we use the hash code of the tag name to generate a random color, so that the color remain consistent
+        // between different runs of the program while still making it random enough between tags.
+        return TAG_COLOR_STYLES[Math.abs(tagName.hashCode()) % TAG_COLOR_STYLES.length];
+    }
+
+    /**
+     * Asserts that the tags in {@code actualCard} matches all the tags in {@code expectedPerson} with the correct
+     * color.
+     */
+    private static void assertTagsEqual(Person expectedPerson, PersonCardHandle actualCard) {
+        List<String> expectedTags = expectedPerson.getTags().stream()
+                .map(tag -> tag.tagName).collect(Collectors.toList());
+        assertEquals(expectedTags, actualCard.getTags());
+        expectedTags.forEach(tag ->
+            assertEquals(Arrays.asList(LABEL_DEFAULT_STYLE, getTagColorStyleFor(tag)),
+                    actualCard.getTagStyleClasses(tag)));
     }
 
     /**
