a4fc66d54179d7d60263d410124f2aeba707b067
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-04 11:28:32 +0800

    Update tests to call EventsCenter#clearSubscribers()
    Tests that use EventsCollector are not calling
    EventsCenter#clearSubscribers().
    
    This causes a state leak between tests
    as EventsCenter is a static class, and the EventsCollector object
    registers itself to the EventsCenter to receive events.
    
    Let’s update tests to call EventsCenter#clearSubscribers() to prevent
    state leak between tests.

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
 
+import org.junit.After;
 import org.junit.Test;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.testutil.EventsCollector;
 
 public class ExitCommandTest {
+
+    @After
+    public void tearDown() {
+        EventsCenter.clearSubscribers();
+    }
 
     @Test
     public void execute_exit_success() {
--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
+import org.junit.After;
 import org.junit.Test;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
 import seedu.address.testutil.EventsCollector;
 
 public class HelpCommandTest {
+
+    @After
+    public void tearDown() {
+        EventsCenter.clearSubscribers();
+    }
 
     @Test
     public void execute_help_success() {
