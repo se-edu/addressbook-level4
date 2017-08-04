4492ae125b86b3364b93e0b2330852cd647ea591
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-04 13:07:50 +0800

    BrowserPanelHandle: Post WebViewLoadedEvent when WebView is done loading
    There is no way to determine whether the BrowserPanel’s WebView is done
    loading.
    
    This may lead to scenarios whereby BrowserPanelTest tries to verify
    the expectedUrl before the WebView is done loading, which causes the
    test to fail.
    
    Let’s teach BrowserPanelHandle to post WebViewLoadedEvent when the
    BrowserPanel’s WebView is done loading, so that BrowserPanelTest can
    catch the event and do the test verification afterwards.

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -11,13 +11,19 @@ import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 import java.net.URL;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import guitests.GuiRobot;
 import guitests.guihandles.BrowserPanelHandle;
+import guitests.guihandles.WebViewLoadedEvent;
 import seedu.address.MainApp;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+import seedu.address.ui.testutil.EventsCollectorRule;
 
 public class BrowserPanelTest extends GuiUnitTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     private PersonPanelSelectionChangedEvent selectionChangedEventStub;
 
@@ -44,6 +50,9 @@ public class BrowserPanelTest extends GuiUnitTest {
         post(selectionChangedEventStub);
         URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX
                 + ALICE.getName().fullName.replaceAll(" ", "+") + GOOGLE_SEARCH_URL_SUFFIX);
+
+        new GuiRobot().waitForEvent(() ->
+                eventsCollectorRule.eventsCollector.getMostRecent() instanceof WebViewLoadedEvent);
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
     }
 }
