979dedbf8a9e5906288a2c19a3cf9aad643a294a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-05 18:24:03 +0800

    PersonListPanelHandle: Add isCardSelected()
    To check whether a card is selected, the caller has to call
    PersonListPanelHandle#getHandleToSelectedCard()#isPresent().
    
    This is not as intuitive as creating a method that returns true if a
    card is selected.
    
    Let’s add isCardSelected() which returns true if a card is selected,
    and let’s update callers to call this method instead.

--- src/test/java/guitests/guihandles/PersonListPanelHandle.java
+++ src/test/java/guitests/guihandles/PersonListPanelHandle.java
@@ -36,6 +36,19 @@ public class PersonListPanelHandle extends NodeHandle<ListView<PersonCard>> {
     }
 
     /**
+     * Returns true if a card is currently selected.
+     */
+    public boolean isCardSelected() {
+        List<PersonCard> selectedCardsList = getRootNode().getSelectionModel().getSelectedItems();
+
+        if (selectedCardsList.size() > 1) {
+            throw new AssertionError("Card list size expected 0 or 1.");
+        }
+
+        return !selectedCardsList.isEmpty();
+    }
+
+    /**
      * Navigates the listview to display and select the person.
      */
     public void navigateToCard(ReadOnlyPerson person) throws PersonNotFoundException {
