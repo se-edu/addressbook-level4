9694fb92e830172dcc83d3265e8a5e8b93d33b9a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-05 17:44:35 +0800

    GuiRobot: Update visibility modifiers of constants
    The visibility modifiers of these constants are public.
    
    There is no need for these constants to be public as no other classes
    are using them.
    
    Let’s update the visibility modifiers of these constants to be private.

--- src/test/java/guitests/GuiRobot.java
+++ src/test/java/guitests/GuiRobot.java
@@ -14,8 +14,8 @@ import javafx.stage.Stage;
  */
 public class GuiRobot extends FxRobot {
 
-    public static final int PAUSE_FOR_HUMAN_DELAY_MILLISECONDS = 250;
-    public static final int DEFAULT_WAIT_FOR_EVENT_TIMEOUT_MILLISECONDS = 5000;
+    private static final int PAUSE_FOR_HUMAN_DELAY_MILLISECONDS = 250;
+    private static final int DEFAULT_WAIT_FOR_EVENT_TIMEOUT_MILLISECONDS = 5000;
 
     private static final String PROPERTY_TESTFX_HEADLESS = "testfx.headless";
 
