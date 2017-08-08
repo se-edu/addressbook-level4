238b373ac891d529548200d9fcc11df4b81f9c3d
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-08 10:23:59 +0800

    MainApp: Update version number

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -40,7 +40,7 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 4, 0, true);
+    public static final Version VERSION = new Version(0, 6, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
