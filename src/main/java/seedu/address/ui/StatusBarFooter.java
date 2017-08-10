bc1854f1772df55c493dcbd0ba31033eecf0b5de
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-10 17:37:05 +0800

    StatusBarFooter: Fix multithreading bug
    setSyncStatus(String) must run on the JavaFX thread.
    
    The current implementation doesn’t ensure that this method runs on the
    JavaFX thread.
    
    Let’s update the method to ensure that this method runs on the JavaFX
    thread.

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -76,6 +76,6 @@ public class StatusBarFooter extends UiPart<Region> {
         long now = clock.millis();
         String lastUpdated = new Date(now).toString();
         logger.info(LogsCenter.getEventHandlingLogMessage(abce, "Setting last updated status to " + lastUpdated));
-        setSyncStatus(String.format(SYNC_STATUS_UPDATED, lastUpdated));
+        Platform.runLater(() -> setSyncStatus(String.format(SYNC_STATUS_UPDATED, lastUpdated)));
     }
 }
