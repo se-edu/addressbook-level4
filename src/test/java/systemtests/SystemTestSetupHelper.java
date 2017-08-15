9c09d298f3b3e71580d9ef9ed1ff4a09413eda07
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-15 19:42:57 +0800

    AddressBookSystemTest: Extract setup methods
    AddressBookSystemTest is getting bloated and its' responsibility begins
    to become unclear.
    
    This violates SRP.
    
    Let's extract setup methods into new classes, so that the
    responsibility of AddressBookSystemTest is solely to provide helper
    methods for tests to use.

---
+++ src/test/java/systemtests/SystemTestSetupHelper.java
@@ -0,0 +1,71 @@
+package systemtests;
+
+import java.util.concurrent.TimeoutException;
+
+import org.testfx.api.FxToolkit;
+
+import guitests.guihandles.MainWindowHandle;
+import seedu.address.TestApp;
+import seedu.address.testutil.TypicalPersons;
+
+/**
+ * Contains helper methods that system tests require.
+ */
+public class SystemTestSetupHelper {
+    private TestApp testApp;
+    private MainWindowHandle mainWindowHandle;
+
+    /**
+     * Sets up the {@code TestApp} and returns it.
+     */
+    public TestApp setupApplication() {
+        try {
+            FxToolkit.setupApplication(() -> testApp = new TestApp(TypicalPersons::getTypicalAddressBook,
+                    TestApp.SAVE_LOCATION_FOR_TESTING));
+        } catch (TimeoutException te) {
+            throw new AssertionError("Application takes too long to set up.");
+        }
+
+        return testApp;
+    }
+
+    /**
+     * Initializes the stage to be used by the tests.
+     */
+    public static void initializeStage() {
+        try {
+            FxToolkit.registerPrimaryStage();
+            FxToolkit.hideStage();
+        } catch (TimeoutException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    /**
+     * Encapsulates the stage initialized by {@code initializeStage} in a {@code MainWindowHandle} and returns it.
+     */
+    public MainWindowHandle setupMainWindowHandle() {
+        try {
+            FxToolkit.setupStage((stage) -> {
+                mainWindowHandle = new MainWindowHandle(stage);
+                mainWindowHandle.focus();
+            });
+            FxToolkit.showStage();
+        } catch (TimeoutException te) {
+            throw new AssertionError("Stage takes too long to set up.");
+        }
+
+        return mainWindowHandle;
+    }
+
+    /**
+     * Tears down existing stages.
+     */
+    public void tearDownStage() {
+        try {
+            FxToolkit.cleanupStages();
+        } catch (TimeoutException te) {
+            throw new AssertionError("Stage takes too long to tear down.");
+        }
+    }
+}
