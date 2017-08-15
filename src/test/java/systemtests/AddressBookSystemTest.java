d40057bc8210a3b60c7b46e53ca25df3b70599eb
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-15 11:41:00 +0800

    AddressBookSystemTest: Update verifyApplicationStartingStateIsCorrect()
    This method verifies that the starting state of the application is
    correct through the use of assertions.
    
    The method name should be updated to
    assertApplicationStartingStateIsCorrect() to better reflect what it is
    doing.
    
    Let’s update the method name and the header comments accordingly.

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -61,7 +61,7 @@ public abstract class AddressBookSystemTest {
         testApp = setupHelper.setupApplication();
         mainWindowHandle = setupHelper.setupMainWindowHandle();
 
-        verifyApplicationStartingStateIsCorrect();
+        assertApplicationStartingStateIsCorrect();
     }
 
     @After
@@ -189,9 +189,9 @@ public abstract class AddressBookSystemTest {
     }
 
     /**
-     * Checks that the starting state of the application is correct.
+     * Asserts that the starting state of the application is correct.
      */
-    private void verifyApplicationStartingStateIsCorrect() {
+    private void assertApplicationStartingStateIsCorrect() {
         try {
             assertEquals("", getCommandBox().getInput());
             assertEquals("", getResultDisplay().getText());
