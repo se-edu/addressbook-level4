0c556354d28df96501bb11510c86ab687ac9dd28
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-22 19:31:15 +0800

    Convert AddCommandTest to AddCommandSystemTest

---
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -0,0 +1,101 @@
+package systemtests;
+
+import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.AMY;
+import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.model.Model;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.testutil.PersonUtil;
+
+public class AddCommandSystemTest extends AddressBookSystemTest {
+
+    @Test
+    public void add() {
+        /* Case: add a person with tags to a non-empty address book, command with leading spaces, trailing spaces and
+         * multiple spaces between each field -> added
+         */
+        ReadOnlyPerson toAdd = AMY;
+        String command = "   " + AddCommand.COMMAND_WORD + "  " + NAME_DESC_AMY + "  " + PHONE_DESC_AMY + " "
+                + EMAIL_DESC_AMY + "   " + ADDRESS_DESC_AMY + "   " + TAG_DESC_FRIEND + " ";
+        assertCommandSuccess(command, toAdd);
+
+        /* Case: add a duplicate person -> rejected */
+        command = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY
+                + TAG_DESC_FRIEND;
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_PERSON);
+
+        /* Case: add to empty address book -> added */
+        executeCommand(ClearCommand.COMMAND_WORD);
+        assert getTestApp().getModel().getAddressBook().getPersonList().size() == 0;
+        assertCommandSuccess(ALICE);
+
+        /* Case: invalid keyword -> rejected */
+        command = "adds " + PersonUtil.getPersonDetails(toAdd);
+        assertCommandFailure(command, Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    /**
+     * Executes the {@code AddCommand} that adds {@code toAdd} to the model and verifies that the command box displays
+     * an empty string, the result display box displays the success message of executing {@code AddCommand} with the
+     * details of {@code toAdd}, and the model related components equal to the current model added with {@code toAdd}.
+     * These verifications are done by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the command box has the default style class, the status bar's sync status changes,
+     * the browser url and selected card remains unchanged.
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandSuccess(ReadOnlyPerson toAdd) {
+        assertCommandSuccess(PersonUtil.getAddCommand(toAdd), toAdd);
+    }
+
+    /**
+     * Performs the same verification as {@code assertCommandSuccess(ReadOnlyPerson)}. Executes {@code command}
+     * instead.
+     * @see AddCommandSystemTest#assertCommandSuccess(ReadOnlyPerson)
+     */
+    private void assertCommandSuccess(String command, ReadOnlyPerson toAdd) {
+        Model expectedModel = getTestApp().getModel();
+        try {
+            expectedModel.addPerson(toAdd);
+        } catch (DuplicatePersonException dpe) {
+            throw new IllegalArgumentException("toAdd already exists in the model.");
+        }
+        String expectedResultMessage = String.format(AddCommand.MESSAGE_SUCCESS, toAdd);
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchangedExceptSyncStatus();
+    }
+
+    /**
+     * Executes {@code command} and verifies that the command box displays {@code command}, the result display
+     * box displays {@code expectedResultMessage} and the model related components equal to the current model.
+     * These verifications are done by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the browser url, selected card and status bar remain unchanged, and the command box has the
+     * error style.
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandFailure(String command, String expectedResultMessage) {
+        Model expectedModel = getTestApp().getModel();
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsErrorStyle();
+        assertStatusBarUnchanged();
+    }
+}
