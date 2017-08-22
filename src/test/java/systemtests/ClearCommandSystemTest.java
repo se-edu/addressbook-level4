d90a5dc19ec46f32ef8c162031a37271b21e000a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-22 19:04:13 +0800

    ClearCommandSystemTest: Add additional tests

--- src/test/java/systemtests/ClearCommandSystemTest.java
+++ src/test/java/systemtests/ClearCommandSystemTest.java
@@ -1,10 +1,20 @@
 package systemtests;
 
+import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
 import org.junit.Test;
 
 import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.testutil.TypicalPersons;
 
 public class ClearCommandSystemTest extends AddressBookSystemTest {
 
@@ -13,9 +23,43 @@ public class ClearCommandSystemTest extends AddressBookSystemTest {
         /* Case: clear non-empty address book, command with leading spaces and trailing alphanumeric characters and
         * spaces -> cleared */
         assertCommandSuccess("   " + ClearCommand.COMMAND_WORD + " ab12   ");
+        assertSelectedCardUnchanged();
+
+        /* Case: undo clearing address book -> original address book restored */
+        String command = UndoCommand.COMMAND_WORD;
+        String expectedResultMessage = UndoCommand.MESSAGE_SUCCESS;
+        assertCommandSuccess(command,  expectedResultMessage,
+                new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs()));
+        assertSelectedCardUnchanged();
+
+        /* Case: redo clearing address book -> cleared */
+        command = RedoCommand.COMMAND_WORD;
+        expectedResultMessage = RedoCommand.MESSAGE_SUCCESS;
+        assertCommandSuccess(command, expectedResultMessage, new ModelManager());
+        assertSelectedCardUnchanged();
+
+        /* Case: selects first card in person list and clears address book -> cleared and no card selected */
+        executeCommand(UndoCommand.COMMAND_WORD); // restore the original address book
+        executeCommand(SelectCommand.COMMAND_WORD + " 1");
+        assert getPersonListPanel().isAnyCardSelected();
+        waitUntilBrowserLoaded(getBrowserPanel());
+        assertCommandSuccess(ClearCommand.COMMAND_WORD);
+        assertCardDeselected();
+
+        /* Case: filters the person list before clearing -> entire address book cleared */
+        executeCommand(UndoCommand.COMMAND_WORD); // restore the original address book
+        executeCommand(FindCommand.COMMAND_WORD + " Meier");
+        assert getTestApp().getModel().getFilteredPersonList().size()
+                < getTestApp().getModel().getAddressBook().getPersonList().size();
+        assertCommandSuccess(ClearCommand.COMMAND_WORD);
+        assertSelectedCardUnchanged();
 
         /* Case: clear empty address book -> cleared */
         assertCommandSuccess(ClearCommand.COMMAND_WORD);
+        assertSelectedCardUnchanged();
+
+        /* Case: mixed case command word -> rejected */
+        assertCommandFailure("ClEaR", MESSAGE_UNKNOWN_COMMAND);
     }
 
     /**
@@ -23,16 +67,46 @@ public class ClearCommandSystemTest extends AddressBookSystemTest {
      * box displays {@code ClearCommand#MESSAGE_SUCCESS} and the model related components equal to an empty model.
      * These verifications are done by
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * Also verifies that the command box has the default style class, the status bar's sync status changes, the browser
-     * url and selected card remains unchanged.
+     * Also verifies that the command box has the default style class and the status bar's sync status changes.
      * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
     private void assertCommandSuccess(String command) throws Exception {
+        assertCommandSuccess(command, ClearCommand.MESSAGE_SUCCESS, new ModelManager());
+    }
+
+    /**
+     * Performs the same verification as {@code assertCommandSuccess(String)} except that the result box displays
+     * {@code expectedResultMessage} and the model related components equal to {@code expectedModel}.
+     * @see ClearCommandSystemTest#assertCommandSuccess(String)
+     */
+    private void assertCommandSuccess(String command, String expectedResultMessage, Model expectedModel)
+            throws Exception {
         executeCommand(command);
-        assertApplicationDisplaysExpected("", ClearCommand.MESSAGE_SUCCESS, new ModelManager());
-        assertSelectedCardUnchanged();
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
         assertCommandBoxStyleDefault();
         assertStatusBarUnchangedExceptSyncStatus();
+
+        clockRule.setInjectedClockToCurrentTime();
+    }
+
+    /**
+     * Executes {@code command} and verifies that the command box displays {@code command}, the result display
+     * box displays {@code expectedResultMessage} and the model related components equal to the current model.
+     * These verifications are done by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the browser url, selected card and status bar remain unchanged, and the command box has the
+     * error style.
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandFailure(String command, String expectedResultMessage) throws Exception {
+        Model expectedModel = new ModelManager(
+                new AddressBook(getTestApp().getModel().getAddressBook()), new UserPrefs());
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxStyleError();
+        assertStatusBarUnchanged();
 
         clockRule.setInjectedClockToCurrentTime();
     }
