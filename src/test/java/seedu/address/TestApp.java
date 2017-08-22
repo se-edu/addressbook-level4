56a05347cd35aadc9ce5d26cd77c760ce9a960b7
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-22 20:00:54 +0800

    AddCommandSystemTest: Add additional tests

--- src/test/java/seedu/address/TestApp.java
+++ src/test/java/seedu/address/TestApp.java
@@ -13,7 +13,6 @@ import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.XmlUtil;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.storage.UserPrefsStorage;
@@ -90,13 +89,8 @@ public class TestApp extends MainApp {
         return storage.getAddressBookFilePath();
     }
 
-    /**
-     * Returns a defensive copy of the model.
-     */
     public Model getModel() {
-        Model copy = new ModelManager((model.getAddressBook()), new UserPrefs());
-        ModelHelper.setFilteredList(copy, model.getFilteredPersonList());
-        return copy;
+        return model;
     }
 
     @Override
