1c8fbf24d0aa63e000fa43b4ddd9a6a91a5b56ae
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-22 18:21:15 +0800

    Convert ClearCommandTest to ClearCommandSystemTest

---
+++ src/test/java/systemtests/ClearCommandSystemTest.java
@@ -0,0 +1,38 @@
+package systemtests;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class ClearCommandSystemTest extends AddressBookSystemTest {
+
+    @Test
+    public void clear() {
+        /* Case: clear non-empty address book, command with leading spaces and trailing alphanumeric characters and
+         * spaces -> cleared
+         */
+        assertCommandSuccess("   " + ClearCommand.COMMAND_WORD + " ab12   ");
+
+        /* Case: clear empty address book -> cleared */
+        assertCommandSuccess(ClearCommand.COMMAND_WORD);
+    }
+
+    /**
+     * Executes {@code command} and verifies that the command box displays an empty string, the result display
+     * box displays {@code ClearCommand#MESSAGE_SUCCESS} and the model related components equal to an empty model.
+     * These verifications are done by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the command box has the default style class, the status bar's sync status changes, the browser
+     * url and selected card remains unchanged.
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandSuccess(String command) {
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", ClearCommand.MESSAGE_SUCCESS, new ModelManager());
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchangedExceptSyncStatus();
+    }
+}
