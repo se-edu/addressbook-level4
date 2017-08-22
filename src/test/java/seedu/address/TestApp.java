4002c7ae3f3eebdedc5651cc9bc541c904dff08a
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-22 15:38:02 +0800

    TestApp#getModel(): Return actual model
    This method returns a defensive copy of the model.
    
    This isn’t an exact copy of the model as the ModelManager’s constructor
    initialises the FilteredList to show all persons, even though the
    original model’s FilteredList may not be showing all persons.
    
    As there is no way to make a defensive copy of the model, let’s return
    the original model instead.

--- src/test/java/seedu/address/TestApp.java
+++ src/test/java/seedu/address/TestApp.java
@@ -13,7 +13,6 @@ import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.XmlUtil;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.storage.UserPrefsStorage;
@@ -89,11 +88,8 @@ public class TestApp extends MainApp {
         return storage.getAddressBookFilePath();
     }
 
-    /**
-     * Returns a defensive copy of the model.
-     */
     public Model getModel() {
-        return new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        return model;
     }
 
     @Override
