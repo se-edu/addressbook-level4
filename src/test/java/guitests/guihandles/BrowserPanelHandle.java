0d1e6d701cc80489bb8323854ddf273c9a48a812
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-08-23 09:49:52 +0800

    AddressBookSystemTest: Wait for browser to load after command execution
    Test classes have to call
    WebViewUtil#waitUntilBrowserLoaded(BrowserPanelHandle) whenever a
    command that causes the browser to load a new page is executed.
    
    This causes code repetition when other test classes are implemented.
    
    Let’s shift this method call to executeCommand(String), so that this
    method only returns after the browser has loaded.
    
    Also, let’s enhance the concurrency check so that
    WebViewUtil#waitUntilBrowserLoaded(BrowserPanelHandle) does not have to
    manually reset the boolean concurrency variable.

--- src/test/java/guitests/guihandles/BrowserPanelHandle.java
+++ src/test/java/guitests/guihandles/BrowserPanelHandle.java
@@ -16,7 +16,7 @@ public class BrowserPanelHandle extends NodeHandle<Node> {
 
     public static final String BROWSER_ID = "#browser";
 
-    private boolean isWebViewLoaded = false;
+    private boolean isWebViewLoaded = true;
 
     private URL lastRememberedUrl;
 
@@ -26,6 +26,10 @@ public class BrowserPanelHandle extends NodeHandle<Node> {
         WebView webView = getChildNode(BROWSER_ID);
         WebEngine engine = webView.getEngine();
         new GuiRobot().interact(() -> engine.getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
+            if (newState == Worker.State.RUNNING) {
+                isWebViewLoaded = false;
+            }
+
             if (newState == Worker.State.SUCCEEDED) {
                 isWebViewLoaded = true;
             }
@@ -54,11 +58,10 @@ public class BrowserPanelHandle extends NodeHandle<Node> {
         return !lastRememberedUrl.equals(getLoadedUrl());
     }
 
-    public boolean getIsWebViewLoaded() {
+    /**
+     * Returns true if the browser is done loading a page, or if this browser has yet to load any page.
+     */
+    public boolean isLoaded() {
         return isWebViewLoaded;
-    }
-
-    public void setIsWebViewLoaded(boolean isWebViewLoaded) {
-        this.isWebViewLoaded = isWebViewLoaded;
     }
 }
