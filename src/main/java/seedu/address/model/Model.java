ce9e969ae13ad654a8397b768b7603e3546858f5
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-09-01 17:08:34 +0800

    Update classes to not use UnmodifiableObservableList
    The JavaFX library provides classes that work similarly to
    UnmodifiableObservableList.
    
    There is no need for us to use our own version of
    UnmodifiableObservableList.
    
    As part of deleting UnmodifiableObservableList, let's update classes to
    use the classes provided by the JavaFX library instead of
    UnmodifiableObservableList.

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -2,7 +2,7 @@ package seedu.address.model;
 
 import java.util.function.Predicate;
 
-import seedu.address.commons.core.UnmodifiableObservableList;
+import javafx.collections.ObservableList;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
@@ -36,8 +36,8 @@ public interface Model {
     void updatePerson(ReadOnlyPerson target, ReadOnlyPerson editedPerson)
             throws DuplicatePersonException, PersonNotFoundException;
 
-    /** Returns the filtered person list as an {@code UnmodifiableObservableList<ReadOnlyPerson>} */
-    UnmodifiableObservableList<ReadOnlyPerson> getFilteredPersonList();
+    /** Returns an unmodifiable view of the filtered person list */
+    ObservableList<ReadOnlyPerson> getFilteredPersonList();
 
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
