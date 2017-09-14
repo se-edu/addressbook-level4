f39a364b1fdfc13de6b6a0e0c15a921ec9ddd20c
eugenepeh <e0012835@u.nus.edu>
2017-09-14 08:52:47 +0800

    Checkstyle: enforce one variable declaration per line
    Having several variables are on the same line risk having conflicts
    for unrelated modifications by different developers.
    
    Let's teach Checkstyle to enforce the rule
    MultipleVariableDeclarations to check that there are no multiple
    variable declarations on single lines.

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -24,40 +24,39 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
  */
 public class TypicalPersons {
 
-    public static final ReadOnlyPerson ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE, HOON, IDA, AMY, BOB;
+    public static final ReadOnlyPerson ALICE = new PersonBuilder().withName("Alice Pauline")
+            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
+            .withPhone("85355255")
+            .withTags("friends").build();
+    public static final ReadOnlyPerson BENSON = new PersonBuilder().withName("Benson Meier")
+            .withAddress("311, Clementi Ave 2, #02-25")
+            .withEmail("johnd@example.com").withPhone("98765432")
+            .withTags("owesMoney", "friends").build();
+    public static final ReadOnlyPerson CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
+            .withEmail("heinz@example.com").withAddress("wall street").build();
+    public static final ReadOnlyPerson DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
+            .withEmail("cornelia@example.com").withAddress("10th street").build();
+    public static final ReadOnlyPerson ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
+            .withEmail("werner@example.com").withAddress("michegan ave").build();
+    public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
+            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+    public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
+            .withEmail("anna@example.com").withAddress("4th street").build();
+
+    // Manually added
+    public static final ReadOnlyPerson HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
+            .withEmail("stefan@example.com").withAddress("little india").build();
+    public static final ReadOnlyPerson IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
+            .withEmail("hans@example.com").withAddress("chicago ave").build();
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final ReadOnlyPerson AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
+    public static final ReadOnlyPerson BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
+            .build();
+
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
-
-    static {
-        ALICE = new PersonBuilder().withName("Alice Pauline")
-                .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-                .withPhone("85355255")
-                .withTags("friends").build();
-        BENSON = new PersonBuilder().withName("Benson Meier").withAddress("311, Clementi Ave 2, #02-25")
-                .withEmail("johnd@example.com").withPhone("98765432")
-                .withTags("owesMoney", "friends").build();
-        CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-                .withEmail("heinz@example.com").withAddress("wall street").build();
-        DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-                .withEmail("cornelia@example.com").withAddress("10th street").build();
-        ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-                .withEmail("werner@example.com").withAddress("michegan ave").build();
-        FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-                .withEmail("lydia@example.com").withAddress("little tokyo").build();
-        GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-                .withEmail("anna@example.com").withAddress("4th street").build();
-
-        // Manually added
-        HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-                .withEmail("stefan@example.com").withAddress("little india").build();
-        IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-                .withEmail("hans@example.com").withAddress("chicago ave").build();
-
-        // Manually added - Person's details found in {@code CommandTestUtil}
-        AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY)
-                .withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
-        BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
-                .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
-    }
 
     private TypicalPersons() {} // prevents instantiation
 
