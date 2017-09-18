baaaffebdf1500b657b10120dfd7e8772522a812
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-09-18 17:42:51 +0800

    SampleDataTest: Fix buggy implementation of getDataFileLocation()
    getDataFileLocation() must return a non-existent file location as part
    of this method's post-condition.
    
    However, this isn't ensured in the current implementation. As a result,
    the test case in SampleDataTest may fail.
    
    Let's update getDataFileLocation() to ensure that it returns a
    non-existent file location.

--- src/test/java/guitests/SampleDataTest.java
+++ src/test/java/guitests/SampleDataTest.java
@@ -2,6 +2,10 @@ package guitests;
 
 import static seedu.address.ui.testutil.GuiTestAssert.assertListMatching;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 import seedu.address.model.AddressBook;
@@ -19,7 +23,20 @@ public class SampleDataTest extends AddressBookGuiTest {
     @Override
     protected String getDataFileLocation() {
         // return a non-existent file location to force test app to load sample data
-        return TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
+        String filePath = TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
+        deleteFileIfExists(filePath);
+        return filePath;
+    }
+
+    /**
+     * Deletes the file at {@code filePath} if it exists.
+     */
+    private void deleteFileIfExists(String filePath) {
+        try {
+            Files.deleteIfExists(Paths.get(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError(ioe);
+        }
     }
 
     @Test
