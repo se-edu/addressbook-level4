b46c3f4fe1567a1b2d95530a95049c5f8b8052d3
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-09-19 13:19:58 +0800

    Interdiff between v1 and v2

--- src/test/java/systemtests/HelpCommandTest.java
+++ src/test/java/systemtests/HelpCommandTest.java
@@ -13,6 +13,10 @@ import seedu.address.logic.commands.HelpCommand;
  * TODO: This test is incomplete.
  */
 public class HelpCommandTest extends AddressBookSystemTest {
+    private static final String ERROR_MESSAGE = "ATTENTION!!!! : On some computers, this test may fail when run on "
+            + "non-headless mode as FxRobot#clickOn(Node, MouseButton...) clicks on the wrong location. We suspect "
+            + "that this is a bug with TestFX library that we are using. If this test fails, you have to run your "
+            + "tests on headless mode. See UsingGradle.adoc on how to do so.";
 
     private final GuiRobot guiRobot = new GuiRobot();
 
