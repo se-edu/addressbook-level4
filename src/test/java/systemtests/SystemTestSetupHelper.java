758c3c1d8e10c1278fb6db19b7be82da447f8a64
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-09-19 13:14:20 +0800

    SampleDataTest: Move to systemtests
    As AddressBookGuiTest and AddressBookSystemTest are very similar, it’s
    not necessary to have both of them.
    
    As part of removing AddressBookGuiTest, let’s move SampleDataTest to
    systemtests.

--- src/test/java/systemtests/SystemTestSetupHelper.java
+++ src/test/java/systemtests/SystemTestSetupHelper.java
@@ -1,12 +1,13 @@
 package systemtests;
 
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
 import org.testfx.api.FxToolkit;
 
 import guitests.guihandles.MainWindowHandle;
 import seedu.address.TestApp;
-import seedu.address.testutil.TypicalPersons;
+import seedu.address.model.ReadOnlyAddressBook;
 
 /**
  * Contains helper methods that system tests require.
@@ -18,10 +19,9 @@ public class SystemTestSetupHelper {
     /**
      * Sets up the {@code TestApp} and returns it.
      */
-    public TestApp setupApplication() {
+    public TestApp setupApplication(Supplier<ReadOnlyAddressBook> addressBook, String saveFileLocation) {
         try {
-            FxToolkit.setupApplication(() -> testApp = new TestApp(TypicalPersons::getTypicalAddressBook,
-                    TestApp.SAVE_LOCATION_FOR_TESTING));
+            FxToolkit.setupApplication(() -> testApp = new TestApp(addressBook, saveFileLocation));
         } catch (TimeoutException te) {
             throw new AssertionError("Application takes too long to set up.");
         }
