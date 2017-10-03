ba2eebc426f1ad5d639a09c5030790eb1314558f
Lawrence Li <31027322+Zzmobie@users.noreply.github.com>
2017-10-03 23:42:03 +0800

    Update Storage Component (#7)
    * Update Storage Component
    
    * Update Storage Component

--- src/main/java/seedu/address/storage/AddressBookStorage.java
+++ src/main/java/seedu/address/storage/AddressBookStorage.java
@@ -41,4 +41,6 @@ public interface AddressBookStorage {
      */
     void saveAddressBook(ReadOnlyAddressBook addressBook, String filePath) throws IOException;
 
+    void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+
 }
