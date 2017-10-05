7c5ae266189827905175757cc263552e06b34da0
Junkai <reginleiff@gmail.com>
2017-10-05 17:48:56 +0800

    Modify RemarkCommand to take in an Index and String and print those two parameters as the error message.

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,14 +1,22 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.UndoableCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 
 public class RemarkCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "remark";
 
+    public final int INDEX;
+
+    public final String STR;
+
+    public RemarkCommand(int index, String str) {
+        INDEX = index;
+        STR = str;
+    }
+
     @Override
     protected CommandResult executeUndoableCommand() throws CommandException {
-        throw new CommandException("Lol");
+        throw new CommandException(INDEX + " " + STR);
     }
 }
