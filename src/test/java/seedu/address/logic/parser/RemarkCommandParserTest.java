cadac003f621aeffde86fcea713ef17782a8bbc6
Junkai <reginleiff@gmail.com>
2017-10-05 22:30:04 +0800

    Fix trailing whitespace

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -15,8 +15,6 @@ public class RemarkCommandParserTest {
 
     @Test
     public void parse_invalidArguments_throwsParseException() throws Exception {
-
-        
         assertParseFailure(parser, "r/hey", String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "r/lol 1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "1 hey", String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE));
@@ -26,7 +24,6 @@ public class RemarkCommandParserTest {
     public void parse_validArguments_success() throws Exception {
         Command expectedCommand;
         String userInput;
-
 //        expectedCommand= new RemarkCommand(1, "lmao");
 //        userInput  = "1 r/lmao";
 //        assertParseSuccess(parser, userInput, expectedCommand);
@@ -35,7 +32,5 @@ public class RemarkCommandParserTest {
 //        expectedCommand= new RemarkCommand(5, "hey");
 //        userInput  = "5 r/hey";
 //        assertParseSuccess(parser, userInput, expectedCommand);
-
     }
-
 }
