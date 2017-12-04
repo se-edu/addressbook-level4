a0df032132bf5047d8338bbd081cf93ffb0a9dfd
Paul Tan <pyokagan@gmail.com>
2017-12-04 14:48:52 +0800

    NameTest, PhoneTest, EmailTest, AddressTest, TagTest: add missing null tests
    The constructors and isValidX() methods of these classes would throw
    NullPointerExceptions on null inputs, but this behavior is not tested.
    Let's add tests for them.

---
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -0,0 +1,20 @@
+package seedu.address.model.tag;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class TagTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Tag(null));
+    }
+
+    @Test
+    public void isValidTagName() {
+        // null tag name
+        Assert.assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+    }
+
+}
