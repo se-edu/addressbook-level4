808a8b0037721de7c95dc42729cd39594c1e6f54
Paul Tan <pyokagan@gmail.com>
2017-12-05 15:00:27 +0800

    XmlAdaptedPersonTest: add tests for #toModelType()
    XmlAdaptedPersonTest#toModelType() doesn't have any dedicated tests.
    Let's add some.
    
    This is mainly done to prevent code coverage drops when we tweak
    XmlAdaptedPersonTest#toModelType() in later commits.

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -39,6 +39,18 @@ public class XmlAdaptedPerson {
      */
     public XmlAdaptedPerson() {}
 
+    /**
+     * Constructs an {@code XmlAdaptedPerson} with the given person details.
+     */
+    public XmlAdaptedPerson(String name, String phone, String email, String address, List<XmlAdaptedTag> tagged) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        if (tagged != null) {
+            this.tagged = new ArrayList<>(tagged);
+        }
+    }
 
     /**
      * Converts a given Person into this class for JAXB use.
