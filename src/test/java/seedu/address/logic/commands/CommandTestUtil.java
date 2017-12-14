50b7b3c9101a428494e1a64a965aa138c1145356
Unknown <vivekscl5@gmail.com>
2017-12-14 15:19:18 +0800

    CommandTestUtil: add non-empty preamble constant

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -53,6 +53,8 @@ public class CommandTestUtil {
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
+    public static final String NON_EMPTY_PREAMBLE = " " + "FormattingProblem";
+
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
 
