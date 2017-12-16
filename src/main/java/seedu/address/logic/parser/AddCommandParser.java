71634e0048aa88c027562127db5e175eb924f478
Unknown <vivekscl5@gmail.com>
2017-12-17 00:24:17 +0800

    AddCommandParser: add check to ensure preamble is empty
    AddCommandParser accepts an invalid command format, in which the
    preamble in not empty.
    
    A check to ensure the preamble is empty is more user-friendly as the
    user is notified of the invalid input.
    
    Let's add a check to ensure that argMultimap.getPreamble() is empty.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -35,7 +35,8 @@ public class AddCommandParser implements Parser<AddCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
+                || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
