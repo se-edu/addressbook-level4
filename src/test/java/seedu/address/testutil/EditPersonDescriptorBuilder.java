36571011b5082c3527b8c653e5a0feaa4960a2cb
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-12-20 12:02:49 +0800

    PersonBuilder: Update methods to not use Person’s setter methods
    PersonBuilder creates a Person object upon initialization, and uses
    Person’s setter methods to update the values of the Person object.
    
    As part of making Person to be immutable, let's update these methods
    to not use Person’s setter methods. Instead of having a field of type
    Person, PersonBuilder will have 5 fields of types Name, Phone, Email,
    Address & Set<Tag> to represent the values of the Person object that it
    is building.

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -1,5 +1,6 @@
 package seedu.address.testutil;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -36,7 +37,7 @@ public class EditPersonDescriptorBuilder {
         descriptor.setPhone(person.getPhone());
         descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
-        descriptor.setTags(person.getTags());
+        descriptor.setTags(new HashSet<>(person.getTags()));
     }
 
     /**
