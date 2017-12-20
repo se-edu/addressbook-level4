47da4fcac615ff88b37d6e4afb01784265f5ff7b
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-12-20 12:02:49 +0800

    PersonBuilder: Update methods to not use Person’s setter methods
    PersonBuilder creates a Person object upon initialisation, and uses
    Person’s setter methods to update the values of the Person object.
    
    As part of making Person to be immutable, let's update these methods
    to not use Person’s setter methods. Instead of having a field of type
    Person, PersonBuilder will have 5 fields of types Name, Phone, Email,
    Address & Set<Tag> to represent the values of the Person object that it
    is building.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -22,29 +22,44 @@ public class PersonBuilder {
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
     public static final String DEFAULT_TAGS = "friends";
 
-    private Person person;
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
+    private Set<Tag> tags;
 
     public PersonBuilder() {
-        Name defaultName = new Name(DEFAULT_NAME);
-        Phone defaultPhone = new Phone(DEFAULT_PHONE);
-        Email defaultEmail = new Email(DEFAULT_EMAIL);
-        Address defaultAddress = new Address(DEFAULT_ADDRESS);
-        Set<Tag> defaultTags = SampleDataUtil.getTagSet(DEFAULT_TAGS);
-        this.person = new Person(defaultName, defaultPhone, defaultEmail, defaultAddress, defaultTags);
+        try {
+            name = new Name(DEFAULT_NAME);
+            phone = new Phone(DEFAULT_PHONE);
+            email = new Email(DEFAULT_EMAIL);
+            address = new Address(DEFAULT_ADDRESS);
+            tags = SampleDataUtil.getTagSet(DEFAULT_TAGS);
+        } catch (IllegalValueException ive) {
+            throw new AssertionError("Default person's values are invalid.");
+        }
     }
 
     /**
      * Initializes the PersonBuilder with the data of {@code personToCopy}.
      */
     public PersonBuilder(ReadOnlyPerson personToCopy) {
-        this.person = new Person(personToCopy);
+        name = personToCopy.getName();
+        phone = personToCopy.getPhone();
+        email = personToCopy.getEmail();
+        address = personToCopy.getAddress();
+        tags = personToCopy.getTags();
     }
 
     /**
      * Sets the {@code Name} of the {@code Person} that we are building.
      */
     public PersonBuilder withName(String name) {
-        this.person.setName(new Name(name));
+        try {
+            this.name = new Name(name);
+        } catch (IllegalValueException ive) {
+            throw new IllegalArgumentException("name is expected to be unique.");
+        }
         return this;
     }
 
@@ -52,7 +67,11 @@ public class PersonBuilder {
      * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
      */
     public PersonBuilder withTags(String ... tags) {
-        this.person.setTags(SampleDataUtil.getTagSet(tags));
+        try {
+            this.tags = SampleDataUtil.getTagSet(tags);
+        } catch (IllegalValueException ive) {
+            throw new IllegalArgumentException("tags are expected to be unique.");
+        }
         return this;
     }
 
@@ -60,7 +79,11 @@ public class PersonBuilder {
      * Sets the {@code Address} of the {@code Person} that we are building.
      */
     public PersonBuilder withAddress(String address) {
-        this.person.setAddress(new Address(address));
+        try {
+            this.address = new Address(address);
+        } catch (IllegalValueException ive) {
+            throw new IllegalArgumentException("address is expected to be unique.");
+        }
         return this;
     }
 
@@ -68,7 +91,11 @@ public class PersonBuilder {
      * Sets the {@code Phone} of the {@code Person} that we are building.
      */
     public PersonBuilder withPhone(String phone) {
-        this.person.setPhone(new Phone(phone));
+        try {
+            this.phone = new Phone(phone);
+        } catch (IllegalValueException ive) {
+            throw new IllegalArgumentException("phone is expected to be unique.");
+        }
         return this;
     }
 
@@ -76,12 +103,16 @@ public class PersonBuilder {
      * Sets the {@code Email} of the {@code Person} that we are building.
      */
     public PersonBuilder withEmail(String email) {
-        this.person.setEmail(new Email(email));
+        try {
+            this.email = new Email(email);
+        } catch (IllegalValueException ive) {
+            throw new IllegalArgumentException("email is expected to be unique.");
+        }
         return this;
     }
 
     public Person build() {
-        return this.person;
+        return new Person(name, phone, email, address, tags);
     }
 
 }
