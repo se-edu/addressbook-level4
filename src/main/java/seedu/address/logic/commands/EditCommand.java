a8ee0d85b9140871775d3245098bffdeee567f41
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-01 19:48:56 +0800

    AddressBookParser: Add alias for EditCommand
    In order to invoke EditCommand, the user has to type "edit ...".
    
    As the command is commonly used, typing the full command word
    "edit" can be troublesome.
    
    Let's teach AddressBookParser to accept the alias "e" as another way
    of invoking the EditCommand.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -32,6 +32,7 @@ import seedu.address.model.tag.Tag;
 public class EditCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "edit";
+    public static final String COMMAND_ALIAS = "e";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
             + "by the index number used in the last person listing. "
