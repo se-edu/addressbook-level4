c398d585e9050ba945a422e6c4eed31c25d6157b
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-04 15:35:08 +0800

    Model: Add deleteTag(Tag)
    The Model API does not expose a way to delete a particular Tag entirely
    from the AddressBook (i.e. removing the Tag from all Persons and purging
    the Tag in the AddressBook's tag list).
    
    Let's add Model#deleteTag(Tag).

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -15,6 +15,7 @@ import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -79,6 +80,11 @@ public class ModelManager extends ComponentManager implements Model {
 
         addressBook.updatePerson(target, editedPerson);
         indicateAddressBookChanged();
+    }
+
+    @Override
+    public void deleteTag(Tag tag) {
+        addressBook.removeTag(tag);
     }
 
     //=========== Filtered Person List Accessors =============================================================
