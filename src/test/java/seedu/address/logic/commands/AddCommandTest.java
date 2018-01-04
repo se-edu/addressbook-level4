e348432ca15d10c867dc34ace950b9724fd1fd8a
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-04 15:35:08 +0800

    Model: Add deleteTag(Tag)
    The Model API does not expose a way to delete a particular Tag entirely
    from the AddressBook (i.e. removing the Tag from all Persons and purging
    the Tag in the AddressBook's tag list).
    
    Let's add Model#deleteTag(Tag).

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -24,6 +24,7 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandTest {
@@ -131,6 +132,11 @@ public class AddCommandTest {
 
         @Override
         public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
+            fail("This method should not be called.");
+        }
+
+        @Override
+        public void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException {
             fail("This method should not be called.");
         }
     }
