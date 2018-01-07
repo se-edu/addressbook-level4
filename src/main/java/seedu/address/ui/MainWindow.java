a740e78c74710a1ddf407bf3952dc940ff03072e
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-07 19:15:52 +0800

    StatusBarFooter: Show total persons
    In order to determine the total number of contacts in the address book,
    the user has to scroll the list all the way down to the last person, as
    the ID of the last person gives an indication of the total number of
    persons.
    
    This forces the user to use the graphical user interface to find such an
    information.
    
    Let's modify StatusBarFooter so that it shows the total number of
    persons in the current address book.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -125,7 +125,8 @@ public class MainWindow extends UiPart<Stage> {
         ResultDisplay resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(prefs.getAddressBookFilePath());
+        StatusBarFooter statusBarFooter = new StatusBarFooter(prefs.getAddressBookFilePath(),
+                logic.getFilteredPersonList().size());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
         CommandBox commandBox = new CommandBox(logic);
