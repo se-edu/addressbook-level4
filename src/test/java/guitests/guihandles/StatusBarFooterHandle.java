473bf239a5d33387665ffc3e07107a464fa79e38
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-07 19:15:52 +0800

    StatusBarFooter: Show total persons
    In order to determine the total number of contacts in the address book,
    the user has to scroll the list all the way down to the last person, as
    the ID of the last person gives an indication of the total number of
    persons.
    
    This forces the user to use the graphical user interface to find such an
    information.
    
    Let's modify StatusBarFooter so that it shows the total number of
    persons in the current address book.

--- src/test/java/guitests/guihandles/StatusBarFooterHandle.java
+++ src/test/java/guitests/guihandles/StatusBarFooterHandle.java
@@ -11,18 +11,22 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
     public static final String STATUS_BAR_PLACEHOLDER = "#statusbarPlaceholder";
 
     private static final String SYNC_STATUS_ID = "#syncStatus";
+    private static final String TOTAL_PERSONS_STATUS_ID = "#totalPersonsStatus";
     private static final String SAVE_LOCATION_STATUS_ID = "#saveLocationStatus";
 
     private final StatusBar syncStatusNode;
+    private final StatusBar totalPersonsStatusNode;
     private final StatusBar saveLocationNode;
 
     private String lastRememberedSyncStatus;
+    private String lastRememberedTotalPersonsStatus;
     private String lastRememberedSaveLocation;
 
     public StatusBarFooterHandle(Node statusBarFooterNode) {
         super(statusBarFooterNode);
 
         this.syncStatusNode = getChildNode(SYNC_STATUS_ID);
+        this.totalPersonsStatusNode = getChildNode(TOTAL_PERSONS_STATUS_ID);
         this.saveLocationNode = getChildNode(SAVE_LOCATION_STATUS_ID);
     }
 
@@ -31,6 +35,13 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
      */
     public String getSyncStatus() {
         return syncStatusNode.getText();
+    }
+
+    /**
+     * Returns the text of the 'total persons' portion of the status bar.
+     */
+    public String getTotalPersonsStatus() {
+        return totalPersonsStatusNode.getText();
     }
 
     /**
@@ -53,6 +64,22 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
      */
     public boolean isSyncStatusChanged() {
         return !lastRememberedSyncStatus.equals(getSyncStatus());
+    }
+
+
+    /**
+     * Remembers the content of the 'total persons' portion of the status bar.
+     */
+    public void rememberTotalPersonsStatus() {
+        lastRememberedTotalPersonsStatus = getTotalPersonsStatus();
+    }
+
+    /**
+     * Returns true if the current content of the 'total persons' is different from the value remembered by the most
+     * recent {@code rememberTotalPersonsStatus()} call.
+     */
+    public boolean isTotalPersonsStatusChanged() {
+        return !lastRememberedTotalPersonsStatus.equals(getTotalPersonsStatus());
     }
 
     /**
