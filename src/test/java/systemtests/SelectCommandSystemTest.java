9b4657a0f0664d0716e47b261e2e466699a3cad9
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-07 18:04:45 +0800

    AddressBookSystemTest: Verify that ResultDisplay style changes
    The ResultDisplay style changes if there is an error in command
    execution.
    
    This behavior is not verified by any system tests.
    
    Let's update the system tests to verify that.

--- src/test/java/systemtests/SelectCommandSystemTest.java
+++ src/test/java/systemtests/SelectCommandSystemTest.java
@@ -126,7 +126,7 @@ public class SelectCommandSystemTest extends AddressBookSystemTest {
             assertSelectedCardChanged(expectedSelectedCardIndex);
         }
 
-        assertCommandBoxShowsDefaultStyle();
+        assertCommandBoxAndResultDisplayShowsDefaultStyle();
         assertStatusBarUnchanged();
     }
 
@@ -147,7 +147,7 @@ public class SelectCommandSystemTest extends AddressBookSystemTest {
         executeCommand(command);
         assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
         assertSelectedCardUnchanged();
-        assertCommandBoxShowsErrorStyle();
+        assertCommandBoxAndResultDisplayShowsErrorStyle();
         assertStatusBarUnchanged();
     }
 }
