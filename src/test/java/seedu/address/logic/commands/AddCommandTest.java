a038c1999c95a1a4f39a3046d2338cbbcc820966
YAMIDARK\Jun An <e0002937@u.nus.edu>
2018-01-09 00:49:01 +0800

    Model: Add method to get Predicate of filteredPersons
    The Model does not contain any method to get the Predicate
    of its filteredPersons.
    
    As such other classes have no means to get the Predicate
    of the filteredPersons if needed.
    
    Let's add a method in Model to get the Predicate of
    its filteredPersons.

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -130,6 +130,12 @@ public class AddCommandTest {
         }
 
         @Override
+        public Predicate<ReadOnlyPerson> getFilteredPersonListPredicate() {
+            fail("This method should not be called.");
+            return null;
+        }
+
+        @Override
         public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
             fail("This method should not be called.");
         }
@@ -148,6 +154,11 @@ public class AddCommandTest {
         public ReadOnlyAddressBook getAddressBook() {
             return new AddressBook();
         }
+
+        @Override
+        public Predicate<ReadOnlyPerson> getFilteredPersonListPredicate() {
+            return PREDICATE_SHOW_ALL_PERSONS;
+        }
     }
 
     /**
@@ -164,6 +175,11 @@ public class AddCommandTest {
         @Override
         public ReadOnlyAddressBook getAddressBook() {
             return new AddressBook();
+        }
+
+        @Override
+        public Predicate<ReadOnlyPerson> getFilteredPersonListPredicate() {
+            return PREDICATE_SHOW_ALL_PERSONS;
         }
     }
 
