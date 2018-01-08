7688076c0f4fe9a2d646a5b782908a739f0b5a55
YAMIDARK\Jun An <e0002937@u.nus.edu>
2018-01-09 00:49:01 +0800

    Model: Add method to get Predicate of filteredPersons
    The Model does not contain any method to get the Predicate
    of its filteredPersons.
    
    As such other classes have no means to get the Predicate
    of the filteredPersons if needed.
    
    Let's add a method in Model to get the Predicate of
    its filteredPersons.

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -93,6 +93,12 @@ public class ModelManager extends ComponentManager implements Model {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
+    public Predicate<ReadOnlyPerson> getFilteredPersonListPredicate() {
+        return (Predicate<ReadOnlyPerson>) filteredPersons.getPredicate();
+    }
+
+    @Override
     public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
         requireNonNull(predicate);
         filteredPersons.setPredicate(predicate);
