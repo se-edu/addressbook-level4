c4e483522e48300a98ae48750b3bb937fd6f2cf9
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-01-10 09:24:49 +0800

    Person: Inline ReadOnlyPerson’s methods
    ReadOnlyPerson#isSameStateAs(ReadOnlyPerson) and
    ReadOnlyPerson#getAsText() are used by Person.
    
    In the next commit, we will be deleting ReadOnlyPerson. In preparation
    for that, let's inline these methods into Person.

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -15,34 +15,4 @@ public interface ReadOnlyPerson {
     Email getEmail();
     Address getAddress();
     Set<Tag> getTags();
-
-    /**
-     * Returns true if both have the same state. (interfaces cannot override .equals)
-     */
-    default boolean isSameStateAs(ReadOnlyPerson other) {
-        return other == this // short circuit if same object
-                || (other != null // this is first to avoid NPE below
-                && other.getName().equals(this.getName()) // state checks here onwards
-                && other.getPhone().equals(this.getPhone())
-                && other.getEmail().equals(this.getEmail())
-                && other.getAddress().equals(this.getAddress()));
-    }
-
-    /**
-     * Formats the person as text, showing all contact details.
-     */
-    default String getAsText() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" Phone: ")
-                .append(getPhone())
-                .append(" Email: ")
-                .append(getEmail())
-                .append(" Address: ")
-                .append(getAddress())
-                .append(" Tags: ");
-        getTags().forEach(builder::append);
-        return builder.toString();
-    }
-
 }
