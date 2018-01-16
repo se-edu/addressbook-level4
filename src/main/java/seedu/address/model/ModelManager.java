7ca1b13c9f0c504d8b69aedfe7569a413c62546a
YAMIDARK\Jun An <e0002937@u.nus.edu>
2018-01-16 20:39:02 +0800

    ModelManager: Set default Predicate for filteredPersons
    A default predicate is not set for the filtered list when the
    ModelManager is initialized, thus a null value is set instead.
    
    This could cause problems when accessing and using the filtered list
    predicate since null predicates are not dealt with in our code base.
    
    Let's update the ModelManager constructor to set the default predicate
    of the filtered list to show all persons.
    
    Refer to this commit on why we do not deal with null predicates
    https://github.com/se-edu/addressbook-level4/commit/60753179a8875fd4b2af4da7ed43185284e210b7

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -37,6 +37,8 @@ public class ModelManager extends ComponentManager implements Model {
 
         this.addressBook = new AddressBook(addressBook);
         filteredPersons = new FilteredList<>(this.addressBook.getPersonList());
+        // Sets a default predicate as null predicates are not dealt with
+        filteredPersons.setPredicate(PREDICATE_SHOW_ALL_PERSONS);
     }
 
     public ModelManager() {
