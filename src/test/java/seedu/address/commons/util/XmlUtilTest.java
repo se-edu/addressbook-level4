6485df9bc88136b440f564e47f9b76db3c935fec
Jun An <e0002937@u.nus.edu>
2018-01-20 15:37:23 +0800

    XmlAdaptedPerson#toModelType: add non-null check for parameters
    XmlAdaptedPerson#toModelType() does not have a non-null check for
    Person's fields before isValid*(String) is called.
    
    When null value is passed as a parameter into isValid*(String), a
    NullPointerException is thrown, causing the app to crash.
    
    Let's update XmlAdaptedPerson#toModelType() to perform a separate
    non-null check for each parameter before passing them to
    isValid*(String).
    
    Let's also update the old tests in XmlAdaptedPersonTest to call
    Assert#assertThrows(Class, String, VoidCallable) instead to verify
    that the correct error message is thrown too, and add tests in
    XmlUtilTest for reading of XmlAdaptedPerson from .xml files.

--- src/test/java/seedu/address/commons/util/XmlUtilTest.java
+++ src/test/java/seedu/address/commons/util/XmlUtilTest.java
@@ -4,14 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.model.AddressBook;
+import seedu.address.storage.XmlAdaptedPerson;
+import seedu.address.storage.XmlAdaptedTag;
 import seedu.address.storage.XmlSerializableAddressBook;
 import seedu.address.testutil.AddressBookBuilder;
 import seedu.address.testutil.PersonBuilder;
@@ -23,7 +28,18 @@ public class XmlUtilTest {
     private static final File EMPTY_FILE = new File(TEST_DATA_FOLDER + "empty.xml");
     private static final File MISSING_FILE = new File(TEST_DATA_FOLDER + "missing.xml");
     private static final File VALID_FILE = new File(TEST_DATA_FOLDER + "validAddressBook.xml");
+    private static final File MISSING_PERSON_FIELD_FILE = new File(TEST_DATA_FOLDER + "missingPersonField.xml");
+    private static final File INVALID_PERSON_FIELD_FILE = new File(TEST_DATA_FOLDER + "invalidPersonField.xml");
+    private static final File VALID_PERSON_FILE = new File(TEST_DATA_FOLDER + "validPerson.xml");
     private static final File TEMP_FILE = new File(TestUtil.getFilePathInSandboxFolder("tempAddressBook.xml"));
+
+    private static final String INVALID_PHONE = "9482asf424";
+
+    private static final String VALID_NAME = "Hans Muster";
+    private static final String VALID_PHONE = "9482424";
+    private static final String VALID_EMAIL = "hans@example";
+    private static final String VALID_ADDRESS = "4th street";
+    private static final List<XmlAdaptedTag> VALID_TAGS = Collections.singletonList(new XmlAdaptedTag("friends"));
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -60,6 +76,33 @@ public class XmlUtilTest {
     }
 
     @Test
+    public void xmlAdaptedPersonFromFile_fileWithMissingPersonField_validResult() throws Exception {
+        XmlAdaptedPerson actualPerson = XmlUtil.getDataFromFile(
+                MISSING_PERSON_FIELD_FILE, XmlAdaptedPersonWithRootElement.class);
+        XmlAdaptedPerson expectedPerson = new XmlAdaptedPerson(
+                null, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
+        assertEquals(expectedPerson, actualPerson);
+    }
+
+    @Test
+    public void xmlAdaptedPersonFromFile_fileWithinvalidPersonField_validResult() throws Exception {
+        XmlAdaptedPerson actualPerson = XmlUtil.getDataFromFile(
+                INVALID_PERSON_FIELD_FILE, XmlAdaptedPersonWithRootElement.class);
+        XmlAdaptedPerson expectedPerson = new XmlAdaptedPerson(
+                VALID_NAME, INVALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
+        assertEquals(expectedPerson, actualPerson);
+    }
+
+    @Test
+    public void xmlAdaptedPersonFromFile_fileWithvalidPerson_validResult() throws Exception {
+        XmlAdaptedPerson actualPerson = XmlUtil.getDataFromFile(
+                VALID_PERSON_FILE, XmlAdaptedPersonWithRootElement.class);
+        XmlAdaptedPerson expectedPerson = new XmlAdaptedPerson(
+                VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
+        assertEquals(expectedPerson, actualPerson);
+    }
+
+    @Test
     public void saveDataToFile_nullFile_throwsNullPointerException() throws Exception {
         thrown.expect(NullPointerException.class);
         XmlUtil.saveDataToFile(null, new AddressBook());
@@ -93,4 +136,11 @@ public class XmlUtilTest {
         dataFromFile = XmlUtil.getDataFromFile(TEMP_FILE, XmlSerializableAddressBook.class);
         assertEquals(dataToWrite, dataFromFile);
     }
+
+    /**
+     * Test class annotated with {@code XmlRootElement} to allow unmarshalling of .xml data to {@code XmlAdaptedPerson}
+     * objects.
+     */
+    @XmlRootElement(name = "person")
+    private static class XmlAdaptedPersonWithRootElement extends XmlAdaptedPerson {}
 }
