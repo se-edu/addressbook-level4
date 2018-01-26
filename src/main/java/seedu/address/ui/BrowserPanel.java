2be61349f2c8d2e49d37967abb6ceb07fb4e38c1
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-01-26 10:57:51 +0800

    BrowserPanel: load placeholder page instead of Google search page
    BrowserPanel loads the Google search page whenever a person card is
    selected, and there’s tests written for this feature.
    
    When many forks of this project are running automated tests, too many
    search requests are sent to Google, causing us to exceed the search
    service’s rate limits. This is an abuse of Google’s search service.
    
    Let’s update BrowserPanel to load a placeholder page hosted internally
    on NUS SoC web server. SoC web servers are rather stable though they may
    go down once in a while.

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -23,6 +23,7 @@ public class BrowserPanel extends UiPart<Region> {
     public static final String DEFAULT_PAGE = "default.html";
     public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
     public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
+    public static final String PLACEHOLDER_URL = "http://www.comp.nus.edu.sg/~seer/search.html?name=";
 
     private static final String FXML = "BrowserPanel.fxml";
 
@@ -42,8 +43,7 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     private void loadPersonPage(Person person) {
-        loadPage(GOOGLE_SEARCH_URL_PREFIX + person.getName().fullName.replaceAll(" ", "+")
-                + GOOGLE_SEARCH_URL_SUFFIX);
+        loadPage(PLACEHOLDER_URL + person.getName().fullName);
     }
 
     public void loadPage(String url) {
