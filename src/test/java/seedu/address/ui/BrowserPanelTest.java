1f4185013c697e7a373fd90c4b4ea5fc7258adfc
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-01-26 10:57:51 +0800

    BrowserPanel: load placeholder page instead of Google search page
    BrowserPanel loads the Google search page whenever a person card is
    selected, and there’s tests written for this feature.
    
    When many forks of this project are running automated tests, too many
    search requests are sent to Google, causing us to exceed the search
    service’s rate limits. This is an abuse of Google’s search service.
    
    Let’s update BrowserPanel to load a placeholder page hosted internally
    on NUS SoC web server. SoC web servers are rather stable though they may
    go down once in a while.

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_SUFFIX;
 import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
@@ -42,8 +40,7 @@ public class BrowserPanelTest extends GuiUnitTest {
 
         // associated web page of a person
         postNow(selectionChangedEventStub);
-        URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX
-                + ALICE.getName().fullName.replaceAll(" ", "+") + GOOGLE_SEARCH_URL_SUFFIX);
+        URL expectedPersonUrl = new URL(BrowserPanel.PLACEHOLDER_URL);
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
