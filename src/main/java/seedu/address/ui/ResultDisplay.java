324616d0d03b78913fadd8c2c9d2cb487e6e187e
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-28 17:21:00 +0800

    ResultDisplay: Display message in red when command fails
    The ResultDisplay shows the same color (white) regardless of whether
    the command executed by the user succeeded or failed.
    
    Changing the color of the message when the command execution failed
    gives a clear visual signal to the user that he made a mistake, and
    that the message shown is an error, rather than a normal message.
    
    Let's modify ResultDisplay to display the message in red when the
    command execution fails.

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -7,6 +7,7 @@ import com.google.common.eventbus.Subscribe;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
@@ -17,6 +18,8 @@ import seedu.address.commons.events.ui.NewResultAvailableEvent;
  * A ui for the status bar that is displayed at the header of the application.
  */
 public class ResultDisplay extends UiPart<Region> {
+
+    public static final String ERROR_STYLE_CLASS = "error";
 
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
@@ -35,7 +38,35 @@ public class ResultDisplay extends UiPart<Region> {
     @Subscribe
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        Platform.runLater(() -> displayed.setValue(event.message));
+        Platform.runLater(() -> {
+            displayed.setValue(event.message);
+
+            if (event.isSuccessful) {
+                setStyleToIndicateCommandSuccess();
+            } else {
+                setStyleToIndicateCommandFailure();
+            }
+        });
+    }
+
+    /**
+     * Sets the {@code ResultDisplay} style to use the default style.
+     */
+    private void setStyleToIndicateCommandSuccess() {
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
+    }
+
+    /**
+     * Sets the {@code ResultDisplay} style to indicate a failed command.
+     */
+    private void setStyleToIndicateCommandFailure() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(ERROR_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(ERROR_STYLE_CLASS);
     }
 
 }
