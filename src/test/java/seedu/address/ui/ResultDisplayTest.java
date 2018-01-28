bdee10aa273d021c3ef9d24275fd5d30655029e5
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-28 17:22:42 +0800

    ResultDisplayTest: Verify display turns red on error
    ResultDisplay will turn red when command execution fails.
    
    This behavior is not verified by any of the unit tests for ResultDisplay.
    
    Let's update the unit tests in ResultDisplayTest to verify that
    behavior.

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -3,6 +3,9 @@ package seedu.address.ui;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,8 +14,14 @@ import seedu.address.commons.events.ui.NewResultAvailableEvent;
 
 public class ResultDisplayTest extends GuiUnitTest {
 
-    private static final NewResultAvailableEvent NEW_RESULT_EVENT_STUB =
-            new NewResultAvailableEvent("Stub", true);
+    private static final NewResultAvailableEvent NEW_RESULT_SUCCESS_EVENT_STUB =
+            new NewResultAvailableEvent("success", true);
+
+    private static final NewResultAvailableEvent NEW_RESULT_FAILURE_EVENT_STUB =
+            new NewResultAvailableEvent("failure", false);
+
+    private List<String> defaultStyleOfResultDisplay;
+    private List<String> errorStyleOfResultDisplay;
 
     private ResultDisplayHandle resultDisplayHandle;
 
@@ -23,6 +32,11 @@ public class ResultDisplayTest extends GuiUnitTest {
 
         resultDisplayHandle = new ResultDisplayHandle(getChildNode(resultDisplay.getRoot(),
                 ResultDisplayHandle.RESULT_DISPLAY_ID));
+
+        defaultStyleOfResultDisplay = new ArrayList<>(resultDisplayHandle.getStyleClass());
+
+        errorStyleOfResultDisplay = new ArrayList<>(defaultStyleOfResultDisplay);
+        errorStyleOfResultDisplay.add(ResultDisplay.ERROR_STYLE_CLASS);
     }
 
     @Test
@@ -30,10 +44,26 @@ public class ResultDisplayTest extends GuiUnitTest {
         // default result text
         guiRobot.pauseForHuman();
         assertEquals("", resultDisplayHandle.getText());
+        assertEquals(defaultStyleOfResultDisplay, resultDisplayHandle.getStyleClass());
 
-        // new result received
-        postNow(NEW_RESULT_EVENT_STUB);
+        // receiving new results
+        assertResultDisplay(NEW_RESULT_SUCCESS_EVENT_STUB);
+        assertResultDisplay(NEW_RESULT_FAILURE_EVENT_STUB);
+    }
+
+    /**
+     * Posts the {@code event} to the {@code EventsCenter}, then verifies that <br>
+     *      - the text on the result display matches the {@code event}'s message <br>
+     *      - the result display's style is the same as {@code defaultStyleOfResultDisplay} if event is successful,
+     *        {@code errorStyleOfResultDisplay} otherwise.
+     */
+    private void assertResultDisplay(NewResultAvailableEvent event) {
+        postNow(event);
         guiRobot.pauseForHuman();
-        assertEquals(NEW_RESULT_EVENT_STUB.message, resultDisplayHandle.getText());
+
+        List<String> expectedStyleClass = event.isSuccessful ? defaultStyleOfResultDisplay : errorStyleOfResultDisplay;
+
+        assertEquals(event.message, resultDisplayHandle.getText());
+        assertEquals(expectedStyleClass, resultDisplayHandle.getStyleClass());
     }
 }
