caf45b4974f737b3a0145420e03c9940dc7ecf4c
Tan Wang Leng <tanwangleng@outlook.com>
2018-01-28 17:16:35 +0800

    CommandBox: Store result's success status in NewResultAvailableEvent
    NewResultAvailableEvent only stores the message of the command
    execution. It does not store the success status.
    
    Subscribers such as ResultDisplay requires the success status to
    work.
    
    As part of teaching ResultDisplay to update the display according to
    the success status, let's modify NewResultAvailableEvent to store
    the success status.

--- src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
+++ src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
@@ -8,9 +8,11 @@ import seedu.address.commons.events.BaseEvent;
 public class NewResultAvailableEvent extends BaseEvent {
 
     public final String message;
+    public final boolean isSuccessful;
 
-    public NewResultAvailableEvent(String message) {
+    public NewResultAvailableEvent(String message, boolean isSuccessful) {
         this.message = message;
+        this.isSuccessful = isSuccessful;
     }
 
     @Override
