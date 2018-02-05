258ed0cddc71fcf2cec05293aaf96276333b62a1
Unknown <vivekscl5@gmail.com>
2018-02-05 18:46:56 +0800

    HelpWindow: modify HelpWindow#show() to use Stage#show()
    Some UI components do not function properly once the help window is 
    opened by typing help in the command box. For instance, the command box 
    does not display the result message of subsequent commands. However, 
    this does not occur when opening the help window by pressing F1 or 
    pressing help from the help menu. 
    
    This occurs due to a combination of using the default Dispatcher used by 
    the Guava EventBus system, Dispatcher#perThreadDispatchQueue() along 
    with calling Stage#showAndWait(). Calling Stage#showAndWait(), starts a 
    nested event loop to handle subsequent JavaFX events. However, the 
    default Dispatcher used by the EventBus does not allow nested callback 
    invocations, and instead queues them to be invoked after the current 
    invoked callback completes. This callback is the nested event loop 
    started by Stage#showAndWait() which only dispatches JavaFX events. As 
    such, EventBus events are queued up in the meantime resulting in the 
    subscribers of these events to wait until the nested event loop is 
    terminated. Therefore, the UI components that subscribe to the EventBus 
    events do not receive them in a timely manner and will not be updated.
    
    However, in the case of pressing F1 or pressing help from the help menu, 
    the MainWindow#handleHelp() is called immediately without posting an 
    EventBus event. As such, a nested event loop is not started and 
    subsequent EventBus events get dispatched as usual.
    
    Let's replace Stage#showAndWait() with Stage#show(), so 
    that there will be no entering of a nested loop and hence no blockage 
    of the Guava EventBus system.
    
    Likewise, this could be fixed by using the Dispatcher#immediate() 
    dispatcher instead of the default dispatcher used by the Guava EventBus 
    system. However, replacing Stage#showAndWait() with Stage#show() is 
    more suitable in this case as it is sufficient to show the help window 
    instead of starting a nested event loop to handle subsequent JavaFX 
    events while waiting for the help window to be closed.
    
    JavaFX documentation for Stage:
    https://docs.oracle.com/javase/8/javafx/api/javafx/stage/Stage.html
    
    Code and documentation for the Guava Dispatcher:
    https://github.com/google/guava/blob/master/guava/src/com/google/common/eventbus/Dispatcher.java
    
    More information on the Guava EventBus system:
    https://github.com/google/guava/wiki/EventBusExplained

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -1,13 +1,19 @@
 package systemtests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.ui.testutil.GuiTestAssert.assertListMatching;
 
 import org.junit.Test;
 
 import guitests.GuiRobot;
 import guitests.guihandles.HelpWindowHandle;
+import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.ui.BrowserPanel;
 
 /**
  * TODO: This test is incomplete as it is missing test cases.
@@ -46,6 +52,11 @@ public class HelpCommandSystemTest extends AddressBookSystemTest {
         //use command box
         executeCommand(HelpCommand.COMMAND_WORD);
         assertHelpWindowOpen();
+
+        //use command box to open help window and then assert that the UI has been updated
+        executeCommand(HelpCommand.COMMAND_WORD);
+        getMainWindowHandle().focus();
+        assertUiUpdated();
     }
 
     /**
@@ -64,6 +75,27 @@ public class HelpCommandSystemTest extends AddressBookSystemTest {
      */
     private void assertHelpWindowNotOpen() {
         assertFalse(ERROR_MESSAGE, HelpWindowHandle.isWindowPresent());
+    }
+
+    /**
+     * After execution of multiple commands, asserts that the,<br>
+     * 1. Command box displays an empty string.<br>
+     * 2. Command box has the default style class.<br>
+     * 3. Result display box is not empty.<br>
+     * 4. BrowserPanel does not show the default page.<br>
+     * 5. Status bar sync status changes.<br>
+     * 6.{@code PersonListPanel} equals to the corresponding components in
+     * the current model.<br>
+     */
+    private void assertUiUpdated() {
+        executeCommand(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        executeCommand(DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals("", getCommandBox().getInput());
+        assertCommandBoxShowsDefaultStyle();
+        assertFalse(getResultDisplay().getText().isEmpty());
+        assertFalse(getBrowserPanel().getLoadedUrl().equals(BrowserPanel.DEFAULT_PAGE));
+        assertStatusBarUnchangedExceptSyncStatus();
+        assertListMatching(getPersonListPanel(), getModel().getFilteredPersonList());
     }
 
 }
