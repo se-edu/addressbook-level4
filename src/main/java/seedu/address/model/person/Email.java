2db9df34f9dcaeb8e41f21dd4b0cbdce26cacda0
Vivek <vivekscl5@gmail.com>
2018-02-08 13:22:11 +0800

    Email: modify email regex to accept RFC 5322 permitted characters
    The email regex validation rejects valid inputs such as 
    peter+jack@example.com. This due to the regex rejecting valid special 
    characters such as "+".
    
    Rejecting such inputs prevents the user from adding contacts with an
    email that contains special characters.
    
    Let's modify the email regex validation to accept characters permitted 
    by RFC 5322, the Internet Message Format.
    
    More details on email validation:
    https://en.wikipedia.org/wiki/Email_address#Syntax

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -9,9 +9,20 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Email {
 
-    public static final String MESSAGE_EMAIL_CONSTRAINTS =
-            "Person emails should be 2 alphanumeric/period strings separated by '@'";
-    public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
+    public static final String MESSAGE_EMAIL_CONSTRAINTS = "Person emails should follow the following constraints:\n"
+            + "1. The local part should only contain alphanumeric characters and these special characters, excluding "
+            + "the parentheses, (!#$%&'*+/=?`{|}~^.-) .\n"
+            + "2. This is followed by a '@' and then a domain name. "
+            + "The domain name must:\n"
+            + "    - be at least 2 characters long\n"
+            + "    - start and end with alphanumeric characters\n"
+            + "    - consist of alphanumeric characters, a period or a hyphen for characters in between, if any.";
+    private static final String LOCAL_PART_REGEX = "^[\\w!#$%&'*+/=?`{|}~^.-]+"; // alphanumeric and special characters
+    private static final String DOMAIN_FIRST_CHARACTER_REGEX = "[^\\W_]"; // alphanumeric characters except underscore
+    private static final String DOMAIN_MIDDLE_REGEX = "[a-zA-Z0-9.-]*"; // alphanumeric and period and hyphen
+    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
+    public static final String EMAIL_VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
+            + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
 
     public final String value;
 
