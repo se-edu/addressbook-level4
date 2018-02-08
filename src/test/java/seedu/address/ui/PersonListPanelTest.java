191335a55f8a613145c9873447b0a2a057c1c713
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-02-08 20:28:42 +0800

    PersonListPanel#personListView: revert type change
    personListView is of type ListView<PersonCard>. We use EasyBind library
    to map the Person objects to PersonCard objects. The mapping / applying
    of Function<? super F, ? extends E> occurs only when either get(int) or
    getRemoved() methods are called[1]; the creation of PersonCard objects
    occur only when either of these methods are called.
    
    The ClearCommand deletes all Person objects. When we have large number
    of persons and getRemoved() is called, all the corresponding cards
    will be created prior to them being removed. As creation of PersonCard
    objects are costly, the app slows down tremendously.
    
    Let's revert ListView<PersonCard> to ListView<Person>.
    PersonListViewCell will create the PersonCard object. According to
    javadoc for Cell that "it is not feasible to create an actual Cell for
    every single item in the control. We represent extremely large data
    sets using only very few Cells. Each Cell is "recycled", or reused,"[2]
    this implies that there will only be about 10 Cells at any point in
    time, since the default screen size is only large enough to display 10
    Cells. This implies that there will only be < 10 PersonCard objects at
    any point. Creation of 10 PersonCard objects is performed in negligible
    time, thus the app no longer slows down. This commit is performed by
    reverting from 75cbfa3804ff365646e903514282d9313da4300e.
    
    Also, since PersonCard objects are only created when the card is
    displayed on the list view, let's teach
    GuiTestAssert#assertListMatching(PersonListPanelHandle, Person...) and
    AddressBookSystemTest#assertSelectedCardChange(Index) to navigate to
    the card that they are verifying before performing the verification.
    
    [1]: https://github.com/TomasMikula/EasyBind/blob/master/src/main/java/org/fxmisc/easybind/MappedList.java
    [2]: https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/Cell.html

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -1,21 +1,29 @@
 package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersons;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardDisplaysPerson;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardEquals;
 
-import org.junit.Before;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+
+import com.google.common.base.Stopwatch;
 
 import guitests.guihandles.PersonCardHandle;
 import guitests.guihandles.PersonListPanelHandle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.util.FileUtil;
+import seedu.address.commons.util.XmlUtil;
 import seedu.address.model.person.Person;
+import seedu.address.storage.XmlSerializableAddressBook;
 
 public class PersonListPanelTest extends GuiUnitTest {
     private static final ObservableList<Person> TYPICAL_PERSONS =
@@ -23,19 +31,14 @@ public class PersonListPanelTest extends GuiUnitTest {
 
     private static final JumpToListRequestEvent JUMP_TO_SECOND_EVENT = new JumpToListRequestEvent(INDEX_SECOND_PERSON);
 
+    private static final String TEST_DATA_FOLDER = FileUtil.getPath("src/test/data/PersonListPanelTest/");
+
     private PersonListPanelHandle personListPanelHandle;
-
-    @Before
-    public void setUp() {
-        PersonListPanel personListPanel = new PersonListPanel(TYPICAL_PERSONS);
-        uiPartRule.setUiPart(personListPanel);
-
-        personListPanelHandle = new PersonListPanelHandle(getChildNode(personListPanel.getRoot(),
-                PersonListPanelHandle.PERSON_LIST_VIEW_ID));
-    }
 
     @Test
     public void display() {
+        setUp(TYPICAL_PERSONS);
+
         for (int i = 0; i < TYPICAL_PERSONS.size(); i++) {
             personListPanelHandle.navigateToCard(TYPICAL_PERSONS.get(i));
             Person expectedPerson = TYPICAL_PERSONS.get(i);
@@ -48,11 +51,63 @@ public class PersonListPanelTest extends GuiUnitTest {
 
     @Test
     public void handleJumpToListRequestEvent() {
+        setUp(TYPICAL_PERSONS);
         postNow(JUMP_TO_SECOND_EVENT);
         guiRobot.pauseForHuman();
 
-        PersonCardHandle expectedCard = personListPanelHandle.getPersonCardHandle(INDEX_SECOND_PERSON.getZeroBased());
-        PersonCardHandle selectedCard = personListPanelHandle.getHandleToSelectedCard();
-        assertCardEquals(expectedCard, selectedCard);
+        PersonCardHandle expectedPerson = personListPanelHandle.getPersonCardHandle(INDEX_SECOND_PERSON.getZeroBased());
+        PersonCardHandle selectedPerson = personListPanelHandle.getHandleToSelectedCard();
+        assertCardEquals(expectedPerson, selectedPerson);
+    }
+
+    @Test
+    public void performanceTest() throws Exception {
+        File xmlFile = createManyPersonsXmlFile();
+        XmlSerializableAddressBook xmlAddressBook =
+                XmlUtil.getDataFromFile(xmlFile, XmlSerializableAddressBook.class);
+        ObservableList<Person> personList =
+                FXCollections.observableArrayList(xmlAddressBook.toModelType().getPersonList());
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        setUp(personList);
+        guiRobot.interact(personList::clear);
+        if (stopwatch.elapsed(TimeUnit.MILLISECONDS) >= 2000) {
+            fail();
+        }
+    }
+
+    /**
+     * Creates a .xml file containing 10000 persons. This file will be deleted when the JVM terminates.
+     */
+    private File createManyPersonsXmlFile() throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n");
+        builder.append("<addressbook>\n");
+        for (int i = 0; i < 10000; i++) {
+            builder.append("<persons>\n");
+            builder.append("<name>a</name>\n");
+            builder.append("<phone>").append(i).append("00</phone>\n");
+            builder.append("<email>a@aa</email>\n");
+            builder.append("<address>a</address>\n");
+            builder.append("</persons>\n");
+        }
+        builder.append("</addressbook>\n");
+
+        File manyPersonsFile = new File(TEST_DATA_FOLDER + "9999persons.xml");
+        FileUtil.createFile(manyPersonsFile);
+        FileUtil.writeToFile(manyPersonsFile, builder.toString());
+        manyPersonsFile.deleteOnExit();
+        return manyPersonsFile;
+    }
+
+    /**
+     * Initializes {@code personListPanelHandle} with a {@code PersonListPanel} backed by {@code backingList}.
+     */
+    private void setUp(ObservableList<Person> backingList) {
+        PersonListPanel personListPanel = new PersonListPanel(backingList);
+        uiPartRule.setUiPart(personListPanel);
+
+        personListPanelHandle = new PersonListPanelHandle(getChildNode(personListPanel.getRoot(),
+                PersonListPanelHandle.PERSON_LIST_VIEW_ID));
     }
 }
