76c830d4dd711ad3d456935ff597c98f17a267fe
Jun An <e0002937@u.nus.edu>
2018-02-16 22:57:51 +0800

    Interdiff between v5 and v6

--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -17,18 +17,22 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
+/**
+ * Contains test for closing of the {@code MainWindow}.
+ */
 public class MainWindowTest extends GuiUnitTest {
     @Rule
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     private MainWindow mainWindow;
     private MainWindowHandleUnit mainWindowHandle;
+    private Stage stage;
 
     @Before
     public void setUp() throws Exception {
         FxToolkit.setupStage(stage -> {
-            mainWindow =
-                    new MainWindow(stage, new Config(), new UserPrefs(), new LogicManager(new ModelManager()));
+            this.stage = stage;
+            mainWindow = prepareMainWindow(stage);
             stage.setScene(mainWindow.getRoot().getScene());
             mainWindowHandle = new MainWindowHandleUnit(stage);
             mainWindowHandle.focus();
@@ -37,32 +41,37 @@ public class MainWindowTest extends GuiUnitTest {
     }
 
     @Test
-    public void menuBar_exit_success() {
+    public void exit_menuBarExitButton_exitAppRequestEventPosted() {
         mainWindowHandle.clickOnMenuExitButton();
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ExitAppRequestEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
     }
 
     @Test
-    public void externalRequest_exit_success() {
+    public void exit_externalRequest_exitAppRequestEventPosted() {
         mainWindowHandle.closeMainWindowExternally();
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ExitAppRequestEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
     }
 
     /**
+     * Returns a default {@code MainWindow} with the given {@code stage} and default values for its other parameters.
+     */
+    private MainWindow prepareMainWindow(Stage stage) {
+        return new MainWindow(stage, new Config(), new UserPrefs(), new LogicManager(new ModelManager()));
+    }
+
+    /**
      * {@code MainWindow} unit test class without any other components to test closing of the {@code MainWindow}.
      */
     private class MainWindowHandleUnit extends StageHandle {
-        private final Stage stage;
 
         private MainWindowHandleUnit(Stage stage) {
             super(stage);
-            this.stage = stage;
         }
 
         /**
-         * Closes the {@code MainWindow} using it's menu bar.
+         * Closes the {@code MainWindow} using its menu bar.
          */
         private void clickOnMenuExitButton() {
             guiRobot.clickOn("File");
@@ -70,7 +79,8 @@ public class MainWindowTest extends GuiUnitTest {
         }
 
         /**
-         * Simulate an external request to close the {@code MainWindow}.
+         * Simulate an external request to close the {@code MainWindow} (e.g pressing the 'X' button on the
+         * {@code MainWindow} or closing the app through the taskbar).
          */
         private void closeMainWindowExternally() {
             guiRobot.interact(() -> stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST)));
