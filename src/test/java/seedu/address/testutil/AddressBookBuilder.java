410d3002453acce58f036afece6192b0923af245
A0162011A <e0112235@u.nus.edu>
2018-02-22 13:34:58 +0800

    AddressBook: remove master tag list
    We keep track of a master tag list which holds all the tags in our
    address book.
    
    The tag list is not used at all. The original intention was for
    persons; tags to point to the tags in the master tag list. However,
    implementing it in this way may be too complex for CS2103 students to
    grasp, as discussed in #753. As discussed in #794, the tag list in its
    current state is not needed.
    
    Let's remove the master tag list in AddressBook.

--- src/test/java/seedu/address/testutil/AddressBookBuilder.java
+++ src/test/java/seedu/address/testutil/AddressBookBuilder.java
@@ -1,15 +1,13 @@
 package seedu.address.testutil;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.tag.Tag;
 
 /**
  * A utility class to help with building Addressbook objects.
  * Example usage: <br>
- *     {@code AddressBook ab = new AddressBookBuilder().withPerson("John", "Doe").withTag("Friend").build();}
+ *     {@code AddressBook ab = new AddressBookBuilder().withPerson("John", "Doe").build();}
  */
 public class AddressBookBuilder {
 
@@ -31,18 +29,6 @@ public class AddressBookBuilder {
             addressBook.addPerson(person);
         } catch (DuplicatePersonException dpe) {
             throw new IllegalArgumentException("person is expected to be unique.");
-        }
-        return this;
-    }
-
-    /**
-     * Parses {@code tagName} into a {@code Tag} and adds it to the {@code AddressBook} that we are building.
-     */
-    public AddressBookBuilder withTag(String tagName) {
-        try {
-            addressBook.addTag(new Tag(tagName));
-        } catch (IllegalValueException ive) {
-            throw new IllegalArgumentException("tagName is expected to be valid.");
         }
         return this;
     }
