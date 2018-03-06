ccc1c67edf0d02580ea5bfc01c0fd53f782d17b9
crizyli <e0134315@u.nus.edu>
2018-03-06 14:53:32 +0800

    add UnknockCommandParser class

---
+++ src/main/java/seedu/address/logic/parser/UnknockCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.UnknockCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class UnknockCommandParser implements Parser<UnknockCommand>{
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns an FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UnknockCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        String[] inputs = trimmedArgs.split("\\s+");
+        if (trimmedArgs.isEmpty() || inputs.length != 2) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnknockCommand.MESSAGE_USAGE));
+        }
+
+        return new UnknockCommand(inputs[1]);
+    }
+}
