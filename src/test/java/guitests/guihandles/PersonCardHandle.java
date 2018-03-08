d820827b686e0ee7a6a3c731c1db051b04a032ff
Sebry9 <e0031720@u.nus.edu>
2018-03-09 00:58:04 +0800

    Highligh tags with color code

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -68,4 +68,14 @@ public class PersonCardHandle extends NodeHandle<Node> {
                 .map(Label::getText)
                 .collect(Collectors.toList());
     }
+
+    public List<String> getTagStyleClasses(String tag) {
+        return tagLabels
+                .stream()
+                .filter(label -> label.getText().equals(tag))
+                .map(Label::getStyleClass)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("input tag unavailable "));
+    }
+
 }
