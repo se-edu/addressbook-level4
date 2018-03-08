f9370afddda6baddc4cce9c7d19890087ebf87e0
crizyli <e0134315@u.nus.edu>
2018-03-08 17:16:14 +0800

    fix style error

--- src/test/java/seedu/address/logic/commands/LockCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LockCommandTest.java
@@ -1,5 +1,11 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
@@ -7,13 +13,9 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
+/**
+ * Contains integration tests (interaction with the Model) for {@code LockCommand}.
+ */
 public class LockCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
