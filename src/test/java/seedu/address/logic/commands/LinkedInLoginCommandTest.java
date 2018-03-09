32e766ebab9716573c00724db44d81b2da9a168e
davidten <david10tco+1@gmail.com>
2018-03-09 19:15:55 +0800

    styling

--- src/test/java/seedu/address/logic/commands/LinkedInLoginCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LinkedInLoginCommandTest.java
@@ -1,12 +1,13 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
 import org.junit.Test;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 public class LinkedInLoginCommandTest {
 
