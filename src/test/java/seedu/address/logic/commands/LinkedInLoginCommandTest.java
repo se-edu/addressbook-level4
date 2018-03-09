643f169e6a4c0577e5b514c8d6a194f944122129
davidten <david10tco+1@gmail.com>
2018-03-09 18:06:03 +0800

    Adds in test for LinkedInLoginCommand

---
+++ src/test/java/seedu/address/logic/commands/LinkedInLoginCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+public class LinkedInLoginCommandTest {
+
+    @Test
+    public void execute_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        assertCommandSuccess(prepareCommand(model), model, LinkedInLoginCommand.MESSAGE_SUCCESS, model);
+    }
+
+    /**
+     * Generates a new {@code LinkedInCommand} which upon execution,
+     * does nothing for now but will eventually log a user in to LinkedIn.
+     */
+    private LinkedInLoginCommand prepareCommand(Model model) {
+        LinkedInLoginCommand command = new LinkedInLoginCommand ();
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+
+}
