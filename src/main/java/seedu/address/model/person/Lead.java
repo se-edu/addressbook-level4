8be53f4d1b66c6b3af54a99e9f8dc9ced4965a05
Sheikh-Umar <sheikh.umar.banafe@gmail.com>
2018-03-13 23:41:42 +0800

    Updated Type method position

--- src/main/java/seedu/address/model/person/Lead.java
+++ src/main/java/seedu/address/model/person/Lead.java
@@ -48,7 +48,11 @@ public class Lead extends Person {
     public Email getEmail() {
         return email;
     }
-
+    
+    public Type getType() {
+        return type;
+    }
+    
     public Address getAddress() {
         return address;
     }
