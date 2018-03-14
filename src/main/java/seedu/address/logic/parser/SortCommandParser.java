335c1a256416e19a2819b3a83f995fdb75bbcf4f
Lim Zheng Kai <limzk1994@hotmail.com>
2018-03-14 21:03:53 +0800

    Added SortCommandParser

---
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new SortCommand object
+ */
+public class SortCommandParser implements Parser<SortCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SelectCommand
+     * and returns an SelectCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SortCommand parse(String args) throws ParseException {
+
+        return new SortCommand();
+    }
+}
