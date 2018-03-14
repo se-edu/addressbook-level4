dafb6faab50782bcade23cf1634444b956d6059d
Lim Zheng Kai <limzk1994@hotmail.com>
2018-03-14 20:21:25 +0800

    Added dummy sort command

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+/**
+ * Sorts all persons in the address book to the user.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_SUCCESS = "sorted all persons";
+
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
