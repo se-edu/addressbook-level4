05be099093acad5aae14abb3c07c89afb340a654
Sebry9 <e0031720@u.nus.edu>
2018-03-14 18:55:37 +0800

    added Alias to all command and to AddressBookParser

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -10,6 +10,8 @@ import seedu.address.model.AddressBook;
 public class ClearCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "clear";
+    public static final String COMMAND_WORD_ALIAS = "c";
+
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
 
--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -9,6 +9,7 @@ import seedu.address.commons.events.ui.ShowHelpRequestEvent;
 public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
+    public static final String COMMAND_WORD_ALIAS = "h";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
