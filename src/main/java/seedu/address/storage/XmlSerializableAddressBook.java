b7b1690ff8dbfd6423a5cf1ffe9a859df1674b89
A0162011A <e0112235@u.nus.edu>
2018-03-15 15:25:08 +0800

    XmlSerializableAddressBook: remove tags list
    XmlSerializableAddressBook keeps a list of tags, which is used for
    storing the master tag list in the AddressBook object.
    
    Since AddressBook no longer has a master tag list, it is unnecessary
    to store the master tag list in XmlSerializableAddressBook.
    
    Let's remove tags from XmlSerializableAddressBook.

--- src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
@@ -19,8 +19,6 @@ public class XmlSerializableAddressBook {
 
     @XmlElement
     private List<XmlAdaptedPerson> persons;
-    @XmlElement
-    private List<XmlAdaptedTag> tags;
 
     /**
      * Creates an empty XmlSerializableAddressBook.
@@ -28,7 +26,6 @@ public class XmlSerializableAddressBook {
      */
     public XmlSerializableAddressBook() {
         persons = new ArrayList<>();
-        tags = new ArrayList<>();
     }
 
     /**
@@ -43,7 +40,7 @@ public class XmlSerializableAddressBook {
      * Converts this addressbook into the model's {@code AddressBook} object.
      *
      * @throws IllegalValueException if there were any data constraints violated or duplicates in the
-     * {@code XmlAdaptedPerson} or {@code XmlAdaptedTag}.
+     * {@code XmlAdaptedPerson}.
      */
     public AddressBook toModelType() throws IllegalValueException {
         AddressBook addressBook = new AddressBook();
@@ -62,8 +59,6 @@ public class XmlSerializableAddressBook {
         if (!(other instanceof XmlSerializableAddressBook)) {
             return false;
         }
-
-        XmlSerializableAddressBook otherAb = (XmlSerializableAddressBook) other;
-        return persons.equals(otherAb.persons) && tags.equals(otherAb.tags);
+        return persons.equals(((XmlSerializableAddressBook) other).persons);
     }
 }
