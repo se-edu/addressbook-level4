2e8e09cf991b8f79db7d095251f88c2b677e5b6a
Vivek <vivekscl5@gmail.com>
2018-03-17 01:05:53 +0800

    Interdiff between v3 and v4

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -14,7 +14,7 @@ import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  * A list of persons that enforces uniqueness between its elements and does not allow nulls.
- * A person is considered unique by comparing using Person#isSamePerson(Person).
+ * A person is considered unique by comparing using {@code Person#isSamePerson(Person)}.
  *
  * Supports a minimal set of list operations.
  *
