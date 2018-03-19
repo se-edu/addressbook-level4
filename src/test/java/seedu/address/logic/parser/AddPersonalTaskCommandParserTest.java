26583e41954e38f1bd7294cc361a7dae771b94e3
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-03-20 07:45:06 +0800

    Remove test cases

--- src/test/java/seedu/address/logic/parser/AddPersonalTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddPersonalTaskCommandParserTest.java
@@ -3,48 +3,14 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DATE_TIME;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DURATION;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_TIME;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DURATION;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMPTY_TASK_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_WITHOUT_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_WITH_DESC;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.ResolverStyle;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.AddPersonalTaskCommand;
-import seedu.address.model.personal.PersonalTask;
 
 public class AddPersonalTaskCommandParserTest {
     private AddPersonalTaskCommandParser parser = new AddPersonalTaskCommandParser();
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm")
-            .withResolverStyle(ResolverStyle.STRICT);
-
-    @Test
-    public void parse_validArgs_success() {
-        // With description
-        LocalDateTime taskDateTime = LocalDateTime.parse(VALID_DATE_TIME, formatter);
-        PersonalTask personalTask = new PersonalTask(taskDateTime, VALID_DURATION, VALID_TASK_DESC);
-        assertParseSuccess(parser, VALID_TASK_WITH_DESC,
-                new AddPersonalTaskCommand(personalTask));
-
-        // Without description
-        personalTask = new PersonalTask(taskDateTime, VALID_DURATION, VALID_EMPTY_TASK_DESC);
-        assertParseSuccess(parser, VALID_TASK_WITHOUT_DESC,
-                new AddPersonalTaskCommand(personalTask));
-
-        // Check leap year
-        personalTask = new PersonalTask(LocalDateTime.parse("29/02/2016 11:20", formatter),
-                VALID_DURATION, VALID_EMPTY_TASK_DESC);
-        assertParseSuccess(parser, "29/02/2016 11:20 1h11m",
-                new AddPersonalTaskCommand(personalTask));
-    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
