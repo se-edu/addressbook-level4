8857b755cb3dc6c042a2f8b3c99f6fa0b011431e
A0162011A <e0112235@u.nus.edu>
2018-03-23 14:46:58 +0800

    PersonUtil: Add method getPersonDetails(EditPersonDescriptor)
    The test AddressBookParserTest#parseCommand_edit() uses
    getPersonDetails(Person) to check whether the assertion is true.
    However, this never takes into account the case where the tag list
    should be empty, causing it to fail if a person with no tags is used.
    
    Let's add the method getPersonDetails(EditPersonDescriptor) to
    PersonUtil.

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.model.person.Person;
 
 /**
@@ -33,6 +34,31 @@ public class PersonUtil {
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
+        return sb.toString();
+    }
+
+    public static String getPersonDetails(EditCommand.EditPersonDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        if (descriptor.getName().isPresent()) {
+            sb.append(PREFIX_NAME + descriptor.getName().get().fullName + " ");
+        }
+        if (descriptor.getPhone().isPresent()) {
+            sb.append(PREFIX_PHONE + descriptor.getPhone().get().value + " ");
+        }
+        if (descriptor.getEmail().isPresent()) {
+            sb.append(PREFIX_EMAIL + descriptor.getEmail().get().value + " ");
+        }
+        if (descriptor.getAddress().isPresent()) {
+            sb.append(PREFIX_ADDRESS + descriptor.getAddress().get().value + " ");
+        }
+        if (descriptor.getTags().isPresent()) {
+            if (descriptor.getTags().get().isEmpty()) {
+                sb.append(PREFIX_TAG);
+            }
+            descriptor.getTags().get().stream().forEach(
+                s -> sb.append(PREFIX_TAG + s.tagName + " ")
+            );
+        }
         return sb.toString();
     }
 }
