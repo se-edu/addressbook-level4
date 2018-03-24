71a0dd17c01e937bf74001d3d3e17a6160f2473f
Jun An <e0002937@u.nus.edu>
2018-03-24 23:06:47 +0800

    UndoRedoStack: update to store ReadOnlyAddressBook
    UndoRedoStack stores the UndoableCommands that were executed for
    undoing and redoing of commands.
    
    This forces UndoableCommand to store the previous AddressBook state
    required for undoing the command, which is unnecessary coupling.
    
    Let's update UndoRedoStack to store ReadOnlyAddressBook instead, and
    update the DeveloperGuide to reflect this change.
    
    Following this change, we are no longer able to do the check of
    whether to push into the undo-stack or to clear the redo-stack of the
    UndoRedoStack within the UndoRedoStack class as we are no longer pushing
    Commands into the UndoRedoStack.
    
    Let's also move the responsibility of checking whether to push into the
    undo-stack or to clear the redo-stack to the LogicManager by adding a
    helper method LogicManager#updateUndoRedoStack(Command).

--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -1,10 +1,8 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.UndoRedoStackUtil.prepareStack;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.deleteFirstPerson;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
@@ -15,43 +13,50 @@ import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.UndoRedoStackUtil;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class UndoCommandTest {
     private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
-    private static final UndoRedoStack EMPTY_STACK = new UndoRedoStack();
 
     private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private final DeleteCommand deleteCommandOne = new DeleteCommand(INDEX_FIRST_PERSON);
-    private final DeleteCommand deleteCommandTwo = new DeleteCommand(INDEX_FIRST_PERSON);
+
+    private UndoRedoStack undoRedoStack;
 
     @Before
     public void setUp() {
-        deleteCommandOne.setData(model, EMPTY_COMMAND_HISTORY, EMPTY_STACK);
-        deleteCommandTwo.setData(model, EMPTY_COMMAND_HISTORY, EMPTY_STACK);
+        Model setUpModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        AddressBook initialAddressBook = new AddressBook(setUpModel.getAddressBook());
+
+        deleteFirstPerson(setUpModel);
+        AddressBook secondAddressBookToUndo = new AddressBook(setUpModel.getAddressBook());
+
+        deleteFirstPerson(setUpModel);
+        AddressBook firstAddressBookToUndo = new AddressBook(setUpModel.getAddressBook());
+
+        undoRedoStack = UndoRedoStackUtil.prepareStack(
+                Arrays.asList(initialAddressBook, secondAddressBookToUndo, firstAddressBookToUndo),
+                Collections.emptyList());
     }
 
     @Test
-    public void execute() throws Exception {
-        UndoRedoStack undoRedoStack = prepareStack(
-                Arrays.asList(deleteCommandOne, deleteCommandTwo), Collections.emptyList());
+    public void execute() {
         UndoCommand undoCommand = new UndoCommand();
         undoCommand.setData(model, EMPTY_COMMAND_HISTORY, undoRedoStack);
-        deleteCommandOne.execute();
-        deleteCommandTwo.execute();
 
-        // multiple commands in undoStack
+        // multiple address books in undoStack
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         deleteFirstPerson(expectedModel);
         assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
 
-        // single command in undoStack
+        // single address book in undoStack
         expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
 
-        // no command in undoStack
+        // no address book in undoStack
         assertCommandFailure(undoCommand, model, UndoCommand.MESSAGE_FAILURE);
     }
 }
