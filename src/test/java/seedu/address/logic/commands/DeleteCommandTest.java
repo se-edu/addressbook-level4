5a6fb606165ce0baaaf733a3e54ddf4d40ddb25f
Jun An <e0002937@u.nus.edu>
2018-03-25 02:23:19 +0800

    Remove UndoableCommand class
    UndoableCommand was previously used to undo and redo commands through
    its UndoableCommand#undo() and UndoableCommand#redo() method
    respectively.
    
    Since these the UndoRedoStack now stores ReadOnlyAddressBook to
    directly restore the AddressBook state for undo and redo commands,
    and Model now handles the responsibility of undo redo mechanism,
    there is no longer a need for UndoableCommand class.
    
    Let's remove UndoableCommand class, and make all commands extend from
    Command class.
    
    Following this change, UndoableCommand#preprocessUndoableCommand is no
    longer called when it is required for commands such as DeleteCommand
    and EditCommand. The intention of having
    UndoableCommand#preprocessUndoableCommand was to resolve an issue[1]
    in our app.
    
    Since this issue is also resolved here as we have updated UndoRedoStack
    to UndoRedoCareTaker which tracks AddressBook states instead of
    UndoableCommands, there is no longer a need for a separate method to
    preprocess the commands.
    
    Let's inline DeleteCommand#preprocessUndoableCommand into
    DeleteCommand#execute and EditCommand#preprocessUndoableCommand
    into EditCommand#execute.

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -143,7 +143,7 @@ public class DeleteCommandTest {
     }
 
     @Test
-    public void equals() throws Exception {
+    public void equals() {
         DeleteCommand deleteFirstCommand = prepareCommand(INDEX_FIRST_PERSON);
         DeleteCommand deleteSecondCommand = prepareCommand(INDEX_SECOND_PERSON);
 
@@ -153,10 +153,6 @@ public class DeleteCommandTest {
         // same values -> returns true
         DeleteCommand deleteFirstCommandCopy = prepareCommand(INDEX_FIRST_PERSON);
         assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
-
-        // one command preprocessed when previously equal -> returns false
-        deleteFirstCommandCopy.preprocessUndoableCommand();
-        assertFalse(deleteFirstCommand.equals(deleteFirstCommandCopy));
 
         // different types -> returns false
         assertFalse(deleteFirstCommand.equals(1));
