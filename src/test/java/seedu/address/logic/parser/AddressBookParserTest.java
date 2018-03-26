4980867b9614f6162d06c040a7bc9ec3c3ddf8cb
A0162011A <e0112235@u.nus.edu>
2018-03-26 15:01:04 +0800

    PersonUtil: add method getEditPersonDescriptorDetails
    AddressBookParserTest#parseCommand_edit() uses
    PersonUtil#getPersonDetails(Person) to create the argument to be
    parsed by EditCommandParser. It then compares this with a newly
    constructed EditCommand. However, in the case where a Person is being
    edited to be without tags, PersonUtil#getPersonDetails(Person) does
    not make a distinction between these two cases:
    1. The person originally has no tags.
    2. The person originally has tags, and we are clearing these tags.
    
    As a result, the AddressBookParserTest#parseCommand_edit() may fail if
    we want to test for case 2, but PersonUtil#getPersonDetails(Person)
    will only give us attributes for case 1 instead.
    
    Let's add a getEditPersonDescriptorDetails(EditPersonDescriptor) method
    to PersonUtil so that AddressBookParserTest#parseCommand_edit() can
    distinguish between the two cases.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -66,7 +66,7 @@ public class AddressBookParserTest {
         Person person = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getPersonDetails(person));
+                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
