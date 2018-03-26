90bd6b378939e706dfbf9b675fab6089701c84ff
A0162011A <e0112235@u.nus.edu>
2018-03-26 13:38:30 +0800

    Interdiff between v3 and v4

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -45,15 +45,9 @@ public class PersonUtil {
      */
     public static String getPersonDetails(EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
-        descriptor.getName().ifPresent(
-            name -> sb.append(PREFIX_NAME).append(name.fullName).append(" ")
-        );
-        descriptor.getPhone().ifPresent(
-            phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" ")
-        );
-        descriptor.getEmail().ifPresent(
-            email -> sb.append(PREFIX_EMAIL).append(email.value).append(" ")
-        );
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
+        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
+        descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(
             address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" ")
         );
@@ -61,10 +55,9 @@ public class PersonUtil {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
                 sb.append(PREFIX_TAG);
+            } else {
+                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
             }
-            tags.forEach(
-                s -> sb.append(PREFIX_TAG).append(s.tagName).append(" ")
-            );
         }
         return sb.toString();
     }
