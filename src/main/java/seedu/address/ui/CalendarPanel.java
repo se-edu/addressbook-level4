bdd87675bfbeb30864a676fa91e17ac8c390e559
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-03-31 01:14:20 +0800

    Add and delete task

--- src/main/java/seedu/address/ui/CalendarPanel.java
+++ src/main/java/seedu/address/ui/CalendarPanel.java
@@ -1,8 +1,15 @@
 package seedu.address.ui;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.model.Entry;
+import com.calendarfx.model.Interval;
 import com.calendarfx.view.CalendarView;
 
 import javafx.fxml.FXML;
@@ -19,9 +26,13 @@ public class CalendarPanel extends UiPart<Region> {
     private static final char WEEK = 'w';
     private static final char MONTH = 'm';
     private static final char YEAR = 'y';
-
+    private static CalendarSource source = new CalendarSource("Schedule");
+    private static Calendar calendar = new Calendar("Task");
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm")
+            .withResolverStyle(ResolverStyle.STRICT);
     @FXML
     private static CalendarView calendarView = new CalendarView();
+
 
     public CalendarPanel() {
         super(FXML);
@@ -30,10 +41,21 @@ public class CalendarPanel extends UiPart<Region> {
         calendarView.setTime(LocalTime.now());
         calendarView.showDayPage();
         disableViews();
+        setupCalendar();
     }
 
     /**
-     * Remove unnecessary buttons from interface
+     * Initialises the calendar
+     */
+    private void setupCalendar() {
+        source.getCalendars().add(calendar);
+        calendarView.getCalendarSources().add(source);
+        addTask2();
+        addTask1();
+    }
+
+    /**
+     * Removes unnecessary buttons from interface
      */
     private void disableViews() {
         calendarView.setShowAddCalendarButton(false);
@@ -63,6 +85,41 @@ public class CalendarPanel extends UiPart<Region> {
         default:
             assert(false);
         }
+    }
+
+    /**
+     * Test add task 1
+     */
+    public static void addTask1() {
+        LocalDateTime startDateTime;
+        startDateTime = LocalDateTime.parse("30/03/2018 22:30", formatter);
+        LocalDateTime endDateTime = startDateTime.plusHours(1);
+        Interval interval = new Interval(startDateTime, endDateTime);
+        Entry entry = new Entry("Tuition1");
+        entry.setInterval(interval);
+        calendar.addEntry(entry);
+    }
+
+    /**
+     * Test add task 2
+     */
+    public static void addTask2() {
+        LocalDateTime startDateTime;
+        startDateTime = LocalDateTime.parse("30/03/2018 22:30", formatter);
+        LocalDateTime endDateTime = startDateTime.plusHours(1);
+        Interval interval = new Interval(startDateTime, endDateTime);
+        Entry entry = new Entry("Tuition2");
+        entry.setInterval(interval);
+        calendar.addEntry(entry);
+    }
+
+    /**
+     * Deletes a task from the calendar's schedule
+     *
+     * @param task to be deleted
+     */
+    public static void deleteTask(Entry task) {
+        calendar.removeEntry(task);
     }
 
     @Override
