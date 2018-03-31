ae6dff825eb5f5fcc4104d7cfbfba4993ba5e154
A0162011A <e0112235@u.nus.edu>
2018-04-01 02:33:17 +0800

    Rename CommandBox#handleCommandInputChanged()
    CommandBox#handleCommandInputChanged() method name is misleading as
    this event only triggers when the user presses the "Enter" button and
    not every time when the user types something.
    
    Let's rename CommandBox#handleCommandInputChanged() to
    CommandBox#handleCommandEntered().

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -99,7 +99,7 @@ public class CommandBox extends UiPart<Region> {
      * Handles the Enter button pressed event.
      */
     @FXML
-    private void handleCommandInputChanged() {
+    private void handleCommandEntered() {
         try {
             CommandResult commandResult = logic.execute(commandTextField.getText());
             initHistory();
