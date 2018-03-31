b9e86b5be94c20da869181c7f7c7eb1eafe7f125
A0162011A <e0112235@u.nus.edu>
2018-04-01 02:20:00 +0800

    FileUtil: remove isFileExists(File) method
    The current method FileUtil#isFileExists(File) is a combination of two
    boolean method calls:
    1. file.exists()
    2. file.isFile()
    
    However, isFile() returns "true if and only if the file denoted by
    this abstract pathname exists and is a normal file; false otherwise",
    as per
    https://docs.oracle.com/javase/8/docs/api/java/io/File.html#isFile.
    
    Since isFile() already checks for existence,
    FileUtil#isFileExists(File) is unnecessary.
    
    Let's remove FileUtil#isFileExists(File)

--- src/main/java/seedu/address/commons/util/FileUtil.java
+++ src/main/java/seedu/address/commons/util/FileUtil.java
@@ -13,16 +13,12 @@ public class FileUtil {
 
     private static final String CHARSET = "UTF-8";
 
-    public static boolean isFileExists(File file) {
-        return file.exists() && file.isFile();
-    }
-
     /**
      * Creates a file if it does not exist along with its missing parent directories.
      * @throws IOException if the file or directory cannot be created.
      */
     public static void createIfMissing(File file) throws IOException {
-        if (!isFileExists(file)) {
+        if (!file.isFile()) {
             createFile(file);
         }
     }
