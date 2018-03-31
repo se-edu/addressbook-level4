b142481ec5ee7fcdef7a6ac05cccb258b3174f4c
A0162011A <e0112235@u.nus.edu>
2018-04-01 02:26:35 +0800

    Command: inline getMessageForPersonListShownSummary(int)
    This method is only called by FindCommand.
    
    There is no need for this method to be placed in the super class
    Command as other commands do not use this method.
    
    Let's move getMessageForPersonListShownSummary(int) to FindCommand as a
    private method.
    
    It is unnecessary to have the logic of forming the message for the
    person list summary extracted as a separate method, since its logic
    isn't meant to be used by outsiders,and does not help to introduce a
    new level of abstraction.
    
    Let's inline FindCommand#getMessageForPersonListShownSummary(int).

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
@@ -24,7 +25,8 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute() {
         model.updateFilteredPersonList(predicate);
-        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
 
     @Override
