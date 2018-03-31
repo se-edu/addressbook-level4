2f8074d54de8571e726ca4aafca493f2dd278147
A0162011A <e0112235@u.nus.edu>
2018-04-01 03:05:54 +0800

    FindCommand: update messages to specify that it is case-insensitive
    The header comment and MESSAGE_USAGE states that FindCommand is
    case-sensitive. This is not correct.
    
    Let's update the header comment and MESSAGE_USAGE to reflect that
    FindCommand is case-insensitive.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,14 +5,14 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
