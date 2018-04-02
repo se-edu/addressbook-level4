52b83832951ff0dffbf24b86909cc58c72a799dc
Vivek <vivekscl5@gmail.com>
2018-04-02 22:51:00 +0800

    FileUtil: remove unsued methods and FileUtilTest
    After refactoring the codebase to use java.nio.file for file/path 
    handling, several methods become unused. Following that, FileUtilTest 
    would be testing unused methods.
    
    For code quality purposes, unused methods and tests should be removed. 
    
    Let's remove unused methods in FileUtil and FileUtilTest itself.

--- src/main/java/seedu/address/commons/util/FileUtil.java
+++ src/main/java/seedu/address/commons/util/FileUtil.java
@@ -1,7 +1,5 @@
 package seedu.address.commons.util;
 
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,45 +21,6 @@ public class FileUtil {
      */
     public static void createIfMissing(Path file) throws IOException {
         if (!isFileExists(file)) {
-            createFile(file);
-        }
-    }
-
-    /**
-     * Creates a file if it does not exist along with its missing parent directories
-     *
-     * @return true if file is created, false if file already exists
-     */
-    public static boolean createFile(File file) throws IOException {
-        if (file.exists()) {
-            return false;
-        }
-
-        createParentDirsOfFile(file);
-
-        return file.createNewFile();
-    }
-
-    /**
-     * Creates the given directory along with its parent directories
-     *
-     * @param dir the directory to be created; assumed not null
-     * @throws IOException if the directory or a parent directory cannot be created
-     */
-    public static void createDirs(File dir) throws IOException {
-        if (!dir.exists() && !dir.mkdirs()) {
-            throw new IOException("Failed to make directories of " + dir.getName());
-        }
-    }
-
-    /**
-     * Creates parent directories of file if it has a parent directory
-     */
-    public static void createParentDirsOfFile(File file) throws IOException {
-        File parentDir = file.getParentFile();
-
-        if (parentDir != null) {
-            createDirs(parentDir);
             Files.createFile(file);
         }
     }
@@ -77,18 +36,6 @@ public class FileUtil {
      * Writes given string to a file.
      * Will create the file if it does not exist yet.
      */
-    public static void writeToFile(File file, String content) throws IOException {
-        Files.write(file.toPath(), content.getBytes(CHARSET));
-    }
-
-    /**
-     * Converts a string to a platform-specific file path
-     * @param pathWithForwardSlash A String representing a file path but using '/' as the separator
-     * @return {@code pathWithForwardSlash} but '/' replaced with {@code File.separator}
-     */
-    public static String getPath(String pathWithForwardSlash) {
-        checkArgument(pathWithForwardSlash.contains("/"));
-        return pathWithForwardSlash.replace("/", File.separator);
     public static void writeToFile(Path filePath, String content) throws IOException {
         Files.write(filePath, content.getBytes(CHARSET));
     }
