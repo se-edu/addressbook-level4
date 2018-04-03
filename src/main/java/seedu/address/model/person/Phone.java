29201f7fc6562032a7bd88015a59b91f2f660611
A0162011A <e0112235@u.nus.edu>
2018-04-04 04:42:45 +0800

    Person's field classes: standardize Phone's error message
    Phone uses a different format from other error messages in Person's
    field classes: it uses the word "can" instead of "should", and leaves
    out "it" for the second part of its error message.
    
    Let's standardize Phone's error message to be similar to other
    Person's field classes.

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,7 +11,7 @@ public class Phone {
 
 
     public static final String MESSAGE_PHONE_CONSTRAINTS =
-            "Phone numbers can only contain numbers, and should be at least 3 digits long";
+            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
     public static final String PHONE_VALIDATION_REGEX = "\\d{3,}";
     public final String value;
 
