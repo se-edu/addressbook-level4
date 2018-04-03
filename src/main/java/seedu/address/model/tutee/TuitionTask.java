a3fd49c092d605f13103b6358acb62def8e1b8b0
Eka Buyung Lienadi <35619973+yungyung04@users.noreply.github.com>
2018-04-04 00:18:41 +0800

    Update TuitionTask.java

--- src/main/java/seedu/address/model/tutee/TuitionTask.java
+++ src/main/java/seedu/address/model/tutee/TuitionTask.java
@@ -9,13 +9,13 @@ import seedu.address.model.Task;
 
 //@@author ChoChihTun
 /**
- * Represents a tuition task that the person has
+ * Represents a tuition task that the tutee has
  */
 public class TuitionTask implements Task {
 
     public static final String MESSAGE_TASK_CONSTRAINT =
             "Task can only be tuition\n"
-                    + ", the person involved must already be inside the contact list\n"
+                    + ", the tutee involved must already be inside the contact list\n"
                     + ", Date can only contain numbers in the format of dd/mm/yyyy\n"
                     + ", Time must in the format of HH:mm\n"
                     + " and Duration must be the format of 01h30m";
@@ -141,6 +141,6 @@ public class TuitionTask implements Task {
     */
   
     public String getTuitionTitle() {
-        return String.format(TUITION_TITLE, person);
+        return String.format(TUITION_TITLE, tutee);
     }
 }
