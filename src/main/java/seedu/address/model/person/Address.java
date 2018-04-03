a0f74029cc15a92c55ef20806215a6d69ef193cd
A0162011A <e0112235@u.nus.edu>
2018-04-04 04:42:36 +0800

    Person's field classes: remove the word "Person" from error messages
    The invalid values error message for each person's field - Address,
    Email, and Name - starts with a redundant "Person" word.
    
    Let's remove the word "Person" in the error messages of Person's
    field classes.

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Address {
 
     public static final String MESSAGE_ADDRESS_CONSTRAINTS =
-            "Person addresses can take any values, and it should not be blank";
+            "Addresses can take any values, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
