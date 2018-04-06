749d2808054574e908eb4c9ea80c34db0d2640aa
Unknown <vivekscl5@gmail.com>
2018-04-06 20:08:11 +0800

    FileUtil: remove unsued methods
    After refactoring the codebase to use java.nio.file for file/path 
    handling, several methods become unused. Following that, FileUtilTest 
    would be testing unused methods.
    
    For code quality purposes, unused methods and tests should be removed. 
    
    Let's remove unused methods in FileUtil and FileUtilTest.

--- src/main/java/seedu/address/commons/util/FileUtil.java
+++ src/main/java/seedu/address/commons/util/FileUtil.java
@@ -1,8 +1,5 @@
 package seedu.address.commons.util;
 
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
@@ -45,18 +42,6 @@ public class FileUtil {
     }
 
     /**
-     * Creates the given directory along with its parent directories
-     *
-     * @param dir the directory to be created; assumed not null
-     * @throws IOException if the directory or a parent directory cannot be created
-     */
-    public static void createDirs(File dir) throws IOException {
-        if (!dir.exists() && !dir.mkdirs()) {
-            throw new IOException("Failed to make directories of " + dir.getName());
-        }
-    }
-
-    /**
      * Creates parent directories of file if it has a parent directory
      */
     public static void createParentDirsOfFile(Path filePath) throws IOException {
@@ -82,17 +67,6 @@ public class FileUtil {
      */
     public static String readFromFile(Path filePath) throws IOException {
         return new String(Files.readAllBytes(filePath), CHARSET);
-    }
-
-
-    /**
-     * Converts a string to a platform-specific file path
-     * @param pathWithForwardSlash A String representing a file path but using '/' as the separator
-     * @return {@code pathWithForwardSlash} but '/' replaced with {@code File.separator}
-     */
-    public static String getPath(String pathWithForwardSlash) {
-        checkArgument(pathWithForwardSlash.contains("/"));
-        return pathWithForwardSlash.replace("/", File.separator);
     }
 
     /**
