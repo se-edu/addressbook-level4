e4fd478c8167a6397b68bfa5fc21a5a4e45bb2fe
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-04-07 11:07:11 +0800

    remove tuitionschedule method call statement

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -11,7 +11,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 
 import seedu.address.model.Task;
 import seedu.address.model.task.exceptions.TaskNotFoundException;
-import seedu.address.model.tutee.TuitionSchedule;
 import seedu.address.model.tutee.TuitionTask;
 
 //@@author yungyung04
@@ -41,7 +40,6 @@ public class DeleteTaskCommand extends UndoableCommand {
     public CommandResult executeUndoableCommand() {
         requireNonNull(toDelete);
         try {
-            TuitionSchedule.deleteTask(toDelete);
             model.deleteTask(toDelete);
         } catch (TaskNotFoundException tnfe) {
             throw new AssertionError("The target person cannot be missing");
