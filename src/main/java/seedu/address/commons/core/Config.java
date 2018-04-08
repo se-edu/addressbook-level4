5b66a3ec3e3b11ef1796a70b58385a04d83bbefa
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-04-08 15:14:54 +0800

    add default title

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -11,7 +11,7 @@ public class Config {
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
     // Config values customizable through config file
-    private String appTitle = "TuitionConnect";
+    private String appTitle = "Address App";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
 
