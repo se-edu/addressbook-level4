8f06c1e8df3b6b8a4b8ce780bcd1c087b518a035
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-04-08 14:31:16 +0800

    Add initialisation for tests

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -16,6 +17,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import systemtests.SystemTestSetupHelper;
 
 
 public class LogicManagerTest {
@@ -24,6 +26,12 @@ public class LogicManagerTest {
 
     private Model model = new ModelManager();
     private Logic logic = new LogicManager(model);
+
+    @BeforeClass
+    public static void setupBeforeClass() {
+        SystemTestSetupHelper.initialize();
+    }
+
 
     @Test
     public void execute_invalidCommandFormat_throwsParseException() {
