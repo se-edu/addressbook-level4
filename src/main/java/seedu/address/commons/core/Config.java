32dbef8bd65566027feda93bcc2e03910c283c73
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-04-08 15:13:38 +0800

    Update app title

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -11,7 +11,7 @@ public class Config {
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
     // Config values customizable through config file
-    private String appTitle = "Address App";
+    private String appTitle = "TuitionConnect";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
 
