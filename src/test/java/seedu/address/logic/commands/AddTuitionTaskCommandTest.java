c476492498110c389d9592b0c982d854e4dc7e7c
yungyung04 <ekabuyung@gmail.com>
2018-04-09 13:58:45 +0800

    addpersonal test

--- src/test/java/seedu/address/logic/commands/AddTuitionTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTuitionTaskCommandTest.java
@@ -4,16 +4,14 @@ import static java.util.Objects.requireNonNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import static seedu.address.commons.core.Messages.MESSAGE_TASK_TIMING_CLASHES;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_TIME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_TIME_BOB;
-import static seedu.address.testutil.TaskBuilder.DEFAULT_DESCRIPTION;
-import static seedu.address.testutil.TaskBuilder.DEFAULT_DURATION;
-import static seedu.address.testutil.TaskUtil.FORMATTER;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DURATION_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_DESC_AMY;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,7 +19,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
@@ -34,10 +31,16 @@ import seedu.address.testutil.ModelStub;
 import seedu.address.testutil.TaskBuilder;
 
 //@@author yungyung04
-public class AddPersonalTaskCommandTest {
+public class AddTuitionTaskCommandTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm")
+            .withResolverStyle(ResolverStyle.STRICT);
+    private LocalDateTime taskDateTime = LocalDateTime.parse(VALID_DATE_TIME_AMY, formatter);
+    private PersonalTask task = new PersonalTask(taskDateTime, VALID_DURATION_AMY, VALID_TASK_DESC_AMY);
 
     @Test
     public void constructor_nullTask_throwsNullPointerException() {
@@ -57,7 +60,7 @@ public class AddPersonalTaskCommandTest {
     }
 
     @Test
-    public void execute_clashingTask_throwsCommandException() throws Exception {
+    public void execute_duplicatePerson_throwsCommandException() throws Exception {
         ModelStub modelStub = new ModelStubThrowingTimingClashException();
         PersonalTask validTask = new TaskBuilder().buildPersonalTask();
 
@@ -69,16 +72,12 @@ public class AddPersonalTaskCommandTest {
 
     @Test
     public void equals() {
-        PersonalTask firstPersonalTask = new TaskBuilder().withDateTime(VALID_DATE_TIME_AMY).buildPersonalTask();
-        PersonalTask secondPersonalTask = new TaskBuilder().withDateTime(VALID_DATE_TIME_BOB).buildPersonalTask();
+        LocalDateTime taskDateTime2 = LocalDateTime.parse("08/08/1988 18:00", formatter);
+        PersonalTask task2 = new PersonalTask(taskDateTime2, VALID_DURATION_AMY, VALID_TASK_DESC_AMY);
 
-        AddPersonalTaskCommand addFirstTask = new AddPersonalTaskCommand(firstPersonalTask);
-        AddPersonalTaskCommand addFirstTaskCopy = new AddPersonalTaskCommand(firstPersonalTask);
-        AddPersonalTaskCommand addSecondTask = new AddPersonalTaskCommand(secondPersonalTask);
-
-        LocalDateTime tuitionDateTime = LocalDateTime.parse(VALID_DATE_TIME_AMY, FORMATTER);
-        AddTuitionTaskCommand addTuitionTask = new AddTuitionTaskCommand(
-                INDEX_FIRST_PERSON, tuitionDateTime, DEFAULT_DURATION, DEFAULT_DESCRIPTION);
+        AddPersonalTaskCommand addFirstTask = new AddPersonalTaskCommand(task);
+        AddPersonalTaskCommand addFirstTaskCopy = new AddPersonalTaskCommand(task);
+        AddPersonalTaskCommand addSecondTask = new AddPersonalTaskCommand(task2);
 
         // same object -> returns true
         assertTrue(addFirstTask.equals(addFirstTask));
@@ -92,10 +91,7 @@ public class AddPersonalTaskCommandTest {
         // null -> returns false
         assertFalse(addFirstTask.equals(null));
 
-        // different task type -> returns false
-        assertFalse(addFirstTask.equals(addTuitionTask));
-
-        // different detail -> returns false
+        // different person -> returns false
         assertFalse(addFirstTask.equals(addSecondTask));
     }
 
