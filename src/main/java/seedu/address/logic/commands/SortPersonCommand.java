bbfb23c952998d55f09c9180e2d734d8ec2b7e5b
yungyung04 <ekabuyung@gmail.com>
2018-04-11 02:45:25 +0800

    example

--- src/main/java/seedu/address/logic/commands/SortPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/SortPersonCommand.java
@@ -18,22 +18,20 @@ import seedu.address.model.person.PersonSortUtil;
  * a Person who is not a tutee will be listed last when such information is selected to be the sorting category.
  */
 public class SortPersonCommand extends Command {
-    public static final String COMMAND_WORD = "sort";
+    public static final String COMMAND_WORD = "sortpersonby";
 
-    public static final String MESSAGE_SUCCESS = "sorted successfully";
+    public static final String MESSAGE_SUCCESS = "sorted list of persons successfully";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": sorts all visible persons lexicographically according to the specified sorting category.\n"
-            + "Since tutee contains tutee-specific information such as grades and school, \n"
-            + "Person who are not Tutees will be listed last "
-            + "when such information is selected as the sorting category."
+            + "Person who are not Tutees will be listed last when a tutee detail is a selected category (refer to User Guide)\n"
             + "Parameters: sort_category\n"
             + "Choice of sort_categories: "
-            + CATEGORY_NAME + "\n"
-            + CATEGORY_EDUCATION_LEVEL + "[Tutee specific]\n"
-            + CATEGORY_GRADE + "[Tutee specific]\n"
-            + CATEGORY_SCHOOL + "[Tutee specific]\n"
-            + CATEGORY_SUBJECT + "[Tutee specific]\n"
+            + CATEGORY_NAME + " "
+            + CATEGORY_EDUCATION_LEVEL + " "
+            + CATEGORY_GRADE + " "
+            + CATEGORY_SCHOOL + " "
+            + CATEGORY_SUBJECT + "\n"
             + "Example: " + COMMAND_WORD + " " + CATEGORY_GRADE;
 
     private final String category;
