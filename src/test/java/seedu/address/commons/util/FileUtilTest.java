fc01bc01e05812d7656b1312d660d58ece0a0365
Unknown <vivekscl5@gmail.com>
2018-04-12 23:12:48 +0800

    FileUtil: remove unused methods
    After refactoring the codebase to use java.nio.file for file/path 
    handling, several methods become unused. Following that, FileUtilTest 
    would be testing unused methods.
    
    For code quality purposes, unused methods and tests should be removed. 
    
    Let's remove unused methods in FileUtil and FileUtilTest.

--- src/test/java/seedu/address/commons/util/FileUtilTest.java
+++ src/test/java/seedu/address/commons/util/FileUtilTest.java
@@ -1,36 +1,13 @@
 package seedu.address.commons.util;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import seedu.address.testutil.Assert;
 
 public class FileUtilTest {
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void getPath() {
-
-        // valid case
-        assertEquals("folder" + File.separator + "sub-folder", FileUtil.getPath("folder/sub-folder"));
-
-        // null parameter -> throws NullPointerException
-        thrown.expect(NullPointerException.class);
-        FileUtil.getPath(null);
-
-        // no forwards slash -> assertion failure
-        thrown.expect(AssertionError.class);
-        FileUtil.getPath("folder");
-    }
 
     @Test
     public void isValidPath() {
