e812981bcd1b46cc336baa51ac82813fe2ddaf1e
Unknown <vivekscl5@gmail.com>
2018-04-15 04:58:32 +0800

    Person: add equality method for identity fields
    Person class contains only one equality method Person#equals(Object) 
    which checks if all fields of the Person except tags are identical.
    
    Two Persons of the same name that have at least one other equivalent 
    identity field (phone or email) but different data fields (address and/
    or tags), is considered different even though they are actually the same 
    person.
    
    Let's add another equality method, Person#isSamePerson(Person), for 
    identity fields in Person class. Adding an equality method for identity 
    fields will allow detection of such cases which may be unintuitive to 
    users.
    
    Following this change, some tests throw DuplicatePersonException when 
    they try to add the default person using PersonBuilder to a model that 
    already contains TypicalPersons#ALICE. This is because the default 
    person in PersonBuilder has the same values for the name and phone 
    fields as ALICE. To prevent that, let's change the value of the phone 
    field for ALICE.
    
    Also, TypicalPersons#DANIEL now contains the "friends" tag while 
    typicalPersonsAddressBook.xml#DANIEL does not. This causes the 
    XmlSerializableAddressBookTest#toModelType_typicalPersonsFile_success 
    test to fail. To prevent that, let's add the "friends" tag to  
    typicalPersonsAddressBook.xml#DANIEL.
    
    Furthermore, PersonListPanelTest#createXmlFileWithPersons(int) throws a
    DuplicatePersonException as all Persons created have the same name and 
    email. To prevent that, let's change the format of the name and phone 
    creation such that each Person is unique.

--- src/test/java/systemtests/EditCommandSystemTest.java
+++ src/test/java/systemtests/EditCommandSystemTest.java
@@ -186,6 +186,22 @@ public class EditCommandSystemTest extends AddressBookSystemTest {
         command = EditCommand.COMMAND_WORD + " " + index.getOneBased() + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
                 + ADDRESS_DESC_BOB + TAG_DESC_HUSBAND;
         assertCommandFailure(command, EditCommand.MESSAGE_DUPLICATE_PERSON);
+
+        /* Case: edit a person with new values same as another person's values but with different phone -> rejected */
+        command = EditCommand.COMMAND_WORD + " " + index.getOneBased() + NAME_DESC_BOB + PHONE_DESC_AMY + EMAIL_DESC_BOB
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TAG_DESC_HUSBAND;
+        assertCommandFailure(command, EditCommand.MESSAGE_DUPLICATE_PERSON);
+
+        /* Case: edit a person with new values same as another person's values but with different email -> rejected */
+        command = EditCommand.COMMAND_WORD + " " + index.getOneBased() + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TAG_DESC_HUSBAND;
+        assertCommandFailure(command, EditCommand.MESSAGE_DUPLICATE_PERSON);
+
+        /* Case: edit a person with new values same as another person's identity fields but with different data fields
+        -> rejected */
+        command = EditCommand.COMMAND_WORD + " " + index.getOneBased() + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                + ADDRESS_DESC_AMY + TAG_DESC_HUSBAND;
+        assertCommandFailure(command, EditCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     /**
