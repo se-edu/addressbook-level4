5b889e4aee2756900189901b07c077e8c7f9704c
Unknown <vivekscl5@gmail.com>
2018-04-15 02:00:37 +0800

    Add AppParameters class to handle application parameter parsing
    The handling of application parameters is done in the MainApp.
    
    This decreases cohesiveness as handling and parsing of application 
    parameters should be done separately and not in the MainApp as
    command-line parsing is at a much lower level compared to the high level 
    setup of the application architecture. 
    
    Let's add the AppParameters class to handle application parameter 
    parsing in one place and for future parsing of application parameters.

---
+++ src/main/java/seedu/address/AppParameters.java
@@ -0,0 +1,44 @@
+package seedu.address;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.util.FileUtil;
+
+/**
+* Represents the parsed command-line parameters given to the application.
+*/
+public class AppParameters {
+    private static final Logger logger = LogsCenter.getLogger(AppParameters.class);
+
+    private Path configPath;
+
+    public Path getConfigPath() {
+        return configPath;
+    }
+
+    public void setConfigPath(Path configPath) {
+        this.configPath = configPath;
+    }
+
+    /**
+    * Parses the application command-line parameters.
+    */
+    public static AppParameters parse(Application.Parameters parameters) {
+        AppParameters appParameters = new AppParameters();
+        Map<String, String> namedParameters = parameters.getNamed();
+
+        String configPathParameter = namedParameters.get("config");
+        if (configPathParameter != null && !FileUtil.isValidPath(configPathParameter)) {
+            logger.warning("Invalid config path " + configPathParameter + ". Using default config path.");
+            configPathParameter = null;
+        }
+        appParameters.setConfigPath(configPathParameter != null ? Paths.get(configPathParameter) : null);
+
+        return appParameters;
+    }
+}
