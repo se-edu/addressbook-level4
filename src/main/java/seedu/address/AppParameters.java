9a20763a6d94137a8feaa890eecbb6e0e6dd153b
Unknown <vivekscl5@gmail.com>
2018-04-17 21:40:33 +0800

    MainApp: extract handling of application parameters into AppParameters
    The handling of application parameters is done in the MainApp.
    
    This decreases cohesiveness as handling and parsing of application 
    parameters should be done separately and not in the MainApp as
    command-line parsing is at a much lower level compared to the high level 
    setup of the application architecture. This also allows the parsing of
    application parameters to be tested independently.
    
    Let's extract the handling of application parameters from MainApp into 
    the AppParameters for future parsing of application parameters.

---
+++ src/main/java/seedu/address/AppParameters.java
@@ -0,0 +1,54 @@
+package seedu.address;
+
+import java.util.Map;
+import java.util.Objects;
+
+import javafx.application.Application;
+
+/**
+ * Represents the parsed command-line parameters given to the application.
+ */
+public class AppParameters {
+
+    private String configPath;
+
+    public String getConfigPath() {
+        return configPath;
+    }
+
+    public void setConfigPath(String configPath) {
+        this.configPath = configPath;
+    }
+
+    /**
+     * Parses the application command-line parameters.
+     */
+    public static AppParameters parse(Application.Parameters parameters) {
+        AppParameters appParameters = new AppParameters();
+        Map<String, String> namedParameters = parameters.getNamed();
+
+        String configPathParameter = namedParameters.get("config");
+        appParameters.setConfigPath(configPathParameter);
+
+        return appParameters;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof AppParameters)) {
+            return false;
+        }
+
+        AppParameters otherAppParameters = (AppParameters) other;
+        return Objects.equals(otherAppParameters.getConfigPath(), getConfigPath());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(configPath);
+    }
+}
