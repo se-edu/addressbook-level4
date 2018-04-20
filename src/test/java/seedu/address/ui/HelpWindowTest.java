1f6a8554133824b98dadd224c43145a577f3b5c6
Jun An <e0002937@u.nus.edu>
2018-04-20 22:47:35 +0800

    HelpWindowTest: skip test with undefined behaviour in headless mode
    HelpWindowTest#focus_helpWindowNotFocused_focused test checks
    whether calling HelpWindow#focus() will bring the HelpWindow to focus.
    Normally, when HelpWindow#show() is called, the HelpWindow's
    Stage should automatically be in focus.
    
    However, when tests are run in headless mode, the HelpWindow in
    HelpWindowTest#focus_helpWindowNotFocused_focused test does not
    automatically come into focus after it is shown.
    
    This is due to HelpWindowStage in setUp() setting the HelpWindow's
    Scene to itself. According to the JavaFx docs for
    Window#setScene(Scene)[1], a Scene can only be on one Stage at a time,
    and setting a Scene on a different Stage will cause the old Stage to
    lose the reference. As a result, the Stage which does not have a
    reference to a Scene will have undefined behaviour when run in headless
    mode.
    
    Let's skip HelpWindowTest#focus_helpWindowNotFocused_focused test when
    tests are run in headless mode as it is relying on undefined behaviour.
    Let's also update build.gradle to enable logging of tests that are
    skipped.
    
    [1]: JavaFx docs for Window#setScene(Scene):
    https://docs.oracle.com/javase/8/javafx/api/javafx/stage/Window.html#setScene-javafx.scene.Scene-

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -3,6 +3,7 @@ package seedu.address.ui;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
 import static seedu.address.ui.HelpWindow.USERGUIDE_FILE_PATH;
 
 import java.net.URL;
@@ -47,6 +48,8 @@ public class HelpWindowTest extends GuiUnitTest {
 
     @Test
     public void focus_helpWindowNotFocused_focused() throws Exception {
+        assumeFalse("TEST SKIPPED IN HEADLESS MODE DUE TO UNDEFINED BEHAVIOUR, RUN IT IN HEAD-FULL MODE INSTEAD",
+                guiRobot.isHeadlessMode());
         guiRobot.interact(helpWindow::show);
 
         // Focus on another stage to remove focus from the helpWindow
