75fdb9bb6a07290ab5d4b277012b2d2f4eb6ecfe
Vivek <vivekscl5@gmail.com>
2018-04-21 00:15:41 +0800

    Replace use of java.io.File with java.nio.file.Path
    There are many usages of java.io.File throughout the codebase.
    
    Since Path is meant to replace File as mentioned in the Oracle
    Documentation on Legacy File I/O Code, let's convert all usages of File
    in the codebase to Path.
    
    Oracle Documentation on Legacy File I/O Code:
    https://docs.oracle.com/javase/tutorial/essential/io/legacy.html

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -9,7 +9,8 @@ import static seedu.address.testutil.TypicalPersons.getTypicalPersons;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardDisplaysPerson;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardEquals;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -79,7 +80,7 @@ public class PersonListPanelTest extends GuiUnitTest {
      * {@code PersonListPanel}.
      */
     private ObservableList<Person> createBackingList(int personCount) throws Exception {
-        File xmlFile = createXmlFileWithPersons(personCount);
+        Path xmlFile = createXmlFileWithPersons(personCount);
         XmlSerializableAddressBook xmlAddressBook =
                 XmlUtil.getDataFromFile(xmlFile, XmlSerializableAddressBook.class);
         return FXCollections.observableArrayList(xmlAddressBook.toModelType().getPersonList());
@@ -88,7 +89,7 @@ public class PersonListPanelTest extends GuiUnitTest {
     /**
      * Returns a .xml file containing {@code personCount} persons. This file will be deleted when the JVM terminates.
      */
-    private File createXmlFileWithPersons(int personCount) throws Exception {
+    private Path createXmlFileWithPersons(int personCount) throws Exception {
         StringBuilder builder = new StringBuilder();
         builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n");
         builder.append("<addressbook>\n");
@@ -102,10 +103,10 @@ public class PersonListPanelTest extends GuiUnitTest {
         }
         builder.append("</addressbook>\n");
 
-        File manyPersonsFile = new File(TEST_DATA_FOLDER + "manyPersons.xml");
+        Path manyPersonsFile = Paths.get(TEST_DATA_FOLDER + "manyPersons.xml");
         FileUtil.createFile(manyPersonsFile);
         FileUtil.writeToFile(manyPersonsFile, builder.toString());
-        manyPersonsFile.deleteOnExit();
+        manyPersonsFile.toFile().deleteOnExit();
         return manyPersonsFile;
     }
 
