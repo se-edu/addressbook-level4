8786f44fc37716b68b9104cf5930f352a0972052
Jun An <e0002937@u.nus.edu>
2018-04-22 15:32:30 +0800

    System Tests: remove model verification checks
    Whenever System Tests asserts if a command is executed correctly,
    there will be a verification to check that the internal
    components, such as the model, is correct.
    
    As System Tests are categorised as black box testing, it isn't
    necessary to verify the internal components, such as the model.
    
    Let's remove model verification checks for System Tests.

--- src/test/java/systemtests/SelectCommandSystemTest.java
+++ src/test/java/systemtests/SelectCommandSystemTest.java
@@ -103,7 +103,7 @@ public class SelectCommandSystemTest extends AddressBookSystemTest {
      * 2. Command box has the default style class.<br>
      * 3. Result display box displays the success message of executing select command with the
      * {@code expectedSelectedCardIndex} of the selected person.<br>
-     * 4. {@code Model}, {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
+     * 4. {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
      * 5. Selected card is at {@code expectedSelectedCardIndex} and the browser url is updated accordingly.<br>
      * 6. Status bar remains unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
@@ -135,7 +135,7 @@ public class SelectCommandSystemTest extends AddressBookSystemTest {
      * 1. Command box displays {@code command}.<br>
      * 2. Command box has the error style class.<br>
      * 3. Result display box displays {@code expectedResultMessage}.<br>
-     * 4. {@code Model}, {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
+     * 4. {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
      * 5. Browser url, selected card and status bar remain unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
