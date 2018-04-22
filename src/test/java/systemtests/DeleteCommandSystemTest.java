92303d042345ee3c3f156b63521224d734dcca29
Jun An <e0002937@u.nus.edu>
2018-04-22 15:32:30 +0800

    System Tests: remove model verification checks
    Whenever System Tests asserts if a command is executed correctly,
    there will be a verification to check that the internal
    components, such as the model, is correct.
    
    As System Tests are categorised as black box testing, it isn't
    necessary to verify the internal components, such as the model.
    
    Let's remove model verification checks for System Tests.

--- src/test/java/systemtests/DeleteCommandSystemTest.java
+++ src/test/java/systemtests/DeleteCommandSystemTest.java
@@ -144,11 +144,10 @@ public class DeleteCommandSystemTest extends AddressBookSystemTest {
      * Executes {@code command} and in addition,<br>
      * 1. Asserts that the command box displays an empty string.<br>
      * 2. Asserts that the result display box displays {@code expectedResultMessage}.<br>
-     * 3. Asserts that the model related components equal to {@code expectedModel}.<br>
-     * 4. Asserts that the browser url and selected card remains unchanged.<br>
-     * 5. Asserts that the status bar's sync status changes.<br>
-     * 6. Asserts that the command box has the default style class.<br>
-     * Verifications 1 to 3 are performed by
+     * 3. Asserts that the browser url and selected card remains unchanged.<br>
+     * 4. Asserts that the status bar's sync status changes.<br>
+     * 5. Asserts that the command box has the default style class.<br>
+     * Verifications 1 and 2 are performed by
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.
      * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
@@ -181,10 +180,9 @@ public class DeleteCommandSystemTest extends AddressBookSystemTest {
      * Executes {@code command} and in addition,<br>
      * 1. Asserts that the command box displays {@code command}.<br>
      * 2. Asserts that result display box displays {@code expectedResultMessage}.<br>
-     * 3. Asserts that the model related components equal to the current model.<br>
-     * 4. Asserts that the browser url, selected card and status bar remain unchanged.<br>
-     * 5. Asserts that the command box has the error style.<br>
-     * Verifications 1 to 3 are performed by
+     * 3. Asserts that the browser url, selected card and status bar remain unchanged.<br>
+     * 4. Asserts that the command box has the error style.<br>
+     * Verifications 1 and 2 are performed by
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
