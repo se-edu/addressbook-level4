bfabfeca1a846009f0a19474ed446de00e08ee34
Paul Tan <pyokagan@gmail.com>
2018-08-06 18:57:26 +0800

    model: expose hasPerson(Person) functionality
    In the next few commits, we are going to be converting
    DuplicatePersonException and PersonNotFoundException into runtime
    exceptions.
    
    To do that, we need to be able to query the model to determine if a
    person exists in the address book.
    
    This functionality is already available in
    `UniquePersonList#contains(Person)`, but was simply not exposed. Let's
    expose it.

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -109,6 +109,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void deletePerson(Person target) {
             throw new AssertionError("This method should not be called.");
         }
