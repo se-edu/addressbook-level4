f431e5cf73d773325cb56456d22c7606070dcca6
Paul Tan <pyokagan@gmail.com>
2018-08-08 18:44:59 +0800

    Command: combine setData(...) and execute() together
    To execute a command, (e.g. AddCommand), we need to
    
      1. Construct it:
    
             AddCommand addCommand = new AddCommand(person);
    
      2. Bind the model and command history to it:
    
             addCommand.setData(model, commandHistory);
    
      3. And then execute it:
    
             addCommand.execute();
    
    However, there is no reason why step (2) and step (3) must be separate,
    when it could just be:
    
        addCommand.execute(model, commandHistory);
    
    In fact, it causes more trouble:
    
      * Since the model and commandHistory are stored as protected fields in
        the Command base class, many commands forget to handle them
        appropriately. For example, all commands don't consider the model
        and commandHistory in their `#equals(...)` methods, even though
        these fields are part of their state.
    
      * Many command tests need to introduce a helper `prepareCommand(...)`
        method to streamline the command "setData and execute" process.
    
    So, let's fix this by combining Command#setData(...) and
    Command#execute() together. We can then simplify all the command tests
    by removing their `prepareCommand(...)` helper methods.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
 
 /**
  * Lists all persons in the address book to the user.
@@ -13,7 +17,8 @@ public class ListCommand extends Command {
 
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
