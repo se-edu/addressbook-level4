2037fb441b852f71bd641bd45e74e775ed015090
Paul Tan <pyokagan@gmail.com>
2018-08-08 23:12:30 +0800

    HelpCommandTest: verify model state as well
    When executed, HelpCommand should:
    
     (1) Raise a ShowHelpRequestEvent, and
    
     (2) Not modify the model at all.
    
    HelpCommandTest verifies (1), but does not verify (2).
    
    Given that in the actual application, LogicManager will pass the Model
    to HelpCommand, we should verify that HelpCommand behaves as expected
    and does not modify the model.
    
    As such, let's make HelpCommandTest verify the model state as well. This
    is done by rewriting it to use
    `CommandTestUtil.assertCommandSuccess(...)`, for consistency with the
    other command tests.

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,23 +1,30 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
 public class HelpCommandTest {
     @Rule
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
     @Test
     public void execute_help_success() {
-        CommandResult result = new HelpCommand().execute();
-        assertEquals(SHOWING_HELP_MESSAGE, result.feedbackToUser);
+        HelpCommand helpCommand = new HelpCommand();
+        helpCommand.setData(model, new CommandHistory());
+        assertCommandSuccess(helpCommand, model, SHOWING_HELP_MESSAGE, expectedModel);
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ShowHelpRequestEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
     }
