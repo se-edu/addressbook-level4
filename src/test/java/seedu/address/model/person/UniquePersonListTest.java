8e370e418958244d8a842b32161762d545f401e7
Paul Tan <pyokagan@gmail.com>
2018-08-09 14:23:49 +0800

    UniquePersonListTest: move to `seedu.address.model.person` package
    Since UniquePersonList is in the `seedu.address.model.person` package,
    its corresponding test UniquePersonListTest should be in the same
    package as well.

--- src/test/java/seedu/address/model/person/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/person/UniquePersonListTest.java
@@ -1,10 +1,8 @@
-package seedu.address.model;
+package seedu.address.model.person;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import seedu.address.model.person.UniquePersonList;
 
 public class UniquePersonListTest {
     @Rule
