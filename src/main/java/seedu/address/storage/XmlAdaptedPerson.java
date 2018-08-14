d655a2eb64017e1148c24bc583048f687615f6dd
Paul Tan <pyokagan@gmail.com>
2018-08-14 22:28:23 +0800

    XmlAdaptedPerson#toModelType(): remove shadowing of fields
    The method body references the fields of XmlAdaptedPerson (`name`,
    `phone`, etc.), but also shadows them later on by declaring local
    variables with the same name.
    
    Such mixing of instance fields and local variables within the same
    method is out of the ordinary in the code base, and can be accidentally
    mis-read if the reader is not paying attention.
    
    Let's take the path of least surprise by renaming the local variables
    such that they do not shadow XmlAdaptedPerson's fields.

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -87,7 +87,7 @@ public class XmlAdaptedPerson {
         if (!Name.isValidName(name)) {
             throw new IllegalValueException(Name.MESSAGE_NAME_CONSTRAINTS);
         }
-        final Name name = new Name(this.name);
+        final Name modelName = new Name(name);
 
         if (phone == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
@@ -95,7 +95,7 @@ public class XmlAdaptedPerson {
         if (!Phone.isValidPhone(phone)) {
             throw new IllegalValueException(Phone.MESSAGE_PHONE_CONSTRAINTS);
         }
-        final Phone phone = new Phone(this.phone);
+        final Phone modelPhone = new Phone(phone);
 
         if (email == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
@@ -103,7 +103,7 @@ public class XmlAdaptedPerson {
         if (!Email.isValidEmail(email)) {
             throw new IllegalValueException(Email.MESSAGE_EMAIL_CONSTRAINTS);
         }
-        final Email email = new Email(this.email);
+        final Email modelEmail = new Email(email);
 
         if (address == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
@@ -111,10 +111,10 @@ public class XmlAdaptedPerson {
         if (!Address.isValidAddress(address)) {
             throw new IllegalValueException(Address.MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        final Address address = new Address(this.address);
+        final Address modelAddress = new Address(address);
 
-        final Set<Tag> tags = new HashSet<>(personTags);
-        return new Person(name, phone, email, address, tags);
+        final Set<Tag> modelTags = new HashSet<>(personTags);
+        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
     }
 
     @Override
