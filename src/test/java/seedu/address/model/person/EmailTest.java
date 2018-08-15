c2b8e9de59c7d0ef52d9706af55c319e48fc2973
Paul Tan <pyokagan@gmail.com>
2018-08-15 15:11:29 +0800

    checkstyle: add SingleSpaceSeparator
    Most of the code in our the code base only puts a single space between
    non-whitespace characters,
    
        "Like " + "this."
    
    It is highly likely that there will not be a case where we will need to
    put more than a single space between non-whitespace characters,
    
        "Like "  + "this."
    
    To enforce consistency throughout the code base, let's add the
    SingleSpaceSeparator module to our checkstyle configuration, and fix all
    of the existing violations in our code base.

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -51,12 +51,12 @@ public class EmailTest {
 
         // valid email
         assertTrue(Email.isValidEmail("PeterJack_1190@example.com"));
-        assertTrue(Email.isValidEmail("a@bc"));  // minimal
-        assertTrue(Email.isValidEmail("test@localhost"));   // alphabets only
+        assertTrue(Email.isValidEmail("a@bc")); // minimal
+        assertTrue(Email.isValidEmail("test@localhost")); // alphabets only
         assertTrue(Email.isValidEmail("!#$%&'*+/=?`{|}~^.-@example.org")); // special characters local part
-        assertTrue(Email.isValidEmail("123@145"));  // numeric local part and domain name
+        assertTrue(Email.isValidEmail("123@145")); // numeric local part and domain name
         assertTrue(Email.isValidEmail("a1+be!@example1.com")); // mixture of alphanumeric and special characters
-        assertTrue(Email.isValidEmail("peter_jack@very-very-very-long-example.com"));   // long domain name
-        assertTrue(Email.isValidEmail("if.you.dream.it_you.can.do.it@example.com"));    // long local part
+        assertTrue(Email.isValidEmail("peter_jack@very-very-very-long-example.com")); // long domain name
+        assertTrue(Email.isValidEmail("if.you.dream.it_you.can.do.it@example.com")); // long local part
     }
 }
