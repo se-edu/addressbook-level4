a1a54a155a27a24a876f97195f32ccd640ba600d
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2018-09-22 22:31:24 +0800

    Interdiff between v2 and v3

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -9,8 +9,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS =
-            "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
