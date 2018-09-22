1ee51dd0b00cb6f2348be47175456173e2488fdc
Lee Jin Yao <jylee.w94@gmail.com>
2018-09-22 20:21:48 +0800

    Added Nric object

---
+++ src/main/java/seedu/address/model/person/Nric.java
@@ -0,0 +1,35 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Nric {
+    public final String nric;
+    public static final String MESSAGE_NRIC_CONSTRAINTS =
+            "NRIC should contain a prefix containing 'S', 'T', 'F' or 'G', followed by a seven-digit number, followed " +
+                    "by an alphabetical suffix; it should not be blank.";
+    /*
+     * Prefix should be a S, T, F or G, followed by a 7-digit number, followed by an alphabetical suffix.
+     */
+    public static final String NRIC_VALIDATION_REGEX = "^[STFG]\\d{7}[A-Z]$";
+
+    public Nric(String nric) {
+        requireNonNull(nric);
+
+        //Checks for valid NRIC regex. if false, throws IllegalArgumentException
+        checkArgument(isValidNric(nric), MESSAGE_NRIC_CONSTRAINTS);
+        this.nric = nric;
+    }
+
+    /**
+     * Returns true if a given string is a valid NRIC.
+     */
+    public static boolean isValidNric(String test) {
+        return test.matches(NRIC_VALIDATION_REGEX);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return nric.hashCode();
+//    }
+}
