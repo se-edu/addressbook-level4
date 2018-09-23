9ece26f21e4b63e891d25814376122d2c2e73d63
jeremiah-ang <jeremiahang94@gmail.com>
2018-09-23 17:53:47 +0800

    Fix styleguide

--- src/test/java/seedu/address/logic/parser/GoalCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/GoalCommandParserTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Test;
 
