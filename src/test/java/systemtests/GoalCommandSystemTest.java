d4651f29a74c12ca5c1ad178bf173275f5c259c9
jeremiah-ang <jeremiahang94@gmail.com>
2018-09-23 15:52:11 +0800

    Fix styleguide

--- src/test/java/systemtests/GoalCommandSystemTest.java
+++ src/test/java/systemtests/GoalCommandSystemTest.java
@@ -1,10 +1,14 @@
 package systemtests;
 
 import org.junit.Test;
+
 import seedu.address.logic.commands.GoalCommand;
 import seedu.address.model.Model;
 
-public class GoalCommandSystemTest extends AddressBookSystemTest{
+/**
+ * System test for Goal Command
+ */
+public class GoalCommandSystemTest extends AddressBookSystemTest {
     @Test
     public void goal() {
         /* Case: Set goal with valid value
@@ -14,6 +18,10 @@ public class GoalCommandSystemTest extends AddressBookSystemTest{
         assertGoalSuccess(newGoal);
     }
 
+    /**
+     * Assert that the given goal would result in a successful action.
+     * @param goal
+     */
     public void assertGoalSuccess(double goal) {
         String command = GoalCommand.COMMAND_WORD + " " + goal;
         Model expectedModel = getModel();
@@ -21,6 +29,12 @@ public class GoalCommandSystemTest extends AddressBookSystemTest{
         assertCommandSuccess(command, expectedModel, expectedResultMessage);
     }
 
+    /**
+     * Assert given command would be successful
+     * @param command
+     * @param expectedModel
+     * @param expectedResultMessage
+     */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
         executeCommand(command);
         assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
