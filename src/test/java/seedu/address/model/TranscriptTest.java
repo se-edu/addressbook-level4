3d51e11742e86eeaae4748cb85165ee5734d8e04
jeremiah-ang <jeremiahang94@gmail.com>
2018-09-26 21:29:18 +0800

    Add tests

---
+++ src/test/java/seedu/address/model/TranscriptTest.java
@@ -0,0 +1,37 @@
+package seedu.address.model;
+
+import org.junit.Test;
+import seedu.address.model.module.Module;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+import static seedu.address.testutil.TypicalModules.ASKING_QUESTIONS;
+import static seedu.address.testutil.TypicalModules.TYPICAL_MODULES_CAP;
+import static seedu.address.testutil.TypicalModules.getTypicalModules;
+import static seedu.address.testutil.TypicalModules.getModulesWithoutNonGradeAffectingModules;
+
+public class TranscriptTest {
+
+    @Test
+    public void typicalModulesCapScore() {
+        List<Module> modules = getModulesWithoutNonGradeAffectingModules();
+        assertCapScoreEquals(modules, TYPICAL_MODULES_CAP);
+    }
+
+    @Test
+    public void calculateCapScoreWithSuModule() {
+        List<Module> modules = getTypicalModules();
+        assertCapScoreEquals(modules, TYPICAL_MODULES_CAP);
+
+    }
+
+    private void assertCapScoreEquals(List<Module> modules, Double expectedCapScore) {
+        Transcript transcript = new Transcript();
+        transcript.setModules(modules);
+        double cap = transcript.calculateCapScore();
+        assertEquals(Double.valueOf(cap), expectedCapScore);
+    }
+
+}
