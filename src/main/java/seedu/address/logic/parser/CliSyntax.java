28f5c6b1a934babfc8065470c804989ce40a4550
LimYiSheng <e0176969@u.nus.edu>
2018-09-28 23:33:40 +0800

    Edited CliSyntax.java to include all required prefixes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -8,9 +8,13 @@ public class CliSyntax {
     /* Prefix definitions */
     public static final Prefix PREFIX_EMPLOYEEID = new Prefix("id/");
     public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_DATEOFBIRTH = new Prefix("dob/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_DEPARTMENT =new Prefix("d/");
+    public static final Prefix PREFIX_POSITION = new Prefix("r/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_SALARY = new Prefix("s/");
 
 }
