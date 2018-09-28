3b91b4b1f8246d15f4e4ba4c8cb126bc1ffbc922
Woonhian <limwoonhian@u.nus.edu>
2018-09-29 04:58:47 +0800

    Fix Test

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -98,7 +98,7 @@ public class PersonListPanelTest extends GuiUnitTest {
             builder.append("<name>").append(i).append("a</name>\n");
             builder.append("<phone>000</phone>\n");
             builder.append("<email>a@aa</email>\n");
-            builder.append("<department>d</department>\n")
+            builder.append("<department>d</department>\n");
             builder.append("<address>a</address>\n");
             builder.append("</persons>\n");
         }
