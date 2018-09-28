e2bc22b90163845a4837a54bbb485364f752cfc3
Woonhian <limwoonhian@u.nus.edu>
2018-09-29 05:54:12 +0800

    Test: EditPersonDescriptorBuilder

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -36,6 +36,7 @@ public class EditPersonDescriptorBuilder {
         descriptor.setName(person.getName());
         descriptor.setPhone(person.getPhone());
         descriptor.setEmail(person.getEmail());
+        descriptor.setDepartment(person.getDepartment());
         descriptor.setAddress(person.getAddress());
         descriptor.setTags(person.getTags());
     }
