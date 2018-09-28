1dda321b56fd0808b1ea9c9491c479952ce15acd
LimYiSheng <e0176969@u.nus.edu>
2018-09-29 04:24:37 +0800

    Edited EmployeeID fields to test Travis-CI v2.5

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -23,11 +23,11 @@ import seedu.address.model.person.Person;
  */
 public class TypicalPersons {
 
-    public static final Person ALICE = new PersonBuilder().withEmployeeId("000001").withName("Alice Pauline")
+    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
             .withTags("friends").build();
-    public static final Person BENSON = new PersonBuilder().withEmployeeId("000002").withName("Benson Meier")
+    public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTags("owesMoney", "friends").build();
@@ -56,10 +56,10 @@ public class TypicalPersons {
             .withEmail("hans@example.com").withAddress("chicago ave").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Person AMY = new PersonBuilder().withEmployeeId(VALID_EMPLOYEEID_AMY).withName(VALID_NAME_AMY)
+    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
-    public static final Person BOB = new PersonBuilder().withEmployeeId(VALID_EMPLOYEEID_BOB).withName(VALID_NAME_BOB)
+    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
