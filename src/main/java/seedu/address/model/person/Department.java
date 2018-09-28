b0d17cad0c2959f1e179af0b8813c0a8ab878cc8
Woonhian <limwoonhian@u.nus.edu>
2018-09-29 01:59:07 +0800

    Model: Added Department

--- src/main/java/seedu/address/model/person/Department.java
+++ src/main/java/seedu/address/model/person/Department.java
@@ -1,4 +1,4 @@
-package seedu.address.model.person.exceptions;
+package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
