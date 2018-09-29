d7c3f53e41ee2841cecd94f8edf0b49a613c0315
pinjuen <limpinjuen@gmail.com>
2018-09-29 21:59:11 +0800

    CheckCommand - Step 1

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,6 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_MODE = new Prefix("m/");
 
     public static final Prefix PREFIX_PASSWORD = new Prefix("pwd/");
 }
