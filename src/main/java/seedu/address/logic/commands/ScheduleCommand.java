3f9b0218631389c568d630230d27788a15c43854
Chong Yu <chuayu37@gmail.com>
2018-10-02 00:16:27 +0800

    Add Schedule Command

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -1,4 +1,24 @@
-package seedu.address.logic.commands.exceptions;
+package seedu.address.logic.commands;
 
-public class ScheduleCommand {
+import static java.util.Objects.requireNonNull;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+public class ScheduleCommand extends Command{
+
+    public static final String COMMAND_WORD = "schedule";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists the schedule of the person identified"
+            + "Parameters: INDEX (must be a positive integer)";
+
+    public static final String MESSAGE_SCHEDULE_SUCCESS = "Schedule of Person: %1$s";
+    public static final String MESSAGE_SCHEDULE_FAIL = "Person not found in address book.";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history){
+        requireNonNull(model);
+        return new CommandResult(MESSAGE_SCHEDULE_SUCCESS);
+    }
+
 }
--- src/main/java/seedu/address/logic/commands/SetScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/SetScheduleCommand.java
@@ -1,4 +1,24 @@
 package seedu.address.logic.commands;
 
-public class SetScheduleCommand {
+import static java.util.Objects.requireNonNull;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+public class SetScheduleCommand extends Command{
+
+    public static final String COMMAND_WORD = "setschedule";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists the schedule of the person identified"
+            + "Parameters: INDEX (must be a positive integer)";
+
+    public static final String MESSAGE_SCHEDULE_SUCCESS = "Set Schedule of Person: %1$s";
+    public static final String MESSAGE_SCHEDULE_FAIL = "Person not found in address book.";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history){
+        requireNonNull(model);
+        return new CommandResult(MESSAGE_SCHEDULE_SUCCESS);
+    }
+
 }
