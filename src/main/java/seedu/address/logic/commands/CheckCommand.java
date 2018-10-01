3dad61fb49a2cac5ba7078cfdd791e0780d09e37
pinjuen <limpinjuen@gmail.com>
2018-10-01 22:11:37 +0800

    Fix travis

--- src/main/java/seedu/address/logic/commands/CheckCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckCommand.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
