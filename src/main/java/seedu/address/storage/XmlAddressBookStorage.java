972133b38d6efcb755e4c4a6912d635bb166f092
jitwei98 <limjitwei@gmail.com>
2018-10-02 02:42:05 +0800

    Remove unused import

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -2,7 +2,6 @@ package seedu.address.storage;
 
 import static java.util.Objects.requireNonNull;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
