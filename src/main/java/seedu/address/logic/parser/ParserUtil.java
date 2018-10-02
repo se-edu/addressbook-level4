5979a2f46750b0a19e8506fa4f1f8e2b59d326b2
zhihong8888 <zhihong8888@gmail.com>
2018-10-03 03:47:56 +0800

    travis fix

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -18,8 +18,8 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Position;
 import seedu.address.model.person.Salary;
-import seedu.address.model.schedule.Date;
 import seedu.address.model.person.tag.Tag;
+import seedu.address.model.schedule.Date;
 import seedu.address.model.schedule.Type;
 
 /**
