b52a92f473df36559d664cc9d9030494b66788ff
ryanchen2018 <cqunming@u.nus.edu>
2018-10-03 23:03:27 +0800

    update the prefix with examples and remove extra spaces to shorten the function.

--- src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
+++ src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
@@ -1,11 +1,12 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_POST;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_JOB_POSITION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MINIMUM_EXPERIENCE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_JOB_DESCRIPTION;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-
 
 /**
  * Adds a recruitment post on the address book.
@@ -14,19 +15,20 @@ public class AddRecruitmentPostCommand extends Command {
 
     public static final String COMMAND_WORD = "recruitmentPost";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Job Position "
-            + PREFIX_POST;
-
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Available Jobs "
+            + PREFIX_JOB_POSITION + "[Job Position:]"
+            + PREFIX_MINIMUM_EXPERIENCE + "[min working experience(Integer):]"
+            + PREFIX_JOB_DESCRIPTION + "[Job Description:]"
+            + "Example: " + COMMAND_WORD + PREFIX_JOB_POSITION
+            + "IT Manager" + PREFIX_MINIMUM_EXPERIENCE + "3"
+            + PREFIX_JOB_DESCRIPTION + "To maintain the network server in company";
 
     //public static final String MESSAGE_SUCCESS = "Recruitment Posts are added";
     public static final String MESSAGE_FAILURE = "Recruitment Posts are failed";
 
-
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-
         throw new CommandException(MESSAGE_FAILURE);
-
     }
 
 }
