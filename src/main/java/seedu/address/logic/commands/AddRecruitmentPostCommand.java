ffc6fc9452eab5f82073bc71b50a69c126d1a107
ryanchen2018 <cqunming@u.nus.edu>
2018-10-03 22:30:54 +0800

    fix travis error about cap of letters, lines between import, and order of import.

--- src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
+++ src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POST;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -18,13 +19,13 @@ public class AddRecruitmentPostCommand extends Command {
 
 
     //public static final String MESSAGE_SUCCESS = "Recruitment Posts are added";
-    public static final String MESSAGE_Failure = "Recruitment Posts are failed";
+    public static final String MESSAGE_FAILURE = "Recruitment Posts are failed";
 
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
 
-        throw new CommandException(MESSAGE_Failure);
+        throw new CommandException(MESSAGE_FAILURE);
 
     }
 
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,8 +7,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddScheduleCommand;
 import seedu.address.logic.commands.AddRecruitmentPostCommand;
+import seedu.address.logic.commands.AddScheduleCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
