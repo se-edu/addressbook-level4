822ef89d6fb4fcdfbd51b34803f697e99d891cf1
ryanchen2018 <cqunming@u.nus.edu>
2018-10-03 23:09:32 +0800

    change order of import

--- src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
+++ src/main/java/seedu/address/logic/commands/AddRecruitmentPostCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_JOB_POSITION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MINIMUM_EXPERIENCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_JOB_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MINIMUM_EXPERIENCE;
+
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -19,6 +20,7 @@ public class AddRecruitmentPostCommand extends Command {
             + PREFIX_JOB_POSITION + "[Job Position:]"
             + PREFIX_MINIMUM_EXPERIENCE + "[min working experience(Integer):]"
             + PREFIX_JOB_DESCRIPTION + "[Job Description:]"
+
             + "Example: " + COMMAND_WORD + PREFIX_JOB_POSITION
             + "IT Manager" + PREFIX_MINIMUM_EXPERIENCE + "3"
             + PREFIX_JOB_DESCRIPTION + "To maintain the network server in company";
