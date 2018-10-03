8a3711628316c3b4b37d9e3a5914a62dca3fb788
driedmelon <sourmilo@gmail.com>
2018-10-03 11:31:16 +0800

    non working schedule test

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -1,11 +1,9 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-//import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-//import org.junit.Before;
-import org.junit.Test;
+import org.junit.Test;//import org.junit.Before;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
