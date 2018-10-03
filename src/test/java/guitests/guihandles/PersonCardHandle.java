004f07eab3ed007577d04ed916d523ae3e9746c5
souless94 <cl.wk@hotmail.com>
2018-10-03 18:58:16 +0800

    fix tests temporary

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -14,6 +14,7 @@ import seedu.address.model.person.Person;
  * Provides a handle to a person card in the person list panel.
  */
 public class PersonCardHandle extends NodeHandle<Node> {
+
     private static final String ID_FIELD_ID = "#id";
     private static final String NAME_FIELD_ID = "#name";
     private static final String ADDRESS_FIELD_ID = "#address";
@@ -42,10 +43,10 @@ public class PersonCardHandle extends NodeHandle<Node> {
 
         Region tagsContainer = getChildNode(TAGS_FIELD_ID);
         tagLabels = tagsContainer
-                .getChildrenUnmodifiable()
-                .stream()
-                .map(Label.class::cast)
-                .collect(Collectors.toList());
+            .getChildrenUnmodifiable()
+            .stream()
+            .map(Label.class::cast)
+            .collect(Collectors.toList());
     }
 
     public String getId() {
@@ -68,11 +69,15 @@ public class PersonCardHandle extends NodeHandle<Node> {
         return emailLabel.getText();
     }
 
+    public String getFormat() {
+        return formatLabel.getText();
+    }
+
     public List<String> getTags() {
         return tagLabels
-                .stream()
-                .map(Label::getText)
-                .collect(Collectors.toList());
+            .stream()
+            .map(Label::getText)
+            .collect(Collectors.toList());
     }
 
     /**
@@ -80,11 +85,12 @@ public class PersonCardHandle extends NodeHandle<Node> {
      */
     public boolean equals(Person person) {
         return getName().equals(person.getName().fullName)
-                && getAddress().equals(person.getAddress().value)
-                && getPhone().equals(person.getPhone().value)
-                && getEmail().equals(person.getEmail().value)
-                && ImmutableMultiset.copyOf(getTags()).equals(ImmutableMultiset.copyOf(person.getTags().stream()
-                        .map(tag -> tag.tagName)
-                        .collect(Collectors.toList())));
+            && getAddress().equals(person.getAddress().value)
+            && getPhone().equals(person.getPhone().value)
+            && getEmail().equals(person.getEmail().value)
+            && ImmutableMultiset.copyOf(getTags())
+            .equals(ImmutableMultiset.copyOf(person.getTags().stream()
+                .map(tag -> tag.tagName)
+                .collect(Collectors.toList())));
     }
 }
