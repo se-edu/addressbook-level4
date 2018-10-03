2d2b37a7025bc726336a75ae7a20984a13e7a958
ChuaZhenWei <e0176888@u.nus.edu>
2018-10-03 21:07:40 +0800

    modifyPayV1.1.1

--- src/main/java/seedu/address/logic/commands/ModifyPayCommand.java
+++ src/main/java/seedu/address/logic/commands/ModifyPayCommand.java
@@ -1,18 +1,22 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
+//import static java.util.Objects.requireNonNull;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BONUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMPLOYEEID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SALARY;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Salary;
-import seedu.address.model.person.Bonus;
-import seedu.address.model.person.tag.Tag;
 
+//import seedu.address.model.person.Bonus;
+//import seedu.address.model.person.Salary;
+//import seedu.address.model.person.tag.Tag;
+
+/**
+ *  Modify the salary and bonus of an employee's in CHRS
+ */
 public class ModifyPayCommand extends Command {
 
     public static final String COMMAND_WORD = "modifypay";
