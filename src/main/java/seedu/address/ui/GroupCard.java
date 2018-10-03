170690ab2aacf10551c99bf8fafd9aa7d1771bf4
Happytreat <cowfats@gmail.com>
2018-10-03 12:42:56 +0800

    Fix Travis Stylecheck

--- src/main/java/seedu/address/ui/GroupCard.java
+++ src/main/java/seedu/address/ui/GroupCard.java
@@ -2,7 +2,6 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.group.Group;
