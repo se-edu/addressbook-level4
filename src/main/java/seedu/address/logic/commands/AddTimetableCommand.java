d58ac33b7440b84547dd70383dd0324aa8239db4
souless94 <cl.wk@hotmail.com>
2018-10-04 14:11:22 +0800

    fix test in addtimetablecommandtest

--- src/main/java/seedu/address/logic/commands/AddTimetableCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTimetableCommand.java
@@ -97,10 +97,11 @@ public class AddTimetableCommand extends Command {
         EditPersonDescriptor editPersonDescriptor) {
         assert personToEdit != null;
 
-        Name updatedName = personToEdit.getName();
-        Phone updatedPhone = personToEdit.getPhone();
-        Email updatedEmail = personToEdit.getEmail();
-        Address updatedAddress = personToEdit.getAddress();
+        Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
+        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
+        Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
+        Address updatedAddress = editPersonDescriptor.getAddress()
+            .orElse(personToEdit.getAddress());
         Timetable updatedTimetable = editPersonDescriptor.getTimetable()
             .orElse(personToEdit.getTimetable());
         Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
