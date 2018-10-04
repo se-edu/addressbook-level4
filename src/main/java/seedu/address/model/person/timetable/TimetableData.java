a0f7eb24734dd58b49c5bc0bd902ed074df1b7ed
souless94 <cl.wk@hotmail.com>
2018-10-04 23:03:55 +0800

    cite authors in correct format

--- src/main/java/seedu/address/model/person/timetable/TimetableData.java
+++ src/main/java/seedu/address/model/person/timetable/TimetableData.java
@@ -77,7 +77,7 @@ public class TimetableData {
     private String[][] readHorizontalTimetableData(String locationOfFile) {
         String[][] timetableMatrix;
         Scanner inputStream;
-        //used code from mikeL from https://stackoverflow.com/questions/40074840/reading-a-csv-file-into-a-array
+        //Solution below adapted from mikeL from https://stackoverflow.com/questions/40074840/reading-a-csv-file-into-a-array
         timetableMatrix = generateNewHorizontalTimetable();
         try {
             File toRead = new File(locationOfFile);
@@ -106,7 +106,7 @@ public class TimetableData {
     private String[][] readVerticalTimetableData(String locationOfFile) {
         String[][] timetableMatrix;
         Scanner inputStream;
-        //used code from mikeL from https://stackoverflow.com/questions/40074840/reading-a-csv-file-into-a-array
+        //Solution below adapted from mikeL from https://stackoverflow.com/questions/40074840/reading-a-csv-file-into-a-array
         timetableMatrix = generateNewVerticalTimetable();
         try {
             File toRead = new File(locationOfFile);
@@ -197,7 +197,7 @@ public class TimetableData {
      * @param locationTo location of where to save the file
      */
     public void downloadTimetableData(String locationTo) {
-        // used code from bit-question from https://stackoverflow.com/questions/6271796/issues-of-saving-a-matrix-to-a-csv-file
+        // Solution below adapted from bit-question from https://stackoverflow.com/questions/6271796/issues-of-saving-a-matrix-to-a-csv-file
         String filePath = locationTo;
         try {
             File toWrite = new File(filePath);
