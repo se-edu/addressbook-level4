c4b6d29018e4dc5d71f4a9b494a421d3a8e84e2c
zhihong8888 <zhihong8888@gmail.com>
2018-10-07 04:01:18 +0800

    no changes

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -44,7 +44,7 @@ import seedu.address.ui.Ui;
 import seedu.address.ui.UiManager;
 
 /**
- * The main entry point to the application.
+ * The main entry point to the application begins here.
  */
 public class MainApp extends Application {
 
