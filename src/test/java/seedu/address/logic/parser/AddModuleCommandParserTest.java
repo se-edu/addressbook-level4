e2944ff863ece0b90e54bf559d44d4345e5b31dc
alexkmj <alex_koh_@live.com>
2018-10-16 06:00:04 +0800

    Fixed complaints by Codacy

--- src/test/java/seedu/address/logic/parser/AddModuleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddModuleCommandParserTest.java
@@ -1,26 +1,20 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalModules.DISCRETE_MATH;
-import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.AddModuleCommand;
-import seedu.address.model.module.Code;
-import seedu.address.model.module.Grade;
 import seedu.address.model.module.Module;
-import seedu.address.model.module.Semester;
 import seedu.address.model.util.ModuleBuilder;
 
 public class AddModuleCommandParserTest {
     private AddModuleCommandParser parser = new AddModuleCommandParser();
 
     @Test
-    public void parse_allFieldsPresent_success() throws Exception {
+    public void parseAllFieldsPresentSuccess() throws Exception {
         // leading and trailing whitespace
         assertParseSuccess(parser, PREAMBLE_WHITESPACE
                 + " " + DISCRETE_MATH.getCode().value
@@ -31,7 +25,7 @@ public class AddModuleCommandParserTest {
     }
 
     @Test
-    public void parse_optionalFieldsMissing_success() {
+    public void parseOptionalFieldsMissingSuccess() {
         Module expectedModule = new ModuleBuilder(DISCRETE_MATH)
                 .noGrade()
                 .withCompleted(false)
