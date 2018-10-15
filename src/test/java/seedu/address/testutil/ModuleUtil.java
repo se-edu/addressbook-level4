4dde151245e6e329dd4a008bc3fbe8d358e796aa
alexkmj <alex_koh_@live.com>
2018-10-16 06:12:42 +0800

    Added test for TranscriptParser

---
+++ src/test/java/seedu/address/testutil/ModuleUtil.java
@@ -0,0 +1,34 @@
+package seedu.address.testutil;
+
+import java.util.Set;
+
+import seedu.address.logic.commands.AddModuleCommand;
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.model.module.Module;
+import seedu.address.model.tag.Tag;
+
+/**
+ * A utility class for Module.
+ */
+public class ModuleUtil {
+
+    /**
+     * Returns an add command string for adding the {@code module}.
+     */
+    public static String getAddModuleCommand(Module module) {
+        return AddModuleCommand.COMMAND_WORD + " " + getModuleDetails(module);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code person}'s details.
+     */
+    public static String getModuleDetails(Module module) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(module.getCode().value + " ");
+        sb.append(module.getYear().value + " ");
+        sb.append(module.getSemester().value + " ");
+        sb.append(module.getCredits().value + " ");
+        sb.append(module.getGrade().value + " ");
+        return sb.toString();
+    }
+}
