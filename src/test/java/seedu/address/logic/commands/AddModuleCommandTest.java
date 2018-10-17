3092057f2b5edd75cd645c78b40bea52caed85cc
jeremiah-ang <jeremiahang94@gmail.com>
2018-10-17 21:41:49 +0800

    Add tests

--- src/test/java/seedu/address/logic/commands/AddModuleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddModuleCommandTest.java
@@ -20,6 +20,8 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyTranscript;
 import seedu.address.model.Transcript;
 import seedu.address.model.capgoal.CapGoal;
+import seedu.address.model.module.Code;
+import seedu.address.model.module.Grade;
 import seedu.address.model.module.Module;
 import seedu.address.model.person.Person;
 import seedu.address.model.util.ModuleBuilder;
@@ -212,6 +214,12 @@ public class AddModuleCommandTest {
         public void commitAddressBook() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public Module adjustModule(Code code, Grade grade) {
+            throw new AssertionError("This method should not be called.");
+        }
+
     }
 
     /**
--- src/test/java/seedu/address/logic/commands/AdjustCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AdjustCommandTest.java
@@ -1,4 +1,46 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.Transcript;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.module.Grade;
+import seedu.address.model.module.Module;
+import seedu.address.model.util.ModuleBuilder;
+
 public class AdjustCommandTest {
+    private Model model = new ModelManager(new Transcript(), new UserPrefs());
+    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void constructorNullModuleThrowsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AdjustCommand(null, null);
+    }
+
+    @Test
+    public void executeSuccess() {
+        Module module = new ModuleBuilder().withCode("ABC").noGrade().build();
+        Module expectedModule = new ModuleBuilder().withCode("ABC").withGrade("A").build();
+        model.addModule(module);
+
+        Transcript expectedTranscript = new Transcript();
+        expectedTranscript.addModule(expectedModule);
+        Model expectedModel = new ModelManager(expectedTranscript, new UserPrefs());
+        AdjustCommand adjustCommand = new AdjustCommand(module.getCode(), new Grade("A"));
+        String expectedMessage = String.format(AdjustCommand.MESSAGE_SUCCESS, expectedModule);
+        assertCommandSuccess(adjustCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
 }
