4042508263bdd246ab08d4aaee10a524139d4683
Hyukiejun <parkjunhyukssi@gmail.com>
2018-10-23 16:00:31 +0800

    Added new imports for the program to run without error

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -4,7 +4,7 @@ import javafx.collections.ObservableList;
 
 import seedu.address.model.member.Person;
 import seedu.address.model.ledger.Ledger;
-import seedu.address.model.person.Person;
+import seedu.address.model.member.Person;
 
 /**
  * Unmodifiable view of an address book
