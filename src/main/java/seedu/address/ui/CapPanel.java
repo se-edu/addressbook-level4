3b0e29f2ed0526b01b7c45c442315672ec34ae5c
jeremyyew <yewern@gmail.com>
2018-10-29 12:17:26 +0800

    Annotate author

--- src/main/java/seedu/address/ui/CapPanel.java
+++ src/main/java/seedu/address/ui/CapPanel.java
@@ -5,28 +5,27 @@ import java.util.logging.Logger;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
-import javafx.util.converter.NumberStringConverter;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.TranscriptChangedEvent;
 import seedu.address.model.Transcript;
 import seedu.address.model.capgoal.CapGoal;
-
+//@@author jeremyyew
 /**
  * A ui for the status bar that is displayed at the header of the application.
  */
 public class CapPanel extends UiPart<Region> {
-
+    
     private static final Logger logger = LogsCenter.getLogger(CapPanel.class);
     private static final String FXML = "CapPanel.fxml";
-    
+
     private final DoubleProperty currentCapDouble = new SimpleDoubleProperty(0);
     private final StringProperty capGoalString = new SimpleStringProperty("NIL");
 
@@ -43,7 +42,7 @@ public class CapPanel extends UiPart<Region> {
         super(FXML);
         currentCapValue.textProperty().bind(Bindings.convert(currentCapDouble));
         capGoalValue.textProperty().bind(capGoalString);
-        
+
         Platform.runLater(() -> currentCapDouble.setValue(transcript.getCap()));
         CapGoal goal = transcript.getCapGoal();
         Platform.runLater(() -> capGoalString.setValue(goal.isSet ? String.valueOf(goal.capGoal) : "NIL"));
@@ -52,7 +51,8 @@ public class CapPanel extends UiPart<Region> {
 
     @Subscribe
     public void handleTranscriptChangedEvent(TranscriptChangedEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event, "Local transcript data changed, obtaining new cap and cap goal"));
+        logger.info(LogsCenter.getEventHandlingLogMessage(event,
+                "Local transcript data changed, obtaining new cap and cap goal"));
         try {
             Transcript transcript = (Transcript) event.data;
             Platform.runLater(() -> currentCapDouble.setValue(transcript.getCap()));
