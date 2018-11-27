1c405b85235020e65e5c27a8ce8345a7b37a42a7
Paul Tan <pyokagan@gmail.com>
2018-11-27 17:15:43 +0800

    model: make UserPrefs part of model state
    Despite the architecture of AB-4 saying that UserPrefs is part of the
    model, and UserPrefs being in the model package, UserPrefs is not
    treated as part of the model state. This makes it harder to write tests
    against the model, since we need to check both the Model _and_
    UserPrefs.
    
    Make UserPrefs part of the model state, so that we can reason about and
    write tests against the model as one single entity.

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -1,8 +1,10 @@
 package seedu.address.model;
 
+import java.nio.file.Path;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.person.Person;
 
 /**
@@ -11,6 +13,36 @@ import seedu.address.model.person.Person;
 public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
+
+    /**
+     * Sets model's {@code UserPrefs} object to {@code userPrefs}.
+     */
+    void setUserPrefs(UserPrefs userPrefs);
+
+    /**
+     * Returns the user prefs.
+     */
+    ReadOnlyUserPrefs getUserPrefs();
+
+    /**
+     * Returns the user prefs' GUI settings.
+     */
+    GuiSettings getGuiSettings();
+
+    /**
+     * Sets the user prefs' GUI settings.
+     */
+    void setGuiSettings(GuiSettings guiSettings);
+
+    /**
+     * Returns the user prefs' address book file path.
+     */
+    Path getAddressBookFilePath();
+
+    /**
+     * Sets the user prefs' address book file path.
+     */
+    void setAddressBookFilePath(Path addressBookFilePath);
 
     /**
      * Replaces address book data with the data in {@code addressBook}.
