2fb5d7e6485ce5d3a8b1e7049c876e51beece1d2
Paul Tan <pyokagan@gmail.com>
2018-11-27 15:34:22 +0800

    GuiSettings: use `double` instead of `Double`
    There is no reason why we must use the boxed variant.

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -12,8 +12,8 @@ public class GuiSettings implements Serializable {
     private static final double DEFAULT_HEIGHT = 600;
     private static final double DEFAULT_WIDTH = 740;
 
-    private Double windowWidth;
-    private Double windowHeight;
+    private double windowWidth;
+    private double windowHeight;
     private Point windowCoordinates;
 
     public GuiSettings() {
@@ -22,17 +22,17 @@ public class GuiSettings implements Serializable {
         windowCoordinates = null; // null represent no coordinates
     }
 
-    public GuiSettings(Double windowWidth, Double windowHeight, int xPosition, int yPosition) {
+    public GuiSettings(double windowWidth, double windowHeight, int xPosition, int yPosition) {
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
         windowCoordinates = new Point(xPosition, yPosition);
     }
 
-    public Double getWindowWidth() {
+    public double getWindowWidth() {
         return windowWidth;
     }
 
-    public Double getWindowHeight() {
+    public double getWindowHeight() {
         return windowHeight;
     }
 
@@ -51,8 +51,8 @@ public class GuiSettings implements Serializable {
 
         GuiSettings o = (GuiSettings) other;
 
-        return Objects.equals(windowWidth, o.windowWidth)
-                && Objects.equals(windowHeight, o.windowHeight)
+        return windowWidth == o.windowWidth
+                && windowHeight == o.windowHeight
                 && Objects.equals(windowCoordinates.x, o.windowCoordinates.x)
                 && Objects.equals(windowCoordinates.y, o.windowCoordinates.y);
     }
