7688318876d7b62bdb01f833930e1034122fa21e
Paul Tan <pyokagan@gmail.com>
2018-11-27 16:08:58 +0800

    UserPrefs: make guiSettings non-nullable
    UserPrefs#guiSettings can be internally `null`. If that is so,
    UserPrefs#getGuiSettings() will return a `new GuiSettings()`.
    
    Generally, for the AB-4 codebase, we try not to make fields nullable
    unless absolutely necessary -- this is because more code will need to be
    written to handle the case when a field is null, and more code = more
    bugs.
    
    In this case, it is not necessary for guiSettings to be nullable.
    In fact, in normal operation of AB-4, guiSettings will *never* be null:
    
      * When deserializing a JSON file which does not have guiSettings
        specified, the default value of guiSettings will be used, which is
        not null. (This is because deserialization will call the no-arg
        constructor of UserPrefs, which will initialize guiSettings to a
        non-null value)
    
      * For all usages of UserPrefs#setGuiSettings(...), the provided
        GuiSettings is not null.
    
    So, let's make this field non-nullable.
    
    (In fact, if guiSettings is null, UserPrefs#toString() will throw an NPE
    :-P)

---
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -0,0 +1,15 @@
+package seedu.address.model;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class UserPrefsTest {
+
+    @Test
+    public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
+        UserPrefs userPref = new UserPrefs();
+        Assert.assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+    }
+
+}
