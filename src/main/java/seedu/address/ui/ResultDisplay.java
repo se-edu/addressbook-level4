8529cdf446bce17420c3c60a02f29aa7ef61d48f
Paul Tan <pyokagan@gmail.com>
2018-12-09 16:50:34 +0800

    ResultDisplay: migrate away from EventsCenter
    When MainWindow gets a CommandResult or exception from calling
    Logic#execute(String), it would notify ResultDisplay of the result by
    posting a NewResultAvailableEvent to EventsCenter.
    
    As we are going to remove EventsCenter in the future, let's migrate
    ResultDisplay away from EventCenter.
    
    Rather than having this event-posting-and-receiving arrangement, let's
    add a ResultDisplay#setFeedbackToUser() method to ResultDisplay, and
    teach MainWindow to call this method when it receives a CommandResult
    from Logic#execute(String).

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -1,41 +1,28 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
+import static java.util.Objects.requireNonNull;
 
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.NewResultAvailableEvent;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
  */
 public class ResultDisplay extends UiPart<Region> {
 
-    private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
-
-    private final StringProperty displayed = new SimpleStringProperty("");
 
     @FXML
     private TextArea resultDisplay;
 
     public ResultDisplay() {
         super(FXML);
-        resultDisplay.textProperty().bind(displayed);
-        registerAsAnEventHandler(this);
     }
 
-    @Subscribe
-    private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        Platform.runLater(() -> displayed.setValue(event.message));
+    public void setFeedbackToUser(String feedbackToUser) {
+        requireNonNull(feedbackToUser);
+        resultDisplay.setText(feedbackToUser);
     }
 
 }
