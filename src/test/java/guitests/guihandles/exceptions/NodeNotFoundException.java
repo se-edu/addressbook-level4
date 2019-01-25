cc1ea1944915327b0407f37389674ce785275468
Paul Tan <pyokagan@gmail.com>
2019-01-25 17:47:26 +0800

    Remove GUI testing

--- src/test/java/guitests/guihandles/exceptions/NodeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package guitests.guihandles.exceptions;
-
-import java.util.NoSuchElementException;
-
-/**
- * Signals that the node cannot be found.
- */
-public class NodeNotFoundException extends NoSuchElementException {
-}
--- src/test/java/guitests/guihandles/exceptions/StageNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package guitests.guihandles.exceptions;
-
-import java.util.NoSuchElementException;
-
-/**
- * Signals that the stage cannot be found.
- */
-public class StageNotFoundException extends NoSuchElementException {
-}
