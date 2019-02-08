fdbe4e04a86884f43d84ffa2ea37f9fff25be2b0
WANG CHAO <1229983126@qq.com>
2019-02-08 22:31:56 +0800

    change the entry point of addressbook
    After we add the javafx runtime dependency, addressbook is still unable
    to run in a jdk11 environment. It gives an error of below:
    Error: JavaFX runtime components are missing, and are required to run this application
    
    This error comes from sun.launcher.LauncherHelper in the java.base
    module. The reason for this is that the Main app extends Application
    and has a main method. If that is the case, the LauncherHelper will check
    for the javafx.graphics module to be present as a named module. If that
    module is not present, the launch is aborted. Hence, having the JavaFX
    libraries as jars on the classpath is not allowed in this case. [1]
    
    This is more like a JDK 11 problem which cannot be solved elegantly.
    One simple workaround is to have a separate main class that doesn't
    extend Application. Hence it doesn't do the check on javafx.graphics
    module, and when the required jars are on the classpath, it works fine.
    
    Let’s add another main class to be the new entry point of addressbook
    to solve this problem.
    
    [1]http://mail.openjdk.java.net/pipermail/openjfx-dev/2018-June/021977.html

---
+++ src/main/java/seedu/address/Main.java
@@ -0,0 +1,10 @@
+package seedu.address;
+
+/**
+ * The main entry point to the application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainApp.initiate(args);
+    }
+}
