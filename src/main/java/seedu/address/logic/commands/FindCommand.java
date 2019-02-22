6cf3a4bd892cf7060d1cd154bec9ddc27053df5e
Paul Tan <pyokagan@gmail.com>
2019-02-23 01:17:41 +0800

    Move MESSAGE_PERSONS_LISTED_OVERVIEW to FindCommand
    FindCommand is the only command that uses this message.
    
    Usually, messages that a specific to a command are kept in the command
    itself. For example, MESSAGE_DELETE_PERSON_SUCCESS is a member of
    DeleteCommand, since it is the only user of that message.
    
    As such, to be consistent, let's move MESSAGE_PERSONS_LISTED_OVERVIEW to
    FindCommand.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
@@ -19,6 +18,7 @@ public class FindCommand extends Command {
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
     private final NameContainsKeywordsPredicate predicate;
 
@@ -31,7 +31,7 @@ public class FindCommand extends Command {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
 
     @Override
