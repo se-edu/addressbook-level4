6ba2ac73f5cde5b4eb740ac48967c16be28ad9f3
Paul Tan <pyokagan@gmail.com>
2019-02-23 15:02:00 +0800

    PersonCard: remove #equals(object)
    This method implements a very weak notion of equivalence, where as long
    as the PersonCard's person and id are equal, the object is equal.
    However, such a weak notion of equivalence goes against the spirit of
    the UI architecture, which states that all UiParts represent a distinct
    part of the UI, and more importantly, wraps a subgraph of the JavaFX
    scene graph (accessible via UiPart#getRoot()). As such, just because two
    PersonCards A and B happen to contain the same ID and person value,
    doesn't mean they are "equal", because they may be holding different
    JavaFX scene graph objects. Any JavaFX-related operations on PersonCard
    A may give a different result to PersonCard B.
    
    Furthermore, this implementation of PersonCard#equals(Object) isn't
    actually used, not even by test code. It was introduced in baa5988d
    (PersonListPanelHandle: Update methods, 2017-07-14) so that one could
    do the following to assert that a certain PersonCard was selected:
    
        private void assertCardSelected(Index index) {
            PersonCard selectedCard = getPersonListPanel().getSelectedCard().get();
            assertEquals(getPersonListPanel().getCard(index.getZeroBased()), selectedCard);
        }
    
    where the assertEquals() would then call PersonCard#equals(Object).
    
    However, since 04a675ff (PersonListPanelHandle: Update
    getSelectedCard(), 2017-07-20) this pattern of code has been (rightly)
    removed, with test code preferring to compare what is actually displayed
    on the PersonCard, rather than using PersonCard#equals(Object):
    
        /**
         * Asserts that {@code actualCard} displays the same values as {@code expectedCard}.
         */
        public static void assertCardEquals(PersonCardHandle expectedCard, PersonCardHandle actualCard) {
            assertEquals(expectedCard.getId(), actualCard.getId());
            assertEquals(expectedCard.getAddress(), actualCard.getAddress());
            assertEquals(expectedCard.getEmail(), actualCard.getEmail());
            assertEquals(expectedCard.getName(), actualCard.getName());
            assertEquals(expectedCard.getPhone(), actualCard.getPhone());
            assertEquals(expectedCard.getTags(), actualCard.getTags());
        }
    
    PersonCard#equals(Object) was, however, not removed then. So let's
    remove it now.

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardDisplaysPerson;
 
 import org.junit.Test;
@@ -26,32 +24,6 @@ public class PersonCardTest extends GuiUnitTest {
         personCard = new PersonCard(personWithTags, 2);
         uiPartRule.setUiPart(personCard);
         assertCardDisplay(personCard, personWithTags, 2);
-    }
-
-    @Test
-    public void equals() {
-        Person person = new PersonBuilder().build();
-        PersonCard personCard = new PersonCard(person, 0);
-
-        // same person, same index -> returns true
-        PersonCard copy = new PersonCard(person, 0);
-        assertTrue(personCard.equals(copy));
-
-        // same object -> returns true
-        assertTrue(personCard.equals(personCard));
-
-        // null -> returns false
-        assertFalse(personCard.equals(null));
-
-        // different types -> returns false
-        assertFalse(personCard.equals(0));
-
-        // different person, same index -> returns false
-        Person differentPerson = new PersonBuilder().withName("differentName").build();
-        assertFalse(personCard.equals(new PersonCard(differentPerson, 0)));
-
-        // same person, different index -> returns false
-        assertFalse(personCard.equals(new PersonCard(person, 1)));
     }
 
     /**
