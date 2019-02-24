34d7bb547ec890886c00d7c49ba94b04d5ef974d
Si Jie <sijie123@gmail.com>
2019-02-24 15:26:25 +0800

    JUnit: Migrate to use JUnit 5 test runner
    We use JUnit 4 test runner (org.junit.Test) to execute our unit and
    system testing.
    
    To use new features such as callbacks, extensions and temporary
    directories in JUnit 5, we need to use the new JUnit 5 test runner.
    
    Let's
    * use the new org.junit.jupiter.api.Test instead of org.junit.Test
    * use BeforeEach instead of Before
    * use BeforeAll instead of BeforeClass
    * use AfterEach instead of After
    * use AfterAll instead of AfterClass
    
    The above changes mechanically migrate our unit tests to use JUnit 5
    API.
    
    Note: Migrations involving temporary directories will be done in
    another commit, so as to limit the scope of each commit.

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ConfigTest {
 
