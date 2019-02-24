a32753ce8e86b555d5dfaefd981412b45d9d86ae
Si Jie <sijie123@gmail.com>
2019-02-24 15:26:25 +0800

    JUnit: Migrate to use JUnit 5 test runner
    We use JUnit 4 test runner (org.junit.Test) to execute our unit and
    system testing.
    
    To use new features such as callbacks, extensions and temporary
    directories in JUnit 5, we need to use the new JUnit 5 test runner.
    
    Let's
    * use the new org.junit.jupiter.api.Test instead of org.junit.Test
    * use BeforeEach instead of Before
    * use BeforeAll instead of BeforeClass
    * use AfterEach instead of After
    * use AfterAll instead of AfterClass
    
    Note: Tests that use TemporaryFolders, UiPartRule, UiStageRule and
    ClockRule will be migrated in another commit, so as to limit the
    scope of each commit.

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -10,7 +10,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.SimpleIntegerProperty;
