cbefcb26a736577c1b70487055075aaf52431980
Si Jie <sijie123@gmail.com>
2019-02-24 20:05:47 +0800

    TestApp: Refactor constants into AddressBookSystemTest
    TestApp has the constants SAVE_LOCATION_FOR_TESTING and
    DEFAULT_PREF_FILE_LOCATION_FOR_TESTING. While
    DEFAULT_PREF_FILE_LOCATION_FOR_TESTING is used by TestApp for
    initialisation, SAVE_LOCATION_FOR_TESTING is used only within
    AddressBookSystemTest#getDataFileLocation. In fact,
    AddressBookSystemTest retrieves the value of
    SAVE_LOCATION_FOR_TESTING, then passes it into
    SetupHelper#setupApplication, which then invokes TestApp to initialise
    the saveFileLocation parameter. AddressBookSystemTest essentially
    violates the tell-don't-ask principle, as it retrieves the constant
    and later makes TestApp set that value. Since AddressBookSystemTest is
    responsible for initialising TestApp with these constants (in fact it
    is the only class using these constants), moving the constants to
    AddressBookSystemTest increases cohesion within the class.
    
    This design has been in AB4 since its draft stage [1], and while no
    reason was given, it is likely that this implementation was adopted so
    as to allow other classes (such as SampleDataTest) to initialise
    TestApp with different saveFileLocations.
    
    Let's move these constants to AddressBookSystemTest.
    
    Additionally, let's add another parameter, configFileLocation, to the
    constructor for TestApp, and move the preference file location to
    within AddressBookSystemTest. This allows any class initialising
    TestApp to be able to pass in a custom location in which to initialise
    the config file to. While we currently have no tests that use a
    custom configFileLocation, this future proofs our code by allowing any
    class that wants to supply custom a configFileLocation to do so, and
    increasing flexibility in testing.
    
    [1] https://github.com/se-edu/addressbook-level4/blame/ed799215c859845f1d33131d1089fcb2727c38a6/src/test/java/seedu/address/TestApp.java

--- src/test/java/seedu/address/TestApp.java
+++ src/test/java/seedu/address/TestApp.java
@@ -16,7 +16,6 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.storage.JsonAddressBookStorage;
 import seedu.address.storage.UserPrefsStorage;
-import seedu.address.testutil.TestUtil;
 import systemtests.ModelHelper;
 
 /**
@@ -25,20 +24,15 @@ import systemtests.ModelHelper;
  */
 public class TestApp extends MainApp {
 
-    public static final Path SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("sampleData.json");
-
-    protected static final Path DEFAULT_PREF_FILE_LOCATION_FOR_TESTING =
-            TestUtil.getFilePathInSandboxFolder("pref_testing.json");
     protected Supplier<ReadOnlyAddressBook> initialDataSupplier = () -> null;
-    protected Path saveFileLocation = SAVE_LOCATION_FOR_TESTING;
+    protected Path saveFileLocation = null;
+    protected Path configFileLocation = null;
 
-    public TestApp() {
-    }
-
-    public TestApp(Supplier<ReadOnlyAddressBook> initialDataSupplier, Path saveFileLocation) {
+    public TestApp(Supplier<ReadOnlyAddressBook> initialDataSupplier, Path saveFileLocation, Path configFileLocation) {
         super();
         this.initialDataSupplier = initialDataSupplier;
         this.saveFileLocation = saveFileLocation;
+        this.configFileLocation = configFileLocation;
 
         // If some initial local data has been provided, write those to the file
         if (initialDataSupplier.get() != null) {
@@ -54,7 +48,7 @@ public class TestApp extends MainApp {
     @Override
     protected Config initConfig(Path configFilePath) {
         Config config = super.initConfig(configFilePath);
-        config.setUserPrefsFilePath(DEFAULT_PREF_FILE_LOCATION_FOR_TESTING);
+        config.setUserPrefsFilePath(configFileLocation);
         return config;
     }
 
