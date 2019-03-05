6b4830cafb3a643d61caa6291524fdf2273a96de
DMGreenhouse <dgreenhouse27@gmail.com>
2019-03-05 16:21:36 +0800

    Revert "Add command word to AddressBookParser to give room for the sort command to be used"
    This reverts commit b1efb570e959cdfc97d3a42658705f78104bea6c.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,7 +19,6 @@ import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
-import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -84,9 +83,6 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
-
-        case SortCommand.COMMAND_WORD:
-            return new SortCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
