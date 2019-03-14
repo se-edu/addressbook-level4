fabcae95fd6d661dfe8f68a926cf2f266750fcc2
Si Jie <sijie123@gmail.com>
2019-03-14 14:24:52 +0800

    Standardize Assert#assertThrows(...) calls to use static import
    We use a mixture of calling the static
    Assert.assertThrows(...) and calling a statically imported
    Assert#assertThrows.
    
    This can make it confusing for readers, as we have 2 different ways of
    calling the same assertThrows.
    
    Let's standardise, across all tests, to use a statically imported
    Assert#assertThrows. This also has the benefit of making our code less
    verbose and easier to read.

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -2,28 +2,27 @@ package seedu.address.model.person;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.Test;
-
-import seedu.address.testutil.Assert;
 
 public class NameTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        Assert.assertThrows(NullPointerException.class, () -> new Name(null));
+        assertThrows(NullPointerException.class, () -> new Name(null));
     }
 
     @Test
     public void constructor_invalidName_throwsIllegalArgumentException() {
         String invalidName = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
+        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
     }
 
     @Test
     public void isValidName() {
         // null name
-        Assert.assertThrows(NullPointerException.class, () -> Name.isValidName(null));
+        assertThrows(NullPointerException.class, () -> Name.isValidName(null));
 
         // invalid name
         assertFalse(Name.isValidName("")); // empty string
