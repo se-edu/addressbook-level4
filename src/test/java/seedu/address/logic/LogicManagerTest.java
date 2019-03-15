c2d8ebb12b716ce337add464c8aadff97855d23a
Si Jie <sijie123@gmail.com>
2019-03-15 12:48:51 +0800

    JUnit Assert: Migrate to JUnit 5 API
    We use JUnit 4's Assert API. For example, in most classes, we import
    org.junit.Assert.assertFalse or org.junit.Assert.assertEquals.
    
    In JUnit 5, the API has migrated to
    org.junit.jupiter.api.Assertions.*. While the functionality remains
    largely similar, JUnit 4 APIs are no longer supported by the JUnit 5
    runner (unless a legacy Vintage engine is used [1]). Thus, it is
    necessary to use JUnit 5 APIs as we migrate our tests to JUnit 5.
    
    Let's migrate our API calls to assertion statements to use the
    ones provided by JUnit 5 instead.
    This can be done by changing all references of org.junit.Assert.* to
    org.junit.jupiter.Assertions.*.
    
    In this migration, some methods signatures such as assertTrue(String,
    Boolean) has been changed to assertTrue(Boolean, String). The relevant
    method calls have been updated to reflect this signature change.
    
    [1]: https://junit.org/junit5/docs/current/user-guide/#migrating-from-junit4

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -1,6 +1,6 @@
 package seedu.address.logic;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
