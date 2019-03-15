94dbc4f65126a9c376c5f86f6387f9db3250f0c7
Si Jie <sijie123@gmail.com>
2019-03-15 12:48:51 +0800

    JUnit Assert: Migrate to JUnit 5 API
    We use JUnit 4's Assert API. For example, in most classes, we import
    org.junit.Assert.assertFalse or org.junit.Assert.assertEquals.
    
    In JUnit 5, the API has migrated to
    org.junit.jupiter.api.Assertions.*, while the old APIs are left to
    maintain backward compatibility. While the functionality remains
    largely similar, it is necessary to standardize using JUnit 5 APIs as
    we migrate our tests to JUnit 5 style.
    
    Let's migrate our API calls to assertion statements to use the
    ones provided by JUnit 5 instead.
    This can be done by changing all references of org.junit.Assert.* to
    org.junit.jupiter.Assertions.*.
    
    In this migration, some methods signatures such as assertTrue(String,
    Boolean) has been changed to assertTrue(Boolean, String). The relevant
    method calls have been updated to reflect this signature change.

--- src/test/java/seedu/address/commons/util/StringUtilTest.java
+++ src/test/java/seedu/address/commons/util/StringUtilTest.java
@@ -1,12 +1,12 @@
 package seedu.address.commons.util;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.io.FileNotFoundException;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class StringUtilTest {
 
