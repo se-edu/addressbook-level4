6982d713fb3919881791ad216b44646351253800
Si Jie <sijie123@gmail.com>
2019-03-16 22:59:19 +0800

    UiTest: Migrate StageRule to StageExtension and UiPartRule to UiPartExtension
    We use JUnit 4 style tests in the UI segment of unit tests.
    JUnit 5 offers a more declarative and extension-based unit test, to
    encourage reuse of components common to all tests.
    
    As we migrate towards JUnit 5, it is essential that we migrate the UI
    unit tests to adopt JUnit 5's style. Most importantly, JUnit 5
    replaces Rules with Extensions, hence we need to migrate UiPartRule to
    UiPartExtension and StageRule to StageExtension.
    
    Let's replace UiPartRule with UiPartExtension, and StageRule with
    StageExtension, taking advantage of JUnit 5's extensive hook interface
    (BeforeEachCallback and AfterEachCallback) to simplify our
    implementation of hooks. Notably, we no longer need to implement our
    own before/hook/after sequence.
    
    However, this migration introduces a complication. As extensions are
    specific to JUnit 5, we cannot use extensions without switching the
    test runner to JUnit 5's Jupiter test runner. However, this switch
    means that we also need to migrate other API changes.
    Notably,
    * Before/BeforeClass becomes BeforeEach/BeforeAll
    * After/AfterClass becomes AfterEach/AfterAll
    
    Furthermore, HelpWindowTest needs to continue skipping the test in
    headless mode, due to a bug (still yet to be fixed) in JavaFX. The
    equivalent API in JUnit 5 is located in
    org.junit.jupiter.api.Assumptions.*, so in order to continue skipping
    the test, we need to migrate the call and update its method signature
    as necessary.

---
+++ src/test/java/seedu/address/ui/testutil/StageExtension.java
@@ -0,0 +1,21 @@
+package seedu.address.ui.testutil;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testfx.api.FxToolkit;
+
+/**
+ * Properly sets up and tears down a JavaFx stage for our testing purposes.
+ */
+public class StageExtension implements BeforeEachCallback, AfterEachCallback {
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        FxToolkit.cleanupStages();
+    }
+}
