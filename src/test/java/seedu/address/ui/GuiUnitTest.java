2a094971125a10d73b942e1cda7a783fbb760301
Si Jie <sijie123@gmail.com>
2019-03-16 22:59:19 +0800

    UiTest: Migrate StageRule to StageExtension and UiPartRule to UiPartExtension
    We use JUnit 4 style tests in the UI segment of unit tests.
    JUnit 5 offers a more declarative and extension-based unit test, to
    encourage reuse of components common to all tests.
    
    As we migrate towards JUnit 5, it is essential that we migrate the UI
    unit tests to adopt JUnit 5's style. Most importantly, JUnit 5
    replaces Rules with Extensions, hence we need to migrate UiPartRule to
    UiPartExtension and StageRule to StageExtension.
    
    Let's replace UiPartRule with UiPartExtension, and StageRule with
    StageExtension, taking advantage of JUnit 5's extensive hook interface
    (BeforeEachCallback and AfterEachCallback) to simplify our
    implementation of hooks. Notably, we no longer need to implement our
    own before/hook/after sequence.
    
    However, this migration introduces a complication. As extensions are
    specific to JUnit 5, we cannot use extensions without switching the
    test runner to JUnit 5's Jupiter test runner. However, this switch
    means that we also need to migrate other API changes.
    Notably,
    * Before/BeforeClass becomes BeforeEach/BeforeAll
    * After/AfterClass becomes AfterEach/AfterAll
    
    Furthermore, HelpWindowTest needs to continue skipping the test in
    headless mode, due to a bug (still yet to be fixed) in JavaFX. The
    equivalent API in JUnit 5 is located in
    org.junit.jupiter.api.Assumptions.*, so in order to continue skipping
    the test, we need to migrate the call and update its method signature
    as necessary.
    
    At the same time, we also need to upgrade to use TestFX-JUnit5 so that
    test methods written in JUnit 5 can have access to the FxRobot
    instance [1] to execute actions for testing.
    
    [1]: https://github.com/TestFX/TestFX#junit-5-2

--- src/test/java/seedu/address/ui/GuiUnitTest.java
+++ src/test/java/seedu/address/ui/GuiUnitTest.java
@@ -2,19 +2,19 @@ package seedu.address.ui;
 
 import java.util.Optional;
 
-import org.junit.Rule;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import guitests.GuiRobot;
 import guitests.guihandles.exceptions.NodeNotFoundException;
 import javafx.scene.Node;
-import seedu.address.ui.testutil.UiPartRule;
+import seedu.address.ui.testutil.UiPartExtension;
 
 /**
  * A GUI unit test class for AddressBook.
  */
 public abstract class GuiUnitTest {
-    @Rule
-    public final UiPartRule uiPartRule = new UiPartRule();
+    @RegisterExtension
+    public final UiPartExtension uiPartExtension = new UiPartExtension();
 
     protected final GuiRobot guiRobot = new GuiRobot();
 
