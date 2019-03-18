efe120ed5c46bcbb3cde02e84a31071bb30ad29f
yongjia96 <yongjia96@gmail.com>
2019-03-18 22:32:15 +0800

    Time class done

--- src/main/java/seedu/address/model/workout/Time.java
+++ src/main/java/seedu/address/model/workout/Time.java
@@ -1,4 +1,30 @@
 package seedu.address.model.workout;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Time {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Time should only contain numbers, in minutes";
+    public static final String VALIDATION_REGEX = "\\d";
+    public final String value;
+
+
+    public Time(String time) {
+        requireNonNull(time);
+        checkArgument(isValidTime(time), MESSAGE_CONSTRAINTS);
+        value = time;
+    }
+    public static boolean isValidTime(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
 }
