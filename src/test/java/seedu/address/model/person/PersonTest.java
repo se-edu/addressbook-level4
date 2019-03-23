68182623b209ae1fc1b0142d6b76e424568d6326
Si Jie <sijie123@gmail.com>
2019-03-23 17:13:37 +0800

    Thrown exception checking: Migrate to use AssertThrows
    We use a mix of 4 different ways to test exception throwing. We use:
    * try/catch - catch and ensure we receive the correct exception.
    * Assert#assertThrows
    * JUnit#assertThrows
    * JUnit#ExpectedException.
    All these methods semantically do the same thing.
    
    To streamline and standardise our code, we should only use one way to
    check for thrown exceptions. Rationale: developers only need to learn
    one style of implementation.
    
    The try/catch method is implemented differently in each file that
    requires it and violates the DRY principle. JUnit#assertThrows
    provides a shorter syntax and is easier to read as compared to
    ExpectedException. We should thus aim to use the assertThrows style.
    In Assert.java, we implement custom methods that wraps over
    JUnit#assertThrows to provide additional functionality such as testing
    for correctness of error messages shown to users. With
    Assert#assertThrows providing a superset of required functionality, it
    makes sense to use Assert#assertThrows as the standardised way to test
    for thrown exceptions.
    
    Let's convert all our exception checking to use Assert#assertThrows.
    
    In doing so, some tests no longer need to declare to throw exceptions.
    
    Let's clean up our code to remove these unnecessary declarations.
    
    Additionally, two more classes - StringUtilTest and IndexTest -
    implement methods to encapsulate the idea of checking for thrown
    exceptions. Now that we can directly call Assert#assertThrows, these
    abstractions provide little benefit.
    
    Let's remove them to simplify our code base.

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -7,24 +7,20 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import seedu.address.testutil.PersonBuilder;
 
 public class PersonTest {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void asObservableList_modifyList_throwsUnsupportedOperationException() {
         Person person = new PersonBuilder().build();
-        thrown.expect(UnsupportedOperationException.class);
-        person.getTags().remove(0);
+        assertThrows(UnsupportedOperationException.class, () -> person.getTags().remove(0));
     }
 
     @Test
