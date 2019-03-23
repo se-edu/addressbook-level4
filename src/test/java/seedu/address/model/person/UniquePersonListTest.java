0adc841e3d75ad31eb212843eecab69458af475c
Si Jie <sijie123@gmail.com>
2019-03-23 17:13:37 +0800

    Thrown exception checking: Migrate to use AssertThrows
    We use a mix of 4 different ways to test exception throwing. We use:
    * try/catch - catch and ensure we receive the correct exception.
    * Assert#assertThrows
    * JUnit#assertThrows
    * JUnit#ExpectedException.
    All these methods semantically do the same thing.
    
    To streamline and standardise our code, we should only use one way to
    check for thrown exceptions. Rationale: developers only need to learn
    one style of implementation.
    
    The try/catch method is implemented differently in each file that
    requires it and violates the DRY principle. JUnit#assertThrows
    provides a shorter syntax and is easier to read as compared to
    ExpectedException. We should thus aim to use the assertThrows style.
    In Assert.java, we implement custom methods that provide additional
    functionality such as testing for correctness of error messages shown
    to users. With Assert#assertThrows providing a superset of
    functionality as compared to all other ways, it makes sense to use
    Assert#assertThrows as the standardised way to test for thrown
    exceptions.
    
    Let's convert all our exception checking to use Assert#assertThrows.
    
    In doing so, some tests no longer need to declare to throw exceptions.
    
    Let's clean up our code to remove these unnecessary declarations.
    
    Additionally, two more classes - StringUtilTest and IndexTest -
    implement methods to encapsulate the idea of checking for thrown
    exceptions. Now that we can directly call Assert#assertThrows, these
    abstractions provide little benefit.
    
    Let's remove them to simplify our code base.

--- src/test/java/seedu/address/model/person/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/person/UniquePersonListTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
@@ -12,24 +13,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.testutil.PersonBuilder;
 
 public class UniquePersonListTest {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
     private final UniquePersonList uniquePersonList = new UniquePersonList();
 
     @Test
     public void contains_nullPerson_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.contains(null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.contains(null));
     }
 
     @Test
@@ -53,33 +49,28 @@ public class UniquePersonListTest {
 
     @Test
     public void add_nullPerson_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.add(null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.add(null));
     }
 
     @Test
     public void add_duplicatePerson_throwsDuplicatePersonException() {
         uniquePersonList.add(ALICE);
-        thrown.expect(DuplicatePersonException.class);
-        uniquePersonList.add(ALICE);
+        assertThrows(DuplicatePersonException.class, () -> uniquePersonList.add(ALICE));
     }
 
     @Test
     public void setPerson_nullTargetPerson_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.setPerson(null, ALICE);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.setPerson(null, ALICE));
     }
 
     @Test
     public void setPerson_nullEditedPerson_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.setPerson(ALICE, null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.setPerson(ALICE, null));
     }
 
     @Test
     public void setPerson_targetPersonNotInList_throwsPersonNotFoundException() {
-        thrown.expect(PersonNotFoundException.class);
-        uniquePersonList.setPerson(ALICE, ALICE);
+        assertThrows(PersonNotFoundException.class, () -> uniquePersonList.setPerson(ALICE, ALICE));
     }
 
     @Test
@@ -115,20 +106,17 @@ public class UniquePersonListTest {
     public void setPerson_editedPersonHasNonUniqueIdentity_throwsDuplicatePersonException() {
         uniquePersonList.add(ALICE);
         uniquePersonList.add(BOB);
-        thrown.expect(DuplicatePersonException.class);
-        uniquePersonList.setPerson(ALICE, BOB);
+        assertThrows(DuplicatePersonException.class, () -> uniquePersonList.setPerson(ALICE, BOB));
     }
 
     @Test
     public void remove_nullPerson_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.remove(null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.remove(null));
     }
 
     @Test
     public void remove_personDoesNotExist_throwsPersonNotFoundException() {
-        thrown.expect(PersonNotFoundException.class);
-        uniquePersonList.remove(ALICE);
+        assertThrows(PersonNotFoundException.class, () -> uniquePersonList.remove(ALICE));
     }
 
     @Test
@@ -141,8 +129,7 @@ public class UniquePersonListTest {
 
     @Test
     public void setPersons_nullUniquePersonList_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.setPersons((UniquePersonList) null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.setPersons((UniquePersonList) null));
     }
 
     @Test
@@ -156,8 +143,7 @@ public class UniquePersonListTest {
 
     @Test
     public void setPersons_nullList_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniquePersonList.setPersons((List<Person>) null);
+        assertThrows(NullPointerException.class, () -> uniquePersonList.setPersons((List<Person>) null));
     }
 
     @Test
@@ -173,13 +159,12 @@ public class UniquePersonListTest {
     @Test
     public void setPersons_listWithDuplicatePersons_throwsDuplicatePersonException() {
         List<Person> listWithDuplicatePersons = Arrays.asList(ALICE, ALICE);
-        thrown.expect(DuplicatePersonException.class);
-        uniquePersonList.setPersons(listWithDuplicatePersons);
+        assertThrows(DuplicatePersonException.class, () -> uniquePersonList.setPersons(listWithDuplicatePersons));
     }
 
     @Test
     public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
-        thrown.expect(UnsupportedOperationException.class);
-        uniquePersonList.asUnmodifiableObservableList().remove(0);
+        assertThrows(UnsupportedOperationException.class, ()
+            -> uniquePersonList.asUnmodifiableObservableList().remove(0));
     }
 }
