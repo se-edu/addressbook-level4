2a2d1e14ad6c85acd091c6f48b4e628d8ee05afd
Si Jie <sijie123@gmail.com>
2019-03-27 00:17:41 +0800

    TestApp: Move DEFAULT_PREF_LOCATION_FOR_TESTING into AddressBookSystemTest
    The location for the preference file is fixed within TestApp as a
    constant. TestApp#initConfig overrides MainApp#initConfig, so when the
    TestApp starts, the config file is always initialised to a fixed file.
    This reduces the flexibility for testing.
    
    In contrast, the saveFileLocation is a variable that is set by classes
    that initialise TestApp, such as AddressBookSystemTest. The
    saveFileLocation can be easily mocked and tested, as compared to the
    fixed config file location. Even though we currently have no tests
    that use a custom configFileLocation, having one increases our
    flexibility in testing as we can simply write classes similar to (or
    extending) AddressBookSystemTest to supply a custom
    configFileLocation.
    
    Let's move DEFAULT_PREF_LOCATION_FOR_TESTING into
    AddressBookSystemTest.
    
    As a result, the name of the constant changes - it is no longer a
    "default" preference location. This brings the variable name in line
    with SAVE_LOCATION_FOR_TESTING.
    Furthermore, TestApp now needs to be initialised with the value of
    configFileLocation. The relevant method calls have been updated to
    supply a configFileLocation.

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -50,6 +50,8 @@ public abstract class AddressBookSystemTest {
     public static ClockRule clockRule = new ClockRule();
 
     private static final Path SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("sampleData.json");
+    private static final Path CONFIG_LOCATION_FOR_TESTING =
+            TestUtil.getFilePathInSandboxFolder("pref_testing.json");
 
     private static final List<String> COMMAND_BOX_DEFAULT_STYLE = Arrays.asList("text-input", "text-field");
     private static final List<String> COMMAND_BOX_ERROR_STYLE =
@@ -67,7 +69,7 @@ public abstract class AddressBookSystemTest {
     @Before
     public void setUp() {
         setupHelper = new SystemTestSetupHelper();
-        testApp = setupHelper.setupApplication(this::getInitialData, getDataFileLocation());
+        testApp = setupHelper.setupApplication(this::getInitialData, getDataFileLocation(), getConfigFileLocation());
         mainWindowHandle = setupHelper.setupMainWindowHandle();
 
         waitUntilBrowserLoaded(getBrowserPanel());
@@ -91,6 +93,10 @@ public abstract class AddressBookSystemTest {
      */
     protected Path getDataFileLocation() {
         return SAVE_LOCATION_FOR_TESTING;
+    }
+
+    protected Path getConfigFileLocation() {
+        return CONFIG_LOCATION_FOR_TESTING;
     }
 
     public MainWindowHandle getMainWindowHandle() {
