51156905655417d316f8430552f71dbcc57b33e2
Si Jie <sijie123@gmail.com>
2019-03-27 18:32:44 +0800

    TestUtil: Remove getFilePathInSandboxFolder
    Following a previous commit, our tests do not use getFilePathInSandboxFolder anymore.
    
    Let's remove the redundant method.

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -1,10 +1,5 @@
 package seedu.address.testutil;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
@@ -13,24 +8,6 @@ import seedu.address.model.person.Person;
  * A utility class for test cases.
  */
 public class TestUtil {
-
-    /**
-     * Folder used for temp files created during testing. Ignored by Git.
-     */
-    private static final Path SANDBOX_FOLDER = Paths.get("src", "test", "data", "sandbox");
-
-    /**
-     * Appends {@code fileName} to the sandbox folder path and returns the resulting path.
-     * Creates the sandbox folder if it doesn't exist.
-     */
-    public static Path getFilePathInSandboxFolder(String fileName) {
-        try {
-            Files.createDirectories(SANDBOX_FOLDER);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return SANDBOX_FOLDER.resolve(fileName);
-    }
 
     /**
      * Returns the middle index of the person in the {@code model}'s person list.
