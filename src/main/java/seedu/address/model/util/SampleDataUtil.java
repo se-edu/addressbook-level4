9ea7424854a1978a3ea00cf9cc75813e8b29e43d
weizhonglauw <e0200817@u.nus.edu>
2019-04-08 00:04:18 +0800

    indentations

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -17,8 +17,8 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.purchase.Purchase;
 import seedu.address.model.purchase.Price;
+import seedu.address.model.purchase.Purchase;
 import seedu.address.model.purchase.PurchaseName;
 import seedu.address.model.tag.Tag;
 
