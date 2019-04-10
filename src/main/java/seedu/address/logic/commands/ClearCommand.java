f350940744cdec6f82e0582b20da0063a0c74e39
weizhonglauw <e0200817@u.nus.edu>
2019-04-10 23:40:23 +0800

    modified message upon clearing contact list

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Contact List has been cleared!";
 
 
     @Override
