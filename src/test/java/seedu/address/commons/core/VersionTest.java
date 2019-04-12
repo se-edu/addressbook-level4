1c2c1f79aa5072aa707be6f5122d942fa036f46b
Si Jie <sijie123@gmail.com>
2019-04-12 15:43:43 +0800

    VersionTest, VersionedAddressBookTest: Use 8-spaces indent for parameter lists
    VersionTest and VersionedAddressBookTest contain tests that align
    multi-line parameter lists instead of using 8-spaces to indent the
    lines.
    
    Let's bring them in line with our coding style.

--- src/test/java/seedu/address/commons/core/VersionTest.java
+++ src/test/java/seedu/address/commons/core/VersionTest.java
@@ -133,7 +133,7 @@ public class VersionTest {
     }
 
     private void verifyVersionParsedCorrectly(String versionString,
-                                              int major, int minor, int patch, boolean isEarlyAccess) {
+            int major, int minor, int patch, boolean isEarlyAccess) {
         assertEquals(new Version(major, minor, patch, isEarlyAccess), Version.fromString(versionString));
     }
 }
