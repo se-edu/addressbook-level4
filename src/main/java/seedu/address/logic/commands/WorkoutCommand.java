01e9a44fc6d6bebd3045d21ee0261ed7e7b18b3f
yongjia96 <yongjia96@gmail.com>
2019-04-13 22:31:46 +0800

    WorkoutCommand implemented

--- src/main/java/seedu/address/logic/commands/WorkoutCommand.java
+++ src/main/java/seedu/address/logic/commands/WorkoutCommand.java
@@ -1,4 +1,31 @@
 package seedu.address.logic.commands;
 
-public class WorkoutCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PURCHASES;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+
+/**
+ * Finds and lists all persons in contact list whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class WorkoutCommand extends Command {
+
+    public static final String COMMAND_WORD = "workout";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displaying the 5 most recent workout";
+
+    public static final String MESSAGE_SUCCESS = "Listed recent workouts";
+
+
+    public WorkoutCommand () {}
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+      //  model.updateFilteredWorkoutList(PREDICATE_SHOW_ALL_PURCHASES);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
