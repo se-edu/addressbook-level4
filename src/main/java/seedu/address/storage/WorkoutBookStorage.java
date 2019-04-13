dd0f9452c6b52a20c522e2d6fc1f9b95379b3e43
yongjia96 <yongjia96@gmail.com>
2019-04-14 01:47:10 +0800

    All minor spelling changes, comment changes and tidying up of code

--- src/main/java/seedu/address/storage/WorkoutBookStorage.java
+++ src/main/java/seedu/address/storage/WorkoutBookStorage.java
@@ -18,7 +18,7 @@ public interface WorkoutBookStorage {
     Path getWorkoutBookFilePath();
 
     /**
-     * Returns ContactList data as a {@link ReadOnlyWorkoutBook}.
+     * Returns WorkoutBook data as a {@link ReadOnlyWorkoutBook}.
      *   Returns {@code Optional.empty()} if storage file is not found.
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
