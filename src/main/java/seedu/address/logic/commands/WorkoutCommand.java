00a011da28948352b838ca4a82a5078f784d7952
yongjia96 <yongjia96@gmail.com>
2019-04-13 22:37:03 +0800

    Travis fixed

--- src/main/java/seedu/address/logic/commands/WorkoutCommand.java
+++ src/main/java/seedu/address/logic/commands/WorkoutCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PURCHASES;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
@@ -25,7 +24,6 @@ public class WorkoutCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-      //  model.updateFilteredWorkoutList(PREDICATE_SHOW_ALL_PURCHASES);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
