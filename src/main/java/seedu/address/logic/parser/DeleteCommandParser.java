d4ea3b6f8cbbcffe34f0b80df46934d48e9e566f
WANG CHAO <1229983126@qq.com>
2019-05-11 21:37:58 +0800

    Fix improper usage of `an`
    There are some typos in our codebase related to the usage of `an`.
    
    Let's fix the improper usage of `an` to solve the problem.

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -13,7 +13,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns an DeleteCommand object for execution.
+     * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -20,7 +20,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     private final InvalidationListenerManager invalidationListenerManager = new InvalidationListenerManager();
 
     /*
-     * The 'unusual' code block below is an non-static initialization block, sometimes used to avoid duplication
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
      * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
      *
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
